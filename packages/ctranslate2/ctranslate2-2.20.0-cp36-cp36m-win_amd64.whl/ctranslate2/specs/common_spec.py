e1bbd436bac044e919d6c999936eed2e2d9e19da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:17 +0000

    Add package ctranslate2/2.20.0/ctranslate2-2.20.0-cp36-cp36m-win_amd64.whl

---
+++ packages/ctranslate2/ctranslate2-2.20.0-cp36-cp36m-win_amd64.whl/ctranslate2/specs/common_spec.py
@@ -0,0 +1,39 @@
+import enum
+
+from ctranslate2.specs import model_spec
+
+
+# This enum should match the C++ equivalent in include/ctranslate2/ops/activation.h.
+class Activation(enum.IntEnum):
+    """Activation type."""
+
+    RELU = 0
+    GELU = 1
+    SWISH = 2
+
+
+# This enum should match the C++ equivalent in include/ctranslate2/layers/common.h.
+class EmbeddingsMerge(enum.IntEnum):
+    """Merge strategy for factors embeddings."""
+
+    CONCAT = 0
+    ADD = 1
+
+
+class LayerNormSpec(model_spec.LayerSpec):
+    def __init__(self):
+        self.gamma = None
+        self.beta = None
+
+
+class LinearSpec(model_spec.LayerSpec):
+    def __init__(self):
+        self.weight = None
+        self.weight_scale = model_spec.OPTIONAL
+        self.bias = model_spec.OPTIONAL
+
+
+class EmbeddingsSpec(model_spec.LayerSpec):
+    def __init__(self):
+        self.weight = None
+        self.weight_scale = model_spec.OPTIONAL
