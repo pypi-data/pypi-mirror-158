65f5b7372855880cef61b4982856a082482f73d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:25 +0000

    Add package nicegui/0.7.29/nicegui-0.7.29-py3-none-any.whl

---
+++ packages/nicegui/nicegui-0.7.29-py3-none-any.whl/nicegui/elements/menu_item.py
@@ -0,0 +1,32 @@
+from typing import Callable, Optional
+
+import justpy as jp
+
+from ..events import ClickEventArguments, handle_event
+from .element import Element
+
+
+class MenuItem(Element):
+
+    def __init__(self, text: str = '', on_click: Optional[Callable] = None, *, auto_close: bool = True):
+        """Menu Item Element
+
+        A menu item to be added to a menu.
+
+        :param text: label of the menu item
+        :param on_click: callback to be executed when selecting the menu item
+        :param auto_close: whether the menu should be closed after a click event (default: `True`)
+        """
+        view = jp.QItem(text=text, clickable=True, temp=False)
+
+        def handle_click(view, event) -> Optional[bool]:
+            arguments = ClickEventArguments(sender=self, socket=event.get('websocket'))
+            result = handle_event(on_click, arguments, update=self.parent_view)
+            if auto_close:
+                assert isinstance(self.parent_view, jp.QMenu)
+                self.parent_view.value = False
+            return result
+
+        view.on('click', handle_click)
+
+        super().__init__(view)
