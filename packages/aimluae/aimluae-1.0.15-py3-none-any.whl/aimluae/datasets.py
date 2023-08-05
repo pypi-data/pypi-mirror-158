729d1228237c7f5bb7c6317a6cfcc3d914afd8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:48 +0000

    Add package aimluae/1.0.15/aimluae-1.0.15-py3-none-any.whl

---
+++ packages/aimluae/aimluae-1.0.15-py3-none-any.whl/aimluae/datasets.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import requests
+
+
+
+def get_data(data_set):
+
+   extension = '.csv'
+   dir_name =  'https://raw.githubusercontent.com/maker57sk/aimluae/main/datasets/'
+   filename = dir_name + data_set + extension
+
+   if requests.get(filename).status_code == 200:
+      df = pd.read_csv(filename)
+      return print(df.head())
+   else:
+      extension = '.xlsx'
+      filename = dir_name + data_set + extension
+      df = pd.read_excel(filename)
+      return print(df.head())
+
+def list_datasets():
+   data_sets = [
+      'courier_companies', 'customs_warehouse_licensees', 'Number of teaching staff_in_Higher_Education_2020_06_09', 
+      'customs_warehouses', 'declaration_cargo_detail-1', 'declaration_cargo_detail-2', 'Graduates_in_Highe_ Education_20-06-09',
+      'livebirths', 'Services-En_0', 'vehicle_clearance_certificate', 'UAE_National_Red List_Species',
+   ]
+   return data_sets
+
+
+get_data('customs_warehouse_licensees')
