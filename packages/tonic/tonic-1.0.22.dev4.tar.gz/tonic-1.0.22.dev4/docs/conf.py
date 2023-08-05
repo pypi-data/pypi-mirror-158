06664a39f40ff39f86d0add28d70db8f14de2a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:27 +0000

    Add package tonic/1.0.22.dev4/tonic-1.0.22.dev4.tar.gz

---
+++ packages/tonic/tonic-1.0.22.dev4.tar.gz/tonic-1.0.22.dev4/docs/conf.py
@@ -0,0 +1,51 @@
+import tonic
+
+project = "Tonic"
+copyright = "2019-2021, the neuromorphs of Telluride"
+author = "Gregor Lenz"
+
+master_doc = "index"
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "pbr.sphinxext", "myst_nb"]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# MyST settings
+# jupyter_execute_notebooks = "off"
+execution_timeout = 300
+suppress_warnings = ["myst.header"]
+execution_show_tb = True
+execution_excludepatterns = ["large_datasets.ipynb"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_book_theme"
+html_title = tonic.__version__
+html_logo = "_static/tonic-logo-black.png"
+html_favicon = "_static/tonic_favicon.png"
+html_show_sourcelink = True
+html_sourcelink_suffix = ""
+
+html_theme_options = {
+    "repository_url": "https://github.com/neuromorphs/tonic",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "repository_branch": "develop",
+    "path_to_docs": "docs",
+    "use_fullscreen_button": True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ packages/tonic/tonic-1.0.22.dev4.tar.gz/tonic-1.0.22.dev4/docs/reference/utils.rst
@@ -0,0 +1,12 @@
+utils
+=======
+
+.. currentmodule:: tonic.utils
+
+Tonic utils contain functions to plot events for visual inspection, padding for batching and other functions.
+
+Plot event grid
+----------------
+
+.. autofunction:: plot_event_grid
+
