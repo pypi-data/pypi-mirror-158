22fbd283b80135fdcaea28baec952831378d64ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:22 +0000

    Add package nodejs-bin/18.4.0a4/nodejs_bin-18.4.0a4-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/nodejs-bin/nodejs_bin-18.4.0a4-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/nodejs/__init__.py
@@ -0,0 +1,8 @@
+from .node import path as path, main as main, call as call, run as run, Popen as Popen
+from . import npm as npm
+from . import corepack as corepack
+from . import node as node
+from . import npx as npx
+
+__version__ = "18.4.0a4"
+node_version = "18.4.0"
