83ff4128bbc7bee90a01d4cdc9e124f0f5615582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package nodejs-bin/14.19.3a4/nodejs_bin-14.19.3a4-py3-none-win_amd64.whl

---
+++ packages/nodejs-bin/nodejs_bin-14.19.3a4-py3-none-win_amd64.whl/nodejs/__init__.py
@@ -0,0 +1,8 @@
+from .node import path as path, main as main, call as call, run as run, Popen as Popen
+from . import corepack as corepack
+from . import node as node
+from . import npm as npm
+from . import npx as npx
+
+__version__ = "14.19.3a4"
+node_version = "14.19.3"
