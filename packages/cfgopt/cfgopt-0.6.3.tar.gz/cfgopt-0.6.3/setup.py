3c5f1a5900d84991450462ff2a904791904180dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:16 +0000

    Add package cfgopt/0.6.3/cfgopt-0.6.3.tar.gz

---
+++ packages/cfgopt/cfgopt-0.6.3.tar.gz/cfgopt-0.6.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# read the contents of README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='cfgopt',
+      version='0.6.3',
+      description='You only configure your deep learning experiment once with cfgopt.',
+      url='http://github.com/tjyuyao/cfgopt',
+      author='Yuyao Huang',
+      author_email='huangyuyao@outlook.com',
+      license='MIT',
+      packages=['cfgopt'],
+      entry_points={
+          'console_scripts': ['cfgoptrun=cfgopt.main:main'],
+      },
+      install_requires=[
+          'jsbeautifier',
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
