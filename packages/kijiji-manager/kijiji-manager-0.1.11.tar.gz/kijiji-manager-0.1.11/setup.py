536f0d6c170992d24894bffd8dba3c3df65a6a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:25 +0000

    Add package kijiji-manager/0.1.11/kijiji-manager-0.1.11.tar.gz

---
+++ packages/kijiji-manager/kijiji-manager-0.1.11.tar.gz/kijiji-manager-0.1.11/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='kijiji-manager',
+    version='0.1.11',
+    author='jackm',
+    author_email='jackm@ehelion.com',
+    description='App for viewing, posting, reposting, and deleting your Kijiji ads',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jackm/kijiji-manager',
+    keywords='kijiji, ad, manager, reposter, automation, bot, flask',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT',
+    packages=find_packages(),
+
+    python_requires='>=3.6',
+    install_requires=[
+        'Flask~=2.0.0',
+        'Flask-WTF>=0.15',
+        'Flask-Login',
+        'Flask-Executor',
+        'WTForms>=2.3',
+        'httpx~=0.22.0',
+        'xmltodict>=0.11',
+        'is-safe-url',
+        'phonenumbers',
+        'pgeocode',
+    ],
+    entry_points={
+        'console_scripts': ['kijiji-manager=kijiji_manager.__main__:main']
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
