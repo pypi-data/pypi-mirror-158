ad3568871b9d4c30a2a588e73f72b6e929a9eafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:12 +0000

    Add package redgifs/1.2.0/redgifs-1.2.0.tar.gz

---
+++ packages/redgifs/redgifs-1.2.0.tar.gz/redgifs-1.2.0/redgifs/aio.py
@@ -0,0 +1,114 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present scrazzz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from typing import Any, Dict, List, Optional, Union
+
+import aiohttp
+
+from .http import AsyncHttp, ProxyAuth
+from .enums import Tags, Order
+from .parser import parse_search, parse_creators, parse_search_image
+from .models import Gif, URL, SearchResult, CreatorsResult
+
+class API:
+    def __init__(
+        self,
+        session: Optional[aiohttp.ClientSession] = None,
+        *,
+        proxy: Optional[str] = None,
+        proxy_auth: Optional[ProxyAuth] = None
+    ) -> None:
+        self.http: AsyncHttp = AsyncHttp(session, proxy=proxy, proxy_auth=proxy_auth)
+
+    async def get_tags(self) -> List[Dict[str, Union[str, int]]]:
+        resp = await self.http.get_tags()
+        return resp['tags']
+
+    async def get_gif(self, id: str) -> Gif:
+        json: Dict[str, Any] = (await self.http.get_gif(id))['gif']
+        return Gif(
+            id=json['id'],
+            create_date=json['createDate'],
+            has_audio=json['hasAudio'],
+            width=json['width'],
+            height=json['height'],
+            likes=json['likes'],
+            tags=json['tags'],
+            verified=json['verified'],
+            views=json['views'],
+            duration=json['duration'],
+            published=json['published'],
+            urls=URL(
+                sd=json['urls']['sd'],
+                hd=json['urls']['hd'],
+                poster=json['urls']['poster'],
+                thumbnail=json['urls']['thumbnail'],
+                vthumbnail=json['urls']['vthumbnail']
+            ),
+            username=json['userName'],
+            type=json['type'],
+            avg_color=json['avgColor'],
+        )
+
+    async def search(self, search_text: Union[str, Tags], *, order: Order = Order.recent, count: int = 80, page: int = 1) -> Optional[SearchResult]:
+        if isinstance(search_text, str):
+            st = Tags.search(search_text)
+        elif isinstance(search_text, Tags):
+            st = search_text.value
+        if st is not None:
+            resp = await self.http.search(st, order, count, page)
+            return parse_search(st, resp)
+
+    search_gif = search
+
+    async def search_creators(
+        self,
+        *,
+        page: int = 1,
+        order: Order = Order.recent,
+        verified: bool = False,
+        tags: Optional[Union[List[Tags], List[str]]] = None
+    ) -> CreatorsResult:
+        resp = await self.http.search_creators(page=page, order=order, verified=verified, tags=tags)
+        return parse_creators(resp)
+
+    async def search_image(
+        self,
+        search_text: Union[str, Tags],
+        *,
+        order: Order = Order.trending,
+        count: int = 80,
+        page: int = 1
+    ) -> SearchResult:
+        if isinstance(search_text, str):
+            # We are not going to use Tags.search() here because it doesn't matter
+            # whatever the search_text is, this API endpoints provides images nonetheless.
+            st = search_text
+        elif isinstance(search_text, Tags):
+            st = search_text.value
+        resp = await self.http.search_image(st, order, count, page)
+        return parse_search_image(st, resp)
+
+    async def close(self) -> None:
+        return (await self.http.close())
---
+++ packages/redgifs/redgifs-1.2.0.tar.gz/redgifs-1.2.0/redgifs/api.py
@@ -0,0 +1,216 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present scrazzz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from typing import Any, Dict, List, Optional, Union
+
+import requests
+
+from .http import HTTP, ProxyAuth
+from .enums import Order, Tags
+from .parser import parse_search, parse_creators, parse_search_image
+from .models import URL, Gif, SearchResult, CreatorsResult
+
+class API:
+    """The API Instance to get information from the RedGifs API.
+
+    .. note::
+
+        If you are using this library in an asynchronous code,
+        you should pass a session object which is an instance of
+        :class:`aiohttp.ClientSession`.
+
+    Parameters
+    ----------
+    session: Optional[:class:`requests.Session`]
+        A session object that can be provided to do the requests.
+        If not provided, a new session object is created.
+        See above note too.
+    proxy: Optional[:class:`str`]
+        A valid proxy URL.
+    proxy_auth: Optional[:class:`redgifs.ProxyAuth`]
+        The proxy auth to provide if the proxy requires it.
+    """
+
+    def __init__(
+        self,
+        session: Optional[requests.Session] = None,
+        *,
+        proxy: Optional[str] = None,
+        proxy_auth: Optional[ProxyAuth] = None
+    ) -> None:
+        self.http: HTTP = HTTP(session, proxy=proxy, proxy_auth=proxy_auth)
+
+    def get_tags(self) -> List[Dict[str, Union[str, int]]]:
+        """Get all available RedGifs Tags.
+        
+        Returns
+        -------
+        A list of dicts.
+        """
+        return (self.http.get_tags()['tags'])
+    
+    def get_gif(self, id: str) -> Gif:
+        """
+        Get details of a GIF with its ID.
+
+        Parameters
+        ----------
+        id: :class:`str`
+            The ID of the GIF.
+
+        Returns
+        -------
+        :py:class:`GIF <redgifs.models.Gif>` - The GIF's info.
+        """
+
+        json: Dict[str, Any] = self.http.get_gif(id)['gif']
+        return Gif(
+            id=json['id'],
+            create_date=json['createDate'],
+            has_audio=json['hasAudio'],
+            width=json['width'],
+            height=json['height'],
+            likes=json['likes'],
+            tags=json['tags'],
+            verified=json['verified'],
+            views=json['views'],
+            duration=json['duration'],
+            published=json['published'],
+            urls=URL(
+                sd=json['urls']['sd'],
+                hd=json['urls']['hd'],
+                poster=json['urls']['poster'],
+                thumbnail=json['urls']['thumbnail'],
+                vthumbnail=json['urls']['vthumbnail']
+            ),
+            username=json['userName'],
+            type=json['type'],
+            avg_color=json['avgColor'],
+        )
+
+    def search(
+        self,
+        search_text: Union[str, Tags],
+        *,
+        order: Order = Order.recent,
+        count: int = 80,
+        page: int = 1
+    ) -> Optional[SearchResult]:
+        """
+        Search for a GIF.
+
+        Parameters
+        ----------
+        search_text: Union[:class:`str`, :class:`Tags`]
+            The GIFs to search for. Can be a string or an instance of :class:`Tags`.
+        order: Optional[:class:`Order`]
+            The order of the GIFs to return.
+        count: Optional[:class:`int`]
+            The amount of GIFs to return.
+        page: Optional[:class:`int`]
+            The page number of the GIFs to return.
+
+        Returns
+        -------
+        :py:class:`SearchResult <redgifs.models.SearchResult>` - The search result.
+        """
+
+        if isinstance(search_text, str):
+            st = Tags.search(search_text)
+        elif isinstance(search_text, Tags):
+            st = search_text.value
+        if st is not None:
+            resp = self.http.search(st, order, count, page)
+            return parse_search(st, resp)
+
+    search_gif = search
+
+    def search_creators(
+        self,
+        *,
+        page: int = 1,
+        order: Order = Order.recent,
+        verified: bool = False,
+        tags: Optional[Union[List[Tags], List[str]]] = None
+    ) -> CreatorsResult:
+        """
+        Search for RedGifs Creators.
+
+        Parameters
+        ----------
+        page: Optional[:class:`int`]
+            The number of page to return.
+        order: Optional[:class:`Order`]
+            The order of the creators to return.
+        verified: Optional[:class:`bool`]
+            Wheather to only return verified creators.
+        tags: Optional[Union[List[:class:`Tags`], List[:class:`str`]]]
+            A list of tags to look for.
+            Narrows down the results to content creators that have contents with all the given tags.
+
+        Returns
+        -------
+        :py:class:`CreatorsResult <redgifs.models.CreatorsResult>` - The search result.
+        """
+        resp = self.http.search_creators(page=page, order=order, verified=verified, tags=tags)
+        return parse_creators(resp)
+
+    def search_image(
+        self,
+        search_text: Union[str, Tags],
+        *,
+        order: Order = Order.trending,
+        count: int = 80,
+        page: int = 1
+    ) -> SearchResult:
+        """
+        Search for images on Redgifs.
+
+        Parameters
+        ----------
+        search_text: Union[:class:`str`, :class:`Tags`]
+            The images to search for. Can be a string or an instance of :class:`Tags`.
+        order: Optional[:class:`Order`]
+            The order of the images to return.
+        count: Optional[:class:`int`]
+            The amount of images to return.
+        page: Optional[:class:`int`]
+            The page number of the images to return.
+
+        Returns
+        -------
+        :py:class:`SearchResult <redgifs.models.SearchResult>` - The search result.
+        """
+        if isinstance(search_text, str):
+            # We are not going to use Tags.search() here because it doesn't matter
+            # whatever the search_text is, this API endpoints provides images nonetheless.
+            st = search_text
+        elif isinstance(search_text, Tags):
+            st = search_text.value
+        resp = self.http.search_image(st, order, count, page)
+        return parse_search_image(st, resp)
+
+    def close(self) -> None:
+        """Closes the API session."""
+        return self.http.close()
