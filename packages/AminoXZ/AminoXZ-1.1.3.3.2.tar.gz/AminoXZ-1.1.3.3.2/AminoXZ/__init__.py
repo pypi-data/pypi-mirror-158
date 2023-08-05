83b14263e2695b30c84c366220b8d5ea98609eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package AminoXZ/1.1.3.3.2/AminoXZ-1.1.3.3.2.tar.gz

---
+++ packages/AminoXZ/AminoXZ-1.1.3.3.2.tar.gz/AminoXZ-1.1.3.3.2/AminoXZ/__init__.py
@@ -0,0 +1,29 @@
+"""
+Author: Xsarz
+
+Enjoy using!
+"""
+
+
+from os import system as s
+from .client import Client
+from .lib.util import generator, helpers, exceptions, headers
+from colored import fore
+from json import loads
+from requests import get
+__title__ = 'AminoXZ'
+__author__ = 'Xsarz'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Xsarz'
+__version__ = '1.1.3.3.2'
+__status__ = '(BETA)'
+
+def init():
+	__newest__ = loads(get("https://pypi.org/pypi/aminoxz/json").text)["info"]["version"]
+	s('cls')
+	if __version__ != __newest__:
+		return print(fore.ORANGE_1, f'{__title__} made by {__author__}\nPlease update the library. Your version: {__version__}  A new version:{__newest__}', fore.WHITE)
+	else:
+		return print(f'{__title__} {__version__} {__status__} made by {__author__}\n')
+
+init()
