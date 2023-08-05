d9c8d76628b53483a224c57ddcd89fdf5df52110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:53 +0000

    Add package keyo-coral/0.0.9/keyo_coral-0.0.9.tar.gz

---
+++ packages/keyo-coral/keyo_coral-0.0.9.tar.gz/keyo_coral-0.0.9/keyo/sdk_functions.py
@@ -0,0 +1,22 @@
+"""sdk functions"""
+
+
+def change_led():
+    """function for change LED """
+    response = "lead change"
+    print(response)
+    return response
+
+
+def buzzer_beep():
+    """function for Buzzer beep"""
+    response = "buzzer beep"
+    print(response)
+    return response
+
+
+def get_media():
+    """function for get media"""
+    response = "get media"
+    print(response)
+    return response
