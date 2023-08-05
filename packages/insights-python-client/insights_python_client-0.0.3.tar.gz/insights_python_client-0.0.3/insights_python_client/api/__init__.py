d913e9cdde898c4ac81ce4716573381a350d896a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:23 +0000

    Add package insights-python-client/0.0.3/insights_python_client-0.0.3.tar.gz

---
+++ packages/insights-python-client/insights_python_client-0.0.3.tar.gz/insights_python_client-0.0.3/insights_python_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from insights_python_client.api.authenticate_api import AuthenticateApi
+from insights_python_client.api.boundary_api import BoundaryApi
+from insights_python_client.api.file_api import FileApi
+from insights_python_client.api.metrics_api import MetricsApi
+from insights_python_client.api.weather_api import WeatherApi
+from insights_python_client.api.yield_api import YieldApi
