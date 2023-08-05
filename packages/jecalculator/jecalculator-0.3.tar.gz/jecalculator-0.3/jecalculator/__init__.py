d41c13ebe52afa8a5bb352b850f18586b3b7d024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package jecalculator/0.3/jecalculator-0.3.tar.gz

---
+++ packages/jecalculator/jecalculator-0.3.tar.gz/jecalculator-0.3/jecalculator/__init__.py
@@ -0,0 +1,33 @@
+def add_numbers(num1, num2):
+    results = num1 + num2
+    return f'{results}'
+
+
+def subtract_numbers(num1, num2):
+    results = num1 - num2
+    return f'{results}'
+
+
+def multiply_numbers(num1, num2):
+    results = num1 * num2
+    return f'{results}'
+
+
+def divide_numbers_float(num1, num2):
+    results = num1 / num2
+    return f'{results}'
+
+
+def divide_numbers_floor(num1, num2):
+    results = num1 // num2
+    return f'{results}'
+
+
+def power_numbers(num1, num2):
+    results = num1 ** num2
+    return f'{results}'
+
+
+def mode_numbers(num1, num2):
+    results = num1 % num2
+    return f'{results}'
