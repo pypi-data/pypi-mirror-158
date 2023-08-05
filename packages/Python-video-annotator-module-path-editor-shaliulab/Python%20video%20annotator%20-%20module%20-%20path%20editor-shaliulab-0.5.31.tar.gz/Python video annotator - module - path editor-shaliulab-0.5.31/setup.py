01ce183f5dd393ae1b64c64ba219c260b6aa1188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:55 +0000

    Add package Python-video-annotator-module-path-editor-shaliulab/0.5.31/Python%20video%20annotator%20-%20module%20-%20path%20editor-shaliulab-0.5.31.tar.gz

---
+++ packages/Python-video-annotator-module-path-editor-shaliulab/Python%20video%20annotator%20-%20module%20-%20path%20editor-shaliulab-0.5.31.tar.gz/Python video annotator - module - path editor-shaliulab-0.5.31/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+import os, re;
+with open(os.path.join(os.path.dirname(__file__), 'pythonvideoannotator_module_patheditor','__init__.py')) as fd:
+	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+
+setup(
+	name='Python video annotator - module - path editor-shaliulab',
+	version=version,
+	description="""""",
+	author=['Ricardo Ribeiro'],
+	author_email='ricardojvr@gmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-patheditor',
+	long_description = long_description,
+        install_requires=[
+            "confapp-shaliulab>=1.1.13",
+        ],
+        long_description_content_type = 'text/markdown',
+	packages=find_packages(),	
+)
