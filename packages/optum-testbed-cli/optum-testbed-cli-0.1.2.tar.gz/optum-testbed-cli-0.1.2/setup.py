116b74f27d2f37c90c40c952e9a5f2050a0877ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package optum-testbed-cli/0.1.2/optum-testbed-cli-0.1.2.tar.gz

---
+++ packages/optum-testbed-cli/optum-testbed-cli-0.1.2.tar.gz/optum-testbed-cli-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name = 'optum-testbed-cli',
+    version = '0.1.2',
+    packages = ['otbctl'],
+    author = 'Venkata Krishna Lolla',
+    author_email = 'venkata.lolla@optum.com',
+    maintainer = 'Venkata Krishna Lolla',
+    url = 'https://github.optum.com/OCS-Transformation-Optimization/optum-testbed-cli',
+    install_requires=[
+        'python-dotenv', 'requests', 'pyyaml', 'tabulate', 'numpy', 'munch'
+    ],
+    scripts=['install.sh'],
+    entry_points = {
+        'console_scripts': [
+            'otbctl = otbctl.__main__:main'
+        ]
+    })
