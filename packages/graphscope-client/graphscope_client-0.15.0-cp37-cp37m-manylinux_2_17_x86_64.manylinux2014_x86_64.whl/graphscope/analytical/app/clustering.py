5cd587ff9d77110aed1d6c019e2193acd4a86d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package graphscope-client/0.15.0/graphscope_client-0.15.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/graphscope-client/graphscope_client-0.15.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/graphscope/analytical/app/clustering.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Alibaba Group Holding Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from graphscope.framework.app import AppAssets
+from graphscope.framework.app import not_compatible_for
+from graphscope.framework.app import project_to_simple
+
+__all__ = ["avg_clustering", "clustering"]
+
+
+@project_to_simple
+@not_compatible_for("arrow_property", "dynamic_property")
+def clustering(graph):
+    """Local clustering coefficient of a node in a Graph is the fraction
+    of pairs of the node’s neighbors that are adjacent to each other.
+
+    Args:
+        graph (:class:`graphscope.Graph`): A simple graph.
+
+    Returns:
+        :class:`graphscope.framework.context.VertexDataContextDAGNode`:
+            A context with each vertex assigned the computed clustering value, will be evaluated in eager mode.
+
+    Examples:
+
+    .. code:: python
+
+        >>> import graphscope
+        >>> from graphscope.dataset import load_p2p_network
+        >>> sess = graphscope.session(cluster_type="hosts", mode="eager")
+        >>> g = load_p2p_network(sess)
+        >>> # project to a simple graph (if needed)
+        >>> pg = g.project(vertices={"host": ["id"]}, edges={"connect": ["dist"]})
+        >>> c = graphscope.clustering(pg)
+        >>> sess.close()
+    """
+    if graph.is_directed():
+        return AppAssets(algo="clustering", context="vertex_data")(graph)
+    else:
+        return AppAssets(algo="lcc", context="vertex_data")(graph)
+
+
+@project_to_simple
+@not_compatible_for("arrow_property", "dynamic_property", "undirected")
+def avg_clustering(graph):
+    """Compute the average clustering coefficient for the directed graph.
+
+    Args:
+        graph (:class:`graphscope.Graph`): A simple graph.
+
+    Returns:
+        r: float
+            The average clustering coefficient.
+
+    Examples:
+
+    .. code:: python
+
+        >>> import graphscope
+        >>> from graphscope.dataset import load_p2p_network
+        >>> sess = graphscope.session(cluster_type="hosts", mode="eager")
+        >>> g = load_p2p_network(sess)
+        >>> # project to a simple graph
+        >>> pg = g.project(vertices={"host": ["id"]}, edges={"connect": ["dist"]})
+        >>> c = graphscope.avg_clustering(pg)
+        >>> print(c.to_numpy("r", axis=0)[0])
+        >>> sess.close()
+    """
+    return AppAssets(algo="avg_clustering", context="tensor")(graph)
