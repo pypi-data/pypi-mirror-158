138db097ff6d73e1b7a7abc119b77443dd8a8de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:59 +0000

    Add package ojtool/0.0.9/ojtool-0.0.9.tar.gz

---
+++ packages/ojtool/ojtool-0.0.9.tar.gz/ojtool-0.0.9/src/ojtool/utils.py
@@ -0,0 +1,12 @@
+"""
+包含常用常量与各种工具类
+"""
+import math
+
+PI = math.pi
+E = math.e
+
+ALPHABET_LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+NUMBERS = "0123456789"
