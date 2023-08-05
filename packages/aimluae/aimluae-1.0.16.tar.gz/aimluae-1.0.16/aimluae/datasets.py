ae3b9d6a91d158d832af3851d29f3d1751206dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:24 +0000

    Add package aimluae/1.0.16/aimluae-1.0.16.tar.gz

---
+++ packages/aimluae/aimluae-1.0.16.tar.gz/aimluae-1.0.16/aimluae/datasets.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+
+
+def get_data(data_set):
+
+   extension = '.csv'
+   dir_name =  'https://raw.githubusercontent.com/aimluae/uaedata/main/datasets/'
+   filename = dir_name + data_set + extension
+
+   if requests.get(filename).status_code == 200:
+      return pd.read_csv(filename)
+      
+   else:
+      extension = '.xlsx'
+      filename = dir_name + data_set + extension
+      return pd.read_excel(filename)
+ 
+
+def list_datasets():
+   data_sets = [
+      'courier_companies', 'customs_warehouse_licensees', 'Number of teaching staff_in_Higher_Education_2020_06_09', 
+      'customs_warehouses', 'declaration_cargo_detail-1', 'declaration_cargo_detail-2', 'Graduates_in_Highe_ Education_20-06-09',
+      'livebirths', 'Services-En_0', 'vehicle_clearance_certificate', 'UAE_National_Red List_Species',
+   ]
+   return data_sets
