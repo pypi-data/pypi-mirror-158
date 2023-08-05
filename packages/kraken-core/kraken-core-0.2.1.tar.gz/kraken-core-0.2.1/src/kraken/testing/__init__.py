c60ab594f8d9fbfadfc43885f46e1a4f803181a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package kraken-core/0.2.1/kraken-core-0.2.1.tar.gz

---
+++ packages/kraken-core/kraken-core-0.2.1.tar.gz/kraken-core-0.2.1/src/kraken/testing/__init__.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pytest
+
+from kraken.core.build_context import BuildContext
+from kraken.core.project import Project
+
+__version__ = "0.2.1"
+
+
+@pytest.fixture
+def context() -> BuildContext:
+    return BuildContext(Path(".build"))
+
+
+@pytest.fixture
+def project(context: BuildContext) -> Project:
+    context.root_project = Project("test", Path.cwd(), None, context)
+    return context.root_project
