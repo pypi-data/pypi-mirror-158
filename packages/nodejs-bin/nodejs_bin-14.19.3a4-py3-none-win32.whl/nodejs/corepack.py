bf2667288383b66867c19c86d7edea8fe988aa2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:56 +0000

    Add package nodejs-bin/14.19.3a4/nodejs_bin-14.19.3a4-py3-none-win32.whl

---
+++ packages/nodejs-bin/nodejs_bin-14.19.3a4-py3-none-win32.whl/nodejs/corepack.py
@@ -0,0 +1,32 @@
+import os, sys, subprocess
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    call = subprocess.call
+    run = subprocess.run
+    Popen = subprocess.Popen
+
+else:
+    def call(args, **kwargs):
+        return subprocess.call([
+            os.path.join(os.path.dirname(__file__), "corepack.cmd"),
+            *args
+        ], **kwargs)
+
+    def run(args, **kwargs):
+        return subprocess.run([
+            os.path.join(os.path.dirname(__file__), "corepack.cmd"),
+            *args
+        ], **kwargs)
+    
+    def Popen(args, **kwargs):
+        return subprocess.Popen([
+            os.path.join(os.path.dirname(__file__), "corepack.cmd"),
+            *args
+        ], **kwargs)
+
+def main() -> None:
+    sys.exit(call(sys.argv[1:]))
+
+if __name__ == '__main__':
+    main()
