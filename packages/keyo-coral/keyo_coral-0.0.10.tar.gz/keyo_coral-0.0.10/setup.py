653a03988aab1adbcba8c8068f9261a5621330c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:30 +0000

    Add package keyo-coral/0.0.10/keyo_coral-0.0.10.tar.gz

---
+++ packages/keyo-coral/keyo_coral-0.0.10.tar.gz/keyo_coral-0.0.10/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+from setuptools.command.install import install
+from distutils.command.build_py import build_py as _build_py
+from setuptools.command.develop import develop
+from subprocess import check_call
+
+VERSION = '0.0.10'
+DESCRIPTION = 'SDK sanic application'
+
+class CustomInstall(install):
+    def run(self):
+        print('overriden install command')
+        install.run(self)
+
+class PostDevelopCommand(develop):
+    """Pre-installation for development mode."""
+    def run(self):
+        check_call("apt-get install this-package".split())
+        develop.run(self)
+
+packages = ["keyo"]
+
+# Setting up
+setup(
+    name="keyo_coral",
+    version=VERSION,
+    author="swalih",
+    author_email="<swalihchungath@gmail.com>",
+    cmdclass={'install': CustomInstall, 'develop': PostDevelopCommand,},
+    entry_points = {
+        'console_scripts': ['funniest-joke=keyo.script:main'],
+    },
+    description=DESCRIPTION,
+    packages=packages,
+    install_requires=['sanic'],
+    keywords=['sanic','palm'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
