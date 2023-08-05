d88376372c2d243f206591e8e37600d0407a622e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:23 +0000

    Add package lithopscloud/1.0.0b93/lithopscloud-1.0.0b93-py3-none-any.whl

---
+++ packages/lithopscloud/lithopscloud-1.0.0b93-py3-none-any.whl/lithopscloud/modules/gen2/endpoint.py
@@ -0,0 +1,27 @@
+from lithopscloud.modules.config_builder import ConfigBuilder, update_decorator, spinner
+from typing import Any, Dict
+from lithopscloud.modules.utils import get_option_from_list
+
+
+class EndpointConfig(ConfigBuilder):
+    
+    def __init__(self, base_config: Dict[str, Any]) -> None:
+        super().__init__(base_config)
+
+    @update_decorator
+    def run(self) -> Dict[str, Any]:
+
+        @spinner
+        def get_regions_objects():
+            return ConfigBuilder.ibm_vpc_client.list_regions().get_result()['regions']
+
+        regions_objects = get_regions_objects()
+        
+        default = self.defaults.get('region')
+        region_obj = get_option_from_list("Choose region", regions_objects, default = default)
+
+        # update global ibm_vpc_client to selected endpoint
+        ConfigBuilder.ibm_vpc_client.set_service_url(region_obj['endpoint'] + '/v1')
+        ConfigBuilder.region = region_obj['name']
+        
+        return region_obj['endpoint']
