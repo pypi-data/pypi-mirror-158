64244186439da1b5c9603d8da66a0fe25c90f640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:22 +0000

    Add package oaklib/0.1.27/oaklib-0.1.27-py3-none-any.whl

---
+++ packages/oaklib/oaklib-0.1.27-py3-none-any.whl/oaklib/__init__.py
@@ -0,0 +1,10 @@
+"""
+Oaklib
+------
+"""
+__version__ = "0.1.0"
+
+from oaklib.interfaces import BasicOntologyInterface  # noqa:F401
+from oaklib.resource import OntologyResource  # noqa:F401
+
+schemes = {}
