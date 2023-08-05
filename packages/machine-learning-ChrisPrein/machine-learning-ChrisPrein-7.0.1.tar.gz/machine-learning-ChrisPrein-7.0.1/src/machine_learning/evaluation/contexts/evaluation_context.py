d88d5feffa1a3b2196f5a13441cc804578f51c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package machine-learning-ChrisPrein/7.0.1/machine-learning-ChrisPrein-7.0.1.tar.gz

---
+++ packages/machine-learning-ChrisPrein/machine-learning-ChrisPrein-7.0.1.tar.gz/machine-learning-ChrisPrein-7.0.1/src/machine_learning/evaluation/contexts/evaluation_context.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from logging import Logger
+from typing import Dict, Generic, List, Optional, Tuple, Deque, TypeVar
+from torch.utils.data import Dataset, random_split
+
+from ...modeling.abstractions.model import Model, TInput, TTarget
+
+TModel = TypeVar('TModel', bound=Model)
+
+@dataclass(frozen=True)
+class Prediction(Generic[TInput, TTarget]):
+    input: TInput
+    prediction: TTarget
+    target: TTarget
+
+@dataclass
+class EvaluationContext(Generic[TInput, TTarget, TModel]):
+    model: Optional[TModel]
+    dataset_name: str
+    predictions: Deque[Prediction[TInput, TTarget]]
+    current_batch_index: int
