1e62fdf3bbf849564779b8772b1cc58bb9cf1ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:00 +0000

    Add package godel/0.0.2/godel-0.0.2-py3-none-any.whl

---
+++ packages/godel/godel-0.0.2-py3-none-any.whl/godel/queries/GetTripleForValidation.py
@@ -0,0 +1,40 @@
+from godel.fragments.TripleWidget import fragment_triple_widget
+import sgqlc.types
+import sgqlc.operation
+import godel.schema
+
+_schema = godel.schema
+_schema_root = _schema.schema
+
+__all__ = ('Operations',)
+
+
+def query_get_triple_for_validation():
+    _op = sgqlc.operation.Operation(_schema_root.query_type, name='GetTripleForValidation')
+    _op_unvalidated_triple = _op.unvalidated_triple()
+    _op_unvalidated_triple__as__Statement = _op_unvalidated_triple.__as__(_schema.Statement)
+    _op_unvalidated_triple__as__Statement.__fragment__(fragment_triple_widget())
+    _op_unvalidated_triple__as__Statement_subject = _op_unvalidated_triple__as__Statement.subject()
+    _op_unvalidated_triple__as__Statement_subject.description()
+    _op_unvalidated_triple__as__Statement_subject.thumbnail()
+    _op_unvalidated_triple__as__Statement_subject.website()
+    _op_unvalidated_triple__as__Statement_subject.golden_id()
+    _op_unvalidated_triple__as__Statement_object_entity = _op_unvalidated_triple__as__Statement.object_entity()
+    _op_unvalidated_triple__as__Statement_object_entity.description()
+    _op_unvalidated_triple__as__Statement_object_entity.thumbnail()
+    _op_unvalidated_triple__as__Statement_object_entity.website()
+    _op_unvalidated_triple__as__Statement_object_entity.golden_id()
+    _op_current_user = _op.current_user()
+    _op_current_user.remaining_skips()
+    _op_current_user_user_flags = _op_current_user.user_flags()
+    _op_current_user_user_flags_nodes = _op_current_user_user_flags.nodes()
+    _op_current_user_user_flags_nodes.flag()
+    return _op
+
+
+class Query:
+    get_triple_for_validation = query_get_triple_for_validation()
+
+
+class Operations:
+    query = Query
