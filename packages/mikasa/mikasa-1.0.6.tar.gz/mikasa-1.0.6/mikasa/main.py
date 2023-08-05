2b47608212e4c0b3364c541ffa160296352fe583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package mikasa/1.0.6/mikasa-1.0.6.tar.gz

---
+++ packages/mikasa/mikasa-1.0.6.tar.gz/mikasa-1.0.6/mikasa/main.py
@@ -0,0 +1,58 @@
+import typer
+import os
+from .db import delete, draw,fetch,drop
+from .db import insert as insert_data
+
+
+
+# draw the database 
+draw()
+
+print("-"*20,"\n")
+
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
+def main(name:str):
+        
+    if not fetch(name):
+        exit()
+    os.chdir(fetch(name))
+    os.system("code .")    
+
+@app.command()
+def dropall():
+    drop()
+
+@app.command()
+def insert(name,path):
+    insert_data(name,path)
+
+@app.command()
+def mostaql():
+    os.chdir("E:\\mostaql")
+
+@app.command()
+def get(name):
+    fetch(name)
+
+@app.command()
+def go(project_name):
+    
+    if not fetch(project_name):
+        
+        exit()
+    
+    os.chdir(fetch(project_name))
+    os.system("code .")
+
+@app.command()
+def remove(name):
+    delete(name)
+
+
+
+
+if __name__ == '__main__':
+    app()
