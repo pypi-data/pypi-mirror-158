23144a02ce36030603806be7ef3ef5cef0e43a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:33 +0000

    Add package khodnevis-normalizer/0.0.1/khodnevis_normalizer-0.0.1.tar.gz

---
+++ packages/khodnevis-normalizer/khodnevis_normalizer-0.0.1.tar.gz/khodnevis_normalizer-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Khodnevis Normalizer'
+LONG_DESCRIPTION = 'A Python library for Persian text preprocessing.'
+
+# Setting up
+setup(
+    name="khodnevis_normalizer",
+    version=VERSION,
+    author="khodnevisAI",
+    author_email="<khodnevis.group@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['hazm==0.7.0', 'parsivar==0.2.3'],
+    keywords=['python', 'persian', 'normalizer', 'text'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
