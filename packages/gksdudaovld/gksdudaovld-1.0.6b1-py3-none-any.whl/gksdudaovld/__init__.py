6923d29ffaa8ccfc5d950d04440602d6bce037bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:39 +0000

    Add package gksdudaovld/1.0.6b1/gksdudaovld-1.0.6b1-py3-none-any.whl

---
+++ packages/gksdudaovld/gksdudaovld-1.0.6b1-py3-none-any.whl/gksdudaovld/__init__.py
@@ -0,0 +1,9 @@
+"""
+Mapping module for conversion between Korean and English
+
+Made by ForestHouse
+
+GitHub Repo : https://github.com/ForestHouse2316/gksdudaovld
+"""
+
+__all__ = ['KoEnMapper.py']
