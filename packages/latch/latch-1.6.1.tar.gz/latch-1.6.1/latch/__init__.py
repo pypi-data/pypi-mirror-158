9fcd4144bfc0ffbccf15589b0759569c27c0476c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package latch/1.6.1/latch-1.6.1.tar.gz

---
+++ packages/latch/latch-1.6.1.tar.gz/latch-1.6.1/latch/__init__.py
@@ -0,0 +1,17 @@
+"""The Latch SDK
+
+A commandline toolchain to define and register serverless workflows with the
+Latch platform.
+"""
+
+from latch.functions.messages import message
+from latch.resources.conditional import create_conditional_section
+from latch.resources.map_tasks import map_task
+from latch.resources.tasks import (
+    large_gpu_task,
+    large_task,
+    medium_task,
+    small_gpu_task,
+    small_task,
+)
+from latch.resources.workflow import workflow
