22afa35ec5486ce5f978ac4001c710f4371f5ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:35 +0000

    Add package keyo-coral/0.0.7/keyo_coral-0.0.7.tar.gz

---
+++ packages/keyo-coral/keyo_coral-0.0.7.tar.gz/keyo_coral-0.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+from setuptools.command.install import install
+from distutils.command.build_py import build_py as _build_py
+
+VERSION = '0.0.7'
+DESCRIPTION = 'SDK sanic application'
+
+class CustomInstall(install):
+    def run(self):
+        print('overriden install command')
+        install.run(self)
+        
+
+packages = ["keyo"]
+
+# Setting up
+setup(
+    name="keyo_coral",
+    version=VERSION,
+    author="swalih",
+    author_email="<swalihchungath@gmail.com>",
+    cmdclass={'install': CustomInstall},
+    description=DESCRIPTION,
+    packages=packages,
+    install_requires=['sanic'],
+    keywords=['sanic','palm'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
