735ca421b6b11f7be26641a55e7a41ebfb598b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package vang/1.3.1/vang-1.3.1-py2.py3-none-any.whl

---
+++ packages/vang/vang-1.3.1-py2.py3-none-any.whl/vang/bitbucket/get_projects.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import argparse
+from sys import argv
+
+from vang.bitbucket.api import get_all
+
+
+def get_projects():
+    return get_all("/rest/api/1.0/projects")
+
+
+def main(key=False):
+    for project in get_projects():
+        if key:
+            print(project["key"])
+        else:
+            print(f'{project["key"]}: {project["name"]}')
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="Get projects from Bitbucket")
+    parser.add_argument(
+        "-k", "--key", help="Print only project key", action="store_true"
+    )
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main(**parse_args(argv[1:]).__dict__)
