e5e77fadccfd7c6465df2b1f23b956fb245c7b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:30 +0000

    Add package uw-it-build-fingerprinter/0.1.3/uw-it-build-fingerprinter-0.1.3.tar.gz

---
+++ packages/uw-it-build-fingerprinter/uw-it-build-fingerprinter-0.1.3.tar.gz/uw-it-build-fingerprinter-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fingerprinter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'pydantic>=1.9.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'uw-it-build-fingerprinter',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Tom Thorogood',
+    'author_email': 'tom@tomthorogood.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
