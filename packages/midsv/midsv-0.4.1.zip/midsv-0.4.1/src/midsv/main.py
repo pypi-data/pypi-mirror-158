15cba14412db1dbb904c376845fa99f3fe1b4b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:22 +0000

    Add package midsv/0.4.1/midsv-0.4.1.zip

---
+++ packages/midsv/midsv-0.4.1.zip/midsv-0.4.1/src/midsv/main.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from . import convert
+from . import format
+from . import proofread
+
+
+def transform(sam: list[list]) -> list[dict]:
+    """Integrated function to perform MIDSV conversion
+
+    Args:
+        sam (list[list]): Lists ot SAM format
+
+    Returns:
+        list[dict]: Dictionary containing QNAME, RNAME, MIDSV, and QSCORE
+    """
+
+    format.check_sam_format(sam)
+
+    sqheaders = format.extract_sqheaders(sam)
+    samdict = format.dictionarize_sam(sam)
+
+    samdict = format.remove_softclips(samdict)
+    samdict = format.remove_overlapped(samdict)
+
+    for i, alignment in enumerate(samdict):
+        samdict[i]["MIDSV"] = convert.cstag_to_midsv(alignment["CSTAG"])
+        samdict[i]["CSSPLIT"] = convert.cstag_to_cssplit(alignment["CSTAG"])
+        samdict[i]["QSCORE"] = convert.qual_to_qscore(alignment["QUAL"], alignment["MIDSV"])
+
+    samdict_polished = proofread.join(samdict)
+    samdict_polished = proofread.pad(samdict_polished, sqheaders)
+    samdict_polished = proofread.select(samdict_polished)
+    return samdict_polished
