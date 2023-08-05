0a6ed8e7f9d90a6ea9560976add43940d24d929c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:55 +0000

    Add package deebot-client/1.5.1/deebot_client-1.5.1-py3-none-any.whl

---
+++ packages/deebot-client/deebot_client-1.5.1-py3-none-any.whl/deebot_client/events/const.py
@@ -0,0 +1,76 @@
+"""Event constants."""
+from typing import Mapping
+
+from ..command import Command
+from ..commands import (
+    GetAdvancedMode,
+    GetBattery,
+    GetCachedMapInfo,
+    GetCarpetAutoFanBoost,
+    GetChargeState,
+    GetCleanInfo,
+    GetCleanLogs,
+    GetContinuousCleaning,
+    GetError,
+    GetFanSpeed,
+    GetLifeSpan,
+    GetMajorMap,
+    GetMapTrace,
+    GetPos,
+    GetStats,
+    GetTotalStats,
+    GetVolume,
+    GetWaterInfo,
+)
+from . import (
+    AdvancedModeEvent,
+    BatteryEvent,
+    CarpetAutoFanBoostEvent,
+    CleanLogEvent,
+    ContinuousCleaningEvent,
+    CustomCommandEvent,
+    ErrorEvent,
+    Event,
+    FanSpeedEvent,
+    LifeSpanEvent,
+    PositionsEvent,
+    ReportStatsEvent,
+    RoomsEvent,
+    StatsEvent,
+    StatusEvent,
+    TotalStatsEvent,
+    VolumeEvent,
+    WaterInfoEvent,
+)
+from .map import (
+    MajorMapEvent,
+    MapSetEvent,
+    MapSubsetEvent,
+    MapTraceEvent,
+    MinorMapEvent,
+)
+
+EVENT_DTO_REFRESH_COMMANDS: Mapping[type[Event], list[Command]] = {
+    AdvancedModeEvent: [GetAdvancedMode()],
+    BatteryEvent: [GetBattery()],
+    CarpetAutoFanBoostEvent: [GetCarpetAutoFanBoost()],
+    CleanLogEvent: [GetCleanLogs()],
+    ContinuousCleaningEvent: [GetContinuousCleaning()],
+    CustomCommandEvent: [],
+    ErrorEvent: [GetError()],
+    FanSpeedEvent: [GetFanSpeed()],
+    LifeSpanEvent: [GetLifeSpan()],
+    MajorMapEvent: [GetMajorMap()],
+    MapSetEvent: [],
+    MapSubsetEvent: [],
+    MapTraceEvent: [GetMapTrace()],
+    MinorMapEvent: [],
+    PositionsEvent: [GetPos()],
+    ReportStatsEvent: [],  # ReportStats cannot be pulled
+    RoomsEvent: [GetCachedMapInfo()],
+    StatsEvent: [GetStats()],
+    StatusEvent: [GetChargeState(), GetCleanInfo()],
+    TotalStatsEvent: [GetTotalStats()],
+    VolumeEvent: [GetVolume()],
+    WaterInfoEvent: [GetWaterInfo()],
+}
