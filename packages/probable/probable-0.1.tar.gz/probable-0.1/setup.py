93a7c0c427d2f05933efedcb897d27064efb36db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:13 +0000

    Add package probable/0.1/probable-0.1.tar.gz

---
+++ packages/probable/probable-0.1.tar.gz/probable-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probable',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probable'],
+      author= 'Oluwaseun Ajayi',
+      author_email= 'oluwaseunaajayi@student.oauife.edu.ng',
+      zip_safe=False)
