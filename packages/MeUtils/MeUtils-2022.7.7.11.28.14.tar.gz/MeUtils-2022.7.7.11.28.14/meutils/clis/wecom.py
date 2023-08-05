7ab2444d7c7c80290490c9b3530f051f070828a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:41 +0000

    Add package MeUtils/2022.7.7.11.28.14/MeUtils-2022.7.7.11.28.14.tar.gz

---
+++ packages/MeUtils/MeUtils-2022.7.7.11.28.14.tar.gz/MeUtils-2022.7.7.11.28.14/meutils/clis/wecom.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : wecom
+# @Time         : 2021/9/6 下午5:21
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+import typer
+
+from meutils.pipe import *
+from meutils.notice import nesc_send as nesc_send_
+from meutils.notice.wecom import Wecom
+
+cli = typer.Typer(name="MeUtils CLI")
+
+
+@cli.command()
+def notice(title, text='', hook_url=None):
+    """sh管道传参 echo args | xargs -I {} mecli notice {}"""
+    Wecom(hook_url).send_markdown(title, text)
+    return 'ok'
+
+
+@cli.command()
+def wecom_send_file(path, type='file', hook_url=None):
+    """mecli notice file_path"""
+    Wecom(hook_url).send_file(path, type)
+    return 'ok'
+
+
+@cli.command()
+def nesc_send(content="这是一条测试", chat_id=1162363786, appid='ai', filename=None):
+    return nesc_send_(content=content, chat_id=chat_id, appid=appid, filename=filename)
+
+
+if __name__ == '__main__':
+    cli()
