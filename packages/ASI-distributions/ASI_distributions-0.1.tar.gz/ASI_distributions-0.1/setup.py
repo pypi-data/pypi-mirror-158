96b181708b3aa4571ca59cb80995ca26972dda0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package ASI-distributions/0.1/ASI_distributions-0.1.tar.gz

---
+++ packages/ASI-distributions/ASI_distributions-0.1.tar.gz/ASI_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ASI_distributions',
+version= '0.1',
+description = 'Gaussian distributions',
+packages=['ASI_distributions'],
+author = 'Andrew Ingalls',
+author_email = 'ingallsandrew0@gmail.com',
+zip_safe=False)
