b1606611e3626a9cc9613d8d195a50f85b00e700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:42 +0000

    Add package musicvideos-extras/0.0.9/musicvideos_extras-0.0.9-py3-none-any.whl

---
+++ packages/musicvideos-extras/musicvideos_extras-0.0.9-py3-none-any.whl/musicvideos_extras/__init__.py
@@ -0,0 +1,57 @@
+'''
+Pick it up
+
+Wanted some speakers, Xbox, and t-shirts
+I've been good like every single day
+Looked under my tree, my box was empty
+Santa made an enemy that day (What the fuck?)
+Done being jolly, time to be naughty
+Think it's time we settle up the score
+Fill up the wishlist, let's cause some mischief
+Grab my keys, I'm headin' out the door
+
+I've been good for way too long
+Gonna get the shit I want (Ah, ah)
+
+Never gave me a goddamn thing that I want
+I was good every day but he didn't give a fuck
+This year, put the top on the back of the truck
+Santa's banging on the back of the truck
+Never gave me a goddamn thing that I want
+I was good every day but he didn't give a fuck
+This year, put the top on the back of the truck
+Santa's banging on the back of the truck
+
+La-la-la-la-la, la-la-la-la-la
+La-la-la-la-la-la-la-la-la
+La-la-la-la-la, la-la-la-la-la, la-la-la-la-la
+La-la-la-la-la-la-la-la-la
+
+He's watching TV, he doesn't see me
+Go and get the bag out of his sleigh
+Elves in the kitchen heard the ignition
+Think it's time we make our getaway
+Chains on the tires, he's right behind us
+Screaming 'cause we stole his magic bag (Get back here)
+Boots full of lead feet, gas in the Chevy
+Hundred-fifty miles on the dash
+
+I've been good for way too long
+Gonna get the shit I want (Ah, ah)
+
+Never gave me a goddamn thing that I want
+I was good every day but he didn't give a fuck
+This year, put the top on the back of the truck
+Santa's banging on the back of the truck
+Never gave me a goddamn thing that I want
+I was good every day but he didn't give a fuck
+This year, put the top on the back of the truck
+Santa's banging on the back of the truck
+
+La-la-la-la-la, la-la-la-la-la
+La-la-la-la-la-la-la-la-la
+La-la-la-la-la, la-la-la-la-la, la-la-la-la-la
+La-la-la-la-la-la-la-la-la
+
+https://youtu.be/OvJYkxyqwso
+'''
