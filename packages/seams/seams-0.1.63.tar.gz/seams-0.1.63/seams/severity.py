4676b13165b51f84fbb2d30b6af51aa6d0be4a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:51 +0000

    Add package seams/0.1.63/seams-0.1.63.tar.gz

---
+++ packages/seams/seams-0.1.63.tar.gz/seams-0.1.63/seams/severity.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Severity(Enum):
+
+    CRITICAL = "CRITICAL"
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    INFO = "INFO"
+    DEBUG = "DEBUG"
+    NOTSET = "NOTSET"
