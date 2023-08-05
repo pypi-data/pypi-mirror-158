bd13e98669cfa38c97187156d0e45a0c96619b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:13 +0000

    Add package torchx-nightly/2022.7.6/torchx_nightly-2022.7.6-py3-none-any.whl

---
+++ packages/torchx-nightly/torchx_nightly-2022.7.6-py3-none-any.whl/torchx/cli/cmd_runopts.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import logging
+
+from torchx.cli.cmd_base import SubCommand
+from torchx.cli.colors import ENDC, GREEN
+from torchx.runner.api import get_runner
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class CmdRunopts(SubCommand):
+    def add_arguments(self, subparser: argparse.ArgumentParser) -> None:
+        subparser.add_argument(
+            "scheduler",
+            type=str,
+            nargs="?",
+            help="scheduler to dump the runopts for, dumps for all schedulers if not specified",
+        )
+
+    def run(self, args: argparse.Namespace) -> None:
+        filter = args.scheduler
+        run_opts = get_runner().run_opts()
+
+        for scheduler, opts in run_opts.items():
+            if not filter or scheduler == filter:
+                print(f"{GREEN}{scheduler}{ENDC}:\n{repr(opts)}\n")
