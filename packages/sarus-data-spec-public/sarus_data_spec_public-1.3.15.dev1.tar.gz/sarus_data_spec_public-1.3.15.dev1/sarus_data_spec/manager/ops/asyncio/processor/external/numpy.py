66a9381b54b5d22023fc383e28433f26886f92f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:18 +0000

    Add package sarus-data-spec-public/1.3.15.dev1/sarus_data_spec_public-1.3.15.dev1.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-1.3.15.dev1.tar.gz/sarus_data_spec_public-1.3.15.dev1/sarus_data_spec/manager/ops/asyncio/processor/external/numpy.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import numpy as np
+
+from .utils import sarus_external_op
+
+
+@sarus_external_op
+async def np_mean(*args: Any, **kwargs: Any) -> Any:
+    return np.mean(*args, **kwargs)
+
+
+@sarus_external_op
+async def np_std(*args: Any, **kwargs: Any) -> Any:
+    return np.std(*args, **kwargs)
+
+
+@sarus_external_op
+async def np_rand(*args: Any, **kwargs: Any) -> Any:
+    return np.random.rand()
