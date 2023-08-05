f62c537524760875dc33932e2a3c90fb66e9eefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package nelltest/0.1/nelltest-0.1.tar.gz

---
+++ packages/nelltest/nelltest-0.1.tar.gz/nelltest-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nelltest',
+      version='0.1',
+      description='Test module of nellgram',
+      packages=['nelltest'],
+      author_email='elezor.2009@gmail.com',
+      zip_safe=False)
