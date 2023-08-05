28acbe872668459fca959cd3b58e09b11422ad40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:17 +0000

    Add package selva/0.1.8/selva-0.1.8-py3-none-any.whl

---
+++ packages/selva/selva-0.1.8-py3-none-any.whl/selva/web/routing/converter.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+from typing import Generic, TypeVar
+
+__all__ = ("PathParamConverter",)
+
+T = TypeVar("T")
+
+
+class PathParamConverter(Generic[T], metaclass=ABCMeta):
+    @abstractmethod
+    async def from_path(self, value: str) -> T:
+        pass
+
+    @abstractmethod
+    async def to_path(self, obj: T) -> str:
+        pass
