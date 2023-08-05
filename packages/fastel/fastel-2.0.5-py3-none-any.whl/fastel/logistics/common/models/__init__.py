ac419fab09407ce70e3103f17f9d6dc2b5ee6a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:12 +0000

    Add package fastel/2.0.5/fastel-2.0.5-py3-none-any.whl

---
+++ packages/fastel/fastel-2.0.5-py3-none-any.whl/fastel/logistics/common/models/__init__.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from typing import Any, Dict
+
+from pydantic import BaseModel
+
+
+class LogisticsStatus(str, Enum):
+    pending = "pending"
+    in_delivery = "in_delivery"
+    delivered = "delivered"
+    exception = "exception"
+
+    center_delivered = "center_delivered"
+    store_delivered = "store_delivered"
+
+
+class LogisticTypes(str, Enum):
+    HOME = "HOME"
+    CVS = "CVS"
+    UNKNOWN = "UNKNOWN"
+
+
+class LogisticsResp(BaseModel):
+    logistics_id: str
+    logistics_type: LogisticTypes
+    Logistics_subtype: str = ""
+    logistics_status: LogisticsStatus
+    logistics_message: str
+    logistics_detail: Dict[str, Any] = {}
