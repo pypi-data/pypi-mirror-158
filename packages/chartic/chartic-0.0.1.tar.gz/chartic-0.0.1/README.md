74035d7dcbb9a316ff9b2b525b391496cebdef39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package chartic/0.0.1/chartic-0.0.1.tar.gz

---
+++ packages/chartic/chartic-0.0.1.tar.gz/chartic-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Chartic
+This package will contain the open-source modules of the [Chartic](https://chartic.app) project 
---
+++ packages/chartic/chartic-0.0.1.tar.gz/chartic-0.0.1/src/chartic/chartic.py
@@ -0,0 +1,10 @@
+def main():
+    raise NotImplemented
+
+
+def foo() -> str:
+    return 'bar'
+
+
+if __name__ == '__main__':
+    main()
