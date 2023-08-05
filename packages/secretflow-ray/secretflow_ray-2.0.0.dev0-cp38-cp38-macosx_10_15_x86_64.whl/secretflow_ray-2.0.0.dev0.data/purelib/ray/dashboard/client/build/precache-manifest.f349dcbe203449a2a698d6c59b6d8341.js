436e29e59e0be5fe6e249cf8780976a5d2460b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package secretflow-ray/2.0.0.dev0/secretflow_ray-2.0.0.dev0-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/secretflow-ray/secretflow_ray-2.0.0.dev0-cp38-cp38-macosx_10_15_x86_64.whl/secretflow_ray-2.0.0.dev0.data/purelib/ray/dashboard/client/build/precache-manifest.f349dcbe203449a2a698d6c59b6d8341.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e08a7d854391b6e222b933356dbfd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "477b3b280b2583e525ed",
+    "url": "./static/css/10.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "c11f8c5db0f8bbcd48c7",
+    "url": "./static/css/11.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "25b35a79b73f57395aa3",
+    "url": "./static/css/12.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "d4b5c7e15c48afde45a1",
+    "url": "./static/css/8.b3c2cc50.chunk.css"
+  },
+  {
+    "revision": "5a8e957a287e8cc0f4b3",
+    "url": "./static/js/0.768f0f87.chunk.js"
+  },
+  {
+    "revision": "f41a33d180c6248388b0",
+    "url": "./static/js/1.b3aa501a.chunk.js"
+  },
+  {
+    "revision": "477b3b280b2583e525ed",
+    "url": "./static/js/10.3d2173ce.chunk.js"
+  },
+  {
+    "revision": "c11f8c5db0f8bbcd48c7",
+    "url": "./static/js/11.f858f8b2.chunk.js"
+  },
+  {
+    "revision": "25b35a79b73f57395aa3",
+    "url": "./static/js/12.af767678.chunk.js"
+  },
+  {
+    "revision": "485eec382f279b9856e9",
+    "url": "./static/js/13.41d5c7a0.chunk.js"
+  },
+  {
+    "revision": "05658351868cd411115b",
+    "url": "./static/js/14.fd268023.chunk.js"
+  },
+  {
+    "revision": "abad85a7f6fb3295bdba",
+    "url": "./static/js/15.903d50ad.chunk.js"
+  },
+  {
+    "revision": "40a1df9c4db0e2d84a59",
+    "url": "./static/js/16.4fa27061.chunk.js"
+  },
+  {
+    "revision": "a29fae7c2d06a9648aa1",
+    "url": "./static/js/17.c1c2760e.chunk.js"
+  },
+  {
+    "revision": "d0ee3f1677c031ec777e",
+    "url": "./static/js/2.56985f75.chunk.js"
+  },
+  {
+    "revision": "c356436f304a9e1c6a5b",
+    "url": "./static/js/3.2df46d8a.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/3.2df46d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf70a944383a76afa6c",
+    "url": "./static/js/4.505deb68.chunk.js"
+  },
+  {
+    "revision": "c267478df228c984caf7",
+    "url": "./static/js/7.02e73ab2.chunk.js"
+  },
+  {
+    "revision": "b9d59c7bf327b5c961fd3d7120589581",
+    "url": "./static/js/7.02e73ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b5c7e15c48afde45a1",
+    "url": "./static/js/8.ea3f5bc1.chunk.js"
+  },
+  {
+    "revision": "bc2042ba0824433b20eb",
+    "url": "./static/js/9.ebe282ce.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "./static/js/9.ebe282ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e566e13ed08ae045146",
+    "url": "./static/js/main.f66074a9.chunk.js"
+  },
+  {
+    "revision": "d7282cd39d6d0431c55c",
+    "url": "./static/js/runtime-main.ad7400c3.js"
+  },
+  {
+    "revision": "409917799d72263e1774be450d65fe16",
+    "url": "./static/media/logo.40991779.svg"
+  }
+]);
