4bfe6bf01967b88677e79222c7835bd8b43d3c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:15 +0000

    Add package xedocs/0.1.6/xedocs-0.1.6.tar.gz

---
+++ packages/xedocs/xedocs-0.1.6.tar.gz/xedocs-0.1.6/xedocs/schemas/bodega.py
@@ -0,0 +1,22 @@
+import datetime
+
+import rframe
+
+from .base_schemas import VersionedXeDoc
+
+
+class Bodega(VersionedXeDoc):
+    """Detector parameters
+    A collection of non-time dependent detector
+    values.
+    """
+
+    _ALIAS = "bodega"
+
+    field: str = rframe.Index()
+
+    value: float
+    uncertainty: float
+    definition: str
+    reference: str
+    date: datetime.datetime
