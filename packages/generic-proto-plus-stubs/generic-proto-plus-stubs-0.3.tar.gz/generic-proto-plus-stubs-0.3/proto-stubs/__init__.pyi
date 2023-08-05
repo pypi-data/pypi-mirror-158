72caa50390640c3fd3414c2532bd777a9477c23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package generic-proto-plus-stubs/0.3/generic-proto-plus-stubs-0.3.tar.gz

---
+++ packages/generic-proto-plus-stubs/generic-proto-plus-stubs-0.3.tar.gz/generic-proto-plus-stubs-0.3/proto-stubs/__init__.pyi
@@ -0,0 +1,7 @@
+from typing import Any
+
+from .enums import Enum as Enum
+from .fields import Field as Field, MapField as MapField, RepeatedField as RepeatedField
+from .marshal import Marshal as Marshal
+from .message import Message as Message
+from .modules import define_module as module
---
+++ packages/generic-proto-plus-stubs/generic-proto-plus-stubs-0.3.tar.gz/generic-proto-plus-stubs-0.3/proto-stubs/_package_info.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from typing import Tuple
+from proto.marshal import Marshal
+
+
+def compile(name, attrs) -> Tuple[str, Marshal]: ...
