6717ae61e7632c3dec7189a72dbced9b70f99613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:35 +0000

    Add package veil-api-client/2.2.18/veil-api-client-2.2.18.tar.gz

---
+++ packages/veil-api-client/veil-api-client-2.2.18.tar.gz/veil-api-client-2.2.18/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Python package config."""
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='veil-api-client',
+    version='2.2.18',
+    author='Aleksey Devyatkin, Emile Gareev',
+    author_email='a.devyatkin@mashtab.org, e.gareev@mashtab.org',
+    description='VeiL ECP Api client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jsc-masshtab/veil-api-client',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+    install_requires=['aiohttp==3.6.*', 'ujson==3.0.*']
+)
---
+++ packages/veil-api-client/veil-api-client-2.2.18.tar.gz/veil-api-client-2.2.18/veil_api_client/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Veil api client.
+
+for additional info see README.md
+"""
+
+__version__ = '2.2.6'
+
+from .api_objects import (DomainBackupConfiguration, DomainCloneConfiguration,
+                          DomainConfiguration,
+                          DomainRemoteConnectionConfiguration, DomainTcpUsb,
+                          DomainUpdateConfiguration, VeilDomainExt, VeilGuestAgentCmd)
+from .base import (TagConfiguration, VeilApiObjectStatus, VeilCacheAbstractClient,
+                   VeilCacheConfiguration, VeilRestPaginator, VeilTag)
+from .base.utils import VeilEntityConfiguration
+from .https_client import VeilClient, VeilClientSingleton, VeilRetryConfiguration
+
+__all__ = (
+    'VeilClient', 'VeilRestPaginator', 'DomainConfiguration', 'DomainCloneConfiguration',
+    'VeilClientSingleton',
+    'VeilCacheConfiguration', 'TagConfiguration', 'VeilEntityConfiguration',
+    'VeilGuestAgentCmd', 'DomainTcpUsb', 'VeilRetryConfiguration', 'VeilDomainExt',
+    'DomainBackupConfiguration', 'VeilTag', 'VeilCacheAbstractClient',
+    'DomainUpdateConfiguration', 'VeilApiObjectStatus', 'DomainRemoteConnectionConfiguration'
+)
+
+__author__ = 'Aleksei Deviatkin <a.devyatkin@mashtab.org>, Emile Gareev <e.gareev@mashtab.org>'
