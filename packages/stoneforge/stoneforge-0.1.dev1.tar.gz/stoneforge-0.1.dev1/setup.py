353ebe98af4f85b321e5d27bb588646950f1c2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package stoneforge/0.1.dev1/stoneforge-0.1.dev1.tar.gz

---
+++ packages/stoneforge/stoneforge-0.1.dev1.tar.gz/stoneforge-0.1.dev1/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python ::3"
+]
+
+setup(
+    name="stoneforge",
+    version="0.1.dev1",
+    author="GIECAR - UFF",
+    url="https://github.com/giecaruff/stoneforge",
+    description="Geophysics equations, algorithms and methods",
+    long_description=open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7.13",
+    install_requires=[
+        "numpy>=1.21.6",
+        "pytest>=3.6.4",
+    ],
+)
