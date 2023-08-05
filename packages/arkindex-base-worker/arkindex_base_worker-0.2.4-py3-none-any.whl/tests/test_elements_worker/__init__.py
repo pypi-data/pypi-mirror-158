d7df681a8f4a8e0b38b2bb914403f4c746fffc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:31 +0000

    Add package arkindex-base-worker/0.2.4/arkindex_base_worker-0.2.4-py3-none-any.whl

---
+++ packages/arkindex-base-worker/arkindex_base_worker-0.2.4-py3-none-any.whl/tests/test_elements_worker/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# API calls during worker configuration
+BASE_API_CALLS = [
+    ("GET", "http://testserver/api/v1/user/"),
+    ("GET", "http://testserver/api/v1/imports/aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeffff/"),
+    (
+        "GET",
+        "http://testserver/api/v1/workers/versions/12341234-1234-1234-1234-123412341234/",
+    ),
+]
