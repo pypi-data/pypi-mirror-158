bc32f4adaa24c7384c27c8bf2e312587350797fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package ronswanson/0.0.1/ronswanson-0.0.1.tar.gz

---
+++ packages/ronswanson/ronswanson-0.0.1.tar.gz/ronswanson-0.0.1/ronswanson/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ronswanson."""
+
+__author__ = """J. Michael Burgess"""
+__email__ = 'jburgess@mpe.mpg.de'
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
