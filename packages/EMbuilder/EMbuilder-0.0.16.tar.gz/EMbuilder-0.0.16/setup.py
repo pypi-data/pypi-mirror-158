a7a7c7ee1c444f5bebbdcdef5e2a82ebd1ab9504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:34 +0000

    Add package EMbuilder/0.0.16/EMbuilder-0.0.16.tar.gz

---
+++ packages/EMbuilder/EMbuilder-0.0.16.tar.gz/EMbuilder-0.0.16/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="EMbuilder",
+    version="0.0.16",
+    packages=["embuilder"],
+    author="Pablo Alarcón Moreno",
+    author_email="pabloalarconmoreno@gmail.com",
+    url="https://github.com/pabloalarconm/EMbuilder",
+    description="Etemenanki Builder -- Semantic implementation builder",
+    license="MIT",
+    keywords=["YARRRML","OBDA","RDF","ShEx","FAIR","EJP"]
+    #long_description=readme
+)
