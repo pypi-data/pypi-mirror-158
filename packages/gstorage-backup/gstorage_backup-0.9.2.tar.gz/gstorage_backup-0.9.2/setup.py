b5a94bf68b09eef59e5d5a1549feac475e434f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:45 +0000

    Add package gstorage-backup/0.9.2/gstorage_backup-0.9.2.tar.gz

---
+++ packages/gstorage-backup/gstorage_backup-0.9.2.tar.gz/gstorage_backup-0.9.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gstorage_backup',
+    version='0.9.2',
+    license='MIT',
+    author="Rodrigo F. Sacht",
+    author_email='rodrigofsacht@gmail.com',
+    packages=find_packages('.'),
+    package_dir={'': 'src'},
+    url='https://bitbucket.org/offertech/gstorage_backup',
+    keywords='gcp storage backup',
+    install_requires=[
+          'wheel',
+          'google-cloud-storage',
+          'fire',
+          'python-dotenv',
+          'python-slugify'
+      ],
+    entry_points={
+    'console_scripts': [
+        'gstorage_backup = gstorage_backup:main',
+    ],
+},
+
+)
