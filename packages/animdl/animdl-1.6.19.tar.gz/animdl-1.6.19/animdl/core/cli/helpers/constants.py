755ffb178690d6c53224097d901bcd7786ab8bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:47 +0000

    Add package animdl/1.6.19/animdl-1.6.19.tar.gz

---
+++ packages/animdl/animdl-1.6.19.tar.gz/animdl-1.6.19/animdl/core/cli/helpers/constants.py
@@ -0,0 +1,8 @@
+LABELS = {
+    "storage.googleapis.com": "Google API Storage",
+    "v.vrv.co": "VRV",
+    "pl.crunchyroll.com": "Crunchyroll",
+}
+
+SOURCE_REPOSITORY = "justfoolingaround", "animdl"
+VERSION_FILE_PATH = "master", "animdl/core/__version__.py"
