4be0e530a131b1d12877d6c465fa0bfb90a0b3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:05 +0000

    Add package rucio/1.29.0rc2/rucio-1.29.0rc2.tar.gz

---
+++ packages/rucio/rucio-1.29.0rc2.tar.gz/rucio-1.29.0rc2/lib/rucio/core/monitor.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Graphite counters
+"""
+
+import atexit
+import logging
+import os
+import string
+import time
+from abc import abstractmethod
+from datetime import datetime, timedelta
+from pathlib import Path
+from retrying import retry
+from threading import Lock
+
+from prometheus_client import start_http_server, Counter, Gauge, Histogram, REGISTRY, CollectorRegistry, generate_latest, values, multiprocess
+from statsd import StatsClient
+
+from rucio.common.config import config_get, config_get_bool, config_get_int
+
+PROMETHEUS_MULTIPROC_DIR = os.environ.get('PROMETHEUS_MULTIPROC_DIR', os.environ.get('prometheus_multiproc_dir', None))
+
+
+def cleanup_prometheus_files_at_exit():
+    if PROMETHEUS_MULTIPROC_DIR:
+        multiprocess.mark_process_dead(os.getpid())
+
+
+class MultiprocessMutexValue(values.MultiProcessValue()):
+    """
+    MultiprocessValue protected by mutex
+
+    Rucio usually is deployed using the apache MPM module, which means that it both uses multiple
+    subprocesses, and multiple threads per subprocess.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._lock = Lock()
+
+    def inc(self, amount):
+        with self._lock:
+            return super().inc(amount)
+
+    def set(self, value):
+        with self._lock:
+            return super().set(value)
+
+    def get(self):
+        with self._lock:
+            return super().get()
+
+
+if PROMETHEUS_MULTIPROC_DIR:
+    os.makedirs(PROMETHEUS_MULTIPROC_DIR, exist_ok=True)
+    values.ValueClass = MultiprocessMutexValue
+
+    atexit.register(cleanup_prometheus_files_at_exit)
+
+
+SERVER = config_get('monitor', 'carbon_server', raise_exception=False, default='localhost')
+PORT = config_get('monitor', 'carbon_port', raise_exception=False, default=8125)
+SCOPE = config_get('monitor', 'user_scope', raise_exception=False, default='rucio')
+CLIENT = StatsClient(host=SERVER, port=PORT, prefix=SCOPE)
+
+ENABLE_METRICS = config_get_bool('monitor', 'enable_metrics', raise_exception=False, default=False)
+if ENABLE_METRICS:
+    METRICS_PORT = config_get_int('monitor', 'metrics_port', raise_exception=False, default=8080)
+    start_http_server(METRICS_PORT, registry=REGISTRY)
+
+COUNTERS = {}
+GAUGES = {}
+TIMINGS = {}
+
+
+def _cleanup_old_prometheus_files(path, file_pattern, cleanup_delay, logger):
+    """cleanup behind processes which didn't finish gracefully."""
+
+    oldest_accepted_mtime = datetime.now() - timedelta(seconds=cleanup_delay)
+    for file in Path(path).glob(file_pattern):
+        if not file.is_file():
+            continue
+
+        file_mtime = datetime.fromtimestamp(file.stat().st_mtime)
+
+        if file_mtime < oldest_accepted_mtime:
+            logger(logging.INFO, 'Cleaning up prometheus db file %s', file)
+            try:
+                os.remove(file)
+            except FileNotFoundError:
+                # Probably file already removed by another concurrent process
+                pass
+
+
+def cleanup_old_prometheus_files(logger=logging.log):
+    path = PROMETHEUS_MULTIPROC_DIR
+    if path:
+        _cleanup_old_prometheus_files(path, file_pattern='gauge_live*.db', cleanup_delay=timedelta(hours=1).total_seconds(), logger=logger)
+        _cleanup_old_prometheus_files(path, file_pattern='*.db', cleanup_delay=timedelta(days=7).total_seconds(), logger=logger)
+
+
+@retry(retry_on_exception=lambda _: True,
+       wait_fixed=500,
+       stop_max_attempt_number=2)
+def generate_prometheus_metrics():
+    cleanup_old_prometheus_files()
+
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+    return generate_latest(registry)
+
+
+class MultiMetric:
+    """
+    Thin wrapper class allowing to record both prometheus and statsd metrics.
+
+    Inspired by the prometheus metric behavior: uses labels to parametrize metrics.
+    In case of statsd, metrics are formatted using str.format(**labels). The prometheus
+    ones using metric.labels(**labels) calls.
+
+    If the prometheus metric string is not provided, it is derived from the statsd one.
+    """
+
+    def __init__(self, statsd, prom=None, documentation=None, labelnames=(), registry=None):
+        """
+        :param statsd: a string, eventually with keyword placeholders for the str.format(**labels) call
+        :param prom: a string or a prometheus metric object
+        """
+        self._registry = registry or REGISTRY
+        self._documentation = documentation or ''
+        self._statsd = statsd
+        if not prom:
+            # automatically generate a prometheus metric name
+            #
+            # remove '.{label}' from the string for each `label`
+            stats_without_labels = ''.join(tup[0].rstrip('.') for tup in string.Formatter().parse(self._statsd))
+            prom = 'rucio_{}'.format(stats_without_labels).replace('.', '_')
+        if isinstance(prom, str):
+            self._prom = self.init_prometheus_metric(prom, self._documentation, labelnames=labelnames)
+        else:
+            self._prom = prom
+        self._labelnames = labelnames
+
+    @abstractmethod
+    def init_prometheus_metric(self, name, documentation, labelnames=()):
+        pass
+
+    def labels(self, **labelkwargs):
+        return self.__class__(
+            prom=self._prom.labels(**labelkwargs),
+            statsd=self._statsd.format(**labelkwargs),
+            documentation=self._documentation,
+            labelnames=self._labelnames,
+            registry=self._registry,
+        )
+
+
+class MultiCounter(MultiMetric):
+
+    def inc(self, delta=1):
+        self._prom.inc(delta)
+        CLIENT.incr(self._statsd, delta)
+
+    def init_prometheus_metric(self, name, documentation, labelnames=()):
+        return Counter(name, documentation, labelnames=labelnames, registry=self._registry)
+
+
+class MultiGauge(MultiMetric):
+
+    def set(self, value):
+        self._prom.set(value)
+        CLIENT.gauge(self._statsd, value)
+
+    def init_prometheus_metric(self, name, documentation, labelnames=()):
+        return Gauge(name, documentation, labelnames=labelnames, registry=self._registry)
+
+
+class MultiTiming(MultiMetric):
+
+    def observe(self, value):
+        self._prom.observe(value)
+        CLIENT.timing(self._statsd, value)
+
+    def init_prometheus_metric(self, name, documentation, labelnames=()):
+        return Histogram(name, documentation, labelnames=labelnames, registry=self._registry)
+
+
+def record_counter(name, delta=1, labels=None):
+    """
+    Log one or more counters by arbitrary amounts
+
+    :param name: The counter to be updated.
+    :param delta: The increment for the counter, by default increment by 1.
+    :param labels: labels used to parametrize the metric
+    """
+
+    counter = COUNTERS.get(name)
+    if not counter:
+        COUNTERS[name] = counter = MultiCounter(statsd=name, labelnames=labels.keys() if labels else ())
+
+    delta = abs(delta)
+
+    if labels:
+        counter.labels(**labels).inc(delta)
+    else:
+        counter.inc(delta)
+
+
+def record_gauge(name, value, labels=None):
+    """
+     Log gauge information for a single stat
+
+    :param name: The name of the stat to be updated.
+    :param value: The value to log.
+    :param labels: labels used to parametrize the metric
+    """
+    gauge = GAUGES.get(name)
+    if not gauge:
+        GAUGES[name] = gauge = MultiGauge(statsd=name, labelnames=labels.keys() if labels else ())
+
+    if labels:
+        gauge.labels(**labels).set(value)
+    else:
+        gauge.set(value)
+
+
+def record_timer(name, time, labels=None):
+    """
+     Log timing information for a single stat (in miliseconds)
+
+    :param name: The name of the stat to be updated.
+    :param time: The time to log.
+    :param labels: labels used to parametrize the metric
+    """
+    timing = TIMINGS.get(name)
+    if not timing:
+        TIMINGS[name] = timing = MultiTiming(statsd=name, labelnames=labels.keys() if labels else ())
+
+    if labels:
+        timing.labels(**labels).observe(time)
+    else:
+        timing.observe(time)
+
+
+class record_timer_block(object):
+    """
+    A context manager for timing a block of code.
+
+    :param stats: The name of the stat or list of stats that should be updated.
+        Each stat can be a simple string or a tuple (string, divisor)
+
+    Usage:
+        with monitor.record_timer_block('test.context_timer'):
+            stuff1()
+            stuff2()
+
+       with monitor.record_timer_block(['test.context_timer', ('test.context_timer_normalised', 10)]):
+            stuff1()
+            stuff2()
+    """
+
+    def __init__(self, stats, labels=None):
+        if not isinstance(stats, list):
+            stats = [stats]
+        self.stats = stats
+        self.labels = labels
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, typ, value, tb):
+        dt = time.time() - self.start
+        ms = int(round(1000 * dt))  # Convert to ms.
+        for s in self.stats:
+            if isinstance(s, str):
+                record_timer(s, ms, labels=self.labels)
+            elif isinstance(s, tuple):
+                if s[1] != 0:
+                    ms = ms / s[1]
+                    record_timer(s[0], ms, labels=self.labels)
---
+++ packages/rucio/rucio-1.29.0rc2.tar.gz/rucio-1.29.0rc2/lib/rucio/daemons/reaper/dark_reaper.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+# Copyright European Organization for Nuclear Research (CERN) since 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Dark Reaper is a daemon to manage quarantined file deletion.
+'''
+
+import hashlib
+import logging
+import os
+import random
+import socket
+import sys
+import threading
+import time
+import traceback
+
+import rucio.db.sqla.util
+from rucio.common import exception
+from rucio.common.config import config_get_bool
+from rucio.common.exception import (SourceNotFound, DatabaseException, ServiceUnavailable,
+                                    RSEAccessDenied, ResourceTemporaryUnavailable,
+                                    RSENotFound, VONotFound)
+from rucio.common.logging import setup_logging, formatted_logger
+from rucio.common.utils import daemon_sleep
+from rucio.core.heartbeat import live, die, sanity_check
+from rucio.core.message import add_message
+from rucio.core.quarantined_replica import (list_quarantined_replicas,
+                                            delete_quarantined_replicas,
+                                            list_rses_with_quarantined_replicas)
+import rucio.core.rse as rse_core
+from rucio.core.rse_expression_parser import parse_expression
+from rucio.core.vo import list_vos
+from rucio.rse import rsemanager as rsemgr
+
+logging.getLogger("requests").setLevel(logging.CRITICAL)
+
+GRACEFUL_STOP = threading.Event()
+
+
+def reaper(rses, chunk_size=100, once=False, scheme=None, sleep_time=300):
+    """
+    Main loop to select and delete files.
+
+    :param rses: List of RSEs the reaper should work against.
+    :param chunk_size: the size of chunk for deletion.
+    :param once: If True, only runs one iteration of the main loop.
+    :param scheme: Force the reaper to use a particular protocol, e.g., mock.
+    :param sleep_time: Thread sleep time after each chunk of work.
+    """
+
+    pid = os.getpid()
+    thread = threading.current_thread()
+    hostname = socket.gethostname()
+    executable = ' '.join(sys.argv)
+    hash_executable = hashlib.sha256((sys.argv[0] + ''.join(rses)).encode()).hexdigest()
+    sanity_check(executable=None, hostname=hostname)
+
+    # heartbeat
+    heartbeat = live(executable=executable, hostname=hostname, pid=pid, thread=thread,
+                     hash_executable=hash_executable)
+    prepend_str = 'dark-reaper [%i/%i] : ' % (heartbeat['assign_thread'], heartbeat['nr_threads'])
+    logger = formatted_logger(logging.log, prepend_str + '%s')
+    logger(logging.INFO, 'Starting Dark Reaper on RSEs: %s', ', '.join(rses))
+
+    if not once:
+        logger(logging.INFO, 'Waiting for heartbeat synchonization')
+        GRACEFUL_STOP.wait(10)  # To prevent running on the same partition if all the reapers restart at the same time
+
+    while not GRACEFUL_STOP.is_set():
+        try:
+            heartbeat = live(executable=executable, hostname=hostname, pid=pid, thread=thread,
+                             hash_executable=hash_executable)
+            prepend_str = 'dark-reaper [%i/%i] : ' % (heartbeat['assign_thread'], heartbeat['nr_threads'])
+            logger = formatted_logger(logging.log, prepend_str + '%s')
+            logger(logging.INFO, 'Live gives {0[heartbeat]}'.format(locals()))
+            nothing_to_do = True
+            start_time = time.time()
+
+            rses_to_process = list(set(rses) & set(list_rses_with_quarantined_replicas()))
+            random.shuffle(rses_to_process)
+            for rse_id in rses_to_process:
+                # The following query returns the list of real replicas (deleted_replicas) and list of dark replicas (dark_replicas)
+                # Real replicas can be directly removed from the quarantine table
+                deleted_replicas, dark_replicas = list_quarantined_replicas(rse_id=rse_id,
+                                                                            limit=chunk_size,
+                                                                            worker_number=heartbeat['assign_thread'],
+                                                                            total_workers=heartbeat['nr_threads'])
+
+                rse_info = rsemgr.get_rse_info(rse_id=rse_id)
+                rse = rse_info['rse']
+                prot = rsemgr.create_protocol(rse_info, 'delete', scheme=scheme)
+
+                heartbeat = live(executable=executable, hostname=hostname, pid=pid, thread=thread,
+                                 hash_executable=hash_executable)
+                prepend_str = 'dark-reaper [%i/%i] : ' % (heartbeat['assign_thread'], heartbeat['nr_threads'])
+                logger = formatted_logger(logging.log, prepend_str + '%s')
+                try:
+                    prot.connect()
+                    for replica in dark_replicas:
+                        nothing_to_do = False
+                        scope = ''
+                        if replica['scope']:
+                            scope = replica['scope'].external
+                        try:
+                            pfn = str(list(rsemgr.lfns2pfns(rse_settings=rse_info,
+                                                            lfns=[{'scope': scope,
+                                                                   'name': replica['name'],
+                                                                   'path': replica['path']}],
+                                                            operation='delete',
+                                                            scheme=scheme).values())[0])
+                            logger(logging.INFO, 'Deletion ATTEMPT of %s:%s as %s on %s', scope, replica['name'], pfn, rse)
+                            start = time.time()
+                            prot.delete(pfn)
+                            duration = time.time() - start
+                            logger(logging.INFO, 'Deletion SUCCESS of %s:%s as %s on %s in %s seconds', scope, replica['name'], pfn, rse, duration)
+                            payload = {'scope': scope,
+                                       'name': replica['name'],
+                                       'rse': rse,
+                                       'rse_id': rse_id,
+                                       'file-size': replica.get('bytes') or 0,
+                                       'bytes': replica.get('bytes') or 0,
+                                       'url': pfn,
+                                       'duration': duration,
+                                       'protocol': prot.attributes['scheme']}
+                            if replica['scope'].vo != 'def':
+                                payload['vo'] = replica['scope'].vo
+                            add_message('deletion-done', payload)
+                            deleted_replicas.append(replica)
+                        except SourceNotFound:
+                            err_msg = ('Deletion NOTFOUND of %s:%s as %s on %s'
+                                       % (scope, replica['name'], pfn, rse))
+                            logger(logging.WARNING, err_msg)
+                            deleted_replicas.append(replica)
+                        except (ServiceUnavailable, RSEAccessDenied, ResourceTemporaryUnavailable) as error:
+                            err_msg = ('Deletion NOACCESS of %s:%s as %s on %s: %s'
+                                       % (scope, replica['name'], pfn, rse, str(error)))
+                            logger(logging.WARNING, err_msg)
+                            payload = {'scope': scope,
+                                       'name': replica['name'],
+                                       'rse': rse,
+                                       'rse_id': rse_id,
+                                       'file-size': replica['bytes'] or 0,
+                                       'bytes': replica['bytes'] or 0,
+                                       'url': pfn,
+                                       'reason': str(error),
+                                       'protocol': prot.attributes['scheme']}
+                            if replica['scope'].vo != 'def':
+                                payload['vo'] = replica['scope'].vo
+                            add_message('deletion-failed', payload)
+
+                        except Exception:
+                            logging.critical(traceback.format_exc())
+                finally:
+                    prot.close()
+
+                delete_quarantined_replicas(rse_id=rse_id, replicas=deleted_replicas)
+
+                if once:
+                    break
+
+            if once:
+                break
+
+            if nothing_to_do:
+                logger(logging.INFO, 'Nothing to do')
+                daemon_sleep(start_time=start_time, sleep_time=sleep_time, graceful_stop=GRACEFUL_STOP)
+
+        except DatabaseException as error:
+            logging.warning('Reaper:  %s', str(error))
+        except Exception:
+            logging.critical(traceback.format_exc())
+
+    die(executable=executable, hostname=hostname, pid=pid, thread=thread, hash_executable=hash_executable)
+    logging.info('Graceful stop requested')
+    logging.info('Graceful stop done')
+    return
+
+
+def stop(signum=None, frame=None):
+    """
+    Graceful exit.
+    """
+    GRACEFUL_STOP.set()
+
+
+def run(total_workers=1, chunk_size=100, once=False, rses=[], scheme=None,
+        exclude_rses=None, include_rses=None, vos=None, delay_seconds=0, sleep_time=300):
+    """
+    Starts up the reaper threads.
+
+    :param total_workers: The total number of workers.
+    :param chunk_size: the size of chunk for deletion.
+    :param once: If True, only runs one iteration of the main loop.
+    :param rses: List of RSEs the reaper should work against. If empty, it considers all RSEs (Single-VO only).
+    :param scheme: Force the reaper to use a particular protocol/scheme, e.g., mock.
+    :param exclude_rses: RSE expression to exclude RSEs from the Reaper.
+    :param include_rses: RSE expression to include RSEs.
+    :param vos: VOs on which to look for RSEs. Only used in multi-VO mode.
+                If None, we either use all VOs if run from "def", or the current VO otherwise.
+    """
+    setup_logging()
+
+    if rucio.db.sqla.util.is_old_db():
+        raise exception.DatabaseException('Database was not updated, daemon won\'t start')
+
+    logging.info('main: starting processes')
+
+    multi_vo = config_get_bool('common', 'multi_vo', raise_exception=False, default=False)
+    if not multi_vo:
+        if vos:
+            logging.warning('Ignoring argument vos, this is only applicable in a multi-VO setup.')
+        vos = ['def']
+    else:
+        if vos:
+            invalid = set(vos) - set([v['vo'] for v in list_vos()])
+            if invalid:
+                msg = 'VO{} {} cannot be found'.format('s' if len(invalid) > 1 else '',
+                                                       ', '.join([repr(v) for v in invalid]))
+                raise VONotFound(msg)
+        else:
+            vos = [v['vo'] for v in list_vos()]
+        logging.info('Dark Reaper: This instance will work on VO%s: %s'
+                     % ('s' if len(vos) > 1 else '', ', '.join([v for v in vos])))
+
+    all_rses = []
+    for vo in vos:
+        all_rses.extend([rse['id'] for rse in rse_core.list_rses(filters={'vo': vo})])
+
+    if rses:
+        invalid = set(rses) - set([rse['rse'] for rse in all_rses])
+        if invalid:
+            msg = 'RSE{} {} cannot be found'.format('s' if len(invalid) > 1 else '',
+                                                    ', '.join([repr(rse) for rse in invalid]))
+            raise RSENotFound(msg)
+        rses = [rse for rse in all_rses if rse['rse'] in rses]
+    else:
+        rses = all_rses
+
+    if exclude_rses:
+        excluded_rses = [rse['id'] for rse in parse_expression(exclude_rses)]
+        rses = [rse for rse in rses if rse not in excluded_rses]
+
+    if include_rses:
+        included_rses = [rse['id'] for rse in parse_expression(include_rses)]
+        rses = [rse for rse in rses if rse in included_rses]
+
+    if not rses:
+        logging.error('Dark Reaper: No RSEs found. Exiting.')
+        return
+
+    threads = []
+    for worker in range(total_workers):
+        kwargs = {'rses': rses,
+                  'once': once,
+                  'chunk_size': chunk_size,
+                  'scheme': scheme,
+                  'sleep_time': sleep_time}
+        threads.append(threading.Thread(target=reaper, kwargs=kwargs,
+                                        name='Worker: %s, Total_Workers: %s' % (worker, total_workers)))
+    [t.start() for t in threads]
+    while threads[0].is_alive():
+        [t.join(timeout=3.14) for t in threads]
