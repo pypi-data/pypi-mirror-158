eda3caf0f4f42aee085fc2117e80611edc45c42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:54 +0000

    Add package astrape/0.7.10/astrape-0.7.10.tar.gz

---
+++ packages/astrape/astrape-0.7.10.tar.gz/astrape-0.7.10/astrape/models/__init__.py
@@ -0,0 +1,16 @@
+from astrape.models.models_lightning import MLP, ContractingMLP, CustomMLP, VGG, UNet
+from astrape.models.model_buildingblocks import DoubleConv, Down, Up
+from astrape.utilities.utils_lightning import set_default_parameters
+
+#from astrape.constants import DEFAULT_BATCH_SIZE, DEFAULT_LR, DEFAULT_OPTIMIZER_TYPE
+__all__ = [
+    'MLP', 
+    'ContractingMLP', 
+    'CustomMLP', 
+    'VGG', 
+    'UNet', 
+    'DoubleConv', 
+    'Down', 
+    'Up', 
+    'set_default_parameters'
+    ]
