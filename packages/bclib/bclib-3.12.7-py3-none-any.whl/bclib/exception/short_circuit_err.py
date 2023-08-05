b27d4986e22b410f2d3cbc966829d90706818821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:16 +0000

    Add package bclib/3.12.7/bclib-3.12.7-py3-none-any.whl

---
+++ packages/bclib/bclib-3.12.7-py3-none-any.whl/bclib/exception/short_circuit_err.py
@@ -0,0 +1,5 @@
+class ShortCircuitErr(Exception):
+    def __init__(self, status_code: str, error_code: str = -1, message: str = None):
+        super().__init__(message if message else '')
+        self.status_code = status_code
+        self.error_code = error_code
