ff7c6788c771731b258c51ac03fc02e9edded0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package nelltest/0.2.1/nelltest-0.2.1.tar.gz

---
+++ packages/nelltest/nelltest-0.2.1.tar.gz/nelltest-0.2.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nelltest',
+      version='0.2.1',
+      description='Updated',
+      packages=['nelltest'],
+      author_email='elezor.2009@gmail.com',
+      zip_safe=False)
