0a369decad95fd2c776500f5ea942386fa32736e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package aws-prototyping-sdk.static-website/0.3.2/aws_prototyping_sdk.static_website-0.3.2-py3-none-any.whl

---
+++ packages/aws-prototyping-sdk.static-website/aws_prototyping_sdk.static_website-0.3.2-py3-none-any.whl/aws_prototyping_sdk/static_website/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/static-website",
+    "0.3.2",
+    __name__[0:-6],
+    "static-website@0.3.2.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
