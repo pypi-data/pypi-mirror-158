30157913bc97099aa928df35e872d71401960381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:41 +0000

    Add package pybboxes/0.1.3/pybboxes-0.1.3.tar.gz

---
+++ packages/pybboxes/pybboxes-0.1.3.tar.gz/pybboxes-0.1.3/pybboxes/__init__.py
@@ -0,0 +1,11 @@
+from pybboxes.functional import convert_bbox  # Backwards compatibility
+from pybboxes.types import (
+    AlbumentationsBoundingBox,
+    BoundingBox,
+    CocoBoundingBox,
+    FiftyoneBoundingBox,
+    VocBoundingBox,
+    YoloBoundingBox,
+)
+
+__version__ = "0.1.3"
