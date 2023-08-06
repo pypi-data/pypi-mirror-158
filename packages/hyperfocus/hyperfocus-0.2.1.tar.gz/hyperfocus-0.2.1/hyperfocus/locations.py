a4eecb64532612f457607c821badceba9c0f46f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:02 +0000

    Add package hyperfocus/0.2.1/hyperfocus-0.2.1.tar.gz

---
+++ packages/hyperfocus/hyperfocus-0.2.1.tar.gz/hyperfocus-0.2.1/hyperfocus/locations.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import click
+
+from hyperfocus import __app_name__
+
+
+CONFIG_DIR = Path(click.get_app_dir(__app_name__))
+CONFIG_PATH = CONFIG_DIR / "config.ini"
+
+DEFAULT_DB_PATH = Path.home() / f".{__app_name__}.sqlite"
