a4a423df0b06da0cf0a225d303e14dafd6858a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:07 +0000

    Add package ipmlab/0.1.0rc2/ipmlab-0.1.0rc2.tar.gz

---
+++ packages/ipmlab/ipmlab-0.1.0rc2.tar.gz/ipmlab-0.1.0rc2/ipmlab/config.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+"""Shared configuration constants"""
+
+version = ""
+driveLetter = ""
+cdDeviceName = ""
+reportFormatString = ""
+isoBusterExe = ""
+tempDir = ""
+rootDir = ""
+batchFolder = ""
+batchManifest = ""
+secondsToTimeout = ""
+prefixBatch = ""
+socketHost = "127.0.0.1"
+socketPort = "65432"
+enablePPNLookup = True
+enableSocketAPI = False
+quitFlag = False
+batchIsOpen = False
+readyToStart = False
+finishedMedium = False
+processingMedium = False
+finishedBatch = False
