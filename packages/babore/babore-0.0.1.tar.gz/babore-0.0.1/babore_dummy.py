50500626aa85d32077310b23a855decd882b798a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:40 +0000

    Add package babore/0.0.1/babore-0.0.1.tar.gz

---
+++ packages/babore/babore-0.0.1.tar.gz/babore-0.0.1/babore_dummy.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='babore',
+    version=version,
+    license='BSD-3-Clause license',
+    author='Ivan Ms',
+    author_email='ivanms.ept@gmail.com',
+    description='BombSquad framework for modding servers',
+    url='https://github.com/Ms-company-BombSquad/babore',
+    download_url=f'https://github.com/Ms-company-BombSquad/babore/archive/v{version}.zip',
+    packages=['babore'],
+)
