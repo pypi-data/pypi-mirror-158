6e3ac788263b6024568f3f0b5bed72bf4d0acea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:33 +0000

    Add package pypws/2.4.6/pypws-2.4.6-py3-none-any.whl

---
+++ packages/pypws/pypws-2.4.6-py3-none-any.whl/pypws/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def main():
+    print('Welcome to Python Phast Web Services')
+
+def method2():
+    print('This is method 2')
+
+if __name__ == "__main__":
+    main()
