c6f0feee29c96f22bb198e19f8f2394e9b8992f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:03 +0000

    Add package pysecstring/0.1.2/pysecstring-0.1.2.tar.gz

---
+++ packages/pysecstring/pysecstring-0.1.2.tar.gz/pysecstring-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pysecstring']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pysecstring',
+    'version': '0.1.2',
+    'description': "Python implementation of Window's Securestring forked from aznashwan/py-securestring",
+    'long_description': None,
+    'author': 'Gwang-Jin Kim',
+    'author_email': 'gwang.jin.kim.phd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
