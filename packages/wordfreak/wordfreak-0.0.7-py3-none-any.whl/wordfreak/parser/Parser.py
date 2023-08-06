5dbe2a8a3dd4b71a331f4dbdbd60f2898c6414a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:38 +0000

    Add package wordfreak/0.0.7/wordfreak-0.0.7-py3-none-any.whl

---
+++ packages/wordfreak/wordfreak-0.0.7-py3-none-any.whl/wordfreak/parser/Parser.py
@@ -0,0 +1,26 @@
+import string
+from abc import abstractmethod, ABC
+
+
+class Parser(ABC):
+
+    @classmethod
+    @abstractmethod
+    def getWordFrequency(cls, pathToFile: str) -> dict[str, int]:
+        ...
+
+    @classmethod
+    def _getWordFrequencyFromLines(cls, lines: list[str]) -> dict[str, int]:
+        wordFrequency: dict[str, int] = dict()
+        for line in lines:
+            line = line.strip()
+            words = line.split()
+            for word in words:
+                word = word.translate(str.maketrans('', '', string.punctuation)).lower()
+                # do not add empty strings
+                if word != "":
+                    if word in wordFrequency:
+                        wordFrequency[word] += 1
+                    else:
+                        wordFrequency[word] = 1
+        return wordFrequency
