3062ba051fb79a5456b7ee9e342d2e0ca9e159da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:08 +0000

    Add package stringtools/0.0.5/stringtools-0.0.5.tar.gz

---
+++ packages/stringtools/stringtools-0.0.5.tar.gz/stringtools-0.0.5/stringtools/__init__.py
@@ -0,0 +1,6 @@
+'''String analaysing, converting, generating tools.'''
+# MIT License Copyright (c) 2022 Beksultan Artykbaev
+
+from .analysers import is_pangram, is_heterogram, is_anagram, count_char
+from .converters import bricks
+from .generators import generate_nick, Generate_password
