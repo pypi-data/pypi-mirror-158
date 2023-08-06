e9177e4e49fc4edffcedb4979af3c1d84791e386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:03 +0000

    Add package tfprotocol-client/1.0.3/tfprotocol_client-1.0.3.tar.gz

---
+++ packages/tfprotocol-client/tfprotocol_client-1.0.3.tar.gz/tfprotocol_client-1.0.3/tfprotocol_client/models/transfer_state.py
@@ -0,0 +1,13 @@
+# coded by lagcleaner
+# email: lagcleaner@gmail.com
+
+class TransferStatus:
+    """Transfer status"""
+
+    def __init__(self) -> None:
+        self.dummy = None
+        self.command = None
+        self.dummy_state = None
+
+    def __str__(self) -> str:
+        return f'TransferStatus<{self.dummy}, {self.command}, {self.dummy_state}>'
