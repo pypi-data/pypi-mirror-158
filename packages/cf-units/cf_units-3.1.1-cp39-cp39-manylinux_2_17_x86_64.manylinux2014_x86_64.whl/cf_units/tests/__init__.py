c457d81fe17bfa2715e6e5e35ece12eafae4db5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:45 +0000

    Add package cf-units/3.1.1/cf_units-3.1.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cf-units/cf_units-3.1.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cf_units/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright cf-units contributors
+#
+# This file is part of cf-units and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
