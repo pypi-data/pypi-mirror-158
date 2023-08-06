e3d607780cb68389152b548069e74b09f6b9f7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:30 +0000

    Add package migas/0.0.3/migas-0.0.3-py3-none-any.whl

---
+++ packages/migas/migas-0.0.3-py3-none-any.whl/migas/request.py
@@ -0,0 +1,67 @@
+"""Stripped down, minimal import way to communicate with server"""
+
+import json
+import typing
+from http.client import HTTPConnection, HTTPResponse, HTTPSConnection
+from urllib.parse import urlparse
+
+from . import __version__
+
+ETResponse = typing.Tuple[int, typing.Union[dict, str]]  # status code, body
+
+
+def request(url: str, query: str, *, timeout: int = 3, chunk_size: int = None) -> ETResponse:
+    purl = urlparse(url)
+    # TODO: 3.10 - Replace with match/case
+    if purl.scheme == 'https':
+        Connection = HTTPSConnection
+    elif purl.scheme == 'http':
+        Connection = HTTPConnection
+    else:
+        raise ValueError("URL scheme not supported")
+
+    body = json.dumps({"query": query}).encode("utf-8")
+    conn = Connection(purl.netloc, timeout=timeout)
+    headers = {
+        'User-Agent': f'migas-client/{__version__}',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': '*/*',
+        'Content-Length': len(body),
+        'Content-Type': 'application/json',
+    }
+
+    try:
+        conn.request("POST", purl.path, body, headers)
+        response = conn.getresponse()
+        body = _read_response(response, chunk_size)
+    except TimeoutError:
+        return (408, {"data": None, "errors": [{"message": "Connection to server timed out."}]})
+    except ConnectionError:
+        return (503, {"data": None, "errors": [{"message": "Server is not available."}]})
+    finally:
+        conn.close()
+
+    if not response.headers.get("X-Backend-Server"):
+        # TODO: Implement logging
+        print("migas server is incorrectly configured.")
+
+    if response.headers.get("Content-Type").startswith("application/json"):
+        body = json.loads(body)
+
+    return response.status, body
+
+
+def _read_response(response: HTTPResponse, chunk_size: int = None) -> str:
+    """
+    Read and aggregate the response body.
+
+    If `chunk_size` is `None`, the entire response is read at once.
+    """
+    stream = b''
+    # TODO: 3.8 - Replace with walrus
+    # while chunk := response.read(chunk_size):
+    chunk = 1
+    while chunk:
+        chunk = response.read(chunk_size)
+        stream += chunk
+    return stream.decode()
