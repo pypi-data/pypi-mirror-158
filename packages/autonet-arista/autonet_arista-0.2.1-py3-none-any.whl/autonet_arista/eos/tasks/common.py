d26b7e6541f24d6ac723819c497674bacbda431a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:03 +0000

    Add package autonet-arista/0.2.1/autonet_arista-0.2.1-py3-none-any.whl

---
+++ packages/autonet-arista/autonet_arista-0.2.1-py3-none-any.whl/autonet_arista/eos/tasks/common.py
@@ -0,0 +1,203 @@
+import re
+
+from autonet.core.objects import vxlan as an_vxlan
+from autonet.core.objects import vrf as an_vrf
+from typing import Tuple, Union
+
+
+def get_fq_if_name(if_name: str) -> str:
+    """
+    Returns the fully qualified interface name for the provided
+    shorthand name. For example, "Po22" would evaluate to
+    "Port-Channel22"
+    :param if_name:
+    :return:
+    """
+    match = re.search(r'(?P<if_type>[A-z-]*)(?P<if_id>[\d/]*)', if_name)
+    if_type = match.group('if_type')
+    if_id = match.group('if_id')
+    fq_names = ['Ethernet', 'Loopback', 'Management', 'Port-Channel',
+                'Tunnel', 'Vlan', 'Vxlan']
+    result = None
+    for fq_name in fq_names:
+        # we track if we've matched the entry before to see if the string
+        # passed in is too ambiguous.  EOS does the same thing.
+        matched = fq_name.lower().startswith(if_type.lower())
+        if matched and result:
+            raise ValueError("Provided shorthand name is ambiguous.")
+        if matched:
+            result = f'{fq_name}{if_id}'
+    if result:
+        return result
+    raise ValueError("Could not parse fully qualified interface name.")
+
+
+def get_if_parts(if_name: str) -> Tuple[str, str]:
+    """
+    Parses an interface name and returns a tuple containing the fully
+    qualified interface type and the identifier.
+
+    :param if_name:
+    :return:
+    """
+    fq_name = get_fq_if_name(if_name)
+    matches = re.search(r'(?P<if_name>\D*)(?P<if_id>[\d/]*)$', fq_name)
+    return matches.group('if_name'), matches.group('if_id')
+
+
+def parse_bgp_vpn_config(text_config: str) -> dict:
+    """
+    Parses the textual BGP configuration block into a structured
+    dictionary.
+
+    .. code-block::
+        {
+            'asn': 65500,
+            'rid': 198.18.0.1
+            'vrfs': {
+                'vrf-name': {
+                    'rid': 198.18.0.1
+                    'import_targets': {
+                        'evpn': [],
+                        'vpn-ipv4: [],
+                        'vpn-ipv6: [],
+                    },
+                    'export_targets': {
+                        'evpn': [],
+                        'vpn-ipv4: [],
+                        'vpn-ipv6: [],
+                    },
+                    'route-distinguisher': 198.18.0.1:2
+                }
+            },
+            'vlans': {
+                'vlan-id': {
+                    'import_targets': [],
+                    'export_targets': [],
+                    'route-distinguisher': 198.18.0.1:100
+                }
+            }
+        }
+
+
+    :param text_config: The text configuration block.
+    :return:
+    """
+    # TODO: (kvondersaar) This function is hilariously cumbersome and
+    #  needs to be split into more discrete parts.
+    asn_regex = r'router bgp (?P<asn>[0-9]*$)'
+    rid_regex = r'router-id (?P<rid>[0-9\.]*)$'
+    vlan_regex = r'vlan (?P<vlan_id>[0-9]*$)'
+    vrf_regex = r'vrf (?P<vrf_name>[\S]*$)'
+    rt_regex = r'route-target (?P<direction>[\S]*) ?(?P<afi>evpn|vpn-ipv4|vpn-ipv6|) (?P<rt>[\d\.]*\:[\d]*)$'
+    rd_regex = r'rd (?P<rd>[\d\.]*\:[\d]*)$'
+
+    config_lines = text_config.split('\n')
+    bgp_config = {}
+    node = {}
+    context = None
+    for config_line in config_lines:
+        if match := re.search(asn_regex, config_line):
+            bgp_config['asn'] = match.group('asn')
+        if match := re.search(vlan_regex, config_line):
+            node = bgp_config.setdefault('vlans', {}).setdefault(match.group('vlan_id'), {})
+            context = 'vlan'
+        if match := re.search(vrf_regex, config_line):
+            node = bgp_config.setdefault('vrfs', {}).setdefault(match.group('vrf_name'), {})
+            context = 'vrf'
+        # Once we have a node we can parse out the things that may belong to it.
+        if match := re.search(rid_regex, config_line):
+            if node:
+                node['rid'] = match.group('rid')
+            else:
+                bgp_config['rid'] = match.group('rid')
+        if match := re.search(rt_regex, config_line):
+            # when we match up on an RT we place it only in the AFI for which
+            # it is defined.  If no AFI exists in the config, then it's explicitly
+            # all AFIs.
+            if context == 'vrf':
+                all_afis = ['evpn', 'vpn-ipv4', 'vpn-ipv6']
+                # Initialize the node data structure if not already done.
+
+                if 'import_targets' not in node or not isinstance(node['import_targets'], dict):
+                    node['import_targets'] = {'evpn': [], 'vpn-ipv4': [], 'vpn-ipv6': []}
+                if 'export_targets' not in node or not isinstance(node['export_targets'], dict):
+                    node['export_targets'] = {'evpn': [], 'vpn-ipv4': [], 'vpn-ipv6': []}
+                if match.group('afi'):
+                    afis = [match.group('afi')]
+                else:
+                    afis = all_afis
+                for afi in afis:
+                    if match.group('direction') == 'both':
+                        node.setdefault('import_targets', {}).setdefault(afi, []).append(match.group('rt'))
+                        node.setdefault('export_targets', {}).setdefault(afi, []).append(match.group('rt'))
+                    else:
+                        targets = f"{match.group('direction')}_targets"
+                        node.setdefault(targets, {}).setdefault(afi, []).append(match.group('rt'))
+            elif context == 'vlan':
+                if match.group('direction') == 'both':
+                    node.setdefault('import_targets', []).append(match.group('rt'))
+                    node.setdefault('export_targets', []).append(match.group('rt'))
+                else:
+                    targets = f"{match.group('direction')}_targets"
+                    node.setdefault(targets, []).append(match.group('rt'))
+
+        if match := re.search(rd_regex, config_line):
+            node['rd'] = match.group('rd')
+
+    return bgp_config
+
+
+def generate_rt_commands(conf_obj: Union[an_vxlan.VXLAN, an_vrf.VRF],
+                         bgp_asn: Union[str, int] = None) -> ([str], [str]):
+    """
+    Generate the import and export commands for a list of RTs.  BGP ASN
+    is used for the generation of auto-derived RTs and is only needed if 'auto'
+    is present in the list of RTs.  Auto RT generation is for VXLAN only.
+    :param conf_obj: The configuration object containing RT information in its
+           attributes named `import_targets` and `export_targets`. May be VXLAN,
+           VRF, or possibly others.
+    :param bgp_asn: The BGP ASN to use for auto-derived RTs.
+    :return:
+    """
+    valid_conf_obj = (an_vxlan.VXLAN, an_vrf.VRF)
+    if not isinstance(conf_obj, valid_conf_obj):
+        raise Exception(f"`conf_obj` must be one of {valid_conf_obj}")
+    afis = []
+    if isinstance(conf_obj, an_vxlan.VXLAN):
+        afis.append('evpn')
+    if isinstance(conf_obj, an_vrf.VRF):
+        afis = []
+        if conf_obj.ipv4:
+            afis.append('ipv4')
+        if conf_obj.ipv6:
+            afis.append('ipv6')
+    import_targets = conf_obj.import_targets or []
+    export_targets = conf_obj.export_targets or []
+
+    # Do some input validation.
+    if 'auto' in import_targets or 'auto' in export_targets:
+        auto = True
+    else:
+        auto = False
+    if auto and not bgp_asn:
+        raise Exception('"bgp_asn" must be set when RT value is "auto".')
+    if auto and not isinstance(conf_obj, an_vxlan.VXLAN):
+        raise Exception(f'Auto RT is only supported with {an_vxlan.VXLAN} objects.')
+
+    auto_rt = f'{bgp_asn}:{conf_obj.id}'
+    if 'auto' in import_targets:
+        import_targets = [rt for rt in import_targets if rt != 'auto']
+        import_targets.append(auto_rt)
+    if 'auto' in export_targets:
+        export_targets = [rt for rt in export_targets if rt != 'auto']
+        export_targets.append(auto_rt)
+
+    import_commands = []
+    export_commands = []
+    for afi in afis:
+        import_commands += [f'route-target import {afi} {rt}'
+                            for rt in import_targets]
+        export_commands += [f'route-target export {afi} {rt}'
+                            for rt in export_targets]
+    return import_commands, export_commands
---
+++ packages/autonet-arista/autonet_arista-0.2.1-py3-none-any.whl/autonet_arista/eos/tasks/vxlan.py
@@ -0,0 +1,203 @@
+from autonet.core import exceptions as exc
+from autonet.core.objects import vxlan as an_vxlan
+
+from autonet_arista.eos.tasks import common as common_task
+
+
+def get_vxlans(show_int_vxlan: dict, show_bgp_config: str,
+               vnid: int = None) -> [an_vxlan.VXLAN]:
+    """
+    Parse VXLAN interface and BGP configuration to return a list
+    of `VXLAN` objects.
+    :param show_int_vxlan: Output from "show interfaces vxlan"
+    :param show_bgp_config: Textural BGP configuration.
+    :param vnid: When set only the VXLAN for the requested VNID is returned.
+    :return:
+    """
+    vxlans = []
+    vtep_address = show_int_vxlan['interfaces']['Vxlan1']['srcIpAddr']
+    l2_vnis = show_int_vxlan['interfaces']['Vxlan1']['vlanToVniMap']
+    l3_vnis = show_int_vxlan['interfaces']['Vxlan1']['vrfToVniMap']
+    bgp_config = common_task.parse_bgp_vpn_config(show_bgp_config)
+    # parse l2 VNIS
+    for vlan_id, l2_vni in l2_vnis.items():
+        # If a VNID is requested, we check to see if this is it, otherwise
+        # we skip.
+        if vnid and int(l2_vni['vni']) != vnid:
+            continue
+        # We also ignore VNIs that are not explicitly set since Arista will add
+        # "observed" L3 VNIs from EVPN.
+        if l2_vni['source'] == 'evpn':
+            continue
+        bgp_config_node = bgp_config['vlans'].get(vlan_id, {})
+        vxlans.append(an_vxlan.VXLAN(
+            id=int(l2_vni['vni']),
+            source_address=vtep_address,
+            layer=2,
+            export_targets=bgp_config_node.get('export_targets', []),
+            import_targets=bgp_config_node.get('import_targets', []),
+            route_distinguisher=bgp_config_node.get('rd', None),
+            bound_object_id=int(vlan_id)
+        ))
+    for vrf_name, l3_vni in l3_vnis.items():
+        # Same skip mechanism as above.
+        if vnid and int(l3_vni) != vnid:
+            continue
+        bgp_config_node = bgp_config['vrfs'].get(vrf_name, {})
+        vxlans.append(an_vxlan.VXLAN(
+            id=int(l3_vni),
+            source_address=vtep_address,
+            layer=3,
+            export_targets=bgp_config_node.get('export_targets', {}).get('evpn', []),
+            import_targets=bgp_config_node.get('import_targets', {}).get('evpn', []),
+            route_distinguisher=bgp_config_node.get('rd', None),
+            bound_object_id=vrf_name
+        ))
+
+    return vxlans
+
+
+def generate_l2_vxlan_create_commands(vxlan: an_vxlan.VXLAN) -> [str]:
+    """
+    Generate the commands required to create a l2 vxlan as
+    appropriate.
+    :param vxlan: A `VXLAN` object.
+    :return:
+    """
+    return [
+        'interface vxlan1',
+        f'vxlan vlan {vxlan.bound_object_id} vni {vxlan.id}'
+    ]
+
+
+def generate_l3_vxlan_create_commands(vxlan: an_vxlan.VXLAN) -> [str]:
+    """
+    Generate the commands required to create a l3 vxlan as
+    appropriate.
+    :param vxlan: A `VXLAN` object.
+    :return:
+    """
+    return [
+        'interface vxlan1',
+        f'vxlan vrf {vxlan.bound_object_id} vni {vxlan.id}'
+    ]
+
+
+def generate_vxlan_commands(vxlan: an_vxlan.VXLAN) -> [str]:
+    """
+    Generate the commands required to create given vxlan as
+    appropriate.
+    :param vxlan: A `VXLAN` object.
+    :return:
+    """
+    if vxlan.layer == 2:
+        return generate_l2_vxlan_create_commands(vxlan)
+    if vxlan.layer == 3:
+        return generate_l3_vxlan_create_commands(vxlan)
+
+
+def generate_l2_vxlan_evpn_commands(vxlan: an_vxlan.VXLAN, show_int_vxlan: dict,
+                                    bgp_config: dict) -> [str]:
+    """
+    Generate the commands to advertise an L2 VNI + VLAN in EVPN.
+    :param vxlan: A `VXLAN` object.
+    :param show_int_vxlan: The output of 'show interfaces vxlan1'.
+    :param bgp_config: The parsed output of the textual BGP config.
+    :return:
+    """
+    if vxlan.route_distinguisher == 'auto':
+        vxlan.route_distinguisher = f'{bgp_config["rid"]}:{vxlan.bound_object_id}'
+    auto_rt = f"{bgp_config['asn']}:{vxlan.id}"
+    vxlan.import_targets = [auto_rt if rt == 'auto' else rt
+                            for rt in vxlan.import_targets]
+    vxlan.export_targets = [auto_rt if rt == 'auto' else rt
+                            for rt in vxlan.export_targets]
+    import_rt_cmds = [f"route-target import {rt}"
+                      for rt in vxlan.import_targets]
+    export_rt_cmds = [f"route-target export {rt}"
+                      for rt in vxlan.export_targets]
+    return [
+               f'router bgp {bgp_config["asn"]}',
+               f'vlan {vxlan.bound_object_id}',
+               'redistribute learned',
+               f'rd {vxlan.route_distinguisher}'
+           ] + import_rt_cmds + export_rt_cmds
+
+
+def generate_l3_vxlan_evpn_commands(vxlan: an_vxlan.VXLAN, show_int_vxlan: dict,
+                                    bgp_config: dict) -> [str]:
+    """
+    Generate the commands to advertise an L3 VNI + VRF in EVPN.
+    :param vxlan: A `VXLAN` object.
+    :param show_int_vxlan: The output of 'show interfaces vxlan1'.
+    :param bgp_config: The parsed output of the textual BGP config.
+    :return:
+    """
+    if vxlan.route_distinguisher == 'auto':
+        auto_vlan = None
+        vni_map = show_int_vxlan['interfaces']['Vxlan1']['vlanToVniMap']
+        for vlan_id in vni_map:
+            if vni_map[vlan_id]['vni'] == vxlan.id:
+                auto_vlan = vlan_id
+                break
+        if not auto_vlan:
+            raise exc.AutonetException('Could not auto-derive RD')
+        vxlan.route_distinguisher = f'{bgp_config["rid"]}:{auto_vlan}'
+    import_rt_cmds, export_rt_cmds = common_task.generate_rt_commands(vxlan, bgp_config['asn'])
+    return [
+               f'router bgp {bgp_config["asn"]}',
+               f'vrf {vxlan.bound_object_id}',
+               'redistribute connected',
+               'redistribute attached-host',
+               'redistribute static',
+               f'rd {vxlan.route_distinguisher}'
+           ] + import_rt_cmds + export_rt_cmds
+
+
+def generate_vxlan_evpn_commands(vxlan: an_vxlan.VXLAN, show_int_vxlan: dict,
+                                 show_bgp_config: str) -> [str]:
+    """
+    Generate BGP_EVPN commands to advertise a given VNI.
+    :param vxlan: A `VXLAN` object.
+    :param show_int_vxlan: Output from "show interfaces vxlan"
+    :param show_bgp_config: Textural BGP configuration.
+    :return:
+    """
+    bgp_config = common_task.parse_bgp_vpn_config(show_bgp_config)
+    if vxlan.layer == 2:
+        return generate_l2_vxlan_evpn_commands(
+            vxlan, show_int_vxlan, bgp_config)
+    if vxlan.layer == 3:
+        return generate_l3_vxlan_evpn_commands(
+            vxlan, show_int_vxlan, bgp_config)
+
+
+def generate_vxlan_delete_commands(vxlan: an_vxlan.VXLAN, show_bgp_config: str) -> [str]:
+    """
+    Generates a set of commands to remove a VXLAN tunnel from a device.
+    For L2 VNIs, we will remove the vlan from the BGP EVPN configuration.
+    For L3 VNIs, we will remove only the tunnel definition and
+    redistribution of attached hosts, but not any of the BGP configuration
+    as it's shared with other address families. Subsequent calls to remove
+    the VRF itself would need to be made and are generally expected in
+    teardown use cases.
+    :param vxlan: A `VXLAN` object.
+    :param show_bgp_config: The active textual BGP configuration.
+    :return:
+    """
+    bgp_config = common_task.parse_bgp_vpn_config(show_bgp_config)
+    if vxlan.layer == 2:
+        return [
+            'interface vxlan1',
+            f'no vxlan vlan {vxlan.bound_object_id} vni {vxlan.id}',
+            f'router bgp {bgp_config["asn"]}',
+            f'no vlan {vxlan.bound_object_id}',
+        ]
+    if vxlan.layer == 3:
+        return [
+            'interface vxlan1',
+            f'no vxlan vrf {vxlan.bound_object_id} vni {vxlan.id}',
+            f'router bgp {bgp_config["asn"]}',
+            f'vrf {vxlan.bound_object_id}',
+            'no redistribute attached-host'
+        ]
