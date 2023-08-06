a8f6f01d98933668a589c1cb4232a7b8463bd08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:11 +0000

    Add package databeaver/0.0.36/databeaver-0.0.36.tar.gz

---
+++ packages/databeaver/databeaver-0.0.36.tar.gz/databeaver-0.0.36/src/databeaver/lib/__init__.py
@@ -0,0 +1,7 @@
+from .constants import *
+from .DataBeaver import DataBeaver
+from .MySQL import MySQL
+from .PostgreSQL import PostgreSQL
+from .System import System
+from .Table import Table
+from .Tables import Tables
