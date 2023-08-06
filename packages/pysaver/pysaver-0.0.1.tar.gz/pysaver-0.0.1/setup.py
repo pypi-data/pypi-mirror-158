f34e0bace3144fddd25b8c10332958afb96a6686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:04 +0000

    Add package pysaver/0.0.1/pysaver-0.0.1.tar.gz

---
+++ packages/pysaver/pysaver-0.0.1.tar.gz/pysaver-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pysaver',
+    version = '0.0.1',
+    py_modules = ['pysaver'],
+    author = 'M J Lally',
+    author_email = 'm.j.lally@outlook.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    description = "A simple utility to save python REPL history to file.",
+    url = 'https://github.com/mjlally/pysaver',
+    license = 'MIT',
+    keywords = 'pysaver',
+)
