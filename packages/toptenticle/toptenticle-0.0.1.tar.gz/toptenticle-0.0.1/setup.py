e5c8c2932fafbeb8751a63d2d3748c2cb6765881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package toptenticle/0.0.1/toptenticle-0.0.1.tar.gz

---
+++ packages/toptenticle/toptenticle-0.0.1.tar.gz/toptenticle-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+LONG_DESCRIPTION = "Simple!"
+DESCRIPTION = "Simple!"
+
+setup(
+    name="toptenticle",
+    version="0.0.1",
+    author="Tk",
+    author_email="<tylerfromgw@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pibrella'],
+    keywords=["top ten maker", "csvtools"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
+)
