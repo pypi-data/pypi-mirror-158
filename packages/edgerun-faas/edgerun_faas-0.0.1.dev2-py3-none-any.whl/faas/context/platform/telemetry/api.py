11ae1f8f7745ca5773c98b6d2c2ab7e68049faab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:04 +0000

    Add package edgerun-faas/0.0.1.dev2/edgerun_faas-0.0.1.dev2-py3-none-any.whl

---
+++ packages/edgerun-faas/edgerun_faas-0.0.1.dev2-py3-none-any.whl/faas/context/platform/telemetry/api.py
@@ -0,0 +1,20 @@
+import abc
+import datetime
+from typing import Optional
+
+import pandas as pd
+
+
+class TelemetryService(abc.ABC):
+
+    def get_replica_cpu(self, fn_replica_id: str, start: datetime.datetime = None, end: datetime.datetime = None) -> \
+            Optional[pd.DataFrame]:
+        """
+        Fetch the measured cpu times for the given replica in the given timeframe.
+        If start is None, the start of the selection will be set to 01-01-1970.
+        If end is None, the end will be set to now.
+        """
+        raise NotImplementedError()
+
+    def get_node_cpu(self, node: str) -> Optional[pd.DataFrame]:
+        raise NotImplementedError()
