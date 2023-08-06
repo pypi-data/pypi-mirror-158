de1df39c1c5b4c9a85a49d390a97c3157ee90a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:18 +0000

    Add package rdns-reaper/0.0.5/rdns_reaper-0.0.5.tar.gz

---
+++ packages/rdns-reaper/rdns_reaper-0.0.5.tar.gz/rdns_reaper-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='rdns_reaper',
+    version='0.0.5',
+    author='Will Mullaney',
+    author_email='rdns-reaper@mullaneywt.anonaddy.com',
+    description='Reverse DNS lookup engine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mullaneywt/rdns_reaper',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mullaneywt/rdns_reaper/issues"
+    },
+    license='None',
+    packages=['rdns_reaper'],
+    install_requires=['netaddr', 'pyyaml'],
+)
