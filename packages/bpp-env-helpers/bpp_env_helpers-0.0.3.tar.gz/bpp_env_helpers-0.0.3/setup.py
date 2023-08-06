e263c465ca20160bf4d35510a2a472a9f996f3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:29 +0000

    Add package bpp-env-helpers/0.0.3/bpp_env_helpers-0.0.3.tar.gz

---
+++ packages/bpp-env-helpers/bpp_env_helpers-0.0.3.tar.gz/bpp_env_helpers-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# TODO - set this up
+setuptools.setup(
+    name="bpp_env_helpers",
+    version="0.0.3",
+    author="matthew bahloul",
+    author_email="matthew.bahlol@blueprintprep.com",
+    description="misc helpers for bpp behave tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/blueprintprep/bpp-qa/utilities/environment-helpers",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/blueprintprep/bpp-qa/utilities/environment-helpers/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
