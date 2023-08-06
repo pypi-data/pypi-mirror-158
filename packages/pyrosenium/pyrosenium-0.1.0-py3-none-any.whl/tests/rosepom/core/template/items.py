b19d32635ea2157dc9ef32232f6c63b64734ddde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:25 +0000

    Add package pyrosenium/0.1.0/pyrosenium-0.1.0-py3-none-any.whl

---
+++ packages/pyrosenium/pyrosenium-0.1.0-py3-none-any.whl/tests/rosepom/core/template/items.py
@@ -0,0 +1,12 @@
+from pyrosenium.rosepom.helpers.rosenium_find_elements_methods import RFEM
+from pyrosenium.rosepom.core import rosepom
+
+
+class TPLTemplateLPTItems(rosepom.Items):
+    '''
+    Contains find methods and locators
+    '''
+    # EXAMPLE_ITEM = (RFEM.FIND, ["css", "div", 0])
+    HEADER = (RFEM.FIND, ["id", "Welcome_to_Wikipedia"])
+    WIKI_HREF = (RFEM.FIND, ["css", "a[href='/wiki/Wikipedia']"])
+    WIKI_HEADER = (RFEM.FIND, ["id", "firstHeading"])
