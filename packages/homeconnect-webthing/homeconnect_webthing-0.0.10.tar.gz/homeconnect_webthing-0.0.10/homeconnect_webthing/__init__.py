f74b81194ccc4b244cbacc7785ab897065c62a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package homeconnect-webthing/0.0.10/homeconnect_webthing-0.0.10.tar.gz

---
+++ packages/homeconnect-webthing/homeconnect_webthing-0.0.10.tar.gz/homeconnect_webthing-0.0.10/homeconnect_webthing/__init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from homeconnect_webthing.app import App, ArgumentSpec
+from homeconnect_webthing.auth import Auth
+from homeconnect_webthing.homeappliances_webthing import run_server
+
+
+def main():
+    App.run(run_function=lambda args, desc: run_server(args['port'], description=desc, filename=args['authfile']),
+            packagename="homeconnect_webthing",
+            arg_specs=[ArgumentSpec("authfile", str, "the auth file path", Path.home().joinpath(Auth.DEFAULT_FILENAME), True)])
+
+
+if __name__ == '__main__':
+    main()
