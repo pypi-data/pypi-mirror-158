9e5d976ab7ef63889d48f1d188106fc3ae1ae818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:38 +0000

    Add package wordfreak/0.0.7/wordfreak-0.0.7.tar.gz

---
+++ packages/wordfreak/wordfreak-0.0.7.tar.gz/wordfreak-0.0.7/README.md
@@ -0,0 +1,4 @@
+![Main Build](https://github.com/joeyagreco/wordfreak/actions/workflows/main-build.yml/badge.svg)
+![Last Commit](https://img.shields.io/github/last-commit/joeyagreco/wordfreak)
+
+# word-freak
---
+++ packages/wordfreak/wordfreak-0.0.7.tar.gz/wordfreak-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setuptools.setup(
+    name="wordfreak",
+    version="0.0.7",
+    author="Joey Greco",
+    author_email="joeyagreco@gmail.com",
+    description="",
+    long_description=readme,
+    license=license,
+    packages=setuptools.find_packages(exclude=("test", "docs")),
+    install_requires=["PyPDF2",
+                      "docx2txt",
+                      "setuptools"]
+)
