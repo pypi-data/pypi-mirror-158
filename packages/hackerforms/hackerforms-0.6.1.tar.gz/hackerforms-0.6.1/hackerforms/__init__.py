7da9f8be1aa25736e2056e881be6845d3c0262e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:00 +0000

    Add package hackerforms/0.6.1/hackerforms-0.6.1.tar.gz

---
+++ packages/hackerforms/hackerforms-0.6.1.tar.gz/hackerforms-0.6.1/hackerforms/__init__.py
@@ -0,0 +1,13 @@
+from .generated.page import *
+from .generated.inputs import *
+from .generated.outputs import *
+from .stdio import initialize as _initialize_stdio
+from .auth import *
+from .parameters import url_params
+from .socket import initialize as _initialize_socket
+import os
+
+
+if os.environ.get("ENV") != "test":
+  _initialize_socket()
+  _initialize_stdio()
