75848683d78248f8e04785898063fbad9b2bb367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:03 +0000

    Add package londiste/3.11/londiste-3.11.tar.gz

---
+++ packages/londiste/londiste-3.11.tar.gz/londiste-3.11/londiste/handlers/__init__.py
@@ -0,0 +1,41 @@
+"""Handlers module
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import functools
+import sys
+
+from typing import List
+
+import londiste.handler
+
+DEFAULT_HANDLERS: List["londiste.handler.BaseHandler"] = []
+
+
+def handler_args(name, cls):
+    """Handler arguments initialization decorator
+
+    Define successor for handler class cls with func as argument generator
+    """
+    def wrapper(func):
+        # pylint: disable=unnecessary-dunder-call
+        def _init_override(self, table_name, args, dest_table):
+            cls.__init__(self, table_name, func(args.copy()), dest_table)
+        dct = {'__init__': _init_override, 'handler_name': name}
+        module = sys.modules[cls.__module__]
+        newname = '%s_%s' % (cls.__name__, name.replace('.', '_'))
+        newcls = type(newname, (cls,), dct)
+        setattr(module, newname, newcls)
+        getattr(module, "__londiste_handlers__").append(newcls)
+        getattr(module, "__all__").append(newname)
+        return func
+    return wrapper
+
+
+def update(*p):
+    """ Update dicts given in params with its predecessor param dict
+    in reverse order """
+    return functools.reduce(lambda x, y: x.update(y) or x,
+                            (p[i] for i in range(len(p) - 1, -1, -1)), {})
+
---
+++ packages/londiste/londiste-3.11.tar.gz/londiste-3.11/tests/run.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -e
+set -x
+
+tests="qtable"
+tests="
+    simple
+    qtable
+    obfuscate
+    shard
+    register
+    takeover-merge
+"
+#tests="shard"
+#tests="takeover-merge"
+
+PAGER=cat
+export PAGER
+
+cd $(dirname $0)
+
+for tst in $tests; do
+    (cd $tst; ../zstop.sh; ./init.sh; ./regen.sh; ../zstop.sh)
+done
