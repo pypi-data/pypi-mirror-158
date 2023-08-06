856e1bd222137628b963f08d320de80c6af66c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:03 +0000

    Add package atomica/1.25.11/atomica-1.25.11.tar.gz

---
+++ packages/atomica/atomica-1.25.11.tar.gz/atomica-1.25.11/atomica/version.py
@@ -0,0 +1,11 @@
+"""
+Atomica version file.
+
+Standard location for module version number and date.
+"""
+
+from .utils import fast_gitinfo
+
+version = "1.25.11"
+versiondate = "2022-04-06"
+gitinfo = fast_gitinfo(__file__)
