25f66997eefddef90ac7f263349bc64e748c3720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:01 +0000

    Add package librapid/0.4.9/librapid-0.4.9-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.9-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.9.data/data/_librapid_python_cmake/src/librapid/vendor/Vc/CMakeFiles/Vc.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/avx_sorthelper_AVX.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/avx_sorthelper_AVX.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/avx_sorthelper_AVX.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/avx_sorthelper_AVX2+FMA+BMI2.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/avx_sorthelper_AVX2+FMA+BMI2.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/avx_sorthelper_AVX2+FMA+BMI2.cpp.o.d"
+  "/project/src/librapid/vendor/Vc/src/const.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/const.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/const.cpp.o.d"
+  "/project/src/librapid/vendor/Vc/src/cpuid.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/cpuid.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/cpuid.cpp.o.d"
+  "/project/src/librapid/vendor/Vc/src/support_x86.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/support_x86.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/src/support_x86.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/sse_sorthelper_AVX.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_AVX.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_AVX.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/sse_sorthelper_AVX2+FMA+BMI2.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_AVX2+FMA+BMI2.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_AVX2+FMA+BMI2.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/sse_sorthelper_SSE2.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_SSE2.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_SSE2.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/sse_sorthelper_SSE4_1.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_SSE4_1.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/sse_sorthelper_SSE4_1.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_AVX+FMA.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+FMA.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+FMA.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_AVX+XOP+FMA.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+XOP+FMA.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+XOP+FMA.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_AVX+XOP+FMA4.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+XOP+FMA4.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX+XOP+FMA4.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_AVX.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_AVX2+FMA+BMI2.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX2+FMA+BMI2.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_AVX2+FMA+BMI2.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSE+XOP+FMA.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE+XOP+FMA.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE+XOP+FMA.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSE+XOP+FMA4.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE+XOP+FMA4.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE+XOP+FMA4.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSE2.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE2.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE2.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSE3.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE3.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE3.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSE4_1.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE4_1.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSE4_1.cpp.o.d"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/trigonometric_SSSE3.cpp" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSSE3.cpp.o" "gcc" "src/librapid/vendor/Vc/CMakeFiles/Vc.dir/trigonometric_SSSE3.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
