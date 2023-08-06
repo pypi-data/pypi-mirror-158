1c6e5e4fc86ad765ac5d13cd9b18e9482c444afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:31 +0000

    Add package tkVideoUtils/1.2.10/tkVideoUtils-1.2.10.tar.gz

---
+++ packages/tkVideoUtils/tkVideoUtils-1.2.10.tar.gz/tkVideoUtils-1.2.10/examples/video_test.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+from ttkwidgets import TickScale
+
+from tkvideoutils import VideoPlayer
+from tkinter import messagebox
+
+
+def on_closing():
+    player.close()
+    root.quit()
+    root.destroy()
+
+
+if __name__ == '__main__':
+    # create instance of window
+    root = Tk()
+    # set window title
+    root.title('Video Player')
+    # load images
+    pause_image = PhotoImage(file='pause.png')
+    play_image = PhotoImage(file='play.png')
+    skip_backward = PhotoImage(file='skip_backward.png')
+    skip_forward = PhotoImage(file='skip_forward.png')
+    # create user interface
+    button = Button(root, image=play_image)
+    forward_button = Button(root, image=skip_forward)
+    backward_button = Button(root, image=skip_backward)
+    video_label = Label(root)
+    video_path = 'recorded_video.mp4'
+    audio_path = 'recorded_video.wav'
+    slider_var = IntVar(root)
+    slider = TickScale(root, orient="horizontal", variable=slider_var)
+    # place elements
+    video_label.pack()
+    button.pack()
+    forward_button.pack()
+    backward_button.pack()
+    slider.pack()
+
+    if video_path:
+        # read video to display on label
+        player = VideoPlayer(root, video_path, audio_path, video_label, size=(700, 500),
+                             play_button=button, play_image=play_image, pause_image=pause_image,
+                             slider=slider, slider_var=slider_var, keep_ratio=True, cleanup_audio=True,
+                             loading_gif=r'C:\GitHub\tkVideoUtils\examples\loading.gif')
+    else:
+        messagebox.showwarning("Select Video File", "Please retry and select a video file.")
+        sys.exit(1)
+    player.set_clip(0, 20)
+    forward_button.config(command=player.skip_video_forward)
+    backward_button.config(command=player.skip_video_backward)
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
---
+++ packages/tkVideoUtils/tkVideoUtils-1.2.10.tar.gz/tkVideoUtils-1.2.10/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='tkVideoUtils',
+      version='1.2.10',
+      description='Python module for playing and recording videos with sound inside tkinter Label widget '
+                  'using Pillow, imageio, and PyAudio, including media playback control, slider, '
+                  'and fps aware buffering.',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Multimedia :: Video :: Display'
+      ],
+      keywords='tkVideoUtils tkinter video webcam display label pillow imageio wsarce sound',
+      url='https://github.com/wsarce/tkVideoUtils',
+      author='Walker Arce (wsarce)',
+      author_email='wsarcera@gmail.com',
+      license='MIT',
+      packages=['tkvideoutils'],
+      install_requires=[
+          'imageio',
+          'imageio-ffmpeg',
+          'pillow',
+          'opencv-python',
+          'ttkwidgets',
+          'pyaudio',
+          'ffmpy',
+          'moviepy'
+      ],
+      include_package_data=True,
+      zip_safe=False
+      )
