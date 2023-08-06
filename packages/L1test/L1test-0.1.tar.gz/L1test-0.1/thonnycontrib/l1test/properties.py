b79d0af56d517b50f4dfaacb8d721e22ef6c9146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:28:05 +0000

    Add package L1test/0.1/L1test-0.1.tar.gz

---
+++ packages/L1test/L1test-0.1.tar.gz/L1test-0.1/thonnycontrib/l1test/properties.py
@@ -0,0 +1,6 @@
+PLUGIN_NAME = "L1Test"
+ERROR_VIEW_LABEL = "L1Test error log"
+
+# ici vous pouvez changer la syntaxe du doctest. 
+# Actuellement on garde la syntaxe `$py`.
+L1TEST_SYMBOL = "[$]py"
