f8d3fab963e5fbe681c6e83a95858d730e3d1428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package kubify/9003.1.8/kubify-9003.1.8.tar.gz

---
+++ packages/kubify/kubify-9003.1.8.tar.gz/kubify-9003.1.8/HISTORY.rst
@@ -0,0 +1,11 @@
+=======
+History
+=======
+
+9003.1.8 (2022-07-06)
+------------------
+
+* Refactor.
+* Terraform Migration.
+* Python Migration.
+* PyPI & DockerHub.
---
+++ packages/kubify/kubify-9003.1.8.tar.gz/kubify-9003.1.8/tests/test_kubify_k8s_utils_k8s_failing.py
@@ -0,0 +1,43 @@
+import kubify.k8s_utils.k8s as module_0
+
+
+def test_case_0():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
+
+
+def test_case_1():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
+
+
+def test_case_2():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
+
+
+def test_case_3():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
+
+
+def test_case_4():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
+
+
+def test_case_5():
+    try:
+        module_0.k8s_utils()
+    except BaseException:
+        pass
