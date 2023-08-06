89104ed5ff2ada13bcd7f8554475172503d581dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:51 +0000

    Add package biglist/0.6.6/biglist-0.6.6-py3-none-any.whl

---
+++ packages/biglist/biglist-0.6.6-py3-none-any.whl/biglist/__init__.py
@@ -0,0 +1,10 @@
+'''Biglist'''
+from ._biglist import Biglist, ListView, FileView, ConcurrentIterStat
+
+
+__version__ = '0.6.6'
+
+
+__all__ = [
+    'Biglist', 'ListView', 'FileView', 'ConcurrentIterStat',
+]
