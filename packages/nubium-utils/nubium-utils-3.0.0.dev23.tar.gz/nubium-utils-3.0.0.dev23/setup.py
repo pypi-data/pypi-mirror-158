a5a313944cad200451b56bd84d0fa438d123590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:05 +0000

    Add package nubium-utils/3.0.0.dev23/nubium-utils-3.0.0.dev23.tar.gz

---
+++ packages/nubium-utils/nubium-utils-3.0.0.dev23.tar.gz/nubium-utils-3.0.0.dev23/setup.py
@@ -0,0 +1,55 @@
+import setuptools
+
+with open("README.md", "r") as file_obj:
+    long_description = file_obj.read()
+
+install_requires = [
+    "confluent-kafka[avro]==1.8.2",
+    "lbry-rocksdb",
+    "nubium-schemas",
+    "prometheus_client",
+    "psutil",
+    "python-box",
+    "python-dateutil",
+    "python-dotenv",
+    "python_eloqua_wrapper",
+    "pytz",
+    "pyyaml",
+    "requests",
+    "simple_salesforce",
+    "virtualenv",
+    "virtualenv-api",
+    "dictdiffer",
+    "mmh3",
+    "wrapt-timeout-decorator",
+]
+
+dev_requires = install_requires + [
+    "pip-tools",
+    "pytest",
+    "pytest-cov",
+    "twine",
+    "time-machine",
+]
+
+packages = setuptools.find_packages()
+
+setuptools.setup(
+    name="nubium-utils",
+    version="3.0.0-dev23",
+    author="Edward Brennan",
+    author_email="ebrennan@redhat.com",
+    description="Some Kafka utility functions and patterns for the nubium project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.corp.redhat.com/mkt-ops-de/nubium-utils.git",
+    packages=packages,
+    install_requires=install_requires,
+    include_package_data=True,
+    extras_require={"dev": dev_requires},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
