9407caaac416e682c719e2467dc33625da899f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:43 +0000

    Add package mbank-service/0.1.2/mbank_service-0.1.2.tar.gz

---
+++ packages/mbank-service/mbank_service-0.1.2.tar.gz/mbank_service-0.1.2/mbank_service/base.py
@@ -0,0 +1,44 @@
+import abc
+from urllib.parse import urljoin, urlencode
+
+import requests
+
+from .exceptions import (
+    ClientNotFoundError,
+    ClientBadRequestError,
+    ClientServerError,
+    ClientAPIException
+)
+
+
+class BaseClient(abc.ABC):
+    def __init__(self, token: str, url: str):
+        self._token = token
+        self._main_url = url
+
+    @staticmethod
+    def _strip_phone(phone):
+        return str(phone).strip('+')
+
+    def _build_absolute_url(self, url, data):
+        return f'{urljoin(self._main_url, url)}?{urlencode(data)}'
+
+    @staticmethod
+    def _checker_response(response: requests.Response):
+        status_code = response.status_code
+        if (
+                status_code == 200 or
+                status_code == 201 or
+                status_code == 202
+        ):
+            return
+
+        if status_code == 404:
+            raise ClientNotFoundError(response.status_code, response.text)
+
+        if 400 <= status_code < 500:
+            raise ClientBadRequestError(response.status_code, response.text)
+
+        if status_code >= 500:
+            raise ClientServerError(response.status_code, response.text)
+        raise ClientAPIException(response.status_code, response.text)
---
+++ packages/mbank-service/mbank_service-0.1.2.tar.gz/mbank_service-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mbank_service',
+    version='0.1.2',
+    include_package_data=True,
+    packages=find_packages(),
+    license='TBD',
+    author='Eldiyar Kandzhebaev',
+    python_requires='>=3.9',
+)
