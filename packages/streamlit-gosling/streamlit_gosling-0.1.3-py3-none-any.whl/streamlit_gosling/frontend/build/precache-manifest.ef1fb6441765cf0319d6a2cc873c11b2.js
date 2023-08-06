4f3e24d9ba366e44461e34c2ff590a10bb6e1413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:39 +0000

    Add package streamlit-gosling/0.1.3/streamlit_gosling-0.1.3-py3-none-any.whl

---
+++ packages/streamlit-gosling/streamlit_gosling-0.1.3-py3-none-any.whl/streamlit_gosling/frontend/build/precache-manifest.ef1fb6441765cf0319d6a2cc873c11b2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f6547fccd6ee4bda070af6c6eaf1b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3857e9f2e1e72561709",
+    "url": "./static/css/main.be13ba4e.chunk.css"
+  },
+  {
+    "revision": "4f496da4d1a043737b13",
+    "url": "./static/js/2.91750363.chunk.js"
+  },
+  {
+    "revision": "427cd64f754f0e6833e7bb7254443d24",
+    "url": "./static/js/2.91750363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba662923af28e42073b6",
+    "url": "./static/js/3.524fc320.chunk.js"
+  },
+  {
+    "revision": "6e31a0db202a37e424180fec8d0f2d9a",
+    "url": "./static/js/3.524fc320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9214e430e5f92ca2901",
+    "url": "./static/js/4.ed7e5486.chunk.js"
+  },
+  {
+    "revision": "eddb53691628a667e6498c6230cd8119",
+    "url": "./static/js/4.ed7e5486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f29fb283e3a78c2e9aa",
+    "url": "./static/js/5.48897ae8.chunk.js"
+  },
+  {
+    "revision": "73aea2f6b249805336f747a1f65ca2b9",
+    "url": "./static/js/5.48897ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46045d0f5580c1ebc915",
+    "url": "./static/js/6.70634445.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "./static/js/6.70634445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3857e9f2e1e72561709",
+    "url": "./static/js/main.9b6ea508.chunk.js"
+  },
+  {
+    "revision": "05cecd7a552016e38f59",
+    "url": "./static/js/runtime-main.2852e9d1.js"
+  }
+]);
