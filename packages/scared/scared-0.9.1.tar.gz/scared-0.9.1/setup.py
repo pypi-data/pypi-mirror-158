83f71dba8f91b00d29ec22fbd296dbc575d8d128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:57 +0000

    Add package scared/0.9.1/scared-0.9.1.tar.gz

---
+++ packages/scared/scared-0.9.1.tar.gz/scared-0.9.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class PyTest(test):
+    user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import pytest
+        import shlex
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    cmdclass={"test": PyTest}
+)
