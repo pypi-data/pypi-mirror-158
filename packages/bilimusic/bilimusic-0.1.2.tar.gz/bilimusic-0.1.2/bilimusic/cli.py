f9a47801c54a109a5a8378d7369bc8681082d943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:35 +0000

    Add package bilimusic/0.1.2/bilimusic-0.1.2.tar.gz

---
+++ packages/bilimusic/bilimusic-0.1.2.tar.gz/bilimusic-0.1.2/bilimusic/cli.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-module-docstring
+import argparse
+from .Video import Video
+
+
+def run():
+    '''
+    provides parsing of the command line.
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', help='bvid or link to the video')
+    parser.add_argument('-o', '--offset_cover', type=float,
+                        help='the percentage to offset cover')
+    parser.add_argument('-ar', '--artist',  help='the artist')
+    parser.add_argument('-t', '--title',  help='the artist')
+    parser.add_argument('-al', '--album',  help='the album')
+
+    args = parser.parse_args()
+
+    video = Video(args.id)
+    for key in ('artist', 'title', 'album'):
+        value = getattr(args, key)
+        if value:
+            video.setinfo({key: value})
+    cover_offset = args.offset_cover if args.offset_cover else 0.0
+    video.download_mp3(
+        offset=cover_offset
+    )
