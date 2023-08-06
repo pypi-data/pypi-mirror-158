6b006343de964a3d932726a70b855168ac703445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:46 +0000

    Add package gpsPub/100.0.5/gpsPub-100.0.5.tar.gz

---
+++ packages/gpsPub/gpsPub-100.0.5.tar.gz/gpsPub-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="gpsPub",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="gpsPub of gpsPub of gpsPub",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
