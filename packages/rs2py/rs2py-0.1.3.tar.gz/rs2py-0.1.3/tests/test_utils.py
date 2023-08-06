3b2868cff8f4a104e7aa31ed14b20491c0521328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:54 +0000

    Add package rs2py/0.1.3/rs2py-0.1.3.tar.gz

---
+++ packages/rs2py/rs2py-0.1.3.tar.gz/rs2py-0.1.3/tests/test_utils.py
@@ -0,0 +1,9 @@
+import rs2py
+
+def test_get_price_tick_move():
+    res = rs2py.get_price_tick_move(100, 5)
+
+
+def test_get_price_tick_move():
+    res = rs2py.get_price_between_tick(100, 101)
+     
