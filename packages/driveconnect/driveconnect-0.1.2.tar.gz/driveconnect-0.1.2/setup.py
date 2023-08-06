248f98113f1b6ec375db7b8f5eb6a190617a880c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:07 +0000

    Add package driveconnect/0.1.2/driveconnect-0.1.2.tar.gz

---
+++ packages/driveconnect/driveconnect-0.1.2.tar.gz/driveconnect-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['driveconnect']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pysecstring>=0.1.8,<0.2.0']
+
+setup_kwargs = {
+    'name': 'driveconnect',
+    'version': '0.1.2',
+    'description': 'Test whether a Windows drive is connected, and connect it.',
+    'long_description': None,
+    'author': 'Gwang-Jin Kim',
+    'author_email': 'gwang.jin.kim.phd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
