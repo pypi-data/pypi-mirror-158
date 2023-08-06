4cf21d5de1b72979f11a154c76f2907eb812d846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:28 +0000

    Add package rcri/0.0.1/rcri-0.0.1.tar.gz

---
+++ packages/rcri/rcri-0.0.1.tar.gz/rcri-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import subprocess, os
+from setuptools import setup
+
+setup(
+    name='rcri',
+    version='0.0.1',
+    author='Nikolai Krivoshchapov',
+    packages=['bestpackageever'],
+    package_data={'bestpackageever': ['main.py']}
+)
