6c823912f857dd78274d86e2e9fa6f7ade423f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:27 +0000

    Add package autospares/0.0.2/autospares-0.0.2.tar.gz

---
+++ packages/autospares/autospares-0.0.2.tar.gz/autospares-0.0.2/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(
+name = "autospares",
+version = "0.0.2",
+author = "adrian",
+authoremail= "veldfolds@gmail.com", 
+url = "https://pypi.org/user/adrian_k/",
+description = "A small example package",
+readme = "README.md",
+license = "LICENSE",
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+)
