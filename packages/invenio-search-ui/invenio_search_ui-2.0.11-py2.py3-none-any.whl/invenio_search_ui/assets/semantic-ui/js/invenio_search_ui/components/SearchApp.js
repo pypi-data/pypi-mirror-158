b8d7f8c457432fb3745d9d258c1fc99a9d54b2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:18 +0000

    Add package invenio-search-ui/2.0.11/invenio_search_ui-2.0.11-py2.py3-none-any.whl

---
+++ packages/invenio-search-ui/invenio_search_ui-2.0.11-py2.py3-none-any.whl/invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/components/SearchApp.js
@@ -0,0 +1,199 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2020 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import Overridable, {
+  OverridableContext,
+  overrideStore,
+} from "react-overridable";
+import {
+  BucketAggregation,
+  EmptyResults,
+  Error,
+  InvenioSearchApi,
+  ReactSearchKit,
+  ResultsLoader,
+  withState,
+  buildUID,
+} from "react-searchkit";
+import { Container, Grid } from "semantic-ui-react";
+import { Results, ResultOptions } from "./Results";
+import { SearchBar } from "./SearchBar";
+import { SearchConfigurationContext } from "./context";
+
+const OnResults = withState(Results);
+const ResultOptionsWithState = withState(ResultOptions);
+
+export const SearchAppFacets = ({ aggs }) => {
+  const { buildUID } = useContext(SearchConfigurationContext);
+  return (
+    <Overridable id={buildUID("SearchApp.facets")} aggs={aggs}>
+      <>
+        {aggs.map((agg) => (
+          <BucketAggregation key={agg.title} title={agg.title} agg={agg.agg} />
+        ))}
+      </>
+    </Overridable>
+  );
+};
+
+export const SearchAppResultsPane = ({ layoutOptions }) => {
+  const { buildUID } = useContext(SearchConfigurationContext);
+  return (
+    <Overridable
+      id={buildUID("SearchApp.resultsPane")}
+      layoutOptions={layoutOptions}
+    >
+      <ResultsLoader>
+        <EmptyResults />
+        <Error />
+        <OnResults />
+      </ResultsLoader>
+    </Overridable>
+  );
+};
+
+export const SearchApp = ({ config, appName }) => {
+  const searchApi = new InvenioSearchApi(config.searchApi);
+  const context = {
+    appName,
+    buildUID: (element) => buildUID(element, "", appName),
+    ...config,
+  };
+  return (
+    <OverridableContext.Provider value={overrideStore.getAll()}>
+      <SearchConfigurationContext.Provider value={context}>
+        <ReactSearchKit
+          searchApi={searchApi}
+          appName={appName}
+          initialQueryState={config.initialQueryState}
+          defaultSortingOnEmptyQueryString={
+            config.defaultSortingOnEmptyQueryString
+          }
+        >
+          <Overridable
+            id={buildUID("SearchApp.layout", "", appName)}
+            config={config}
+          >
+            <Container>
+              <Overridable
+                id={buildUID("SearchApp.searchbarContainer", "", appName)}
+              >
+                <Grid relaxed padded>
+                  <Grid.Row>
+                    <Grid.Column width={4} />
+                    <Grid.Column width={12}>
+                      <Overridable
+                        id={buildUID("SearchApp.searchbar", "", appName)}
+                      >
+                        <SearchBar />
+                      </Overridable>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Overridable>
+              <Grid relaxed>
+                <Grid.Row
+                  textAlign="right"
+                  columns={2}
+                  className="result-options rel-mt-2"
+                >
+                  <Grid.Column width={4} />
+                  <Grid.Column width={12}>
+                    <ResultOptionsWithState
+                      sortOptions={config.sortOptions}
+                      layoutOptions={config.layoutOptions}
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={2}>
+                  <Grid.Column width={4}>
+                    <SearchAppFacets aggs={config.aggs} />
+                  </Grid.Column>
+                  <Grid.Column width={12}>
+                    <SearchAppResultsPane
+                      layoutOptions={config.layoutOptions}
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Container>
+          </Overridable>
+        </ReactSearchKit>
+      </SearchConfigurationContext.Provider>
+    </OverridableContext.Provider>
+  );
+};
+
+SearchApp.propTypes = {
+  config: PropTypes.shape({
+    searchApi: PropTypes.object.isRequired, // same as ReactSearchKit.searchApi
+    initialQueryState: PropTypes.shape({
+      queryString: PropTypes.string,
+      sortBy: PropTypes.string,
+      sortOrder: PropTypes.string,
+      page: PropTypes.number,
+      size: PropTypes.number,
+      hiddenParams: PropTypes.array,
+      layout: PropTypes.oneOf(["list", "grid"]),
+    }),
+    aggs: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        aggName: PropTypes.string,
+        access_right: PropTypes.string,
+        mapping: PropTypes.object,
+      })
+    ),
+    sortOptions: PropTypes.arrayOf(
+      PropTypes.shape({
+        sortBy: PropTypes.string,
+        sortOrder: PropTypes.string,
+        text: PropTypes.string,
+      })
+    ),
+    paginationOptions: PropTypes.shape({
+      resultsPerPage: PropTypes.arrayOf(
+        PropTypes.shape({
+          text: PropTypes.string,
+          value: PropTypes.number,
+        })
+      ),
+    }),
+    layoutOptions: PropTypes.shape({
+      listView: PropTypes.bool.isRequired,
+      gridView: PropTypes.bool.isRequired,
+    }).isRequired,
+    defaultSortingOnEmptyQueryString: PropTypes.shape({
+      sortBy: PropTypes.string,
+      sortOrder: PropTypes.string,
+    }),
+  }).isRequired,
+  appName: PropTypes.string,
+};
+
+SearchApp.defaultProps = {
+  config: {
+    searchApi: {
+      url: "",
+      withCredentials: false,
+      headers: {},
+    },
+    initialQueryState: {},
+    aggs: [],
+    sortOptions: [],
+    paginationOptions: {},
+    layoutOptions: {
+      listView: true,
+      gridView: false,
+    },
+    defaultSortingOnEmptyQueryString: {},
+  },
+  appName: null,
+};
