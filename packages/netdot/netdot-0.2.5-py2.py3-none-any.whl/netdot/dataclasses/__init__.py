85ebf084f399ba438a656fa1b10225912bf41db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:49 +0000

    Add package netdot/0.2.5/netdot-0.2.5-py2.py3-none-any.whl

---
+++ packages/netdot/netdot-0.2.5-py2.py3-none-any.whl/netdot/dataclasses/__init__.py
@@ -0,0 +1,34 @@
+from .asset import Asset
+from .base import NetdotAPIDataclass
+from .device import Device
+from .interface import Interface
+from .ipblock import IPBlock
+from .products import Product, ProductType
+from .site import Site
+from .vlan import VLAN
+
+_initialzied = False
+
+def initialize():
+    # TODO can these just be at module-level insted of having this be a runtime function?
+    global _initialzied
+    if not _initialzied:
+        Asset()
+        Device()
+        Interface()
+        IPBlock()
+        Product()
+        ProductType()
+        Site()
+        VLAN()
+        _initialzied = True
+
+
+Subnet = IPBlock
+IPAddr = IPBlock
+IPContainer = IPBlock
+
+__all__ = [
+    'initialize', 'Asset', 'Device', 'Interface', 'IPBlock', 'Subnet', 'IPAddr', 'IPContainer',
+    'Product', 'ProductType', 'Site', 'VLAN', 'NetdotAPIDataclass',
+]
