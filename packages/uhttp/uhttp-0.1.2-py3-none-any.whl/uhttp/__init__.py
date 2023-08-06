d873b0f08349c89df2f46a3c0af1f18996531b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:53 +0000

    Add package uhttp/0.1.2/uhttp-0.1.2-py3-none-any.whl

---
+++ packages/uhttp/uhttp-0.1.2-py3-none-any.whl/uhttp/__init__.py
@@ -0,0 +1,20 @@
+from .__version__ import __title__ as _package_name
+from .__version__ import __version__ as _package_version
+from ._models import Auth, H11Response, HttpPoolResponse, Request
+from ._protocols import H11Pool, W11Protocol
+from ._rest import QueryParam, RestApi
+
+
+
+__all__ = [
+    "Auth",
+    "H11Pool",
+    "H11Response",
+    "HttpPoolResponse",
+    "QueryParam",
+    "Request",
+    "RestApi",
+    "W11Protocol",
+    "_package_name",
+    "_package_version"
+]
