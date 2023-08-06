3f303c3a207e252bccf3fdff100b71504cca4025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:55 +0000

    Add package castor-extractor/0.0.25/castor-extractor-0.0.25.tar.gz

---
+++ packages/castor-extractor/castor-extractor-0.0.25.tar.gz/castor-extractor-0.0.25/castor_extractor/visualization/qlik/assets.py
@@ -0,0 +1,101 @@
+from enum import Enum
+from typing import Dict, Tuple
+
+
+class QlikAsset(Enum):
+    """Enum of Qlik assets"""
+
+    SPACES = "spaces"
+    USERS = "users"
+    APPS = "apps"
+    QVDS = "qvds"
+    LINEAGE = "lineage"
+
+
+ASSET_PATHS: Dict[QlikAsset, str] = {
+    QlikAsset.SPACES: "spaces",
+    QlikAsset.USERS: "users",
+    QlikAsset.APPS: "items?resourceType=app",
+    QlikAsset.QVDS: "items?resourceType=dataset",
+    QlikAsset.LINEAGE: "apps/{app_id}/data/lineage",
+}
+
+
+EXPORTED_FIELDS: Dict[QlikAsset, Tuple[str, ...]] = {
+    QlikAsset.SPACES: (
+        "id",
+        "type",
+        "ownerId",
+        "tenantId",
+        "name",
+        "description",
+        "meta",
+        "links",
+        "createdAt",
+        "createdBy",
+        "updatedAt",
+    ),
+    QlikAsset.USERS: (
+        "id",
+        "tenantId",
+        "status",
+        "name",
+        "email",
+        "links",
+    ),
+    QlikAsset.APPS: (
+        "name",
+        "spaceId",
+        "description",
+        "thumbnailId",
+        "resourceAttributes",
+        "resourceCustomAttributes",
+        "resourceUpdatedAt",
+        "resourceType",
+        "resourceId",
+        "resourceCreatedAt",
+        "id",
+        "createdAt",
+        "updatedAt",
+        "creatorId",
+        "updaterId",
+        "tenantId",
+        "isFavorited",
+        "links",
+        "actions",
+        "collectionIds",
+        "meta",
+        "ownerId",
+        "resourceReloadEndTime",
+        "resourceReloadStatus",
+        "resourceSize",
+        "itemViews",
+    ),
+    QlikAsset.QVDS: (
+        "name",
+        "spaceId",
+        "resourceAttributes",
+        "resourceCustomAttributes",
+        "resourceUpdatedAt",
+        "resourceType",
+        "resourceSubType",
+        "resourceId",
+        "resourceCreatedAt",
+        "id",
+        "createdAt",
+        "updatedAt",
+        "creatorId",
+        "updaterId",
+        "tenantId",
+        "isFavorited",
+        "links",
+        "actions",
+        "collectionIds",
+        "meta",
+        "ownerId",
+        "resourceReloadEndTime",
+        "resourceReloadStatus",
+        "resourceSize",
+        "itemViews",
+    ),
+}
