4b0b4936e34ae0d98c807a0a73fda0bd69a3154e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:34 +0000

    Add package ignition-api-stubs/8.1.18a4/ignition_api_stubs-8.1.18a4-py3-none-any.whl

---
+++ packages/ignition-api-stubs/ignition_api_stubs-8.1.18a4-py3-none-any.whl/system/report.pyi
@@ -0,0 +1,21 @@
+from com.inductiveautomation.ignition.common import BasicDataset
+from java.lang import String
+from typing import Any, Dict, List, Optional
+
+def executeAndDistribute(
+    path: String,
+    project: String = ...,
+    parameters: Optional[Dict[String, int]] = ...,
+    action: Optional[String] = ...,
+    actionSettings: Optional[Dict[String, Any]] = ...,
+) -> None: ...
+def executeReport(
+    path: String,
+    project: String = ...,
+    parameters: Optional[Dict[String, int]] = ...,
+    fileType: String = ...,
+) -> Any: ...
+def getReportNamesAsDataset(
+    project: Optional[String] = ..., includeReportName: Optional[bool] = ...
+) -> BasicDataset: ...
+def getReportNamesAsList(project: Optional[String] = ...) -> List[String]: ...
