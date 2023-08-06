3f6dfec7903b54c9cbf4b97ed5461540c92784b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:36 +0000

    Add package example-package-jty/0.0.1/example_package_jty-0.0.1-py3-none-any.whl

---
+++ packages/example-package-jty/example_package_jty-0.0.1-py3-none-any.whl/example_package_jty/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/12 16:17
+# @Author  : jiatianyu
+# @FileName: __init__.py
---
+++ packages/example-package-jty/example_package_jty-0.0.1-py3-none-any.whl/example_package_jty/example.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/12 16:17
+# @Author  : jiatianyu
+# @FileName: example.py
+
+def add_one(number):
+    return number + 1
