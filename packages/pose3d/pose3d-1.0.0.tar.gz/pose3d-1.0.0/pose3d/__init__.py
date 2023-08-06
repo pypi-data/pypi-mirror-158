178e9ba9066f63da06e2a9ffd77d0f2af4760b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:41 +0000

    Add package pose3d/1.0.0/pose3d-1.0.0.tar.gz

---
+++ packages/pose3d/pose3d-1.0.0.tar.gz/pose3d-1.0.0/pose3d/__init__.py
@@ -0,0 +1,6 @@
+from pose_comparison import PoseComparison
+from pose3d import Pose
+from pose2d import Pose2D
+from rotation2d import Rotation2D
+from transform_set import TransformSet
+from transform import Transform
