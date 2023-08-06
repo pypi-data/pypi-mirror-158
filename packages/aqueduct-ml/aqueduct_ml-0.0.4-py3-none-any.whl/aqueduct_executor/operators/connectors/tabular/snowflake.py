ca78dd2d64e6ce0a763006d50821fcd6035da97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:22 +0000

    Add package aqueduct-ml/0.0.4/aqueduct_ml-0.0.4-py3-none-any.whl

---
+++ packages/aqueduct-ml/aqueduct_ml-0.0.4-py3-none-any.whl/aqueduct_executor/operators/connectors/tabular/snowflake.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, engine
+
+from aqueduct_executor.operators.connectors.tabular import config, relational
+
+
+class SnowflakeConnector(relational.RelationalConnector):
+    def __init__(self, config: config.SnowflakeConfig):
+        conn_engine = _create_engine(config)
+        super().__init__(conn_engine)
+
+
+def _create_engine(config: config.SnowflakeConfig) -> engine.Engine:
+    # Snowflake Dialect:
+    # https://github.com/snowflakedb/snowflake-sqlalchemy
+    url = "snowflake://{username}:{password}@{account_identifier}/{database}/{schema}?warehouse={warehouse}".format(
+        username=config.username,
+        password=config.password,
+        account_identifier=config.account_identifier,
+        database=config.database,
+        schema=config.db_schema,
+        warehouse=config.warehouse,
+    )
+    return create_engine(url)
