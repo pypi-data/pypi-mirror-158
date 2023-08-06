23128ff0710a0698c8a6b0e17f463e83243425d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:03 +0000

    Add package esi-acme/2022.7/esi_acme-2022.7-py3-none-any.whl

---
+++ packages/esi-acme/esi_acme-2022.7-py3-none-any.whl/acme/tests/local_acme.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Simple script for testing acme w/o pip-installing it
+#
+
+# Builtin/3rd party package imports
+import numpy as np
+
+# Add acme to Python search path
+import os
+import sys
+acme_path = os.path.abspath(".." + os.sep + "..")
+if acme_path not in sys.path:
+    sys.path.insert(0, acme_path)
+
+# Import package
+from acme import ParallelMap
+
+def f(x, y, z=3, w=np.zeros((3, 1)), **kwargs):
+    return (sum(x) + y) * z * w.max()
+
+
+# Prepare code to be executed using, e.g., iPython's `%run` magic command
+if __name__ == "__main__":
+
+    # Test stuff within here...
+    # pass
+
+    pmap = ParallelMap(f, [2, 4, 6, 8], [2, 2], z=np.array([1, 2]), w=np.ones((8, 1)), n_inputs=2, dryrun=True)
+    with pmap as p:
+        p.compute()
+
+
