417c4007552f0feb367bcf0f5b6f85c50fa7435c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:29 +0000

    Add package testppversion/1.0.1/testppversion-1.0.1.tar.gz

---
+++ packages/testppversion/testppversion-1.0.1.tar.gz/testppversion-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testppversion']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'testppversion',
+    'version': '1.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Bozhao',
+    'author_email': 'yubz86@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
