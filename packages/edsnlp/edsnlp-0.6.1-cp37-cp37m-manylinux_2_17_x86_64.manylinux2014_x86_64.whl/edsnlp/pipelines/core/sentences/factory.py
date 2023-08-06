ef6dc10ae3dee8da7b13bdfaaab0b4e8a04375f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:13 +0000

    Add package edsnlp/0.6.1/edsnlp-0.6.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.6.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/edsnlp/pipelines/core/sentences/factory.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+
+from spacy.language import Language
+
+from edsnlp.utils.deprecation import deprecated_factory
+
+from . import SentenceSegmenter
+
+DEFAULT_CONFIG = dict(
+    punct_chars=None,
+    ignore_excluded=True,
+    use_endlines=None,
+)
+
+
+@deprecated_factory("sentences", "eds.sentences", default_config=DEFAULT_CONFIG)
+@Language.factory("eds.sentences", default_config=DEFAULT_CONFIG)
+def create_component(
+    nlp: Language,
+    name: str,
+    punct_chars: Optional[List[str]],
+    use_endlines: Optional[bool],
+    ignore_excluded: bool,
+):
+    return SentenceSegmenter(
+        nlp.vocab,
+        punct_chars=punct_chars,
+        use_endlines=use_endlines,
+        ignore_excluded=ignore_excluded,
+    )
