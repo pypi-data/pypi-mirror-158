d6ef4e4408211cdc4f94415770e3a8eea7a44754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:15 +0000

    Add package eda-report/2.6.0/eda_report-2.6.0.tar.gz

---
+++ packages/eda-report/eda_report-2.6.0.tar.gz/eda_report-2.6.0/eda_report/__init__.py
@@ -0,0 +1,66 @@
+from collections.abc import Iterable
+from typing import Union
+
+from eda_report.document import ReportDocument
+from eda_report.multivariate import MultiVariable
+
+__version__ = "2.6.0"
+
+
+def get_word_report(
+    data: Iterable,
+    *,
+    title: str = "Exploratory Data Analysis Report",
+    graph_color: str = "cyan",
+    target_variable: Union[str, int] = None,
+    output_filename: str = "eda-report.docx",
+    table_style: str = "Table Grid",
+) -> ReportDocument:
+    """Analyzes data, and generates a report in *Word* (*.docx*) format.
+
+    Args:
+        data (Iterable): The data to analyze.
+        title (str, optional): The title to assign the report. Defaults to
+            "Exploratory Data Analysis Report".
+        graph_color (str, optional): The color to apply to the graphs.
+            Defaults to "cyan".
+        target_variable (Union[str, int], optional): The label/index for the
+            column to use to group values. Defaults to None.
+        output_filename (str, optional): The name and location to save the
+         report document. Defaults to "eda-report.docx".
+        table_style (str, optional): The style to apply to the tables created.
+            Defaults to "Table Grid".
+
+    Returns:
+        ReportDocument: Document object with analysis results.
+
+    Example:
+        .. literalinclude:: examples.txt
+           :lines: 148-155
+
+    """
+    return ReportDocument(
+        data,
+        title=title,
+        graph_color=graph_color,
+        output_filename=output_filename,
+        target_variable=target_variable,
+        table_style=table_style,
+    )
+
+
+def summarize(data: Iterable) -> MultiVariable:
+    """Get summary statistics for the supplied data.
+
+    Args:
+        data (Iterable): The data to analyze.
+
+    Returns:
+        MultiVariable: Analysis results.
+
+    Examples:
+        .. literalinclude:: examples.txt
+           :lines: 159-217
+
+    """
+    return MultiVariable(data)
---
+++ packages/eda-report/eda_report-2.6.0.tar.gz/eda_report-2.6.0/eda_report/validate.py
@@ -0,0 +1,185 @@
+import logging
+from collections.abc import Iterable
+from typing import Optional, Union
+
+from pandas import DataFrame, RangeIndex, Series
+from pandas.api.types import is_numeric_dtype
+
+from eda_report.exceptions import (
+    EmptyDataError,
+    InputError,
+    TargetVariableError,
+)
+
+
+def clean_column_labels(data: DataFrame) -> DataFrame:
+    """Makes sure that columns have *meaningful* names.
+
+    When creating a ``DataFrame`` from an ``Iterable``, if no column names
+    are provided, the columns are set as a :class:`~pandas.RangeIndex` —
+    [0, 1, 2, ...] (default).
+
+    This function renames such columns to ['var_1', 'var_2, 'var_3', ...],
+    making references and comparisons much more intuitive.
+
+    It also ensures that column labels are all of similar type (``str``) to
+    allow sorting and the use of string methods.
+
+    Args:
+        data (pandas.DataFrame): Data to inspect and perhaps edit.
+
+    Returns:
+        pandas.DataFrame: The ``data``, with reader-friendly column
+        names.
+    """
+    if isinstance(data.columns, RangeIndex):
+        data.columns = [f"var_{i+1}" for i in data.columns]
+    elif is_numeric_dtype(data.columns):
+        data.columns = [f"var_{i}" for i in data.columns]
+        return data
+    else:
+        data.columns = data.columns.map(str)
+    return data
+
+
+def check_cardinality(target_data: Series, *, threshold: int = 10) -> None:
+    """Assesses whether the ``target_data`` is suitable for grouping values,
+    or has too many unique values (> ``threshold``).
+
+    Args:
+        target_data (pandas.Series): The data intended to group values.
+        threshold (int, optional): Maximum allowable cardinality. Defaults to
+            10.
+
+    Raises:
+        TargetVariableError: If the `target_data` has cardinality outside the
+            acceptable range.
+    """
+    if target_data.nunique() > threshold:
+        message = (
+            f"Target variable '{target_data.name}' not used to group values. "
+            f"It has high cardinality ({target_data.nunique()}) "
+            f"and would clutter graphs."
+        )
+        logging.warning(message)
+        raise TargetVariableError(message)
+
+
+def validate_multivariate_input(data: Iterable) -> DataFrame:
+    """Ensures that *multivariate input data* is of type
+    :class:`pandas.DataFrame`.
+
+    If it isn't, this attempts to explicitly cast it as a ``DataFrame``.
+
+    Columns in the data that are completely empty will be dropped.
+
+    Args:
+        data (Iterable): The data to analyze.
+
+    Raises:
+        InputError: If the ``data`` cannot be cast as a
+            :class:`~pandas.DataFrame`.
+        EmptyDataError: If the ``data`` has no items.
+
+    Returns:
+        pandas.DataFrame: The input data as a DataFrame.
+    """
+    try:
+        data_frame = DataFrame(data)
+    except Exception:
+        raise InputError(
+            f"Expected a pandas.Dataframe object, but got {type(data)}."
+        )
+    # The data should not be empty
+    if len(data_frame) == 0:
+        raise EmptyDataError("No data to process.")
+
+    data_frame = (
+        # Attempt to infer better dtypes for columns.
+        data_frame.infer_objects()
+        # Drop completely empty columns.
+        .dropna(axis=1, how="all")
+    )
+    return clean_column_labels(data_frame)
+
+
+def validate_univariate_input(
+    data: Iterable, *, name: str = None
+) -> Optional[Series]:
+    """Ensures that *univariate input data* is of type :class:`pandas.Series`.
+
+    If it isn't, this attempts to explicitly cast it as a ``Series``.
+
+    Args:
+        data (Iterable): The data to analyze.
+        name (str, optional): The name to assign the data. Defaults
+            to None.
+
+    Raises:
+        InputError: If the ``data`` cannot be cast as a
+            :class:`~pandas.Series`.
+        EmptyDataError: If the ``data`` has no items.
+
+    Returns:
+        Optional[pandas.Series]: The input data as a ``Series``.
+    """
+    if data is None:
+        return None
+    else:
+        try:
+            series = Series(data, name=name)
+        except Exception:
+            raise InputError(
+                f"Expected a one-dimensional sequence, but got {type(data)}."
+            )
+    if series.shape[0] == 0:
+        raise EmptyDataError("No data to process.")
+    else:
+        return series
+
+
+def validate_target_variable(
+    *, data: DataFrame, target_variable: Union[int, str]
+) -> Optional[Series]:
+    """Ensures that the specified *target variable* (column label or index) is
+    present in the data.
+
+    Args:
+        data (DataFrame): The data being analyzed.
+        target_variable (Union[int, str]): A column label or index.
+
+    Raises:
+        TargetVariableError: If the supplied column label does not exist, or
+            the supplied column index is out of bounds.
+
+    Returns:
+        Optional[pandas.Series]: The target variable's data.
+    """
+    if target_variable is None:
+        return None
+    elif isinstance(target_variable, int):
+        try:
+            target_data = data.iloc[:, target_variable]
+        except IndexError:
+            raise TargetVariableError(
+                f"Column index {target_variable} is not in the range"
+                f" [0, {data.columns.size}]."
+            )
+        check_cardinality(target_data)
+        return target_data
+    elif isinstance(target_variable, str):
+        try:
+            target_data = data[target_variable]
+        except KeyError:
+            raise TargetVariableError(
+                f"{target_variable!r} is not in {data.columns.to_list()}"
+            )
+        check_cardinality(target_data)
+        return target_data
+    else:
+        # If target_variable is neither an index(int) or label(str)
+        logging.warning(
+            f"Target variable '{target_variable}' ignored."
+            " Not a valid column index or label."
+        )
+        return None
