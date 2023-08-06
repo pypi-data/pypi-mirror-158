c084fb0a2573d0f75981ffacaa0f8db35ef9f476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:41 +0000

    Add package nonebot-plugin-addFriend/2.0.0/nonebot_plugin_addFriend-2.0.0.tar.gz

---
+++ packages/nonebot-plugin-addFriend/nonebot_plugin_addFriend-2.0.0.tar.gz/nonebot_plugin_addFriend-2.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nonebot_plugin_addFriend",
+    version="2.0.0",
+    author="wziru",
+    description="A plug-in based on nonebot2, which is used to process the request to add QQ friends and QQ groups.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ziru-w/nonebot_plugin_addFriend",
+    project_urls={
+        "Bug Tracker": "https://github.com/ziru-w/nonebot_plugin_addFriend/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=["nonebot_plugin_addFriend"],
+    python_requires=">=3.7",
+    install_requires = [
+        'nonebot2>=2.0.0b1'
+    ]
+)
