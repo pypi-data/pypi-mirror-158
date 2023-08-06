f849df8d614e0e308051e96f22502448be96fd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:51 +0000

    Add package simplechinese/0.2.15/simplechinese-0.2.15.tar.gz

---
+++ packages/simplechinese/simplechinese-0.2.15.tar.gz/simplechinese-0.2.15/simplechinese/__init__.py
@@ -0,0 +1,25 @@
+from . import preprocessing
+from .preprocessing import *
+
+from . import representation
+from .representation import *
+
+from . import visualization
+from .visualization import *
+
+from . import nlp
+from .nlp import *
+
+from . import conversion
+from .conversion import *
+
+from . import pinyin
+from .pinyin import *
+
+from . import generation
+from .generation import *
+
+# from . import synonyms
+# from .synonyms import *
+
+__version__ = "0.2.15"
