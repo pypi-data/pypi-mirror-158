768eb4a4b70442922eec3465093c6d4f7c81d206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:44 +0000

    Add package operate-tools/1.0.3/operate_tools-1.0.3.tar.gz

---
+++ packages/operate-tools/operate_tools-1.0.3.tar.gz/operate_tools-1.0.3/operate_tools/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2022/07/12 09:58:25
+@Author  :   Desire
+@Version :   1.0
+@Desc    :   None
+"""
+
+import os
+import sys
+
+sys.path.insert(1, os.getcwd())
+# here put the import lib
+
+from operate_tools.date import DateTools
