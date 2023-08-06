3f684d540eeee379bcccb49afd4b1ede007ae017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:14 +0000

    Add package pytcli/1.0.0/pytcli-1.0.0-py3-none-any.whl

---
+++ packages/pytcli/pytcli-1.0.0-py3-none-any.whl/pytcli/__debug__.py
@@ -0,0 +1,19 @@
+"""
+@author axiner
+@version v1.0.0
+@created 2021/12/14 20:20
+@abstract
+@description
+@history
+"""
+from pathlib import Path
+
+here = Path(__file__).absolute().parent
+
+
+def t001():
+    print(here)
+
+
+if __name__ == '__main__':
+    t001()
