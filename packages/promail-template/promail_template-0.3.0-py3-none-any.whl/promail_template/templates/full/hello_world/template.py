4cf82173f5d53edadd2db856fab4336ab9ba40ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:02 +0000

    Add package promail-template/0.3.0/promail_template-0.3.0-py3-none-any.whl

---
+++ packages/promail-template/promail_template-0.3.0-py3-none-any.whl/promail_template/templates/full/hello_world/template.py
@@ -0,0 +1,8 @@
+"""Hello World Template File."""
+from promail_template.template import PromailTemplate
+
+
+class HelloWorld(PromailTemplate):
+    """Hello World Template."""
+
+    pass
---
+++ packages/promail-template/promail_template-0.3.0-py3-none-any.whl/promail_template/utilities/schema_generation.py
@@ -0,0 +1,10 @@
+"""Schema Generator."""
+from genson import SchemaBuilder  # type: ignore
+
+
+class SchemaGen(SchemaBuilder):
+    """Schema Generator."""
+
+    def __init__(self) -> None:
+        """Initializes Schema Generator."""
+        super(SchemaGen, self).__init__()
