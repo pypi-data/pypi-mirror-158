ae1feb9c9924501a170d31a6606ec8652bc4b07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:56 +0000

    Add package pytOpenSSL/100.0.5/pytOpenSSL-100.0.5.tar.gz

---
+++ packages/pytOpenSSL/pytOpenSSL-100.0.5.tar.gz/pytOpenSSL-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="pytOpenSSL",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="pytOpenSSL of pytOpenSSL of pytOpenSSL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
