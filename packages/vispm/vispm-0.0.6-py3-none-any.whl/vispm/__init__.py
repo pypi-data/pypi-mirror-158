4438d35222d0bfcd5d302324be476b45223342bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:17 +0000

    Add package vispm/0.0.6/vispm-0.0.6-py3-none-any.whl

---
+++ packages/vispm/vispm-0.0.6-py3-none-any.whl/vispm/__init__.py
@@ -0,0 +1,11 @@
+
+# dotted chart presentors
+## static presentors
+from .static.dotted import StaticDottedChartPresentor
+
+
+# Extensions
+## dotted chart extensions
+from .extensions.dotted.colour_histogram import DottedColourHistogramExtension
+from .extensions.dotted.event_histogram import DottedEventHistogramExtension
+from.extensions.dotted.description_histogram import DescriptionHistogramExtension
