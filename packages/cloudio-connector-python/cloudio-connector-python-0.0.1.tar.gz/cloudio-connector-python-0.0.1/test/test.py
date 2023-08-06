00ad06087ae614d852c580c0376206ab75c8e606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:40 +0000

    Add package cloudio-connector-python/0.0.1/cloudio-connector-python-0.0.1.tar.gz

---
+++ packages/cloudio-connector-python/cloudio-connector-python-0.0.1.tar.gz/cloudio-connector-python-0.0.1/test/test.py
@@ -0,0 +1,33 @@
+import time
+from datetime import datetime
+from threading import Thread
+
+from src.cloudio_connector import AttributeListener, CloudioConnector, AttributeId
+
+
+class Tester(AttributeListener):
+    def __init__(self):
+        cc = CloudioConnector("http://192.168.37.130:8080", "admin", "0yveCJ1SZRvOFS24")
+        cc.add_attribute_listener(self)
+
+        # while True:
+        attr = [
+            AttributeId(uuid=cc.get_uuid('demo'), node='myNode', objects=['myObject'], attribute='mySetPoint'),
+            AttributeId(uuid=cc.get_uuid('demo'), node='myNode', objects=['myObject'], attribute='myInt')]
+        while True:
+            cc.subscribe_to_attribute(attr[0])
+            time.sleep(10)
+            # cc.subscribe_to_attributes([])
+            # time.sleep(10)
+
+
+    def attribute_has_changed(self, attribute: AttributeId, value):
+        print(str(attribute) + ' ' + str(value))
+
+
+if __name__ == '__main__':
+    for i in range(0, 1):
+        Thread(target=Tester).start()
+        time.sleep(0.1)
+    while True:
+        time.sleep(0.01)
