171e066e3d8ec6d11458ca66f7d0d0ac12910748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:59 +0000

    Add package checkov/2.1.45/checkov-2.1.45.tar.gz

---
+++ packages/checkov/checkov-2.1.45.tar.gz/checkov-2.1.45/checkov/common/util/consts.py
@@ -0,0 +1,20 @@
+DEFAULT_EXTERNAL_MODULES_DIR = ".external_modules"
+RESOLVED_MODULE_ENTRY_NAME = "__resolved__"
+
+DEV_API_GET_HEADERS = {
+    'Accept': 'application/json'
+}
+
+DEV_API_POST_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+PRISMA_API_GET_HEADERS = {
+    'Accept': 'application/json; charset=UTF-8'
+}
+
+PARSE_ERROR_FAIL_FLAG = 'CKV_PARSE_ERROR_FAIL'
+
+PRISMA_PLATFORM = 'Prisma Cloud Code Security'
+BRIDGECREW_PLATFORM = 'Bridgecrew'
