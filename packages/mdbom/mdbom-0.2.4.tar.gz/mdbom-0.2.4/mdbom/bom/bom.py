93e75550cbf22e3e6c802737b2725c717ce8fde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:36 +0000

    Add package mdbom/0.2.4/mdbom-0.2.4.tar.gz

---
+++ packages/mdbom/mdbom-0.2.4.tar.gz/mdbom-0.2.4/mdbom/bom/bom.py
@@ -0,0 +1,18 @@
+"""General stuff for handling BOM files."""
+
+from typing import NamedTuple
+
+
+class ProcessingError(RuntimeError):
+    """Processing error for raising processing specific errors."""
+
+
+class Package(NamedTuple):
+    """A representation of a package."""
+
+    name: str
+    version: str
+    kind: str
+    licenses: str
+    purl: str
+    url: str
