ccf35e316d884226995a1e13fed6e5557b8a056b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:14 +0000

    Add package ansible/6.1.0/ansible-6.1.0.tar.gz

---
+++ packages/ansible/ansible-6.1.0.tar.gz/ansible-6.1.0/build-ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+VERSION="6.1.0"
+MAJOR="6"
+
+# For idempotency, remove build data or built output first
+rm -rf ansible-build-data built
+
+pip3 install --user --upgrade antsibull
+git clone https://github.com/ansible-community/ansible-build-data.git
+mkdir -p built collection-cache
+BUILD_DATA_DIR="ansible-build-data/${MAJOR}"
+BUILDFILE="ansible-${MAJOR}.build"
+DEPSFILE="ansible-${VERSION}.deps"
+
+antsibull-build rebuild-single "${VERSION}" --collection-cache collection-cache --data-dir "${BUILD_DATA_DIR}" --build-file "${BUILDFILE}" --deps-file "${DEPSFILE}" --sdist-dir built --debian
+
+echo "The result can be found in built/ansible-${VERSION}.tar.gz"
+
+# pip3 install --user twine
+# twine upload "built/ansible-${VERSION}.tar.gz"
