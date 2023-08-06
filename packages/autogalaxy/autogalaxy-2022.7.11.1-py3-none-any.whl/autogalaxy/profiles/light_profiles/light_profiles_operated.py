40166309273508ba86252f13c027647ad2318d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:32 +0000

    Add package autogalaxy/2022.7.11.1/autogalaxy-2022.7.11.1-py3-none-any.whl

---
+++ packages/autogalaxy/autogalaxy-2022.7.11.1-py3-none-any.whl/autogalaxy/profiles/light_profiles/light_profiles_operated.py
@@ -0,0 +1,16 @@
+from autogalaxy.profiles.light_profiles import light_profiles as lp
+
+
+class LightProfileOperated:
+
+    pass
+
+
+class EllGaussian(lp.EllGaussian, LightProfileOperated):
+
+    pass
+
+
+class EllSersic(lp.EllSersic, LightProfileOperated):
+
+    pass
