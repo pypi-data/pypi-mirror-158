1a23e26a058adc3086215bb1310a3991e888198e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:58 +0000

    Add package data-connectors/0.20.19/data_connectors-0.20.19-py3-none-any.whl

---
+++ packages/data-connectors/data_connectors-0.20.19-py3-none-any.whl/data_connectors/gsheets.py
@@ -0,0 +1,36 @@
+import os
+import pygsheets
+import pandas as pd
+
+from .creds import load_creds
+load_creds()
+
+class Gsheets:
+    """
+    Pass in a service account file path and query data stored in Google Sheets
+    Example: Gsheets('SPREADSHEET').query('WORKSHEET')
+    """
+    def __init__(self, spreadsheet):
+        self.client = pygsheets.authorize(service_file=(os.getenv("GSHEETS_SERVICE_ACCOUNT")))
+        self.spreadsheet_key = os.getenv(spreadsheet)
+        self.spreadsheet = self.client.open_by_key(self.spreadsheet_key)
+
+    def query(self, worksheet, start_cell=None, end_cell=None):
+        """
+        Returns results of spreadsheet, worksheet as a pandas DataFrame
+        Spreadsheet is stored as env var
+        Worksheet is passed in as plain text
+        """        
+        ws = self.spreadsheet.worksheet_by_title(worksheet)
+        df = ws.get_as_df(start=start_cell, end=end_cell)
+        print(f"{self.spreadsheet.title}.{worksheet}: Downloaded {df.shape} shape")
+        return df
+
+    # If get_as_df does not work, use this instead
+    def select_all_values(self, worksheet):
+        return pd.DataFrame(
+            self.spreadsheet.worksheet_by_title(worksheet).get_all_records()
+        )
+
+    def write_df(self, df, worksheet, start_cell='A1'):
+        self.spreadsheet.worksheet_by_title(worksheet).set_dataframe(df, start=start_cell)
