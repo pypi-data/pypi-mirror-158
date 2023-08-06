e46b5bb7e0a30ea1fba3b2cfd00e75a5e0b39d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:22 +0000

    Add package meditationtimer/1.0.0/meditationtimer-1.0.0.tar.gz

---
+++ packages/meditationtimer/meditationtimer-1.0.0.tar.gz/meditationtimer-1.0.0/main.py
@@ -0,0 +1,48 @@
+import click
+import time
+import math
+
+defaultargs = {
+ "no_args_is_help" : False,
+ "context_settings": dict(help_option_names=['-h', '--help'])
+}
+
+totalargs = {
+	"default": 300,
+	"help": "time in seconds"
+}
+
+delayargs = {
+	"default": 10,
+	"help": "Delay the countdown, to prepare for the session."
+}
+
+@click.command(**defaultargs)
+@click.option('-t', '--total', **totalargs)
+@click.option('-d', '--delay', **delayargs)
+def progressbar(total, delay):	
+	with click.progressbar(
+		range(delay),
+		show_percent=False,
+		fill_char= "·",
+		empty_char=" ",
+		bar_template='%(label)s %(bar)s %(info)s',
+		width=0, #full terminal width
+		label="Preparing"
+		) as t:
+		for s in t:
+			time.sleep(1)
+
+	with click.progressbar(
+		range(total),
+		show_percent=False,
+		fill_char= "·",
+		empty_char=" ",
+		bar_template='%(label)s %(bar)s %(info)s',
+		width=0, #full terminal width
+		label="Meditating"
+		) as t:
+		for s in t:
+			time.sleep(1)
+
+	click.echo("\a")
