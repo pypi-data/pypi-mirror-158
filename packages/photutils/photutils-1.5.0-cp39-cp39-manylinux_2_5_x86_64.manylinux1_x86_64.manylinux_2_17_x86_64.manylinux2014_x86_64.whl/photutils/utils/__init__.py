a8f5861d520f2e60991daa81a481396da86308c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:06 +0000

    Add package photutils/1.5.0/photutils-1.5.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/photutils/photutils-1.5.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/photutils/utils/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This subpackage provides general-purpose utility functions.
+"""
+
+from .colormaps import *  # noqa
+from .cutouts import *  # noqa
+from .errors import *  # noqa
+from .exceptions import *  # noqa
+from .footprints import *  # noqa
+from .interpolation import *  # noqa
