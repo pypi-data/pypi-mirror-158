90aaaa1a2da993081d133331f3602283d6f59f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:51 +0000

    Add package distributions-SH/0.1/distributions_SH-0.1.tar.gz

---
+++ packages/distributions-SH/distributions_SH-0.1.tar.gz/distributions_SH-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_SH',
+      version='0.1',
+      description='General distribution, Gaussian distributions, and Binomial distribution ',
+      packages=['distributions_SH'],
+      author='Scarlett Huang',
+      author_email='scarlett0413@outlook.com',
+      zip_safe=False)
