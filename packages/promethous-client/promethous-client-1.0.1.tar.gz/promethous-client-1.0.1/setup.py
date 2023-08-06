ca980e5fce685a39513204159c247bc0fee1ce5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:51:28 +0000

    Add package promethous-client/1.0.1/promethous-client-1.0.1.tar.gz

---
+++ packages/promethous-client/promethous-client-1.0.1.tar.gz/promethous-client-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='promethous-client',
+    version='1.0.1',
+    py_modules=['promethous-client'],
+    author='holeunique',
+    author_email='617240374@qq.com',
+    description='模块描述',
+    long_description='',  # 这里是文档内容, 读取readme文件
+    packages=find_packages(),
+    python_requires='>=3.6',  # 这里指定python版本号必须大于3.6才可以安装
+    install_requires=[]  # 我们的模块所用到的依赖, 这里指定的话, 用户安装你的模块时, 会自动安装这些依赖
+)
