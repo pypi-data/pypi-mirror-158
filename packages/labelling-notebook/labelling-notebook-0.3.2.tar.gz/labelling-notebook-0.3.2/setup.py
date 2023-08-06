4859721d269a29adf2b62bb472b14a9f20db1eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:40 +0000

    Add package labelling-notebook/0.3.2/labelling-notebook-0.3.2.tar.gz

---
+++ packages/labelling-notebook/labelling-notebook-0.3.2.tar.gz/labelling-notebook-0.3.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['labelling', 'labelling.notebook']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask>=2.0.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['label = labelling.__main__:main']}
+
+setup_kwargs = {
+    'name': 'labelling-notebook',
+    'version': '0.3.2',
+    'description': 'An image annotation or labelling tool for small project',
+    'long_description': None,
+    'author': 'Wanasit ',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
