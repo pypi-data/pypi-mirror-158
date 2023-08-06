a2858eb0bd8161c575fd2c356098024c637940ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:56 +0000

    Add package osidb-bindings/2.0.0/osidb-bindings-2.0.0.tar.gz

---
+++ packages/osidb-bindings/osidb-bindings-2.0.0.tar.gz/osidb-bindings-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="osidb-bindings",
+    version="2.0.0",
+    author="Jakub Frejlach, Red Hat Product Security",
+    author_email="jfrejlac@redhat.com",
+    description="Python bindings for accessing OSIDB API",
+    url="https://github.com/RedHatProductSecurity/osidb-bindings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["attrs", "requests", "requests-gssapi", "python-dateutil"],
+)
