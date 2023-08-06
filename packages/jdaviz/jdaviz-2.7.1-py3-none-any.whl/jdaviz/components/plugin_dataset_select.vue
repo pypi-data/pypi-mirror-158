8e29ea1d280883ba90562855f0b3cbd15014029b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:34 +0000

    Add package jdaviz/2.7.1/jdaviz-2.7.1-py3-none-any.whl

---
+++ packages/jdaviz/jdaviz-2.7.1-py3-none-any.whl/jdaviz/components/plugin_dataset_select.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+  <v-row v-if="items.length > 1 || show_if_single_entry">
+    <v-select
+      :items="items"
+      v-model="selected"
+      @change="$emit('update:selected', $event)"
+      :label="label ? label : 'Data'"
+      :hint="hint ? hint : 'Select data.'"
+      :rules="rules ? rules : []"
+      item-text="label"
+      item-value="label"
+      persistent-hint
+    >
+    <template slot="selection" slot-scope="data">
+      <div class="single-line">
+        <span>
+          {{ data.item.label }}
+        </span>
+      </div>
+    </template>
+    <template slot="item" slot-scope="data">
+      <div class="single-line">
+        <span>
+          {{ data.item.label }}
+        </span>
+      </div>
+    </template>
+   </v-select>
+  </v-row>
+ </div>
+</template>
+<script>
+module.exports = {
+  props: ['items', 'selected', 'label', 'hint', 'rules', 'show_if_single_entry']
+};
+</script>
+
+<style>
+  .single-line {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+  }
+</style>
