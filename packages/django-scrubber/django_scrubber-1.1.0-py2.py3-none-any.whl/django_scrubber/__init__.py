a5db3029aa62895e6f424729abad351c5f0b456d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:32 +0000

    Add package django-scrubber/1.1.0/django_scrubber-1.1.0-py2.py3-none-any.whl

---
+++ packages/django-scrubber/django_scrubber-1.1.0-py2.py3-none-any.whl/django_scrubber/__init__.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+__version__ = '1.1.0'
+
+defaults = {
+    'SCRUBBER_RANDOM_SEED': 42,  # we prefer idempotent scrubbing
+    'SCRUBBER_ENTRIES_PER_PROVIDER': 1000,
+    'SCRUBBER_GLOBAL_SCRUBBERS': {},
+    'SCRUBBER_SKIP_UNMANAGED': True,
+    'SCRUBBER_APPS_LIST': None,
+    'SCRUBBER_ADDITIONAL_FAKER_PROVIDERS': [],
+    'SCRUBBER_FAKER_LOCALE': None,
+    'SCRUBBER_MAPPING': dict(),
+}
+
+
+# TODO: replace with ChainMap now that we only support py3
+def settings_with_fallback(key):
+    return getattr(settings, key, defaults[key])
+
+
+class ScrubberInitError(Exception):
+    pass
