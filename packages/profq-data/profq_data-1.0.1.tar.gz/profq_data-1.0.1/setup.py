f3598638909ddc98a6f2813c56502f4638f65488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:25 +0000

    Add package profq-data/1.0.1/profq_data-1.0.1.tar.gz

---
+++ packages/profq-data/profq_data-1.0.1.tar.gz/profq_data-1.0.1/setup.py
@@ -0,0 +1,27 @@
+# sourcery skip: path-read
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A library for data structures.'
+
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="profq_data",
+    version=VERSION,
+    author="ProfessorQu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['python', 'data structures', 'algorithms'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
