eb648f17b6274d554141d9e6319198c8b6c3e5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:48 +0000

    Add package plugp100/2.1.15/plugp100-2.1.15.tar.gz

---
+++ packages/plugp100/plugp100-2.1.15.tar.gz/plugp100-2.1.15/plugp100/__init__.py
@@ -0,0 +1,27 @@
+# import actual context
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Vendoring: add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, '_vendor')
+if os.path.exists(vendor_dir):
+    for vendor_path in os.listdir(vendor_dir):
+        sys.path.append(os.path.join(vendor_dir, vendor_path))
+
+from .api import TapoDeviceState, TapoApiClient
+from .discover import TapoApiDiscover
+from .core.params import SwitchParams, LightParams
+
+__all__ = [
+    "TapoApiClient",
+    "TapoDeviceState",
+    "SwitchParams",
+    "LightParams",
+    "TapoApiDiscover"
+]
+
+from . import _version
+__version__ = _version.get_versions()['version']
