47d580d0944b9ce44bf68d94773d13b8eecca284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:55 +0000

    Add package tank-config/0.0.1/tank_config-0.0.1.tar.gz

---
+++ packages/tank-config/tank_config-0.0.1.tar.gz/tank_config-0.0.1/src/__init__.py
@@ -0,0 +1,6 @@
+raise ImportError(
+    """This package was installed from pypi.org, but it is a private package.
+    This is a placeholder to avoid dependency confusion.
+    Please specify an --extra-index-url in your pip command to resolve the package correctly.
+    """
+)
