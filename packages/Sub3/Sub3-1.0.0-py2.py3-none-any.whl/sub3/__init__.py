e58ee4c569746acc8d0833ad52cd73fbe8621166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:30 +0000

    Add package Sub3/1.0.0/Sub3-1.0.0-py2.py3-none-any.whl

---
+++ packages/Sub3/Sub3-1.0.0-py2.py3-none-any.whl/sub3/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Sub3."""
+
+from sub3.main import Sub3
+from sub3.rpc_maker import RPCMaker
+
+__all__ = ["Sub3", "RPCMaker"]
+
+__author__ = """SpeakinTelnet"""
+__email__ = "gui.lac@protonmail.com"
+__version__ = "1.0.0"
