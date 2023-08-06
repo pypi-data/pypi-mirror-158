46895c2057ded100309d0f0fe2ff80b93ea190f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:19:33 +0000

    Add package xclingo/2.0b15.post3/xclingo-2.0b15.post3-py3-none-any.whl

---
+++ packages/xclingo/xclingo-2.0b15.post3-py3-none-any.whl/xclingo/preprocessor/_pipeline.py
@@ -0,0 +1,32 @@
+from ._preprocessor import Preprocessor
+from ._preprocessor import XClingoAnnotationPreprocessor
+from ._preprocessor import ConstraintRelaxer
+from ._preprocessor import XClingoPreprocessor
+
+
+class PreprocessorPipeline:
+    def __init__(self):
+        self.preprocessors = list()
+
+    def register_preprocessor(self, preprocessor: Preprocessor):
+        self.preprocessors.append(preprocessor)
+
+    def translate(self, name: str, program: str):
+        translation = program
+        for p in self.preprocessors:
+            translation = p.process_program(translation)
+        return f"%%%%%%%% {name} %%%%%%%%\n{translation}"
+
+
+class ConstraintRelaxerPipeline(PreprocessorPipeline):
+    def __init__(self):
+        super().__init__()
+        self.register_preprocessor(XClingoAnnotationPreprocessor())
+        self.register_preprocessor(ConstraintRelaxer(preserve_labels=False))
+
+
+class DefaultExplainingPipeline(PreprocessorPipeline):
+    def __init__(self):
+        super().__init__()
+        self.register_preprocessor(XClingoAnnotationPreprocessor())
+        self.register_preprocessor(XClingoPreprocessor())
