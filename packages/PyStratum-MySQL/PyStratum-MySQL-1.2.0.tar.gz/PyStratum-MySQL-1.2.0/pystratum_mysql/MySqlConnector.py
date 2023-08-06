985338decc0520d1892a1c61e362fac958078a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:57 +0000

    Add package PyStratum-MySQL/1.2.0/PyStratum-MySQL-1.2.0.tar.gz

---
+++ packages/PyStratum-MySQL/PyStratum-MySQL-1.2.0.tar.gz/PyStratum-MySQL-1.2.0/pystratum_mysql/MySqlConnector.py
@@ -0,0 +1,36 @@
+import abc
+
+from mysql.connector import MySQLConnection
+
+
+class MySqlConnector:
+    """
+    Interface for classes for connecting to a MySql instances.
+    """
+    # ------------------------------------------------------------------------------------------------------------------
+    @abc.abstractmethod
+    def connect(self) -> MySQLConnection:
+        """
+        Connects to the MySql instance.
+        """
+        raise NotImplementedError()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @abc.abstractmethod
+    def disconnect(self) -> None:
+        """
+        Disconnects from the MySql instance.
+        """
+        raise NotImplementedError()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @abc.abstractmethod
+    def is_alive(self) -> bool:
+        """
+        Returns whether Python is (still) connected to a MySQL or MariaDB instance.
+
+        :rtype: bool
+        """
+        raise NotImplementedError()
+
+# ----------------------------------------------------------------------------------------------------------------------
