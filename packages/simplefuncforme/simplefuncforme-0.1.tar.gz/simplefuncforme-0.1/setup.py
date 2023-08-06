6eec8bd207ec7561937fafc831029718388bbdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:53 +0000

    Add package simplefuncforme/0.1/simplefuncforme-0.1.tar.gz

---
+++ packages/simplefuncforme/simplefuncforme-0.1.tar.gz/simplefuncforme-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="simplefuncforme",
+    version="0.1",
+    description="THis is a simple pack",
+    author="python devlopers",
+    maintainer="pypi devlopers",
+    long_description="This is simple python test program.\n\n Installation command :- sudo python -m pip install simplefuncforme"
+)
