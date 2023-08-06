b8455577025c37b0b84c25cf6a68e2baa93a7a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:29 +0000

    Add package skrl/0.7.0/skrl-0.7.0.tar.gz

---
+++ packages/skrl/skrl-0.7.0.tar.gz/skrl-0.7.0/skrl/models/torch/categorical.py
@@ -0,0 +1,87 @@
+from typing import Union, Tuple
+
+import gym
+
+import torch
+from torch.distributions import Categorical
+
+from . import Model
+
+
+class CategoricalModel(Model):
+    def __init__(self, 
+                 observation_space: Union[int, Tuple[int], gym.Space, None] = None, 
+                 action_space: Union[int, Tuple[int], gym.Space, None] = None, 
+                 device: Union[str, torch.device] = "cuda:0",
+                 unnormalized_log_prob: bool = True) -> None:
+        """Categorical model (stochastic model)
+
+        :param observation_space: Observation/state space or shape (default: None).
+                                  If it is not None, the num_observations property will contain the size of that space
+        :type observation_space: int, tuple or list of integers, gym.Space or None, optional
+        :param action_space: Action space or shape (default: None).
+                             If it is not None, the num_actions property will contain the size of that space
+        :type action_space: int, tuple or list of integers, gym.Space or None, optional
+        :param device: Device on which a torch tensor is or will be allocated (default: "cuda:0")
+        :type device: str or torch.device, optional
+        :param unnormalized_log_prob: Flag to indicate how to be interpreted the model's output (default: True).
+                                      If True, the model's output is interpreted as unnormalized log probabilities 
+                                      (it can be any real number), otherwise as normalized probabilities 
+                                      (the output must be non-negative, finite and have a non-zero sum)
+        :type unnormalized_log_prob: bool, optional
+        """
+        super(CategoricalModel, self).__init__(observation_space, action_space, device)
+
+        self._unnormalized_log_prob = unnormalized_log_prob
+
+        self._distribution = None
+
+    def act(self, 
+            states: torch.Tensor, 
+            taken_actions: Union[torch.Tensor, None] = None, 
+            inference=False) -> Tuple[torch.Tensor]:
+        """Act stochastically in response to the state of the environment
+
+        :param states: Observation/state of the environment used to make the decision
+        :type states: torch.Tensor
+        :param taken_actions: Actions taken by a policy to the given states (default: None).
+                              The use of these actions only makes sense in critical models, e.g.
+        :type taken_actions: torch.Tensor or None, optional
+        :param inference: Flag to indicate whether the model is making inference (default: False).
+                          If True, the returned tensors will be detached from the current graph
+        :type inference: bool, optional
+
+        :return: Action to be taken by the agent given the state of the environment.
+                 The tuple's components are the actions, the log of the probability density function and the model's output
+        :rtype: tuple of torch.Tensor
+        """
+        # map from states/observations to normalized probabilities or unnormalized log probabilities
+        if self._instantiator_net is None:
+            output = self.compute(states.to(self.device), 
+                                  taken_actions.to(self.device) if taken_actions is not None else taken_actions)
+        else:
+            output = self._get_instantiator_output(states.to(self.device), \
+                taken_actions.to(self.device) if taken_actions is not None else taken_actions)
+
+        # unnormalized log probabilities
+        if self._unnormalized_log_prob:
+            self._distribution = Categorical(logits=output)
+        # normalized probabilities
+        else:
+            self._distribution = Categorical(probs=output)
+        
+        # actions and log of the probability density function
+        actions = self._distribution.sample()
+        log_prob = self._distribution.log_prob(actions if taken_actions is None else taken_actions.view(-1))
+
+        if inference:
+            return actions.unsqueeze(-1).detach(), log_prob.unsqueeze(-1).detach(), output.detach()
+        return actions.unsqueeze(-1), log_prob.unsqueeze(-1), output
+
+    def distribution(self) -> torch.distributions.Categorical:
+        """Get the current distribution of the model
+
+        :return: Distribution of the model
+        :rtype: torch.distributions.Categorical
+        """
+        return self._distribution
---
+++ packages/skrl/skrl-0.7.0.tar.gz/skrl-0.7.0/skrl/trainers/torch/sequential.py
@@ -0,0 +1,174 @@
+from typing import Union, List
+
+import torch
+
+from ...envs.torch import Wrapper
+from ...agents.torch import Agent
+
+from . import Trainer
+
+
+class SequentialTrainer(Trainer):
+    def __init__(self, 
+                 cfg: dict, 
+                 env: Wrapper, 
+                 agents: Union[Agent, List[Agent], List[List[Agent]]], 
+                 agents_scope : List[int] = []) -> None:
+        """Sequential trainer
+        
+        Train agents sequentially (i.e., one after the other in each interaction with the environment)
+
+        :param cfg: Configuration dictionary
+        :type cfg: dict
+        :param env: Environment to train on
+        :type env: skrl.env.torch.Wrapper
+        :param agents: Agents to train
+        :type agents: Union[Agent, List[Agent]]
+        :param agents_scope: Number of environments for each agent to train on (default: [])
+        :type agents_scope: tuple or list of integers
+        """
+        super().__init__(cfg, env, agents, agents_scope)
+
+        # init agents
+        if self.num_agents > 1:
+            for agent in self.agents:
+                agent.init()
+        else:
+            self.agents.init()
+
+    def train(self) -> None:
+        """Train the agents sequentially
+
+        This method executes the following steps in loop:
+
+        - Pre-interaction (sequentially)
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Record transitions (sequentially)
+        - Post-interaction (sequentially)
+        - Reset environments
+        """
+        # single agent
+        if self.num_agents == 1:
+            self.single_agent_train()
+            return
+
+        # reset env
+        states = self.env.reset()
+
+        for timestep in range(self.initial_timestep, self.timesteps):
+            # show progress
+            self.show_progress(timestep=timestep, timesteps=self.timesteps)
+
+            # pre-interaction
+            for agent in self.agents:
+                agent.pre_interaction(timestep=timestep, timesteps=self.timesteps)
+            
+            # compute actions
+            with torch.no_grad():
+                actions = torch.vstack([agent.act(states[scope[0]:scope[1]], 
+                                                  inference=True,
+                                                  timestep=timestep, 
+                                                  timesteps=self.timesteps)[0] \
+                                        for agent, scope in zip(self.agents, self.agents_scope)])
+            
+            # step the environments
+            next_states, rewards, dones, infos = self.env.step(actions)
+            
+            # render scene
+            if not self.headless:
+                self.env.render()
+
+            # record the environments' transitions
+            with torch.no_grad():
+                for agent, scope in zip(self.agents, self.agents_scope):
+                    agent.record_transition(states=states[scope[0]:scope[1]], 
+                                            actions=actions[scope[0]:scope[1]], 
+                                            rewards=rewards[scope[0]:scope[1]], 
+                                            next_states=next_states[scope[0]:scope[1]], 
+                                            dones=dones[scope[0]:scope[1]],
+                                            infos=infos,
+                                            timestep=timestep,
+                                            timesteps=self.timesteps)
+            
+            # post-interaction
+            for agent in self.agents:
+                agent.post_interaction(timestep=timestep, timesteps=self.timesteps)
+
+            # reset environments
+            with torch.no_grad():
+                if dones.any():
+                    states = self.env.reset()
+                else:
+                    states.copy_(next_states)
+
+        # close the environment
+        self.env.close()
+
+    def eval(self) -> None:
+        """Evaluate the agents sequentially
+
+        This method executes the following steps in loop:
+        
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Reset environments
+        """
+        # single agent
+        if self.num_agents == 1:
+            self.single_agent_eval()
+            return
+
+        # reset env
+        states = self.env.reset()
+
+        for timestep in range(self.initial_timestep, self.timesteps):
+            # show progress
+            self.show_progress(timestep=timestep, timesteps=self.timesteps)
+            
+            # compute actions
+            with torch.no_grad():
+                actions = torch.vstack([agent.act(states[scope[0]:scope[1]], 
+                                                  inference=True,
+                                                  timestep=timestep, 
+                                                  timesteps=self.timesteps)[0] \
+                                        for agent, scope in zip(self.agents, self.agents_scope)])
+            
+            # step the environments
+            next_states, rewards, dones, infos = self.env.step(actions)
+            
+            # render scene
+            if not self.headless:
+                self.env.render()
+            
+            with torch.no_grad():
+                # write data to TensorBoard
+                for agent, scope in zip(self.agents, self.agents_scope):
+                    super(type(agent), agent).record_transition(states=states[scope[0]:scope[1]], 
+                                                                actions=actions[scope[0]:scope[1]], 
+                                                                rewards=rewards[scope[0]:scope[1]], 
+                                                                next_states=next_states[scope[0]:scope[1]], 
+                                                                dones=dones[scope[0]:scope[1]],
+                                                                infos=infos,
+                                                                timestep=timestep,
+                                                                timesteps=self.timesteps)
+                    super(type(agent), agent).post_interaction(timestep=timestep, timesteps=self.timesteps)
+
+                # reset environments
+                if dones.any():
+                    states = self.env.reset()
+                else:
+                    states.copy_(next_states)
+
+        # close the environment
+        self.env.close()
+
+    def start(self) -> None:
+        """Start training
+
+        This method is deprecated in favour of the '.train()' method 
+        """
+        super().start()
+        self.train()
