73f69e35e6b9c960921cb4b1e3a56fdfd0eedae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:09 +0000

    Add package Python-EasyGraph/0.2a39/Python_EasyGraph-0.2a39-cp38-cp38-win_amd64.whl

---
+++ packages/Python-EasyGraph/Python_EasyGraph-0.2a39-cp38-cp38-win_amd64.whl/easygraph/__init__.py
@@ -0,0 +1,14 @@
+import easygraph.classes
+from easygraph.classes import *
+
+from easygraph.utils import *
+import easygraph.utils
+
+import easygraph.datasets
+from easygraph.datasets import *
+
+import easygraph.functions
+from easygraph.functions import *
+
+import easygraph.readwrite
+from easygraph.readwrite import *
