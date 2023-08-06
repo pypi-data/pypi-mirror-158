b787165cbda3afd6bc733f0b4c5debdb4d6b30ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:00 +0000

    Add package easy-mpl/0.20.5/easy_mpl-0.20.5-py3-none-any.whl

---
+++ packages/easy-mpl/easy_mpl-0.20.5-py3-none-any.whl/easy_mpl/__init__.py
@@ -0,0 +1,20 @@
+
+from ._main import plot
+from ._main import pie
+from ._main import bar_chart
+from ._main import regplot
+from ._main import imshow
+from ._main import hist
+from ._main import scatter
+from ._main import contour
+from ._main import dumbbell_plot
+from ._main import ridge
+from ._main import parallel_coordinates
+from ._taylor_plot import taylor_plot
+from ._main import lollipop_plot
+from ._main import circular_bar_plot
+
+from .utils import process_axis
+
+
+__version__ = "0.20.5"
