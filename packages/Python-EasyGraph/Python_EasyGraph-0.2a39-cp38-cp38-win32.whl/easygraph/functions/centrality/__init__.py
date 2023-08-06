4d25a47b51e101afdc87918035dfa681c3a87454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:09 +0000

    Add package Python-EasyGraph/0.2a39/Python_EasyGraph-0.2a39-cp38-cp38-win32.whl

---
+++ packages/Python-EasyGraph/Python_EasyGraph-0.2a39-cp38-cp38-win32.whl/easygraph/functions/centrality/__init__.py
@@ -0,0 +1,4 @@
+from .flowbetweenness import *
+from .degree import *
+from .clossness import *
+from .betweenness import *
---
+++ packages/Python-EasyGraph/Python_EasyGraph-0.2a39-cp38-cp38-win32.whl/easygraph/functions/graph_embedding/__init__.py
@@ -0,0 +1,5 @@
+from .node2vec import *
+from .line import LINE
+from .deepwalk import *
+from .sdne import SDNE
+from .NOBE import *
