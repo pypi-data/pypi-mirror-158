c009e8dba69bb3a1c15cdfa2be53b77e9b51b746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:33 +0000

    Add package initapp-test/4.0.12/initapp_test-4.0.12.tar.gz

---
+++ packages/initapp-test/initapp_test-4.0.12.tar.gz/initapp_test-4.0.12/match_cost/match_conf.py
@@ -0,0 +1,74 @@
+import yaml
+import os
+
+BASE_DIR = os.path.dirname(__file__).split('match')[0]
+match_conf_path = os.path.join(BASE_DIR, 'tmp_img', 'match_config.yaml')
+conf_path = os.path.join(BASE_DIR, 'initcost_conf', 'config.yaml')
+
+
+def get_yaml(file_path):
+    with open(file_path, encoding='utf-8') as f:
+        try:
+            y_instance = yaml.load(f, Loader=yaml.SafeLoader)
+            return y_instance
+        except yaml.YAMLError as e:
+            print(e)
+
+
+def get_config_value(*args) -> str:
+    """
+    在yaml中根据key获取对应的value
+    :param args: 对应的key值
+    :return:
+    """
+    obj = get_yaml(conf_path)
+    for arg in args:
+        obj = obj.get(arg) if arg else None
+    return obj
+
+
+pkg_name = get_config_value('pkg_name')
+
+type = get_config_value('test_type')
+
+test_platform = get_config_value('test_platform')
+
+
+def get_match_config_value(*args) -> str:
+    """
+    在yaml中根据key获取对应的value
+    :param args: 对应的key值
+    :return:
+    """
+    obj = get_yaml(match_conf_path)
+    for arg in args:
+        obj = obj.get(arg) if arg else None
+    return obj
+
+
+def get_feed_tab_text(pkg_name):
+    tab = get_match_config_value('feed_tab', pkg_name)
+    if tab:
+        return tab.split(",")
+    return None
+
+def get_ad_text(pkg_name):
+    tab = get_match_config_value('ad_text', pkg_name)
+    if tab:
+        return tab.split(",")
+    return None
+
+def update_config_value(*args, value=None):
+    """
+    更新yaml中key对应的value
+    :param args: 对应的key值
+    :param value:
+    """
+    obj = get_yaml(conf_path)
+    tmp = obj
+    for arg in args[:-1]:
+        tmp = tmp[arg]
+    tmp[args[-1]] = value
+    # print('update config: key:{}, value:{}'.format(str(args), value))
+    with open(conf_path, encoding='utf-8', mode='w') as fw:
+        yaml.dump(obj, fw, sort_keys=False)
---
+++ packages/initapp-test/initapp_test-4.0.12.tar.gz/initapp_test-4.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='initapp_test',
+    version='4.0.12',
+    include_package_data=True,
+    packages=['initcost_conf', 'util','match_cost','.'],
+    install_requires=[
+        'numpy',
+        'ffmpeg_python==0.2.0',
+        'opencv_python',
+        'openpyxl',
+        'pandas',
+        'xlrd',
+        'pytesseract==0.3.8',
+        'ffmpeg==1.4',
+        'PyYAML',
+    ],
+    #packages= find_packages(),
+    url='',
+    license='',
+    author='wangkejun',
+    author_email='446093036@qq.com',
+    description='initapp_test'
+)
+# scp -i pbox_id_rsa_kuaishou -P 41022 hs@117.54.227.102:/home/hs/pboxData/2022-07-05/RedmiNote8Pro/pro/pro1.mp4 /Users/wangkejun
+# ssh -i pbox_id_rsa_kuaishou  hs@kuaishou-id-jkt-0.headspin.io -p 41022
