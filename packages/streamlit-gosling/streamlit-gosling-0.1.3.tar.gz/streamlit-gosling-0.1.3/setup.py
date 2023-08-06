0e575c122c8462e1b317790bbb4280e2258dd2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:40 +0000

    Add package streamlit-gosling/0.1.3/streamlit-gosling-0.1.3.tar.gz

---
+++ packages/streamlit-gosling/streamlit-gosling-0.1.3.tar.gz/streamlit-gosling-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from os.path import dirname
+from os.path import join
+import setuptools
+
+
+def readme() -> str:
+    """Utility function to read the README file.
+    :return: content of README.md
+    """
+    return open(join(dirname(__file__), "README.md")).read()
+
+
+setuptools.setup(
+    name="streamlit-gosling",
+    version="0.1.3",
+    author="Qianwen Wang",
+    author_email="qianwen.april.wang@gmail.com",
+    description="Gosling custom component for Streamlit",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gosling-lang/streamlit-gosling",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+        "gosling >= 0.0.9"
+    ]
+)
