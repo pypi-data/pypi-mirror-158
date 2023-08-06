9e4587e8f462af8537957f39ddce100664543d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package stython/0.1.17/stython-0.1.17.tar.gz

---
+++ packages/stython/stython-0.1.17.tar.gz/stython-0.1.17/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='stython',
+    version='0.1.17',
+    description='The Stython package',
+    author='Arun Kapila',
+    author_email='starshootercity@gmail.com',
+    url='https://github.com/cometcake575/stython',
+    license=open('LICENSE', 'r').read(),
+    packages=['stython']
+)
