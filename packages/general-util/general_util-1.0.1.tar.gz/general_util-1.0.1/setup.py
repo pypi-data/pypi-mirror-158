60fa7c8b17aa52fee874abb094e10b8a32d11484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:41 +0000

    Add package general-util/1.0.1/general_util-1.0.1.tar.gz

---
+++ packages/general-util/general_util-1.0.1.tar.gz/general_util-1.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'General utilities for python scripts.'
+
+# Read the contents of README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Add resource links
+project_urls = {
+  'Documentation': 'https://johnhal.gitlab.io/util_python',
+  'Repository': 'https://gitlab.com/johnhal/util_python'
+}
+
+# Setting up
+setup(
+    name="general_util",
+    version=VERSION,
+    author="John Halazonetis",
+    author_email="<john.halazonetis@icloud.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=open("requirements.txt", "r").read().split("\n"),
+    keywords=['python', 'utilities'],
+    project_urls = project_urls,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
