f191017726dceabb59a8dad3ef4360d45323504b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:08 +0000

    Add package py-wrike-v4/0.13/py_wrike_v4-0.13.tar.gz

---
+++ packages/py-wrike-v4/py_wrike_v4-0.13.tar.gz/py_wrike_v4-0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+# read the contents of README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="py_wrike_v4",
+    version="0.13",
+    license="MIT",
+    author="Govinda Hosein",
+    author_email="raziel619dev@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/Raziel619/py_wrike_v4",
+    keywords="wrike",
+    install_requires=[
+        "certifi==2022.6.15",
+        "charset-normalizer==2.0.12",
+        "idna==3.3",
+        "requests==2.28.0",
+        "urllib3==1.26.9",
+    ],
+)
