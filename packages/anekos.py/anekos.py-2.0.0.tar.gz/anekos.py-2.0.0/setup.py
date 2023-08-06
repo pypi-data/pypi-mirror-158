2c944d75963b4d9d3371497d0e413c876367dc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:09 +0000

    Add package anekos.py/2.0.0/anekos.py-2.0.0.tar.gz

---
+++ packages/anekos.py/anekos.py-2.0.0.tar.gz/anekos.py-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="anekos.py",
+      author="regulad",
+      url="https://github.com/regulad/anekos.py",
+      version="2.0.0",
+      packages=["anekos"],
+      install_requires=["aiohttp>=3.6.2"],
+      license="MIT",
+      description="An unofficial async wrapper for nekos.life API!",
+      python_requires=">=3.6"
+)
