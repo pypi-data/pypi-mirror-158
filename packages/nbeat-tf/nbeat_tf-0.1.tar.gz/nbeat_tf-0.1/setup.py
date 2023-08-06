8f3285345b5542c5021ced22e577b4ae28d14a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:13 +0000

    Add package nbeat-tf/0.1/nbeat_tf-0.1.tar.gz

---
+++ packages/nbeat-tf/nbeat_tf-0.1.tar.gz/nbeat_tf-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name= "nbeat_tf",
+    version= "0.1",
+    author= "Janek Zimoch",
+    description= "TensorFlow implementation of N-Beats paper by Boris Oreshkin (2020 ICML)",
+    packages=["nbeats"]
+)
