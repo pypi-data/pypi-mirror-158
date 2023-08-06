f61fdcdc4935a84ba4a3f4d9b33dc5e502f9ae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:18 +0000

    Add package polars/0.13.52/polars-0.13.52.tar.gz

---
+++ packages/polars/polars-0.13.52.tar.gz/polars-0.13.52/local_dependencies/polars-core/src/chunked_array/list/mod.rs
@@ -0,0 +1,28 @@
+//! Special list utility methods
+mod iterator;
+
+use crate::prelude::*;
+
+impl ListChunked {
+    #[cfg(feature = "private")]
+    pub fn set_fast_explode(&mut self) {
+        self.bit_settings |= 1 << 2;
+    }
+
+    pub(crate) fn can_fast_explode(&self) -> bool {
+        self.bit_settings & 1 << 2 != 0
+    }
+
+    pub(crate) fn is_nested(&self) -> bool {
+        match self.dtype() {
+            DataType::List(inner) => matches!(&**inner, DataType::List(_)),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn to_logical(&mut self, inner_dtype: DataType) {
+        assert_eq!(inner_dtype.to_physical(), self.inner_dtype());
+        let fld = Arc::make_mut(&mut self.field);
+        fld.coerce(DataType::List(Box::new(inner_dtype)))
+    }
+}
---
+++ packages/polars/polars-0.13.52.tar.gz/polars-0.13.52/tests/test_groupby.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+import polars as pl
+
+
+def test_groupby_sorted_empty_dataframe_3680() -> None:
+    assert (
+        pl.DataFrame(
+            [
+                pl.Series("key", [], dtype=pl.Categorical),
+                pl.Series("val", [], dtype=pl.Float64),
+            ]
+        )
+        .lazy()
+        .sort("key")
+        .groupby("key")
+        .tail(1)
+        .collect()
+    ).shape == (0, 2)
+
+
+def test_groupby_custom_agg_empty_list() -> None:
+    assert (
+        pl.DataFrame(
+            [
+                pl.Series("key", [], dtype=pl.Categorical),
+                pl.Series("val", [], dtype=pl.Float64),
+            ]
+        )
+        .groupby("key")
+        .agg(
+            [
+                pl.col("val").mean().alias("mean"),
+                pl.col("val").std().alias("std"),
+                pl.col("val").skew().alias("skew"),
+                pl.col("val").kurtosis().alias("kurt"),
+            ]
+        )
+    ).dtypes == [pl.Categorical, pl.Float64, pl.Float64, pl.Float64, pl.Float64]
+
+
+def test_apply_after_take_in_groupby_3869() -> None:
+    assert (
+        pl.DataFrame(
+            {
+                "k": list("aaabbb"),
+                "t": [1, 2, 3, 4, 5, 6],
+                "v": [3, 1, 2, 5, 6, 4],
+            }
+        )
+        .groupby("k", maintain_order=True)
+        .agg(
+            pl.col("v").take(pl.col("t").arg_max()).sqrt()
+        )  # <- fails for sqrt, exp, log, pow, etc.
+    ).to_dict(False) == {"k": ["a", "b"], "v": [1.4142135623730951, 2.0]}
+
+
+def test_groupby_rolling_negative_offset_3914() -> None:
+    df = pl.DataFrame(
+        {
+            "datetime": pl.date_range(datetime(2020, 1, 1), datetime(2020, 1, 5), "1d"),
+        }
+    )
+    assert df.groupby_rolling(index_column="datetime", period="2d", offset="-4d").agg(
+        pl.count().alias("count")
+    )["count"].to_list() == [1, 1, 1, 1, 1]
