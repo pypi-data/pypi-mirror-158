619c3fe32a5d9a49379d7031166efafdf1cc32d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:00 +0000

    Add package tacos/0.0.1/tacos-0.0.1.tar.gz

---
+++ packages/tacos/tacos-0.0.1.tar.gz/tacos-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tacos',
+    version='0.0.1',
+    description='Transformer components',
+    author='Doraemonzzz',
+    author_email='doraemon_zzz@163.com',
+    url='https://github.com/Doraemonzzz/transformer-components',
+    install_requires=[
+        'torch',
+        'einops',
+    ],
+    packages=find_packages(
+        exclude=[
+                "examples",
+                "examples.*",
+                "data",
+                "data.*",
+        ]
+    ),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
---
+++ packages/tacos/tacos-0.0.1.tar.gz/tacos-0.0.1/tacos/xformers/__init__.py
@@ -0,0 +1,2 @@
+from .block import TransformerBlock
+from .vanilla_transformer import VanillaTransformer
