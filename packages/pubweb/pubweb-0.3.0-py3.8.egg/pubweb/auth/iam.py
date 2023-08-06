4e62be766935c1f092735700deb78bb64534d622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:10 +0000

    Add package pubweb/0.3.0/pubweb-0.3.0-py3.8.egg

---
+++ packages/pubweb/pubweb-0.3.0-py3.8.egg/pubweb/auth/iam.py
@@ -0,0 +1,36 @@
+import boto3
+from requests.auth import AuthBase
+from requests_aws4auth import AWS4Auth
+
+from pubweb import config
+from pubweb.auth.base import AuthInfo
+from pubweb.models.auth import Creds
+
+
+class IAMAuth(AuthInfo):
+    """
+    Uses AWS access tokens for authentication
+    Note: this does not with any API calls at the moment
+    """
+    def __init__(self, access_key: str, secret_key: str, token: str = None):
+        self.creds: Creds = {
+            'AccessKeyId': access_key,
+            'SecretAccessKey': secret_key,
+            'SessionToken': token,
+            'Expiration': None
+        }
+
+    @classmethod
+    def load_current(cls):
+        """
+        Loads the current session's AWS credentials
+        """
+        current = boto3.Session().get_credentials().get_frozen_credentials()
+        return cls(current.access_key, current.secret_key, current.token)
+
+    def get_request_auth(self) -> AuthBase:
+        return AWS4Auth(self.creds['AccessKeyId'],
+                        self.creds['SecretAccessKey'],
+                        config.region,
+                        'appsync',
+                        session_token=self.creds['SessionToken'])
---
+++ packages/pubweb/pubweb-0.3.0-py3.8.egg/pubweb/services/dataset.py
@@ -0,0 +1,114 @@
+import logging
+from typing import List, Union
+
+from pubweb.clients.utils import filter_deleted
+from pubweb.models.dataset import CreateIngestDatasetInput, DatasetCreateResponse, Dataset
+from pubweb.models.file import FileAccessContext, File
+from pubweb.services.file import FileEnabledService
+
+logger = logging.getLogger()
+
+
+class DatasetService(FileEnabledService):
+    def find_by_project(self, project_id: str, name: str = None) -> List[Dataset]:
+        """
+         Lists datasets by project with an optional name provided
+         Dataset names are not unique so providing a name doesn't guarantee one result
+        """
+        query = '''
+          query DatasetsByProject(
+            $project: ID!
+            $sortDirection: ModelSortDirection
+            $filter: ModelDatasetFilterInput
+            $limit: Int
+            $nextToken: String
+          ) {
+            datasetsByProject(
+              project: $project
+              sortDirection: $sortDirection
+              filter: $filter
+              limit: $limit
+              nextToken: $nextToken
+            ) {
+              items {
+                id
+                status
+                name
+                desc
+                sourceDatasets
+                infoJson
+                paramJson
+                process
+                createdAt
+                updatedAt
+                _deleted
+              }
+              nextToken
+              startedAt
+            }
+          }
+        '''
+        variables = {
+            'project': project_id,
+            'filter': {
+                'status': {
+                    'eq': 'COMPLETED'
+                }
+            }
+        }
+        if name:
+            variables['filter']['name'] = {'eq': name}
+
+        resp = self._api_client.query(query, variables=variables)['datasetsByProject']
+        not_deleted = filter_deleted(resp['items'])
+        return [Dataset.from_record(item) for item in not_deleted]
+
+    def create(self, create_request: CreateIngestDatasetInput) -> DatasetCreateResponse:
+        """
+        Creates an ingest dataset
+        """
+        logger.info(f"Creating dataset {create_request.name}")
+        query = '''
+          mutation CreateIngestDataset($input: CreateIngestDatasetInput!) {
+            createIngestDataset(input: $input) {
+              datasetId
+              dataPath
+            }
+          }
+        '''
+        variables = {'input': create_request.to_json()}
+        data: DatasetCreateResponse = self._api_client.query(query, variables=variables)['createIngestDataset']
+        logger.info(f"Dataset ID: {data['datasetId']}")
+        return data
+
+    def get_dataset_files(self, project_id: str, dataset_id: str) -> List[File]:
+        """
+        Returns a list of file names that are in the provided dataset
+        """
+        access_context = FileAccessContext.download_dataset(project_id=project_id, dataset_id=dataset_id)
+        return self._file_service.get_file_listing(access_context)
+
+    def upload_files(self, project_id: str, dataset_id: str, directory: str, files: List[str]):
+        """
+        Uploads files to a given dataset from the specified local directory
+        """
+        access_context = FileAccessContext.upload_dataset(project_id=project_id, dataset_id=dataset_id)
+        self._file_service.upload_files(access_context, directory, files)
+
+    def download_files(self, project_id: str, dataset_id: str, download_location: str,
+                       files: Union[List[File], List[str]] = None):
+        """
+         Downloads all the dataset files
+         If the files argument isn't provided, all files will be downloaded
+        """
+        access_context = FileAccessContext.download_dataset(project_id=project_id, dataset_id=dataset_id)
+        if files is None:
+            files = self._file_service.get_file_listing(access_context)
+
+        if len(files) == 0:
+            return
+
+        if isinstance(files[0], File):
+            files = [file.relative_path for file in files]
+
+        self._file_service.download_files(access_context, download_location, files)
