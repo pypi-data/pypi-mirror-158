810f073a6039332ccc6fcf597bf9567926a5929b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:08 +0000

    Add package Lugwit_Module/0.4/Lugwit_Module-0.4.tar.gz

---
+++ packages/Lugwit_Module/Lugwit_Module-0.4.tar.gz/Lugwit_Module-0.4/Lugwit_Module/__init__.py
@@ -0,0 +1,5 @@
+LugwitPath=r'Z:\plug_in\Lugwit_plug'
+plug_inPath=r'Z:\plug_in'
+import sys
+print (sys.version[0]+sys.version[2])
+sys.path.append(plug_inPath+'/Python/Python{}/Lib/site-packages'.format(sys.version[0]+sys.version[2]))
