c8620d97082d5e233afbd204ead83e54673b8909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:12 +0000

    Add package Products.EasyNewsletter/6.0.0a7/Products.EasyNewsletter-6.0.0a7-py3-none-any.whl

---
+++ packages/Products.EasyNewsletter/Products.EasyNewsletter-6.0.0a7-py3-none-any.whl/Products/EasyNewsletter/utils/enl.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.interface import Interface
+
+from Products.EasyNewsletter.browser.controlpanel import IENLSettings
+
+
+class IENLUtils(Interface):
+    """ENL utils methods"""
+
+
+class ENLUtils(object):
+    """ENL utils methods"""
+
+    def get_allowed_content_aggregation_types(self):
+        """return a tuple of allowed content types for content aggregation"""
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IENLSettings, prefix="Products.EasyNewsletter")
+        return settings.allowed_content_aggregation_types
