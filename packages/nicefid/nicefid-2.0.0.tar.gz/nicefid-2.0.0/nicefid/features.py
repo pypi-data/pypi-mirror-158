6b4fbe95ce1cf66797220bce487b56c6694c2804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:53 +0000

    Add package nicefid/2.0.0/nicefid-2.0.0.tar.gz

---
+++ packages/nicefid/nicefid-2.0.0.tar.gz/nicefid-2.0.0/nicefid/features.py
@@ -0,0 +1,106 @@
+from typing import Iterator, Union, Iterator
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+import torch
+from cleanfid import fid
+
+MODE = "clean"
+
+
+class Features:
+    features: np.array
+
+    def __init__(self, features: np.array):
+        self.features = features
+
+    @property
+    def mu(self):
+        return np.mean(self.features, axis=0)
+
+    @property
+    def sigma(self):
+        return np.cov(self.features, rowvar=False)
+
+    @staticmethod
+    def from_directory(
+        path: Union[str, Path],
+        batch_size=32,
+        n_workers=12,
+        device=torch.device("cuda"),
+    ) -> "Features":
+        feature_model = fid.build_feature_extractor(MODE, device)
+        features = fid.get_folder_features(
+            str(path),
+            feature_model,
+            num_workers=n_workers,
+            batch_size=batch_size,
+            device=torch.device("cuda"),
+            mode=MODE,
+            description=f"Extracting features for {Path(path).name} : ",
+            verbose=True,
+            custom_image_tranform=None,
+        )
+        return Features(features=features)
+
+    @staticmethod
+    def from_iterator(
+        iterator: Iterator[torch.Tensor], device=torch.device("cuda")
+    ) -> "Features":
+        """
+        Args:
+            iterator: An iterator that yields batches of images NCHW in the
+                range between 0 and 1.
+            device: The device to use for the feature extractor.
+        """
+        model = fid.build_feature_extractor(MODE, device)
+        fn_resize = fid.build_resizer(MODE)
+
+        features = list()
+        for images in iterator:
+            if images.ndim != 4:
+                raise ValueError(f"Expected NCHW images but got {images.shape}.")
+
+            resized_images = list()
+            for current_image in images:
+                image_numpy = current_image.mul(255).cpu().numpy().transpose((1, 2, 0))
+                resized_image = fn_resize(image_numpy)
+                resized_images.append(torch.tensor(resized_image.transpose((2, 0, 1))))
+            batch_features = fid.get_batch_features(
+                torch.stack(resized_images), model, device
+            )
+            features.append(batch_features)
+        features = np.concatenate(features)
+        return Features(features=features)
+
+    @staticmethod
+    def from_path(path: Union[str, Path]) -> "Features":
+        return Features(np.load(path)["features"])
+
+    def save(self, path: Union[str, Path]):
+        np.savez_compressed(path, features=self.features)
+
+
+def test_folder_and_iterator_equal():
+    from PIL import Image
+    import torchvision.transforms.functional as TF
+
+    directory = Path("tests/pixelart/dataset_a")
+    a = Features.from_directory(directory, batch_size=1)
+
+    def iterator():
+        for image_path in directory.glob("*.png"):
+            yield TF.to_tensor(Image.open(image_path))[None]
+
+    b = Features.from_iterator(iterator())
+
+    assert np.allclose(a.features.mean(axis=0), b.features.mean(axis=0), atol=1e-3)
+
+
+def test_save_load_works():
+    a = Features.from_directory("tests/pixelart/dataset_a")
+    path = Path("test_features.npz")
+    a.save(path)
+    b = Features.from_path(path)
+    path.unlink()
+    assert np.allclose(a.features, b.features)
---
+++ packages/nicefid/nicefid-2.0.0.tar.gz/nicefid-2.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nicefid']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['clean-fid>=0.1.24,<0.2.0']
+
+setup_kwargs = {
+    'name': 'nicefid',
+    'version': '2.0.0',
+    'description': 'Minimalistic wrapper around cleanfid to make the api more user friendly. Calculates FID and KID from sets of images.',
+    'long_description': '# nicefid\n\nMinimalistic wrapper around [cleanfid](https://github.com/GaParmar/clean-fid) to make the api more user friendly. Calculates FID and KID from sets of images.\n\n> [On Aliased Resizing and Surprising Subtleties in GAN Evaluation](https://arxiv.org/abs/2104.11222)\n\n## Install\n\n```bash\npoetry add nicefid\n```\n\nOr, for the old timers:\n\n```bash\npip install nicefid\n```\n\n## API\n\n```python\nnicefid.Features.from_directory(path: Union[str, Path])\nnicefid.Features.from_iterator(iterator: Iterator[torch.Tensor])  # NCHW\nnicefid.Features.from_path(path: Union[str, Path])\nfeatures.save(path: Union[str, Path])\n\nnicefid.compute_fid(features_a, features_b)\nnicefid.compute_kid(features_a, features_b)\n```\n\n## Usage\n\nComparing directory with generated images.\n\n```python\nimport nicefid\n\nfeatures_generated = nicefid.Features.from_iterator(...)\nfeatures_real = nicefid.Features.from_directory(...)\n\nfid = nicefid.compute_fid(features_generated, features_real)\nkid = nicefid.compute_kid(features_generated, features_real)\n```\n',
+    'author': 'Richard Löwenström',
+    'author_email': 'samedii@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/samedii/nicefid',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
