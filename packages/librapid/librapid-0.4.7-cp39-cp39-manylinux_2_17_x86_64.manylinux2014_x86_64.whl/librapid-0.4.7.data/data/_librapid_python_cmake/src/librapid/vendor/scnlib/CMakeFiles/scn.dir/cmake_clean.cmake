e8ffa6d4e4bc088d6a347151045de2cdafa567e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:08 +0000

    Add package librapid/0.4.7/librapid-0.4.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.7.data/data/_librapid_python_cmake/src/librapid/vendor/scnlib/CMakeFiles/scn.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scn.dir/src/file.cpp.o"
+  "CMakeFiles/scn.dir/src/file.cpp.o.d"
+  "CMakeFiles/scn.dir/src/locale.cpp.o"
+  "CMakeFiles/scn.dir/src/locale.cpp.o.d"
+  "CMakeFiles/scn.dir/src/reader_float.cpp.o"
+  "CMakeFiles/scn.dir/src/reader_float.cpp.o.d"
+  "CMakeFiles/scn.dir/src/reader_int.cpp.o"
+  "CMakeFiles/scn.dir/src/reader_int.cpp.o.d"
+  "CMakeFiles/scn.dir/src/vscan.cpp.o"
+  "CMakeFiles/scn.dir/src/vscan.cpp.o.d"
+  "libscn.a"
+  "libscn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
