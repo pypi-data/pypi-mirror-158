d0bb83815bb61d051ab6cb6d39ff20368ebc83b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:28 +0000

    Add package aiopyarr/22.7.0/aiopyarr-22.7.0-py3-none-any.whl

---
+++ packages/aiopyarr/aiopyarr-22.7.0-py3-none-any.whl/aiopyarr/exceptions.py
@@ -0,0 +1,33 @@
+"""Exceptions for Arr Api Client."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from aiohttp import ClientResponse
+
+    from .request_client import RequestClient
+
+
+class ArrException(Exception):
+    """Base arr exception."""
+
+    def __init__(
+        self,
+        client: RequestClient | None = None,
+        message: str | BaseException | ClientResponse | Exception = "",
+    ) -> None:
+        """Initialize."""
+        super().__init__(str(message) if client is not None else message)
+
+
+class ArrConnectionException(ArrException):
+    """Arr connection exception."""
+
+
+class ArrAuthenticationException(ArrException):
+    """Arr authentication exception."""
+
+
+class ArrResourceNotFound(ArrException):
+    """Arr resource not found exception."""
