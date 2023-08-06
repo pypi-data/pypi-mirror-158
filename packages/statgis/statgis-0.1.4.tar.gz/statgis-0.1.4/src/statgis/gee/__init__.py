712236abccd2433e40b2263a38fcc293c03df904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:17 +0000

    Add package statgis/0.1.4/statgis-0.1.4.tar.gz

---
+++ packages/statgis/statgis-0.1.4.tar.gz/statgis-0.1.4/src/statgis/gee/__init__.py
@@ -0,0 +1,12 @@
+'''
+ADVICE
+This functions only works with the next collections:
+- The Landsat surface reflectance collection Tier 1 Level 2:
+    - LANDSAT/LT04/C02/T1_L2
+    - LANDSAT/LT05/C02/T1_L2
+    - LANDSAT/LE07/C02/T1_L2
+    - LANDSAT/LC08/C02/T1_L2
+    - LANDSAT/LC09/C02/T1_L2
+- The Sentinel 2 surface reflectance collection:
+    - COPERNICUS/S2_SR
+'''
