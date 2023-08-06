84602b3e5fc08122554023daaec5d624fa840ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:40 +0000

    Add package nicegui/0.8.0/nicegui-0.8.0.tar.gz

---
+++ packages/nicegui/nicegui-0.8.0.tar.gz/nicegui-0.8.0/nicegui/lifecycle.py
@@ -0,0 +1,15 @@
+from typing import Awaitable, Callable, Union
+
+from .globals import connect_handlers, shutdown_handlers, startup_handlers
+
+
+def on_connect(self, handler: Union[Callable, Awaitable]):
+    connect_handlers.append(handler)
+
+
+def on_startup(self, handler: Union[Callable, Awaitable]):
+    startup_handlers.append(handler)
+
+
+def on_shutdown(self, handler: Union[Callable, Awaitable]):
+    shutdown_handlers.append(handler)
