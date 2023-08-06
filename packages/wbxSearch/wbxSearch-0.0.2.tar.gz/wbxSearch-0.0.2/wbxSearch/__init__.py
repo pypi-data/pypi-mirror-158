9db10429ea25372ad643e7d3a924c08e0db2808e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:30 +0000

    Add package wbxSearch/0.0.2/wbxSearch-0.0.2.tar.gz

---
+++ packages/wbxSearch/wbxSearch-0.0.2.tar.gz/wbxSearch-0.0.2/wbxSearch/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: ZhuravlevDmi
+:license: Apache License, version 2.0, see LICENSE file
+"""
+
+from .classCSV import WorkCSV
+from .extradition import Extradition
+from .filterArgs import FilterArgs
+from .minerArgs import MinerArgs
+
+__author__ = "ZhuravlevDmi"
+__version__ = "0.0.2"
+__email__ = "Dima15129@gmail.com"
