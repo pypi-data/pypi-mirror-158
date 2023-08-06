dc3080f69baa1683e9b0797204e81ab18cf8e559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:57:07 +0000

    Add package sqlitecollections/1.0.0/sqlitecollections-1.0.0-py3-none-any.whl

---
+++ packages/sqlitecollections/sqlitecollections-1.0.0-py3-none-any.whl/sqlitecollections/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.0.0"
+__author__ = "osoken"
+__description__ = "Python collections that are backended by sqlite3 DB and are compatible with the built-in collections"
+__email__ = "osoken.devel@outlook.jp"
+__package_name__ = "sqlitecollections"
+
+
+from .dict import Dict
+from .list import List
+from .set import Set
+
+__all__ = ["Dict", "List", "Set"]
