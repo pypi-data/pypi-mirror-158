68a5c11b9f9a9855b1ed82fdc1cd4e4ebe692ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:35 +0000

    Add package euphonic/0.6.5/euphonic-0.6.5-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/euphonic/euphonic-0.6.5-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/euphonic/__init__.py
@@ -0,0 +1,26 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+import pint
+from pint import UnitRegistry
+from importlib_resources import files
+from packaging.version import parse as parse_version
+
+# Create ureg here so it is only created once
+if parse_version(pint.__version__) < parse_version('0.10'):
+    # Bohr, unified_atomic_mass_unit not defined in pint 0.9, so load
+    # pint 0.16.1 definition file
+    ureg = UnitRegistry(str(files('euphonic.data') / 'default_en.txt'))
+else:
+    ureg = UnitRegistry()
+ureg.enable_contexts('spectroscopy')
+Quantity = ureg.Quantity
+
+from .spectra import Spectrum1D, Spectrum1DCollection, Spectrum2D
+from .crystal import Crystal
+from .debye_waller import DebyeWaller
+from .qpoint_frequencies import QpointFrequencies
+from .structure_factor import StructureFactor
+from .qpoint_phonon_modes import QpointPhononModes
+from .force_constants import ForceConstants
