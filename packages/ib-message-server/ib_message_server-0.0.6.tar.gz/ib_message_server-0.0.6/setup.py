1649437651a36d58ef3f4aa4810dc9fd9fa27b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:25 +0000

    Add package ib-message-server/0.0.6/ib_message_server-0.0.6.tar.gz

---
+++ packages/ib-message-server/ib_message_server-0.0.6.tar.gz/ib_message_server-0.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name="ib_message_server",
+      version="0.0.6",
+      description="Server part of Server to client messanger",
+      author="Igor Burakov",
+      author_email="iburako8@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5',
+                        'sqlalchemy',
+                        'pycryptodome',
+                        'pycryptodomex'],
+      scripts=['server/server_run']
+      )
