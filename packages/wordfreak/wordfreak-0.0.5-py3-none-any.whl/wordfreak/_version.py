6d73d1cea1947da0a59508ff389af4deb1c5064c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:38 +0000

    Add package wordfreak/0.0.5/wordfreak-0.0.5-py3-none-any.whl

---
+++ packages/wordfreak/wordfreak-0.0.5-py3-none-any.whl/wordfreak/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides wordfreak version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update wordfreak` to change this file.
+
+from incremental import Version
+
+__version__ = Version("wordfreak", 22, 7, 0)
+__all__ = ["__version__"]
