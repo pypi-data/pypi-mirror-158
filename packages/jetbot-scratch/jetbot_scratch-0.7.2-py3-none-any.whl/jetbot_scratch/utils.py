888ee3eb39abf8ca984b56a52634e34e1ca7809a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package jetbot-scratch/0.7.2/jetbot_scratch-0.7.2-py3-none-any.whl

---
+++ packages/jetbot-scratch/jetbot_scratch-0.7.2-py3-none-any.whl/jetbot_scratch/utils.py
@@ -0,0 +1,8 @@
+def clamp(value, lower=None, upper=None):
+    if value is None:
+        return None
+    elif (upper is not None) and (value > upper):
+        return upper
+    elif (lower is not None) and (value < lower):
+        return lower
+    return value
