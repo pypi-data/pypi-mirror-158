f709e60b5dd3188ac495bcd78da4f27e7698a322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:19 +0000

    Add package pandas-gdc-agent/0.0.2/pandas_gdc_agent-0.0.2-py3-none-any.whl

---
+++ packages/pandas-gdc-agent/pandas_gdc_agent-0.0.2-py3-none-any.whl/pandas_gdc_agent/__init__.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+from .pandas_gdc_agent import GDCAgent
+
+# _YEAR = date.today().year
+
+# __author__ = 'Paritosh Raj'
+# __about__ = {
+#     'title': 'pandas-gdc-agent',
+#     'version': '0.0.1',
+#     'url': 'https://github.com/paritosh-08/pandas_gdc_agent',
+#     'author': __author__,
+#     'email': 'paritosh@hasura.io',
+#     'license': 'MIT',
+#     'copyright': f'Copyright 2022-{_YEAR} {__author__}',
+# }
