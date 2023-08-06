2205d32e11d85cc7d6c6373200aa83134120b8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:34 +0000

    Add package webstaterator/0.4.1/webstaterator-0.4.1-py3-none-any.whl

---
+++ packages/webstaterator/webstaterator-0.4.1-py3-none-any.whl/webstaterator/__init__.py
@@ -0,0 +1,15 @@
+"""
+Webstaterator
+
+A Python tool for generating static websites based on object models
+
+Documentation: https://gitlab.com/Jon.Keatley.Folio/webstaterator
+Gitlab: https://gitlab.com/Jon.Keatley.Folio/webstaterator
+PyPi: https://pypi.org/project/webstaterator/
+
+Created by Jon Keatley (http://jon-keatley.com)
+Named by Sasha Siegel. It is her fault!
+
+Copyright Jon Keatley 2021
+
+"""
