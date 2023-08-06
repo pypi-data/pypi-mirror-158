6dfff32afdbaf412b1811c21ea3657bdf5cdd614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:33 +0000

    Add package yaml-config-builder/6.0.0/yaml_config_builder-6.0.0-py3-none-any.whl

---
+++ packages/yaml-config-builder/yaml_config_builder-6.0.0-py3-none-any.whl/config_builder/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Open Logistics Foundation
+#
+# Licensed under the Open Logistics License 1.0.
+# For details on the licensing terms, see the LICENSE file.
+
+# -*- coding: utf-8 -*-
+from .BaseConfigClass import BaseConfigClass
+from .ConfigBuilder import ConfigBuilder
+
+__all__ = ["BaseConfigClass", "ConfigBuilder"]
+__version__ = "6.0.0"
