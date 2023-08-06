f8034a7ab563c811ebf29ce5935f1fe3f08d4ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:57 +0000

    Add package hex-color-output/0.1.2/hex-color-output-0.1.2.tar.gz

---
+++ packages/hex-color-output/hex-color-output-0.1.2.tar.gz/hex-color-output-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hex-color-output',
+    version='0.1.2',
+    license='MIT',
+    author="Cownex",
+    author_email='contact@cownex.de',
+    packages=find_packages('source'),
+    package_dir={'': 'source'},
+    url='https://github.com/Cownex/HexOutput',
+    keywords='python hex color terminal output color-output',
+)
