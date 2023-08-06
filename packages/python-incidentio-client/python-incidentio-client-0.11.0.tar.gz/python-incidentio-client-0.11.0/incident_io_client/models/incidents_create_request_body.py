171c2380b47fc8d6dad16ed729713e52a28b6fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:17 +0000

    Add package python-incidentio-client/0.11.0/python-incidentio-client-0.11.0.tar.gz

---
+++ packages/python-incidentio-client/python-incidentio-client-0.11.0.tar.gz/python-incidentio-client-0.11.0/incident_io_client/models/incidents_create_request_body.py
@@ -0,0 +1,204 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+import attr
+
+from ..models.custom_field_entry_payload_request_body import (
+    CustomFieldEntryPayloadRequestBody,
+)
+from ..models.incident_role_assignment_payload_request_body import (
+    IncidentRoleAssignmentPayloadRequestBody,
+)
+from ..models.incidents_create_request_body_visibility import (
+    IncidentsCreateRequestBodyVisibility,
+)
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="IncidentsCreateRequestBody")
+
+
+@attr.s(auto_attribs=True)
+class IncidentsCreateRequestBody:
+    """
+    Example:
+        {'custom_field_entries': [{'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'values': [{'id':
+            '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'},
+            {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'}]},
+            {'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'values': [{'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link':
+            'https://google.com/', 'value_numeric': '123.456', 'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'value_text': 'This is my text field, I hope you like it'}, {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link':
+            'https://google.com/', 'value_numeric': '123.456', 'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'value_text': 'This is my text field, I hope you like it'}]}, {'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'values': [{'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'},
+            {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'}]}],
+            'idempotency_key': 'alert-uuid', 'incident_role_assignments': [{'assignee': {'email': 'Dolore autem ea et
+            voluptatem minus aut.', 'id': 'Ut doloribus porro et sit nihil.', 'slack_user_id': 'Alias aut rerum fugiat
+            exercitationem maiores.'}, 'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}, {'assignee': {'email': 'Dolore
+            autem ea et voluptatem minus aut.', 'id': 'Ut doloribus porro et sit nihil.', 'slack_user_id': 'Alias aut rerum
+            fugiat exercitationem maiores.'}, 'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}, {'assignee': {'email':
+            'Dolore autem ea et voluptatem minus aut.', 'id': 'Ut doloribus porro et sit nihil.', 'slack_user_id': 'Alias
+            aut rerum fugiat exercitationem maiores.'}, 'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}, {'assignee':
+            {'email': 'Dolore autem ea et voluptatem minus aut.', 'id': 'Ut doloribus porro et sit nihil.', 'slack_user_id':
+            'Alias aut rerum fugiat exercitationem maiores.'}, 'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}],
+            'incident_type_id': 'Aut repellendus voluptatem.', 'name': 'Our database is sad', 'severity_id': 'Placeat quod
+            aspernatur quia eveniet.', 'summary': "Our database is really really sad, and we don't know why yet.",
+            'visibility': 'public'}
+
+    Attributes:
+        idempotency_key (str): Unique string used to de-duplicate incident create requests Example: alert-uuid.
+        severity_id (str):  Example: Non quos quod dolor..
+        visibility (IncidentsCreateRequestBodyVisibility): Whether the incident should be open to anyone in your Slack
+            workspace (public), or invite-only (private). For more information on Private Incidents see our [help
+            centre](https://help.incident.io/en/articles/5947963-can-we-mark-incidents-as-sensitive-and-restrict-access).
+            Example: public.
+        custom_field_entries (Union[Unset, List[CustomFieldEntryPayloadRequestBody]]): Set the incident's custom fields
+            to these values Example: [{'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'values': [{'id':
+            '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'},
+            {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'}]},
+            {'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'values': [{'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link':
+            'https://google.com/', 'value_numeric': '123.456', 'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'value_text': 'This is my text field, I hope you like it'}, {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link':
+            'https://google.com/', 'value_numeric': '123.456', 'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'value_text': 'This is my text field, I hope you like it'}]}, {'custom_field_id': '01FCNDV6P870EA6S7TK1DSYDG0',
+            'values': [{'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'},
+            {'id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_link': 'https://google.com/', 'value_numeric': '123.456',
+            'value_option_id': '01FCNDV6P870EA6S7TK1DSYDG0', 'value_text': 'This is my text field, I hope you like it'}]}].
+        incident_role_assignments (Union[Unset, List[IncidentRoleAssignmentPayloadRequestBody]]): Assign incident roles
+            to these people Example: [{'assignee': {'email': 'Dolore autem ea et voluptatem minus aut.', 'id': 'Ut doloribus
+            porro et sit nihil.', 'slack_user_id': 'Alias aut rerum fugiat exercitationem maiores.'}, 'incident_role_id':
+            '01FH5TZRWMNAFB0DZ23FD1TV96'}, {'assignee': {'email': 'Dolore autem ea et voluptatem minus aut.', 'id': 'Ut
+            doloribus porro et sit nihil.', 'slack_user_id': 'Alias aut rerum fugiat exercitationem maiores.'},
+            'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}, {'assignee': {'email': 'Dolore autem ea et voluptatem minus
+            aut.', 'id': 'Ut doloribus porro et sit nihil.', 'slack_user_id': 'Alias aut rerum fugiat exercitationem
+            maiores.'}, 'incident_role_id': '01FH5TZRWMNAFB0DZ23FD1TV96'}].
+        incident_type_id (Union[Unset, str]):  Example: Porro ullam est quidem amet repellendus aut..
+        name (Union[Unset, str]): Explanation of the incident Example: Our database is sad.
+        summary (Union[Unset, str]): Detailed description of the incident Example: Our database is really really sad,
+            and we don't know why yet..
+    """
+
+    idempotency_key: str
+    severity_id: str
+    visibility: IncidentsCreateRequestBodyVisibility
+    custom_field_entries: Union[Unset, List[CustomFieldEntryPayloadRequestBody]] = UNSET
+    incident_role_assignments: Union[Unset, List[IncidentRoleAssignmentPayloadRequestBody]] = UNSET
+    incident_type_id: Union[Unset, str] = UNSET
+    name: Union[Unset, str] = UNSET
+    summary: Union[Unset, str] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        idempotency_key = self.idempotency_key
+        severity_id = self.severity_id
+        visibility = self.visibility.value
+
+        custom_field_entries: Union[Unset, List[Dict[str, Any]]] = UNSET
+        if not isinstance(self.custom_field_entries, Unset):
+            custom_field_entries = []
+            for custom_field_entries_item_data in self.custom_field_entries:
+                custom_field_entries_item = custom_field_entries_item_data.to_dict()
+
+                custom_field_entries.append(custom_field_entries_item)
+
+        incident_role_assignments: Union[Unset, List[Dict[str, Any]]] = UNSET
+        if not isinstance(self.incident_role_assignments, Unset):
+            incident_role_assignments = []
+            for incident_role_assignments_item_data in self.incident_role_assignments:
+                incident_role_assignments_item = incident_role_assignments_item_data.to_dict()
+
+                incident_role_assignments.append(incident_role_assignments_item)
+
+        incident_type_id = self.incident_type_id
+        name = self.name
+        summary = self.summary
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "idempotency_key": idempotency_key,
+                "severity_id": severity_id,
+                "visibility": visibility,
+            }
+        )
+        if custom_field_entries is not UNSET:
+            field_dict["custom_field_entries"] = custom_field_entries
+        if incident_role_assignments is not UNSET:
+            field_dict["incident_role_assignments"] = incident_role_assignments
+        if incident_type_id is not UNSET:
+            field_dict["incident_type_id"] = incident_type_id
+        if name is not UNSET:
+            field_dict["name"] = name
+        if summary is not UNSET:
+            field_dict["summary"] = summary
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        idempotency_key = d.pop("idempotency_key")
+
+        severity_id = d.pop("severity_id")
+
+        visibility = IncidentsCreateRequestBodyVisibility(d.pop("visibility"))
+
+        custom_field_entries = []
+        _custom_field_entries = d.pop("custom_field_entries", UNSET)
+        for custom_field_entries_item_data in _custom_field_entries or []:
+            custom_field_entries_item = CustomFieldEntryPayloadRequestBody.from_dict(
+                custom_field_entries_item_data
+            )
+
+            custom_field_entries.append(custom_field_entries_item)
+
+        incident_role_assignments = []
+        _incident_role_assignments = d.pop("incident_role_assignments", UNSET)
+        for incident_role_assignments_item_data in _incident_role_assignments or []:
+            incident_role_assignments_item = IncidentRoleAssignmentPayloadRequestBody.from_dict(
+                incident_role_assignments_item_data
+            )
+
+            incident_role_assignments.append(incident_role_assignments_item)
+
+        incident_type_id = d.pop("incident_type_id", UNSET)
+
+        name = d.pop("name", UNSET)
+
+        summary = d.pop("summary", UNSET)
+
+        incidents_create_request_body = cls(
+            idempotency_key=idempotency_key,
+            severity_id=severity_id,
+            visibility=visibility,
+            custom_field_entries=custom_field_entries,
+            incident_role_assignments=incident_role_assignments,
+            incident_type_id=incident_type_id,
+            name=name,
+            summary=summary,
+        )
+
+        incidents_create_request_body.additional_properties = d
+        return incidents_create_request_body
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
