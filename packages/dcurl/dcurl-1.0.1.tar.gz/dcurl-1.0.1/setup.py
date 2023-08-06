76b2d9243d48b603e5f6dc412987da0e91c2d820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:47 +0000

    Add package dcurl/1.0.1/dcurl-1.0.1.tar.gz

---
+++ packages/dcurl/dcurl-1.0.1.tar.gz/dcurl-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dcurl',
+    packages=find_packages(),
+    include_package_data=True,
+    version="1.0.1",
+    description='URL SHORTNER',
+    author='AKXVAU',
+    author_email='akxvau@gmail.com',
+    long_description=(open("README.md","r")).read(),
+    long_description_content_type="text/markdown",
+   install_requires=['lolcat','requests'],
+ 
+    keywords=['URL SHORTNER', 'AKXVAU'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    
+    license='MIT',
+    entry_points={
+            'console_scripts': [
+                'dcurl = dcurl',
+                
+            ],
+    },
+    python_requires='>=3.9'
+)
