9a7d94d731628970dff53a7fe399e04395a7bc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:54 +0000

    Add package librapid/0.4.9/librapid-0.4.9-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.9-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.9.data/data/_librapid_python_cmake/CMakeFiles/librapid.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/librapid.dir/src/librapid/src/cudaCodeLoader.cpp.o"
+  "CMakeFiles/librapid.dir/src/librapid/src/cudaCodeLoader.cpp.o.d"
+  "CMakeFiles/librapid.dir/src/librapid/src/dotCBlas.cpp.o"
+  "CMakeFiles/librapid.dir/src/librapid/src/dotCBlas.cpp.o.d"
+  "CMakeFiles/librapid.dir/src/librapid/src/fastMath.cpp.o"
+  "CMakeFiles/librapid.dir/src/librapid/src/fastMath.cpp.o.d"
+  "CMakeFiles/librapid.dir/src/librapid/src/kernelHelper.cpp.o"
+  "CMakeFiles/librapid.dir/src/librapid/src/kernelHelper.cpp.o.d"
+  "liblibrapid.a"
+  "liblibrapid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/librapid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
