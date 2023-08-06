2d5cefdacaed1566bcc6cdde26bd15c3c23dd428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:36 +0000

    Add package document-tools/0.1.2/document_tools-0.1.2-py3-none-any.whl

---
+++ packages/document-tools/document_tools-0.1.2-py3-none-any.whl/document_tools/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Document Tools."""
+
+__author__ = """deeptools.ai"""
+__email__ = "contact@deeptools.ai"
+__version__ = "0.1.2"
+
+
+from .encoders import TARGET_MODELS, LayoutLMv2Encoder, LayoutLMv3Encoder, LayoutXLMEncoder
+from .tokenize import tokenize_dataset
+
+__all__ = ["LayoutLMv2Encoder", "LayoutLMv3Encoder", "LayoutXLMEncoder", "TARGET_MODELS", "tokenize_dataset"]
