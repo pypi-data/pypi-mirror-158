c887ff60ae4dff65244b2eb49b2ca6d7a79b4ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:56 +0000

    Add package librapid/0.4.8/librapid-0.4.8-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.8-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.8.data/data/_librapid_python_cmake/src/librapid/vendor/scnlib/CMakeFiles/scn.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/src/librapid/vendor/scnlib/src/file.cpp" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/file.cpp.o" "gcc" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/file.cpp.o.d"
+  "/project/src/librapid/vendor/scnlib/src/locale.cpp" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/locale.cpp.o" "gcc" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/locale.cpp.o.d"
+  "/project/src/librapid/vendor/scnlib/src/reader_float.cpp" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/reader_float.cpp.o" "gcc" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/reader_float.cpp.o.d"
+  "/project/src/librapid/vendor/scnlib/src/reader_int.cpp" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/reader_int.cpp.o" "gcc" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/reader_int.cpp.o.d"
+  "/project/src/librapid/vendor/scnlib/src/vscan.cpp" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/vscan.cpp.o" "gcc" "src/librapid/vendor/scnlib/CMakeFiles/scn.dir/src/vscan.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
