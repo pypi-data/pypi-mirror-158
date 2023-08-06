e02ad4018ff5a35bc025ff24e9a763e86832b47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:24 +0000

    Add package realisticAF/0.1/realisticAF-0.1.tar.gz

---
+++ packages/realisticAF/realisticAF-0.1.tar.gz/realisticAF-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='realisticAF',
+    version='0.1',
+    license='MIT',
+    packages=find_packages(),
+    url='https://github.com/louzi233/AssessmentFramework',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'Pillow',
+      ],
+
+)
