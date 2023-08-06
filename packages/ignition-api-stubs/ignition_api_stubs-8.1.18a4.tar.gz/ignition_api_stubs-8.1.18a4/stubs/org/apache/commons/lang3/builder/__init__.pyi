08d5459debfe086cef2dd19fd079b5f43523681f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:34 +0000

    Add package ignition-api-stubs/8.1.18a4/ignition_api_stubs-8.1.18a4.tar.gz

---
+++ packages/ignition-api-stubs/ignition_api_stubs-8.1.18a4.tar.gz/ignition_api_stubs-8.1.18a4/stubs/org/apache/commons/lang3/builder/__init__.pyi
@@ -0,0 +1,16 @@
+from java.lang import Object, String, StringBuffer
+from typing import Any
+
+class ToStringStyle(Object):
+    DEFAULT_STYLE: ToStringStyle
+    JSON_STYLE: ToStringStyle
+    MULTI_LINE_STYLE: ToStringStyle
+    NO_CLASS_NAME_STYLE: ToStringStyle
+    NO_FIELD_NAMES_STYLE: ToStringStyle
+    SHORT_PREFIX_STYLE: ToStringStyle
+    SIMPLE_STYLE: ToStringStyle
+    def append(self, buffer: StringBuffer, fieldName: String, *args: Any) -> None: ...
+    def appendEnd(self, buffer: StringBuffer, obj: Any) -> None: ...
+    def appendStart(self, buffer: StringBuffer, obj: Any) -> None: ...
+    def appendSuper(self, buffer: StringBuffer, superToString: String) -> None: ...
+    def appendToString(self, buffer: StringBuffer, toString: String) -> None: ...
---
+++ packages/ignition-api-stubs/ignition_api_stubs-8.1.18a4.tar.gz/ignition_api_stubs-8.1.18a4/stubs/system/print.pyi
@@ -0,0 +1,11 @@
+from com.inductiveautomation.factorypmi.application.script.builtin import PrintUtilities
+from java.awt import Component
+from java.awt.image import BufferedImage
+from java.lang import String
+from typing import Optional
+
+JythonPrintJob = PrintUtilities.JythonPrintJob
+
+def createImage(component: Component) -> BufferedImage: ...
+def createPrintJob(component: Component) -> JythonPrintJob: ...
+def printToImage(component: Component, filename: Optional[String] = ...) -> None: ...
