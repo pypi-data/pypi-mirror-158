cb4832221a4dd6071469cd3d4875d8e1e6f89b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:37 +0000

    Add package cashews/4.6.0/cashews-4.6.0.tar.gz

---
+++ packages/cashews/cashews-4.6.0.tar.gz/cashews-4.6.0/cashews/backends/redis/client.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import socket
+from typing import Any
+
+try:
+    from redis.asyncio import Redis
+    from redis.exceptions import ConnectionError as RedisConnectionError
+except ImportError:
+    from aioredis import Redis
+    from aioredis import RedisError as RedisConnectionError
+
+
+logger = logging.getLogger(__name__)
+
+
+class SafeRedis(Redis):
+    async def execute_command(self, command, *args: Any, **kwargs: Any):
+        try:
+            return await super().execute_command(command, *args, **kwargs)
+        except (RedisConnectionError, socket.gaierror, OSError, asyncio.TimeoutError):
+            logger.error("redis: can not execute command: %s", command, exc_info=True)
+            if command.lower() in ["unlink", "del", "memory", "ttl"]:
+                return 0
+            if command.lower() == "scan":
+                return [0, []]
+            return None
+
+    async def initialize(self):
+        try:
+            return await super().initialize()
+        except Exception:
+            return self
+
+    __aenter__ = initialize
