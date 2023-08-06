338fef2373fba3ea1a32133d0d42bfa40d6c7165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:03 +0000

    Add package Reuters/1.2.1/Reuters-1.2.1.tar.gz

---
+++ packages/Reuters/Reuters-1.2.1.tar.gz/Reuters-1.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Reuters",
+    version="1.2.1",
+    author="Hurin Hu",
+    author_email="hurin@live.ca",
+    description="Reuters ticker information retriever for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HurinHu/Reuters",
+    packages=setuptools.find_packages(),
+    install_requires=["requests","beautifulsoup4"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
