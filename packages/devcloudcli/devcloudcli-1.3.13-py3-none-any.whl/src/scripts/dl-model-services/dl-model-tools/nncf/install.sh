1374f41e487114557833316e2a11f07c8cef8bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:23 +0000

    Add package devcloudcli/1.3.13/devcloudcli-1.3.13-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.3.13-py3-none-any.whl/src/scripts/dl-model-services/dl-model-tools/nncf/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+pip3 install nncf[tf]
+echo -e "\e[1;32mnncf is installed successfully\e[0m"
+
+echo -e "\e[1;31mFor further queries please follow below URL\e[0m"
+
+echo -e "\e[1;31mhttps://github.com/openvinotoolkit/nncf\e[0m"
