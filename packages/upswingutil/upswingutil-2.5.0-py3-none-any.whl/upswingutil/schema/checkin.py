fa0df79626da3b047183badccede41d3ee2d11b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:07 +0000

    Add package upswingutil/2.5.0/upswingutil-2.5.0-py3-none-any.whl

---
+++ packages/upswingutil/upswingutil-2.5.0-py3-none-any.whl/upswingutil/schema/checkin.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from typing import List
+from pydantic import BaseModel
+
+
+class GuestType(str, Enum):
+    PRIMARY = 'Primary'
+    CO_TRAVELLER = 'Co-traveller'
+    SPOUSE = 'Spouse'
+    CHILDREN = 'Children'
+
+
+class CheckInGuestModel(BaseModel):
+    name: str = ""
+    guestEmail: str = ""
+    guestPMSId: str = ""
+    appGuestUID: str = ""
+    guestType: GuestType = GuestType.CO_TRAVELLER
+    primary: bool = ""
+    appAccess: bool = ""
+    doorlockAccess: bool = ""
+    grmsAccess: bool = ""
+
+
+class CheckinReservationModel(BaseModel):
+    reservationId: str
+    hotelId: str
+    arrivalDate: str = ""
+    departureDate: str = ""
+    roomId: str = ""
+    checkin_type: str = ""
+    doorlockLinked: bool = False
+    grmsLinked: bool = False
+    appLinked: bool = False
+    guest_list: List[CheckInGuestModel] = []
+
