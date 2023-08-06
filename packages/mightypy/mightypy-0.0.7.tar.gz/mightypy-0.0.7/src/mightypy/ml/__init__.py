cd550858229c791d14a5bf0d5390601c927e4add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:03 +0000

    Add package mightypy/0.0.7/mightypy-0.0.7.tar.gz

---
+++ packages/mightypy/mightypy-0.0.7.tar.gz/mightypy-0.0.7/src/mightypy/ml/__init__.py
@@ -0,0 +1,47 @@
+"""
+mightypy.ml
+=============
+"""
+
+from ._linear import (
+    LinearRegression,
+    LassoRegression,
+    RidgeRegression,
+    LogisticRegression,
+    polynomial_regression,
+    trend
+)
+
+
+from ._tree import (
+    DecisionTreeClassifier,
+    DecisionTreeRegressor
+)
+
+from ._ensemble import (
+    AdaboostClassifier,
+    RandomForestClassifier,
+    RandomForestRegressor,
+)
+
+from ._utils import (
+    moving_window_matrix,
+    sigmoid
+)
+
+
+__all__ = [
+    "LinearRegression",
+    "LassoRegression",
+    "RidgeRegression",
+    "LogisticRegression",
+    "polynomial_regression",
+    "trend",
+    "DecisionTreeClassifier",
+    "DecisionTreeRegressor",
+    "AdaboostClassifier",
+    "RandomForestClassifier",
+    "RandomForestRegressor",
+    "moving_window_matrix",
+    "sigmoid"
+]
