792ce5e85b1c7c593ed99269bdf0ebb09230b256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:26 +0000

    Add package singularity-hpc/0.0.57/singularity-hpc-0.0.57.tar.gz

---
+++ packages/singularity-hpc/singularity-hpc-0.0.57.tar.gz/singularity-hpc-0.0.57/shpc/utils/__init__.py
@@ -0,0 +1,27 @@
+from .terminal import (
+    run_command,
+    check_install,
+    get_installdir,
+    which,
+    confirm_action,
+    confirm_uninstall,
+)
+from .fileio import (
+    can_be_deleted,
+    copyfile,
+    creation_date,
+    get_file_hash,
+    get_tmpdir,
+    get_tmpfile,
+    mkdir_p,
+    mkdirp,
+    remove_to_base,
+    print_json,
+    read_file,
+    read_json,
+    read_yaml,
+    recursive_find,
+    write_file,
+    write_json,
+    write_yaml,
+)
