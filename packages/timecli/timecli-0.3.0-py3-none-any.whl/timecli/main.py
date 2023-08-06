fcbdb890f9feb316510db37ba9ddd5bde16d4331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:37 +0000

    Add package timecli/0.3.0/timecli-0.3.0-py3-none-any.whl

---
+++ packages/timecli/timecli-0.3.0-py3-none-any.whl/timecli/main.py
@@ -0,0 +1,25 @@
+import typer
+from timecli.commands import oracle, pomodoro
+
+__version__ = "0.3.0"
+
+app = typer.Typer()
+app.add_typer(oracle.app, name="oracle")
+app.add_typer(pomodoro.app, name="pomodoro")
+
+
+def call_version(value: bool):
+    if value:
+        typer.echo(f"timecli: version {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def version_callback(
+        version: bool = typer.Option(None, "--version", callback=call_version),
+):
+    pass
+
+
+if __name__ == '__main__':
+    app()
