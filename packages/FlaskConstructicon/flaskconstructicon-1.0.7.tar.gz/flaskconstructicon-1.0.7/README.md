4a2f654b84d6090d9ac5d5136ee60888e3602e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package FlaskConstructicon/1.0.7/flaskconstructicon-1.0.7.tar.gz

---
+++ packages/FlaskConstructicon/flaskconstructicon-1.0.7.tar.gz/flaskconstructicon-1.0.7/README.md
@@ -0,0 +1,87 @@
+# FlaskConstructicon
+![image](https://user-images.githubusercontent.com/75331586/177195753-b57551ca-c7ab-4299-9b7d-8da8c0b321f5.png)
+
+<h3>Flask Project Builder</h3>
+
+Constructs a file tree for a Flask app using MVC design pattern (Models, Controllers and Templates as Views):
+
+![image](https://user-images.githubusercontent.com/75331586/177694189-daf043f2-c71c-4a71-91ce-00816d877532.png)
+
+# Installation:
+1) `pip install flaskconstructicon`
+
+# Simple Usage:
+1) Navigate to your new directory's project
+2) Run FlaskConstructicon
+`>> python -m flaskconstructicon [system arguments]`
+3) FlaskConstructicon takes several arguments 
+
+<table>
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>Options</th>
+            <th>Description</th>
+            <th>Examples</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>-md</td>
+            <td>test, help</td>
+            <td>Run mode, test or help</td>
+            <td>python FlaskConstructicon.py -md test</td>
+        </tr>
+        <tr>
+            <td>-a</td>
+            <td>Any</td>
+            <td>Specifies app name</td>
+            <td>python FlaskConstructicon.py -a my_app</td>
+        </tr>
+        <tr>
+            <td>-db</td>
+            <td>mysql</td>
+            <td>Add file for database API</td>
+            <td>python FlaskConstructicon.py -db mysql</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>N/A</td>
+            <td>Same as -md test (first argument only)</td>
+            <td>python FlaskConstructicon.py test</td>
+        </tr>
+        <tr>
+            <td>help</td>
+            <td>N/A</td>
+            <td>Same as -md help (first argument only)</td>
+            <td>python FlaskConstructicon.py help</td>
+        </tr>
+    </tbody>
+</table>
+
+<h3>Test output with app name 'simflario'</h3>
+
+![image](https://user-images.githubusercontent.com/75331586/177251274-b439eb82-0787-4179-a4b2-ca8bf2b981f2.png)
+
+<h3>Output creating new project:</h3>
+
+![image](https://user-images.githubusercontent.com/75331586/177251369-0f797d52-c371-4aba-a914-9762cc7c141a.png)
+
+<h3>Test output with existing project</h3>
+
+![image](https://user-images.githubusercontent.com/75331586/177251447-4dcc19a0-05fa-4f92-ba1f-09563d31a280.png)
+
+
+# Use an alias
+Example alias using git bash:
+`alias constructicon='winpty python C:/Path/To/FlaskConstructicon/FlaskConstructicon.py'`
+
+# Next steps:
+
+>-Add source files for other db connections and/or APIs
+
+>-create models files and models option
+
+>-create "Django Style" option
+
+>-create option `dbua` for user model and authentication based on db config
---
+++ packages/FlaskConstructicon/flaskconstructicon-1.0.7.tar.gz/flaskconstructicon-1.0.7/src/FlaskConstructicon/__main__.py
@@ -0,0 +1,169 @@
+from pathlib import Path
+import os
+from sys import argv
+from .resources import *
+
+os.system("color")
+
+COLOR = {
+    "HEADER": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+}
+
+def main(*args):
+
+    arguments = _arg_handler(args)
+
+    if arguments.get("error"):
+        print(COLOR["RED"], arguments.get("error"), COLOR["ENDC"])
+        cli = ""
+        for arg in args[0][1:]:
+            cli += arg + " "
+        print("ARGUMENTS:", cli)
+        print(COLOR["GREEN"], HELP, COLOR["ENDC"])
+        return
+
+    if arguments.get("app_name"):
+        app_name = arguments["app_name"]
+    else:
+        app_name = "flask_app"
+
+    if arguments.get("mode") == "help":
+        print(COLOR["GREEN"], HELP, COLOR["ENDC"])
+        return
+    elif arguments.get("mode") == "test":
+        _test_mode(app_name)
+        return
+
+    print(COLOR["BLUE"], "Current working directory:", Path.cwd(), COLOR["ENDC"])
+
+    #Writing server.py file in current directory
+    print(COLOR["GREEN"], "Creating", "server.py file", COLOR["ENDC"])
+    server = open("server.py", "w+")
+    server.write(server_py(app_name))
+    server.close()
+
+
+
+
+    #creating the app folder and going into it
+    print(COLOR["GREEN"], "Creating", f"{app_name} folder", COLOR["ENDC"])
+    Path(app_name).mkdir()
+    os.chdir(app_name)
+
+    #creating the folders inside the app folder accorindg to MVC design
+    print(COLOR["GREEN"], "Creating", f"{app_name}/config directory", COLOR["ENDC"])
+    Path("config").mkdir()
+    print(COLOR["GREEN"], "Creating", f"{app_name}/controllers directory", COLOR["ENDC"])
+    Path("controllers").mkdir()
+    print(COLOR["GREEN"], "Creating", f"{app_name}/models directory", COLOR["ENDC"])
+    Path("models").mkdir()
+    print(COLOR["GREEN"], "Creating", f"{app_name}/static directory", COLOR["ENDC"])
+    Path("static").mkdir()
+    print(COLOR["GREEN"], "Creating", f"{app_name}/templates directory", COLOR["ENDC"])
+    Path("templates").mkdir()
+
+    #writing the __init__.py file for the module
+    print(COLOR["GREEN"], "Creating", "__init__.py file", COLOR["ENDC"])
+    module_file = open("__init__.py", "w+")
+    module_file.write(APP_MODULE_FILE)
+    module_file.close()
+
+    #Going into the config directory to write the mysqlconnection.py file
+    if arguments.get("database") == "mysql":
+        os.chdir("config")
+        print(COLOR["GREEN"], "Creating", "config/mysqlconnection.py file", COLOR["ENDC"])
+        mysql = open("mysqlconnection.py", "w+")
+        #this .py file is responsible for connecting the app to the MySQL server
+        mysql.write(MYSQLCONNECTION)
+        mysql.close()
+        #finished
+
+    print(COLOR["BLUE"], "Directories and files for your Flask project successfully created", COLOR["ENDC"])
+    return
+
+def _arg_handler(*args):
+    """
+    System Arguments
+    -md, mode --help, test
+    -a app_name --any
+    -db database --mysql
+
+    Shortcuts (applies to first argument only):
+    test - enters test mode
+    help - enters help mode
+
+    Future options
+    -dbua user authentication, will build models and controllers for basic user authentication based on db
+    -models build model files and controllers from source_files/models
+    """
+
+    sys_args = args[0][0]
+    arguments ={}
+    for idx, arg in enumerate(sys_args):
+        try:
+            if str(arg) == "-md":
+                arguments["mode"] = sys_args[idx+1]
+            elif arg == "-a":
+                arguments["app_name"] = sys_args[idx+1]
+            elif arg == "-db":
+                arguments["database"] = sys_args[idx+1]
+                if arguments["database"] not in SUPPORTED_DATABASES:
+                    arguments["error"] = f"{arguments['database']} is not a supported database"
+        except:
+            arguments["error"] = "You may have entered invalid options:"
+    if len(sys_args) > 1:
+        if sys_args[1].lower() == "test":
+            arguments["mode"] = "test"
+        elif sys_args[1].lower() == "help":
+            arguments["mode"] = "help"
+    return arguments
+
+def _test_mode(app_name):
+    #check file paths
+    print(COLOR["BLUE"], "TESTMODE:\nCurrent working directory:", Path.cwd(), COLOR["ENDC"], "\n")
+    errors=[]
+    tree = {
+        app_name : ["config", "controllers", "models", "static", "templates", "__init__.py"],
+        "server.py" : None
+    }
+    for top in tree.keys():
+        if(os.path.exists(os.path.join(os.getcwd(),top))):
+            this_error = COLOR["RED"]+top+" already exists"+COLOR["ENDC"]
+            errors.append(this_error)
+            print(this_error)
+        else:
+            print(COLOR["GREEN"], top, " doesn't exist", COLOR["ENDC"])
+        if tree[top]:
+            for sub in tree[top]:
+                sub_path = os.path.join(top, sub)
+                abs_path = os.path.join(os.getcwd(), sub_path)
+                if(os.path.exists(abs_path)):
+                    this_error = COLOR["RED"]+sub_path+" already exists"+COLOR["ENDC"]
+                    print(this_error)
+                    errors.append(this_error)
+                else:
+                    print(COLOR["GREEN"], sub_path, " doesn't exist", COLOR["ENDC"])
+
+    mysqlcon = os.path.join(app_name, "config", "mysqlconnection.py")
+    if os.path.exists(os.path.join(os.getcwd(), mysqlcon)):
+        this_error = COLOR["RED"]+mysqlcon +" already exists"+COLOR["ENDC"]
+        errors.append(this_error)
+        print(this_error)
+    else:
+        print(COLOR["GREEN"], mysqlcon, " doesn't exist", COLOR["ENDC"])
+
+    if len(errors) > 0:
+        print("\n", COLOR["RED"], len(errors), " error(s):")
+        for error in errors:
+            print(error, COLOR["ENDC"])
+    else:
+        print(COLOR["GREEN"], "\nNo errors, ready to construct app file tree", COLOR["ENDC"])
+    print(COLOR["BLUE"], "\nTEST MODE. TERMINATING SESSION", COLOR["ENDC"])
+    return
+
+if __name__=="__main__":
+    main(argv)
