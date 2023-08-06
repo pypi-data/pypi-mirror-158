988e98fbe2481e8bbf03e6262fede3811f369cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:48 +0000

    Add package superscraper/0.3.0/superscraper-0.3.0-py3-none-any.whl

---
+++ packages/superscraper/superscraper-0.3.0-py3-none-any.whl/superscraper/__init__.py
@@ -0,0 +1,7 @@
+from .superscraper import SuperScraper
+
+# We'd like to use these as needed
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+__version__ = '0.3.0'
