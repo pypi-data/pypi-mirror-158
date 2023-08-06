891bba39ac2771a605a65f50c1db2f369666493d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:15 +0000

    Add package edsnlp/0.6.1/edsnlp-0.6.1.tar.gz

---
+++ packages/edsnlp/edsnlp-0.6.1.tar.gz/edsnlp-0.6.1/edsnlp/pipelines/core/normalizer/accents/factory.py
@@ -0,0 +1,23 @@
+from typing import List, Optional, Tuple
+
+from spacy.language import Language
+
+from edsnlp.utils.deprecation import deprecated_factory
+
+from .accents import Accents
+
+DEFAULT_CONFIG = dict(
+    accents=None,
+)
+
+
+@deprecated_factory("accents", "eds.accents", default_config=DEFAULT_CONFIG)
+@Language.factory("eds.accents", default_config=DEFAULT_CONFIG)
+def create_component(
+    nlp: Language,
+    name: str,
+    accents: Optional[List[Tuple[str, str]]],
+):
+    return Accents(
+        accents=accents,
+    )
