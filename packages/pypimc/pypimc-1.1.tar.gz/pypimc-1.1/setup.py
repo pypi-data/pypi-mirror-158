ee62d5e586a01f19f25d9f68c2e5e238ebdb3a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:49 +0000

    Add package pypimc/1.1/pypimc-1.1.tar.gz

---
+++ packages/pypimc/pypimc-1.1.tar.gz/pypimc-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+ 
+setup(
+  name='pypimc',
+  version='1.1',
+  description='Minecraft Information Library',
+  url='',  
+  author='Beete',
+  author_email='beete@protonmail.com',
+  license='MIT', 
+  keywords='Minecraft', 
+  packages=find_packages(),
+  install_requires=['requests'] 
+)
