4ee68c86271884fc72a4e1274ad00107e7f1e675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:54 +0000

    Add package great-ai/0.1.4/great-ai-0.1.4.tar.gz

---
+++ packages/great-ai/great-ai-0.1.4.tar.gz/great-ai-0.1.4/docs/reference/views.md
@@ -0,0 +1,5 @@
+# View models
+
+::: great_ai.Trace
+    options:
+        show_root_heading: true
---
+++ packages/great-ai/great-ai-0.1.4.tar.gz/great-ai-0.1.4/great_ai/views/__init__.py
@@ -0,0 +1,17 @@
+from .api_metadata import ApiMetadata
+from .cache_statistics import CacheStatistics
+from .evaluation_feedback_request import EvaluationFeedbackRequest
+from .filter import Filter
+from .function_metadata import FunctionMetadata
+from .health_check_response import HealthCheckResponse
+from .model import Model
+from .operators import operators
+from .outputs import (
+    ClassificationOutput,
+    MultiLabelClassificationOutput,
+    RegressionOutput,
+)
+from .query import Query
+from .route_config import RouteConfig
+from .sort_by import SortBy
+from .trace import Trace
