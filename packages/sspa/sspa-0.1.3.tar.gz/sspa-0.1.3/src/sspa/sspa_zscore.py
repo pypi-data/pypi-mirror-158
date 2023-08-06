dc4900ba24425e6e978743ee1bf5cead5ca11ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:29 +0000

    Add package sspa/0.1.3/sspa-0.1.3.tar.gz

---
+++ packages/sspa/sspa-0.1.3.tar.gz/sspa-0.1.3/src/sspa/sspa_zscore.py
@@ -0,0 +1,36 @@
+# Cite Lee et al. 2008
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import sspa.utils as utils
+
+def sspa_zscore(mat, pathway_df, min_entity=2):
+    """
+    Lee at al 2008 z-score method for single sample pathway analysis
+
+    :param mat: pandas DataFrame omics data matrix consisting of m rows (samples) and n columns (entities).
+    Do not include metadata columns
+    :param pathways: Dictionary of pathway identifiers (keys) and corresponding list of pathway entities (values).
+    Entity identifiers must match those in the matrix columns
+
+    :return: pandas DataFrame of pathway scores derived using the z-score method. Columns represent pathways and rows represnt samples.
+    """
+
+    pathways = utils.pathwaydf_to_dict(pathway_df)
+
+    pathway_activities = []
+    pathway_ids = []
+    for pathway, compounds in pathways.items():
+        single_pathway_matrix = mat.drop(mat.columns.difference(compounds), axis=1)
+        if single_pathway_matrix.shape[1] >= min_entity:
+            pathway_mat = single_pathway_matrix.T.values
+            pathway_ids.append(pathway)
+
+            zscores = stats.zscore(pathway_mat, axis=1)
+            sum_zscore = np.sum(zscores, axis=0)
+            pathway_act = sum_zscore / np.sqrt(pathway_mat.shape[0])
+            pathway_activities.append(pathway_act)
+
+    pathway_activities_df = pd.DataFrame(pathway_activities, columns=mat.index, index=pathway_ids).T
+    return pathway_activities_df
+
---
+++ packages/sspa/sspa-0.1.3.tar.gz/sspa-0.1.3/src/sspa/utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import pkg_resources
+import scipy.stats as stats
+import statsmodels.api as sm
+
+def load_example_data(omicstype="metabolomics", processed=True):
+    """
+    Loads example datasets
+
+    :param omicstype: type of omics for example data. Available options are "metabolomics" or "transcriptomics"
+    Metabolomics data are from Su et al 2020 https://doi.org/10.1016/j.cell.2020.10.037
+
+    :return: pre-processed omics data matrix consisting of m samples and n entities (metabolites/genes)
+    in the form of a pandas DataFrame. Contains one of more metadata columns at the end.
+    """
+
+    if omicstype == "metabolomics":
+        if processed:
+            stream = pkg_resources.resource_stream(__name__, 'example_data/Su_covid_metabolomics_processed.csv')
+            f = pd.read_csv(stream, index_col=0, encoding='latin-1')
+            return f
+        else:
+            stream = pkg_resources.resource_stream(__name__, 'example_data/Su_metab_data_raw.csv')
+            f = pd.read_csv(stream, index_col=0, encoding='latin-1')
+            return f
+
+
+def t_tests(matrix, classes, multiple_correction_method, testtype="ttest"):
+    metabolites = matrix.columns.tolist()
+    matrix['Target'] = pd.factorize(classes)[0]
+    disease = matrix.loc[matrix["Target"] == 0]
+    disease.drop(['Target'], axis=1, inplace=True)
+    ctrl = matrix.loc[matrix["Target"] != 0]
+    ctrl.drop(['Target'], axis=1, inplace=True)
+    matrix = matrix.drop(['Target'], axis=1)
+    
+    if testtype == "mwu":
+        pvalues = stats.mannwhitneyu(disease, ctrl, axis=0)[1]
+    else:
+        pvalues = stats.ttest_ind(disease, ctrl)[1]
+
+    padj = sm.stats.multipletests(pvalues, 0.05, method=multiple_correction_method)
+    results = pd.DataFrame(zip(metabolites, pvalues, padj[1]),
+                           columns=["Entity", "P-value", "P-adjust"])
+    return results
+
+def pathwaydf_to_dict(df):
+    pathways_df = df.drop("Pathway_name", axis=1)
+    pathway_dict = {}
+
+    for pathway in df.index:
+        pathway_compounds = list(set(pathways_df.loc[pathway, :].tolist()))
+        pathway_compounds = [str(i) for i in pathway_compounds if str(i) != "None"]
+
+        cpds = pathway_compounds[1:]
+        if len(cpds) > 1:
+            pathway_dict[pathway] = cpds
+    return pathway_dict
