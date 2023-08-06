a884b8205aabaf034de0bc0ce03da872411a3f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:20 +0000

    Add package enhanced-dir/0.2.64/enhanced_dir-0.2.64.tar.gz

---
+++ packages/enhanced-dir/enhanced_dir-0.2.64.tar.gz/enhanced_dir-0.2.64/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='enhanced_dir',
+    version='0.2.64',
+    description='An enhanced version of dir, which gives more details',
+    py_modules=['enhanced_dir'],
+    package_dir={'': 'src'},
+)
