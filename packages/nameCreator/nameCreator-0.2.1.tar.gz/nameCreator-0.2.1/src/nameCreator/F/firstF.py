3b54172fa01f19195bfb51079aa1b422a3899440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:18 +0000

    Add package nameCreator/0.2.1/nameCreator-0.2.1.tar.gz

---
+++ packages/nameCreator/nameCreator-0.2.1.tar.gz/nameCreator-0.2.1/src/nameCreator/F/firstF.py
@@ -0,0 +1,56 @@
+import random
+import os
+from unidecode import unidecode
+
+curDir = os.path.dirname(__file__)
+#lists
+FirstAmericanFemale = []
+FirstRussianFemale = []
+FirstSpanishFemale = []
+FirstItalianFemale = []
+FirstGermanFemale = []
+
+
+def uniLine(obj):
+    return (unidecode(obj.rstrip().lstrip(), "preserve"))
+
+
+## read files
+# american (USA)
+with open(curDir + "\\names\\american.txt") as mytxt:
+    for line in mytxt:
+        FirstAmericanFemale.append(uniLine(line))
+
+# russian (Russian Federation)
+with open(curDir + "\\names\\russian.txt") as mytxt:
+    for line in mytxt:
+        FirstRussianFemale.append(uniLine(line))
+
+# spanish (Spain,Mexico, other Spanish speaking nations) (To be divided)
+with open(curDir + "\\names\\spanish.txt") as mytxt:
+    for line in mytxt:
+        FirstSpanishFemale.append(uniLine(line))
+
+#italian
+with open(curDir + "\\names\\italian.txt") as mytxt:
+    for line in mytxt:
+        FirstItalianFemale.append(uniLine(line))
+
+#german
+with open(curDir + "\\names\\german.txt") as mytxt:
+    for line in mytxt:
+        FirstGermanFemale.append(uniLine(line))
+
+
+#defs
+def FirstNameFemale(nationality):
+    if (nationality.lower() == "american"):
+        return random.choice(FirstAmericanFemale)
+    if (nationality.lower() == "spanish"):
+        return random.choice(FirstSpanishFemale)
+    if (nationality.lower() == "russian"):
+        return random.choice(FirstRussianFemale)
+    if (nationality.lower() == "italian"):
+        return random.choice(FirstItalianFemale)
+    if (nationality.lower() == "german"):
+        return random.choice(FirstGermanFemale)
