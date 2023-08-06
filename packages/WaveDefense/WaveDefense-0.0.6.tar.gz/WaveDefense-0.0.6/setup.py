59a11ed3368e4acf6d77b20eabc49199abbd2189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:46 +0000

    Add package WaveDefense/0.0.6/WaveDefense-0.0.6.tar.gz

---
+++ packages/WaveDefense/WaveDefense-0.0.6.tar.gz/WaveDefense-0.0.6/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='WaveDefense', version='0.0.6', install_requires=['gym', 'pygame', 'numpy'], py_modules=['WaveDefense', 'WaveDefense.envs.wave_defense', 'WaveDefense.envs.wave_defense_tabular', 'WaveDefense.envs', 'WaveDefense.envs.prefabs'], author = "Roger Creus", author_email = "creus99@protonmail.com", long_description=long_description, long_description_content_type='text/markdown')
