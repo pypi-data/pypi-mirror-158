5c9ecd8aa6178826adff9494aa04a62f3f1678ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:58 +0000

    Add package rpaframework/15.3.0/rpaframework-15.3.0.tar.gz

---
+++ packages/rpaframework/rpaframework-15.3.0.tar.gz/rpaframework-15.3.0/tests/python/test_exchange.py
@@ -0,0 +1,243 @@
+import datetime
+import mock
+import pytest
+from RPA.Email.Exchange import Exchange, UTC
+from RPA.Robocorp.Vault import Vault
+from . import RESOURCES_DIR
+
+SENDMAIL_MOCK = "RPA.Email.Exchange.Message.send"
+recipient = "person1@domain.com"
+multi_recipients = "person2@domain.com,person3@domain.com"
+
+pytest.skip("skipped until tests are fixed", allow_module_level=True)
+
+
+@pytest.fixture
+def library():
+    lib = Exchange()
+    return lib
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_no_recipients_raises(mocked, library):
+    with pytest.raises(TypeError):
+        library.send_message(subject="My test email subject")
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_recipients(mocked, library):
+    status = library.send_message(recipients=recipient)
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_multiple_recipients(mocked, library):
+    status = library.send_message(recipients=multi_recipients)
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_multiple_recipients_as_list(mocked, library):
+    multi_recipients_as_list = ["person2@domain.com, person3@domain.com"]
+    status = library.send_message(recipients=multi_recipients_as_list)
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_with_subject(mocked, library):
+    status = library.send_message(recipients=recipient, subject="My test email subject")
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_with_body(mocked, library):
+    status = library.send_message(
+        recipients=recipient, body="email body of the message"
+    )
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_with_html_body_but_in_plaintext(mocked, library):
+    status = library.send_message(
+        recipients=recipient, body="<p>email <b>body</b> of the message</p>"
+    )
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_with_html_body_html_format(mocked, library):
+    status = library.send_message(
+        recipients=recipient, body="<p>email <b>body</b> of the message</p>", html=True
+    )
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_images(mocked, library):
+    status = library.send_message(
+        recipients=recipient, images=RESOURCES_DIR / "approved.png"
+    )
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_attachments(mocked, library):
+    status = library.send_message(
+        recipients=recipient, attachments=RESOURCES_DIR / "approved.png"
+    )
+    assert status
+
+
+@mock.patch(SENDMAIL_MOCK)
+def test_send_message_with_images_and_attachments(mocked, library):
+    imagepath = str(RESOURCES_DIR / "approved.png")
+    status = library.send_message(
+        recipients=recipient, attachments=imagepath, images=imagepath
+    )
+    assert status
+
+
+def test_get_filter_key_value_unknown_criterion_key(library):
+    with pytest.raises(KeyError):
+        _ = library._get_filter_key_value("dfddff")
+    with pytest.raises(KeyError):
+        _ = library._get_filter_key_value("dfddff:a")
+    with pytest.raises(KeyError):
+        _ = library._get_filter_key_value("dfddff:")
+
+
+def test_get_filter_key_value_dates(library):
+    default_start = datetime.datetime(1972, 1, 1, tzinfo=UTC)
+    default_end = datetime.datetime(2050, 1, 1, tzinfo=UTC)
+    date_1 = library._parse_date_from_string("05-05-2020")
+    date_2 = library._parse_date_from_string("01-02-2021")
+
+    criterias = {
+        "before:01-02-2021": {
+            "datetime_received__range": (default_start, date_2),
+        },
+        "after:05-05-2020": {
+            "datetime_received__range": (date_1, default_end),
+        },
+        "between:'05-05-2020 and 01-02-2021'": {
+            "datetime_received__range": (date_1, date_2),
+        },
+    }
+
+    for criteria, expected in criterias.items():
+        result = library._get_filter_key_value(criteria)
+        assert result == expected
+
+
+def test_get_filter_key_value_by_importance(library):
+    result = library._get_filter_key_value("importance:high")
+    assert result == {"importance": "High"}
+
+
+def test_get_filter_key_value_by_category(library):
+    result = library._get_filter_key_value("category:green")
+    assert result == {"categories": "green"}
+
+
+def test_get_filter_key_value_by_category_contains(library):
+    result = library._get_filter_key_value("category_contains:green")
+    assert result == {"categories__contains": "green"}
+
+
+def test_get_filter_key_value_by_body(library):
+    result = library._get_filter_key_value("body:'Content in the email body'")
+    assert result == {"body": "Content in the email body"}
+
+
+def test_get_filter_key_value_by_body_contains(library):
+    result = library._get_filter_key_value("body_contains:'email body'")
+    assert result == {"body__contains": "email body"}
+
+
+def test_get_filter_key_value_by_subject(library):
+    result = library._get_filter_key_value("subject:'Hello World'")
+    assert result == {"subject": "Hello World"}
+
+
+def test_get_filter_key_value_by_subject_contains(library):
+    result = library._get_filter_key_value("subject_contains:buy")
+    assert result == {"subject__contains": "buy"}
+
+
+def test_get_filter_key_value_by_sender(library):
+    result = library._get_filter_key_value("sender:name@domain.com")
+    assert result == {"sender": "name@domain.com"}
+
+
+def test_get_filter_key_value_by_sender_contains(library):
+    result = library._get_filter_key_value("sender_contains:name@domain.com")
+    assert result == {"sender__contains": "name@domain.com"}
+
+
+def test_get_filter_by_key_value_multiple_conditions(library):
+    criterias = {
+        "sender:robocorp.tester@gmail.com and subject:epic": {
+            "sender": "robocorp.tester@gmail.com",
+            "subject": "epic",
+        },
+        "sender_contains:robocorp.tester@gmail.com and subject:'epic level'": {
+            "sender__contains": "robocorp.tester@gmail.com",
+            "subject": "epic level",
+        },
+        # first quoted criteria is prioritized
+        "body_contains:message and body_contains:'epic level'": {
+            "body__contains": "epic level",
+        },
+        # first quoted criteria is prioritized
+        "body_contains:'message' and body_contains:'epic level'": {
+            "body__contains": "message",
+        },
+    }
+
+    for criteria, expected in criterias.items():
+        result = library._get_filter_key_value(criteria)
+        assert result == expected
+
+
+@pytest.mark.skip(reason="requires Vault and valid email account")
+@pytest.mark.parametrize(
+    "is_oauth, to_addr",
+    [
+        (True, "cosmin-robocorp@ztzvn.onmicrosoft.com"),
+        (False, "cosmin-robocorp@ztzvn.onmicrosoft.com"),
+        (False, None),
+    ],
+)
+def test_send_message_with_out_bcc_addresses(library, is_oauth, to_addr):
+    secrets = Vault().get_secret("Exchange")
+    if is_oauth:
+        params = {
+            "access_type": "IMPERSONATE",
+            "client_id": secrets["client_id"],
+            "client_secret": secrets["client_secret"],
+            "token": secrets["token"],
+        }
+    else:
+        params = {
+            "password": secrets["password"],
+        }
+    library.authorize(
+        username=secrets["account"],
+        autodiscover=False,
+        server="outlook.office365.com",
+        is_oauth=is_oauth,
+        **params,
+    )
+    if to_addr:
+        recipient = to_addr
+        bcc_list = []
+    else:
+        recipient = None
+        bcc_list = ["robocorp.tester@gmail.com", "robocorp.tester.2@gmail.com"]
+    library.send_message(
+        recipients=recipient,
+        bcc=bcc_list,
+        subject="test_send_message_with_only_bcc_addresses",
+        body="test_send_message_with_only_bcc_addresses",
+    )
---
+++ packages/rpaframework/rpaframework-15.3.0.tar.gz/rpaframework-15.3.0/tests/robot/test_email.robot
@@ -0,0 +1,175 @@
+*** Settings ***
+Library           Collections
+Library           OperatingSystem
+Library           RPA.Email.ImapSmtp
+Library           RPA.FileSystem
+Library           RPA.HTTP
+Library           RPA.Tables
+
+Suite Setup       Init GMail
+
+
+*** Variables ***
+${RESOURCES}      ${CURDIR}${/}..${/}resources
+${BODY_IMG1}      ${RESOURCES}${/}approved.png
+${BODY_IMG2}      ${RESOURCES}${/}invoice.png
+${EMAIL_BODY}     <h1>Heading</h1><p>Status: <img src='approved.png' alt='approved image'/></p>
+...               <p>INVOICE: <img src='invoice.png' alt='invoice image'/></p>
+${IMAGE_URL}      https://static1.squarespace.com/static/5c37bbd23e2d090f4652b5b9/t/5e6b5afca55453445ebc451b/1586254429285/?format=1500w
+${MAIL_FILE}      ${RESOURCES}${/}emails${/}work-item-documentation.eml
+
+
+*** Keywords ***
+Init GMail
+    Set environment variable    RPA_SECRET_MANAGER    RPA.Robocorp.Vault.FileSecrets
+    Set environment variable    RPA_SECRET_FILE       ${RESOURCES}${/}secrets.yaml
+    Import library      RPA.Robocorp.Vault
+    ${email} =    Get Secret    gmail
+    ${status}   ${ret} =     Run Keyword And Ignore Error    Authorize
+    ...     ${email}[account]    ${email}[password]     is_oauth=${email}[is_oauth]
+    ...     smtp_server=${email}[smtpserver]    imap_server=${email}[imapserver]
+    IF    "${status}" == "FAIL"
+        Pass Execution      ${ret}
+    END
+
+
+*** Tasks ***
+Sending HTML Email With Downloaded Image
+    [Documentation]    Sending email with HTML content and attachment
+    [Tags]   skip
+
+    Download    ${IMAGE_URL}    target_file=logo.png    overwrite=${TRUE}
+    Send Message
+    ...    sender=Robocorporation <robocorp.tester@gmail.com>
+    ...    recipients=mika@robocorp.com
+    ...    subject=HTML email with body images (2) plus one attachment
+    ...    body=${EMAIL_BODY}
+    ...    html=${TRUE}
+    ...    images=${BODY_IMG1}, ${BODY_IMG2}
+    ...    attachments=logo.png
+
+Sending HTML Email With Image
+    [Documentation]    Sending email with HTML content and attachment
+    [Tags]   skip
+
+    Send Message
+    ...    sender=Robocorporation <robocorp.tester@gmail.com>
+    ...    recipients=mika@robocorp.com
+    ...    subject=HTML email with body images (2) plus one attachment
+    ...    body=${EMAIL_BODY}
+    ...    html=${TRUE}
+    ...    images=${BODY_IMG1}, ${BODY_IMG2}
+    ...    attachments=/Users/mika/koodi/syntax_example.png
+
+Sending email with inline images
+    [Documentation]    Sending email with inline images
+    [Tags]   skip
+
+    Send Message
+    ...    sender=Robocorporation <robocorp.tester@gmail.com>
+    ...    recipients=mika@robocorp.com
+    ...    subject=11 Email with inline images and attachment
+    ...    body=Normal body content
+    ...    images=${BODY_IMG1}, ${BODY_IMG2}
+    ...    attachments=/Users/mika/koodi/syntax_example.png
+
+Sending Email
+    [Documentation]    Sending email with GMail account
+    [Tags]   skip
+
+    Send Message
+    ...    sender=Robocorporation <robocorp.tester@gmail.com>
+    ...    recipients=mika@robocorp.com
+    ...    subject=Order confirmationäöäöä
+    ...    body=Thank you for the order!
+
+Filtering emails
+    [Documentation]    Filter emails by some criteria
+    [Tags]   skip
+
+    ${messages}=    List messages    SUBJECT "rpa"
+    ${msgs}=    Create table    ${messages}
+    Filter table by column    ${msgs}    From    contains    mika@robocorp.com
+    FOR    ${msg}    IN    @{msgs}
+        Log    ${msg}[Subject]
+        Log    ${msg}[From]
+    END
+
+Getting emails
+    [Tags]   skip
+
+    List messages    criterion=SUBJECT "rpa"
+
+Saving attachments
+    [Tags]   skip
+
+    Save attachments    criterion=SUBJECT "rpa"    target_folder=../temp
+
+Move messages empty criterion
+    [Tags]   skip
+
+    Run Keyword And Expect Error    KeyError*    Move Messages    ${EMPTY}
+
+Move messages empty target
+    [Tags]   skip
+
+    Run Keyword And Expect Error    KeyError*    Move Messages    SUBJECT 'RPA'
+
+Move messages to target folder from inbox
+    [Tags]   skip
+
+    ${result}=    Move Messages
+    ...    criterion=SUBJECT "order confirmation"
+    ...    target_folder=yyy
+
+Move messages from subfolder to another
+    [Tags]   skip
+
+    ${result}=    Move Messages
+    ...    criterion=ALL
+    ...    source_folder=yyy
+    ...    target_folder=XXX
+
+Performing message actions
+    [Tags]   skip
+
+    ${actions}=    Create List    msg_unflag    msg_read    msg_save    msg_attachment_save
+    Do Message Actions    SUBJECT "Order confirmation"
+    ...    ${actions}
+    ...    source_folder=XXX
+    ...    target_folder=${CURDIR}
+    ...    overwrite=True
+
+Move messages by their IDS
+    [Documentation]    Use case could be one task parsing emails and then passing
+    ...    the Message-IDS to another task to process further
+    [Tags]   skip
+
+    # task 1
+    ${messages}=    List Messages    SUBJECT "incoming orders"
+    @{idlist}=    Create List
+    FOR    ${msg}    IN    @{messages}
+        Append To List    ${idlist}    ${msg}[Message-ID]
+    END
+    # task 2
+    Move Messages By IDs    ${idlist}    target_folder
+
+Convert email to docx
+    ${mail_name} =      Get File Name   ${MAIL_FILE}
+    ${output_doc} =     Set Variable    ${OUTPUT_DIR}${/}${mail_name}.docx
+    Email To Document    ${MAIL_FILE}   ${output_doc}
+
+    File Should Exist   ${output_doc}
+    File Should Not Be Empty    ${output_doc}
+    [Teardown]  RPA.FileSystem.Remove file     ${output_doc}
+
+Send Self Email
+    [Tags]   skip
+    ${email} =    Get Secret    gmail
+    ${auth_type} =  Set Variable    basic
+    IF    ${email}[is_oauth]
+        ${auth_type} =  Set Variable    OAuth2
+    END
+    Send Message    sender=${email}[account]    recipients=${email}[account]
+    ...    subject=E-mail sent through the ${auth_type} flow
+    ...    body=I hope you find this flow easy to understand and use.
