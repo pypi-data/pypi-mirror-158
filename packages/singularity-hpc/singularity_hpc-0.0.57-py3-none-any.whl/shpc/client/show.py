dcecc485ef574b7be0f517b6dc71e25a50a03bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:26 +0000

    Add package singularity-hpc/0.0.57/singularity_hpc-0.0.57-py3-none-any.whl

---
+++ packages/singularity-hpc/singularity_hpc-0.0.57-py3-none-any.whl/shpc/client/show.py
@@ -0,0 +1,14 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2021-2022, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+
+def main(args, parser, extra, subparser):
+
+    from shpc.main import get_client
+
+    cli = get_client(quiet=args.quiet, settings_file=args.settings_file)
+
+    # Update config settings on the fly
+    cli.settings.update_params(args.config_params)
+    cli.show(args.name, names_only=not args.versions, filter_string=args.filter_string)
