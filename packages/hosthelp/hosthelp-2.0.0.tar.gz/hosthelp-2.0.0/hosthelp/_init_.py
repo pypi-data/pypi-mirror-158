73da14e8486c45a10b4b7bdb4c7238e62ef65efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:54 +0000

    Add package hosthelp/2.0.0/hosthelp-2.0.0.tar.gz

---
+++ packages/hosthelp/hosthelp-2.0.0.tar.gz/hosthelp-2.0.0/hosthelp/_init_.py
@@ -0,0 +1,8 @@
+from .hosthelp.py import reqedit,createproc
+
+
+
+
+__author__='Anisimov Nikita'
+__version__='2.0.0'
+__email__='qwerty11111333@mail.ru'
---
+++ packages/hosthelp/hosthelp-2.0.0.tar.gz/hosthelp-2.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hosthelp',
+      version='2.0.0',
+      description='The private helper',
+      author='Nikita_Anisimov',
+      author_email='qwerty111111333@mail.ru',
+      url='https://github.com/Anisimov1123/txt2docx/blob/main/README.md?plain=1',
+      packages=['hosthelp'],
+      install_requires=['pip']
+     )
