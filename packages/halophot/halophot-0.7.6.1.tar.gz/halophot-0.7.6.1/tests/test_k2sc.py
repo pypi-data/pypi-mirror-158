a7190a45d56eeb37b7903c5d3d6ac5088ff650df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:09 +0000

    Add package halophot/0.7.6.1/halophot-0.7.6.1.tar.gz

---
+++ packages/halophot/halophot-0.7.6.1.tar.gz/halophot-0.7.6.1/tests/test_k2sc.py
@@ -0,0 +1,48 @@
+import halophot
+from halophot.halo_tools import halo_tpf
+import lightkurve as lk
+
+import os
+TESTDIR = os.path.abspath(os.path.dirname(__file__))
+ddir = os.path.join(TESTDIR,'../data/')
+
+def test_lk():
+
+	fname = ddir+"ktwo205897543-c03_lpd-targ.fits.gz"
+
+	tpf = halo_tpf(fname)
+
+	lc = tpf.to_lightcurve()
+	lc.primary_header = tpf.hdu[0].header
+	lc.data_header = tpf.hdu[1].header
+
+	meta, corr_lc = tpf.halo(thresh=0.5)
+
+def test_k2sc():
+	import k2sc
+	from k2sc.standalone import k2sc_lc
+	print('k2sc version',k2sc.__version__)
+
+	fname = ddir+"ktwo205897543-c03_lpd-targ.fits.gz"
+
+	tpf = lk.KeplerTargetPixelFile(fname)
+
+	lc = tpf.to_lightcurve()
+
+	lc.__class__ = k2sc_lc
+
+	lc.k2sc(de_max_time=10)
+
+# def test_saturation():
+# 	print('testing')
+
+# 	fname = ddir+"ktwo200128910-c111_lpd-targ.fits.gz" 
+
+# 	tpf = halo_tpf(fname)
+
+# 	lc = tpf.to_lightcurve()
+# 	lc.primary_header = tpf.hdu[0].header
+# 	lc.data_header = tpf.hdu[1].header
+
+# 	meta, corr_lc = tpf.halo(thresh=-1);
+# 	weightmap = meta['weightmap'][0]
