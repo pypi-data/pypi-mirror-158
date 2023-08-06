597e9d353ed17411fc1e64ba9765dfeacdf543ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:22 +0000

    Add package QREDDISH01/1.0/QREDDISH01-1.0.tar.gz

---
+++ packages/QREDDISH01/QREDDISH01-1.0.tar.gz/QREDDISH01-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'QREDDISH01',         # How you named your package folder
+  packages = ['kits'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'YOUR NAME',                   # Type in your name
+
+)
+
