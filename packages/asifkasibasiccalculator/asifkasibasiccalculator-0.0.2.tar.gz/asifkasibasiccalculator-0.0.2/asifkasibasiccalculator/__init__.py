8180fa86a34f723b935b15215e6151ef6ce4a135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:50 +0000

    Add package asifkasibasiccalculator/0.0.2/asifkasibasiccalculator-0.0.2.tar.gz

---
+++ packages/asifkasibasiccalculator/asifkasibasiccalculator-0.0.2.tar.gz/asifkasibasiccalculator-0.0.2/asifkasibasiccalculator/__init__.py
@@ -0,0 +1,14 @@
+def add_numbers(num1,num2):
+    return num1+num2
+
+def subtract_numbers(num1,num2):
+    return num1-num2
+def add_numbers(num1,num2):
+    return num1+num2
+def multiple_numbers(num1,num2):
+    return num1*num2
+def divide_numbers(num1,num2):
+    return num1/num2
+
+def remainder_numbers(num1,num2):
+    return num1%num2
