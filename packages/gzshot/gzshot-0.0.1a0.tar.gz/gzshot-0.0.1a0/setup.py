b478be98529ebff77c9698e44cd9ced50cc11de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:46 +0000

    Add package gzshot/0.0.1a0/gzshot-0.0.1a0.tar.gz

---
+++ packages/gzshot/gzshot-0.0.1a0.tar.gz/gzshot-0.0.1a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gzshot',
+    version='0.0.1a',
+    author='IBM Research',
+    description="Zero and Few shot named entity recognition",
+    packages=[],
+    classifiers=[],
+)
