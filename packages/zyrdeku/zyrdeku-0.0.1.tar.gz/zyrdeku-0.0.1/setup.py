76ad7210d8f2c5dbafcaf826f00a91dc42f46f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:27 +0000

    Add package zyrdeku/0.0.1/zyrdeku-0.0.1.tar.gz

---
+++ packages/zyrdeku/zyrdeku-0.0.1.tar.gz/zyrdeku-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name="zyrdeku",
+    version="0.0.1",
+    author="张焱睿",
+    author_email="hexin991@qq.com",
+    description="A small example package",
+    long_description="还海奥华",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamwusjki/pipa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
