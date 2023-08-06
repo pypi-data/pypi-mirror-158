d6b5b6512e8dd542633e4370b9d54500dafb4f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:21 +0000

    Add package pylejandria/0.1.0/pylejandria-0.1.0-py3-none-any.whl

---
+++ packages/pylejandria/pylejandria-0.1.0-py3-none-any.whl/pylejandria/__init__.py
@@ -0,0 +1,12 @@
+#################################
+#   MADE BY: ARMANDO CHAPARRO   #
+#################################
+
+import os
+
+MAFER = """
+It´s hard to forget
+someone who gave you
+so much to remember.
+I´ll always love you, Mafer.
+"""
