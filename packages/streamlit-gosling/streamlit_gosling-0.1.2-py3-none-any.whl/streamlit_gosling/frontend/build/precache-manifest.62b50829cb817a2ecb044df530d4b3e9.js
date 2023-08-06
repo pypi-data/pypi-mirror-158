ad1ea9018059ec58369a9a31e877d33af483fe68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:04 +0000

    Add package streamlit-gosling/0.1.2/streamlit_gosling-0.1.2-py3-none-any.whl

---
+++ packages/streamlit-gosling/streamlit_gosling-0.1.2-py3-none-any.whl/streamlit_gosling/frontend/build/precache-manifest.62b50829cb817a2ecb044df530d4b3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e10ac5e7071c7960d2dc4fbaba3a80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d1b6291d928bb573ef6",
+    "url": "./static/js/2.0b327732.chunk.js"
+  },
+  {
+    "revision": "5c9b11e84eea6a191c80674324a3dd81",
+    "url": "./static/js/2.0b327732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcc59ab00646d722915",
+    "url": "./static/js/main.8fd8fdba.chunk.js"
+  },
+  {
+    "revision": "7ef1753316713f9d3760",
+    "url": "./static/js/runtime-main.96a8387b.js"
+  }
+]);
