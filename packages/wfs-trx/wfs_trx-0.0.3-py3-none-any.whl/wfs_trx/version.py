10d2b9a3cff8ec56393d6cd4fc9252cbff987045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:48:54 +0000

    Add package wfs-trx/0.0.3/wfs_trx-0.0.3-py3-none-any.whl

---
+++ packages/wfs-trx/wfs_trx-0.0.3-py3-none-any.whl/wfs_trx/version.py
@@ -0,0 +1,11 @@
+author_info = (("Mehdi Kamani", "itsmehdi97@gmail.com"),)
+
+package_info = "generate xml request body for wfs write transactions."
+package_license = "MIT License"
+
+team_email = "itsmehdi97@gmail.com"
+
+version_info = (0, 0, 3)
+
+__author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
+__version__ = ".".join(map(str, version_info))
