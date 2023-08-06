3970d2db9d511f1c1ca129d1efd1e71720f7094a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:01 +0000

    Add package graphene-django-jwt-middleware/0.0.1/graphene-django-jwt-middleware-0.0.1.tar.gz

---
+++ packages/graphene-django-jwt-middleware/graphene-django-jwt-middleware-0.0.1.tar.gz/graphene-django-jwt-middleware-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['graphene_django_jwt_middleware']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=2.2.6,<3.0.0', 'pyjwt>=2.4.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'graphene-django-jwt-middleware',
+    'version': '0.0.1',
+    'description': 'Middleware to check JWT validation in GraphQL schemas',
+    'long_description': '# Graphene Django JWT Middleware\n\nThis is a [Graphene Django](https://docs.graphene-python.org/projects/django/en/latest/) middleware to check JWT in GraphQL schemas.\n',
+    'author': 'Instruct Developers',
+    'author_email': 'oss@instruct.com.br',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/instruct-br/graphene-django-jwt-middleware',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
