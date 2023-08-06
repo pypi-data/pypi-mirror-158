18c22b5c9e48d3ce0c163962ba807f81a20ad0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:16 +0000

    Add package vital/1.1.26/vital-1.1.26-py3-none-any.whl

---
+++ packages/vital/vital-1.1.26-py3-none-any.whl/vital/api/link.py
@@ -0,0 +1,72 @@
+from typing import Mapping, Optional
+
+from vital.api.api import API
+
+
+class Link(API):
+    """Endpoints for managing link tokens."""
+
+    def create(
+        self,
+        user_id: str,
+        provider: Optional[str] = None,
+        redirect_url: Optional[str] = None,
+    ) -> Mapping[str, str]:
+        """
+        Create a Link token.
+        :param str user_id: user's id returned by service.
+        """
+        return self.client.post(
+            "/link/token",
+            {"user_id": user_id, "provider": provider, "redirect_url": redirect_url},
+        )
+
+    def password_provider(
+        self, link_token: str, provider: str, username: str, password: str
+    ) -> Mapping[str, str]:
+        """
+        Connect a password auth provider.
+        :param str link_token: link_token created.
+        :param str provider: Provider name.
+        :param str username: username.
+        :param str password: password.
+        """
+        return self.client.post(
+            f"/link/provider/password/{provider}",
+            {"username": username, "password": password},
+            headers={"LinkToken": link_token},
+        )
+
+    def email_provider(
+        self, link_token: str, provider: str, email: str, region: Optional[str] = None
+    ) -> Mapping[str, str]:
+        """
+        Connect a password auth provider.
+        :param str link_token: link_token created.
+        :param str provider: Provider name.
+        :param str username: username.
+        :param str password: password.
+        """
+        body = {"email": email}
+        if region:
+            body["region"] = region
+        return self.client.post(
+            f"/link/provider/email/{provider}",
+            body,
+            headers={"x-vital-link-token": link_token},
+        )
+
+    def oauth_provider(
+        self,
+        link_token: str,
+        provider: str,
+    ) -> Mapping[str, str]:
+        """
+        Get link to oAuth provider.
+        :param str link_token: link_token created.
+        :param str provider: Provider name.
+        """
+        return self.client.get(
+            f"/link/provider/oauth/{provider}",
+            headers={"LinkToken": link_token},
+        )
