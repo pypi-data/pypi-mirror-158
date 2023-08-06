3c485ca0681f1b099f43b450e821a9d8921c5fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:11 +0000

    Add package ocrmypdf/13.6.1/ocrmypdf-13.6.1-py37-none-any.whl

---
+++ packages/ocrmypdf/ocrmypdf-13.6.1-py37-none-any.whl/ocrmypdf/pdfinfo/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# © 2015 James R. Barlow: github.com/jbarlow83
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+"""For extracting information about PDFs prior to OCR."""
+
+from ocrmypdf.pdfinfo.info import Colorspace, Encoding, PdfInfo
