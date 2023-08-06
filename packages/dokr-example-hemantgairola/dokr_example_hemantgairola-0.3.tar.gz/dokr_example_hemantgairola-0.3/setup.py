fdf3657feac8d7b8eddbb6aab8cfe51378f4b9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:49 +0000

    Add package dokr-example-hemantgairola/0.3/dokr_example_hemantgairola-0.3.tar.gz

---
+++ packages/dokr-example-hemantgairola/dokr_example_hemantgairola-0.3.tar.gz/dokr_example_hemantgairola-0.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name='dokr_example_hemantgairola',
+    version='0.3',
+    scripts=['dokr'] ,
+    author="Hemant Gairola",
+)
