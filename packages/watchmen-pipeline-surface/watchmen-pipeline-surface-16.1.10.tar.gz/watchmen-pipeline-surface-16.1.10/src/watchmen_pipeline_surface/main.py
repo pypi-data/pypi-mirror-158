df40259faf5cc2d3c74fa8351743ba607fdf6339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:40 +0000

    Add package watchmen-pipeline-surface/16.1.10/watchmen-pipeline-surface-16.1.10.tar.gz

---
+++ packages/watchmen-pipeline-surface/watchmen-pipeline-surface-16.1.10.tar.gz/watchmen-pipeline-surface-16.1.10/src/watchmen_pipeline_surface/main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from fastapi import APIRouter
+
+from .data import monitor_log_router, pipeline_trigger_router, topic_trigger_router
+
+
+def get_pipeline_surface_routers() -> List[APIRouter]:
+	return [
+		pipeline_trigger_router.router,
+		topic_trigger_router.router,
+		monitor_log_router.router
+	]
