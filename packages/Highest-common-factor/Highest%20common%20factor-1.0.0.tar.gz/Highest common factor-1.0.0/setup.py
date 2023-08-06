5ff8e579de2501741ac4ff9a78575af4590ae163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:48:26 +0000

    Add package Highest-common-factor/1.0.0/Highest%20common%20factor-1.0.0.tar.gz

---
+++ packages/Highest-common-factor/Highest%20common%20factor-1.0.0.tar.gz/Highest common factor-1.0.0/setup.py
@@ -0,0 +1,4 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(name='Highest common factor', version='1.0.0', long_description=Path("README.md").read_text(),
+                 packages=setuptools.find_packages(exclude=['tests', 'data']), url='https://pypi.org/')
