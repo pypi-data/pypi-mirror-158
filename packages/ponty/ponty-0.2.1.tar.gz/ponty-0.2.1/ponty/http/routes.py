08a026d060953781439d29b84c4f42ed75a3f448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:57 +0000

    Add package ponty/0.2.1/ponty-0.2.1.tar.gz

---
+++ packages/ponty/ponty-0.2.1.tar.gz/ponty-0.2.1/ponty/http/routes.py
@@ -0,0 +1,29 @@
+import typing
+
+import aiohttp.web
+
+
+_route_table = aiohttp.web.RouteTableDef()
+
+
+route = _route_table.route
+get = _route_table.get
+put = _route_table.put
+post = _route_table.post
+delete = _route_table.delete
+patch = _route_table.patch
+
+
+def route_iter() -> typing.Iterator[tuple[str, str]]:
+    for route in ( typing.cast(aiohttp.web.RouteDef, r) for r in _route_table ):
+        yield route.method, route.path
+
+
+def mount_routes(
+    app: aiohttp.web.Application,
+    *route_tables: aiohttp.web.RouteTableDef,
+) -> None:
+
+    app.router.add_routes(_route_table)
+    for rt in route_tables:
+        app.router.add_routes(rt)
