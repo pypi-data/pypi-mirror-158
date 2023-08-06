b3e4e2955edccb5e99057ca1ae7dceb1be19a3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:30 +0000

    Add package intasend-python/1.0.3/intasend-python-1.0.3.tar.gz

---
+++ packages/intasend-python/intasend-python-1.0.3.tar.gz/intasend-python-1.0.3/intasend/__init__.py
@@ -0,0 +1,20 @@
+"""IntaSend Payments API for Python 3."""
+from .collections import Collect
+from .transfers import Transfer
+from .wallets import Wallet
+from .chargebacks import Chagebacks
+from .payment_links import PaymentLinks
+from .devices import Devices
+from .customers import Customers
+
+
+class APIService:
+    def __init__(self, **kwargs):
+        """API Services Initialization."""
+        self.collect = Collect(**kwargs)
+        self.transfer = Transfer(**kwargs)
+        self.wallets = Wallet(**kwargs)
+        self.chargebacks = Chagebacks(**kwargs)
+        self.customers = Customers(**kwargs)
+        self.devices = Devices(**kwargs)
+        self.payment_links = PaymentLinks(**kwargs)
---
+++ packages/intasend-python/intasend-python-1.0.3.tar.gz/intasend-python-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='intasend-python',
+    packages=['intasend'],
+    version='1.0.3',
+    license='MIT',
+    description='Official Python SDK for IntaSend Payments Gateway API',
+    author='Felix Cheruiyot',
+    author_email='support@intasend.com',
+    url='https://github.com/IntaSend/intasend-python',
+    download_url='https://github.com/IntaSend/intasend-python/archive/v_1.0.3.tar.gz',
+    keywords=['payments', 'mpesa', 'card payments', 'visa',
+              'mastercard', 'payments kenya', 'intasend'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
