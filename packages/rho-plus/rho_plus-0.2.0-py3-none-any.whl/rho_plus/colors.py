c44acf4f2022ef9492cb43044f695a7c1f71ad9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:39 +0000

    Add package rho-plus/0.2.0/rho_plus-0.2.0-py3-none-any.whl

---
+++ packages/rho-plus/rho_plus-0.2.0-py3-none-any.whl/rho_plus/colors.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Colors used across themes."""
+
+LIGHT_COLORS = [
+    "#2005ff",
+    "#009917",
+    "#d56200",
+    "#ff2635",
+    "#c146ff",
+    "#5b00b3",
+    "#5879ff",
+    "#009489",
+    "#004367",
+    "#00461e",
+    "#900004",
+    "#682800",
+]
+
+DARK_COLORS = [
+    "#7075ff",
+    "#00e614",
+    "#ff963d",
+    "#ff6e81",
+    "#d06fe7",
+    "#8a50ff",
+    "#adc6ff",
+    "#00decf",
+    "#00a6f6",
+    "#008a33",
+    "#ef000c",
+    "#d34700",
+]
+
+
+# the neutrals are from https://elastic.github.io/eui/#/theming/colors
+# the naming is from a light-scheme perspective
+
+LIGHT_SHADES = [
+    # empty shade: primary background
+    "#FFFFFF",
+    # lightest shade: secondary background
+    "#F1F4FA",
+    # light shade: borders and dividers
+    "#D3DAE6",
+    # medium shade: subdued text
+    "#98A2B3",
+    # dark shade: secondary foreground
+    "#69707D",
+    # darkest shade: primary foreground, text
+    "#343741",
+]
+
+DARK_SHADES = [
+    # empty shade: primary background
+    "#1D1E24",
+    # lightest shade: secondary background
+    "#25262E",
+    # light shade: borders and dividers
+    "#343741",
+    # medium shade: subdued text
+    "#535966",
+    # dark shade: secondary foreground
+    "#98A2B3",
+    # darkest shade: primary foreground, text
+    "#D4DAE5",
+]
