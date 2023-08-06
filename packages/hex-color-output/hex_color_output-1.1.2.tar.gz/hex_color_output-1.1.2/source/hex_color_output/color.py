b0eb8b67aea36860da9e55f0d38ddace321899a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:44 +0000

    Add package hex-color-output/1.1.2/hex_color_output-1.1.2.tar.gz

---
+++ packages/hex-color-output/hex_color_output-1.1.2.tar.gz/hex_color_output-1.1.2/source/hex_color_output/color.py
@@ -0,0 +1,14 @@
+from HexOutput import Color, back
+
+print(back(Color.BLACK))
+print(back(Color.BLACK))
+print(back(Color.BLACK))
+print(back(Color.BLACK))
+print(back(Color.RED))
+print(back(Color.RED))
+print(back(Color.RED))
+print(back(Color.RED))
+print(back(Color.YELLOW))
+print(back(Color.YELLOW))
+print(back(Color.YELLOW))
+print(back(Color.YELLOW))
