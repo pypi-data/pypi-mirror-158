415f5f49e21f62eff08c559482340fd6fb9f1150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:00 +0000

    Add package recsplain/0.0.55/recsplain-0.0.55-py3-none-any.whl

---
+++ packages/recsplain/recsplain-0.0.55-py3-none-any.whl/recsplain/__init__.py
@@ -0,0 +1,4 @@
+__version__="0.0.55"
+from .strategies import BaseStrategy, AvgUserStrategy
+from .encoders import PartitionSchema
+from .endpoint import run_server
---
+++ packages/recsplain/recsplain-0.0.55-py3-none-any.whl/recsplain/__main__.py
@@ -0,0 +1,3 @@
+from endpoint import run_server
+if __name__=="__main__":
+    run_server()
