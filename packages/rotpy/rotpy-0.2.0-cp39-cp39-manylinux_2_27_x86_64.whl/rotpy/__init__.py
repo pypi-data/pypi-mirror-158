5c8bfed8e6777e7c1bcf565185b46f7ddb6aa221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:04 +0000

    Add package rotpy/0.2.0/rotpy-0.2.0-cp39-cp39-manylinux_2_27_x86_64.whl

---
+++ packages/rotpy/rotpy-0.2.0-cp39-cp39-manylinux_2_27_x86_64.whl/rotpy/__init__.py
@@ -0,0 +1,54 @@
+"""RotPy Library
+====================
+
+Project that provides python bindings for the Spinnaker SDK and provides
+access to their GigE and USB cameras.
+"""
+__version__ = '0.2.0'
+
+import sys
+import site
+import os
+from os.path import join
+
+__all__ = ('dep_bins', )
+
+dep_bins = []
+'''A list of paths to the binaries used by the library. It can be used during
+packaging for including required binaries.
+
+It is read only.
+'''
+
+_bitness = '64' if sys.maxsize > 2 ** 32 else '32'
+
+_dirs = [sys.prefix, site.USER_BASE]
+if hasattr(sys, '_MEIPASS'):
+    _dirs.append(sys._MEIPASS)
+for d in _dirs:
+    p = join(d, 'share', 'rotpy', 'spinnaker', 'bin')
+    if os.path.isdir(p):
+        os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
+        if hasattr(os, 'add_dll_directory'):
+            os.add_dll_directory(p)
+        dep_bins.append(p)
+
+    for p in (join(d, 'share', 'rotpy', 'spinnaker', 'cti'), d):
+        if os.path.isdir(p):
+            if os.path.exists(os.path.join(p, 'FLIR_GenTL.cti')):
+                os.environ[f'FLIR_GENTL{_bitness}_CTI'] = os.path.join(
+                    p, 'FLIR_GenTL.cti')
+            if os.path.exists(os.path.join(p, 'FLIR_GenTL_v140.cti')):
+                os.environ[f'FLIR_GENTL{_bitness}_CTI_VS140'] = os.path.join(
+                    p, 'FLIR_GenTL_v140.cti')
+
+            _name = f'GENICAM_GENTL{_bitness}_PATH'
+            if _name in os.environ:
+                os.environ[_name] = p + os.pathsep + os.environ[_name]
+            else:
+                os.environ[_name] = p
+
+            os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
+            if hasattr(os, 'add_dll_directory'):
+                os.add_dll_directory(p)
+            dep_bins.append(p)
