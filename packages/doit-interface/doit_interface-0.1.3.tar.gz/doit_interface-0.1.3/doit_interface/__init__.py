41be918a6ef74c61b6e6e3ef9471329452332abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:29 +0000

    Add package doit-interface/0.1.3/doit_interface-0.1.3.tar.gz

---
+++ packages/doit-interface/doit_interface-0.1.3.tar.gz/doit_interface-0.1.3/doit_interface/__init__.py
@@ -0,0 +1,22 @@
+from .actions import SubprocessAction
+from .config import DOIT_CONFIG
+from .contexts import create_target_dirs, defaults, group_tasks, normalize_dependencies, \
+    path_prefix, prefix
+from .manager import Manager
+from .reporters import DoitInterfaceReporter
+from .util import NoTasksError
+
+
+__all__ = [
+    "Manager",
+    "NoTasksError",
+    "create_target_dirs",
+    "defaults",
+    "group_tasks",
+    "normalize_dependencies",
+    "path_prefix",
+    "prefix",
+    "SubprocessAction",
+    "DoitInterfaceReporter",
+    "DOIT_CONFIG",
+]
