6495a6dc05f7ed9b3a5ba122989f3f82cd41f60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:08 +0000

    Add package cgdb-client/0.1.8/cgdb_client-0.1.8.tar.gz

---
+++ packages/cgdb-client/cgdb_client-0.1.8.tar.gz/cgdb_client-0.1.8/cgdb/recources/stationGroup.py
@@ -0,0 +1,16 @@
+class StationGroup:
+    def __init__(self,  id: int = None, code: str = None, name=None, description: int = None, client=None):
+        self.id = id
+        self.code = code
+        self.name = name
+        self.description = description
+
+        self._client = client
+
+    @property
+    def url(self):
+        if self.code is not None:
+            return "station-groups/" + self.code
+        else:
+            return "station-groups/id:" + str(self.id)
+
