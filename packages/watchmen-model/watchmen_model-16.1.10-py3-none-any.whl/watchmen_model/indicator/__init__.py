b3a4150761116cede7959b8474ec31854bf25b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:38 +0000

    Add package watchmen-model/16.1.10/watchmen_model-16.1.10-py3-none-any.whl

---
+++ packages/watchmen-model/watchmen_model-16.1.10-py3-none-any.whl/watchmen_model/indicator/__init__.py
@@ -0,0 +1,15 @@
+from .bucket import Bucket, BucketSegment, BucketType, CategoryMeasureBucket, CategorySegment, CategorySegmentsHolder, \
+	CategorySegmentValue, EnumMeasureBucket, MeasureBucket, NumericSegmentsHolder, NumericSegmentValue, \
+	NumericValueBucket, NumericValueMeasureBucket, NumericValueSegment, OtherCategorySegmentValue, \
+	RangeBucketValueIncluding
+from .indicator import Indicator, IndicatorAggregateArithmetic, IndicatorBaseOn, RelevantIndicator, \
+	RelevantIndicatorType
+from .inspection import Inspection, InspectionAmPmRange, InspectionDayKindRange, InspectionDayOfMonthRange, \
+	InspectionDayOfWeekRange, InspectionHalfMonthRange, InspectionHalfWeekRange, InspectionHalfYearRange, \
+	InspectionHourKindRange, InspectionHourRange, InspectionMonthRange, InspectionQuarterRange, \
+	InspectionTenDaysRange, InspectionTimeRange, InspectionTimeRangeType, InspectionWeekOfMonthRange, \
+	InspectionWeekOfYearRange, InspectionYearRange, InspectMeasureOn
+from .measure_method import MeasureMethod
+from .navigation import MANUAL_COMPUTE_NAVIGATION_INDICATOR_ID, ManualComputeNavigationIndicator, Navigation, \
+	NavigationIndicator, NavigationIndicatorCriteria, NavigationIndicatorCriteriaOnBucket, \
+	NavigationIndicatorCriteriaOnExpression, NavigationIndicatorCriteriaOperator, NavigationTimeRangeType
