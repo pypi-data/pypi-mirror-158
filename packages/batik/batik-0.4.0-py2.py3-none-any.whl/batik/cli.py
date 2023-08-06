2c207cafcad02e14e174c6116da456f50b25be2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:44 +0000

    Add package batik/0.4.0/batik-0.4.0-py2.py3-none-any.whl

---
+++ packages/batik/batik-0.4.0-py2.py3-none-any.whl/batik/cli.py
@@ -0,0 +1,62 @@
+"""
+batik
+
+Usage:
+  batik serve [--backend <backend>] [--hot-reload]
+  batik invoke <endpoint> [(--json <file> | <payload>)]
+  batik run <endpoint> [(--json <file> | <payload>)] [--trace]
+  batik build [--push]
+  batik validate
+
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+
+Examples:
+  batik serve
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/pfnsec/batik
+"""
+
+
+import sys
+import asyncio
+import inspect
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+import batik.commands
+
+def main():
+    if sys.platform == 'win32':
+        #loop = asyncio.ProactorEventLoop()
+        #asyncio.set_event_loop(loop)
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(co_main())
+    loop.close()
+
+async def co_main():
+    options = docopt(__doc__, version=VERSION)
+
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+
+    for (k, v) in options.items(): 
+        if hasattr(batik.commands, k) and v:
+            module = getattr(batik.commands, k)
+
+            classes = getmembers(module, isclass)
+
+            #command = [command[1] for command in classes if command[0] != 'Base'][0]
+            # What a hack! Oh well
+            command = [command[1] for command in classes if str(command[0]).lower() == k][0]
+            command = command(options)
+            if inspect.iscoroutinefunction(command.run):
+                await command.run()
+            else:
+                command.run()
---
+++ packages/batik/batik-0.4.0-py2.py3-none-any.whl/batik/frontends/http_basic.py
@@ -0,0 +1,141 @@
+
+import json
+from inspect import trace
+import aiohttp
+from aiohttp import web
+
+import asyncio
+
+from batik import server
+
+def custom_dumps(obj):
+    return json.dumps(
+        obj,
+        default=str
+    )
+
+class HTTPServer(server.Server):
+
+    def __init__(self, manifest):
+        super().__init__(manifest)
+        self.app = web.Application()
+        self.add_routes()
+
+    async def websocket_handler(self, request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+
+        async for msg in ws:
+            print(msg.data)
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                data = msg.json()
+                cmd = data['cmd']
+                if cmd == 'invoke':
+                    trace = self.manifest.create_trace()
+                    task = asyncio.get_event_loop().create_task(
+                        self.manifest.run_endpoint(
+                            data['endpoint'], 
+                            data['payload'],
+                            cast=True,
+                            trace=trace
+                        )
+                    )
+
+                    while True:
+                        trace_step = await trace.queue.get()
+                        if trace_step == None: break
+                        await ws.send_json({
+                            'timestamp': trace_step.timestamp.isoformat(),
+                            'node': trace_step.node,
+                            'data': trace_step.data,
+                        }, dumps=custom_dumps)
+                        trace.queue.task_done()
+                    await asyncio.gather(task)
+                    await ws.send_str('goodbye')
+
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print('ws connection closed with exception %s' %
+                    ws.exception())
+
+        return ws
+
+    # Get all endpoints
+    async def get_endpoints(self, request):
+        res = {
+            'endpoints': list(self.manifest.endpoints.keys())
+        }
+        return web.json_response(res)
+
+    async def get_endpoint(self, request):
+        endpoint = request.match_info['endpoint']
+        if endpoint not in self.manifest.endpoints:
+            raise aiohttp.web.HTTPNotFound()
+        else:
+            ep = self.manifest.get_endpoint(endpoint)
+            layers = []
+            for layer in ep.layers():
+                layers.append({''})
+            res = {
+                'layers': layers
+            }
+            return web.json_response(res)
+    
+    async def run_endpoint(self, request):
+        endpoint = request.match_info['endpoint']
+        if endpoint not in self.manifest.endpoints:
+            raise aiohttp.web.HTTPNotFound()
+
+        if request.body_exists:
+            payload = await request.json()
+            print(payload)
+
+        trace = self.manifest.create_trace()
+        asyncio.get_event_loop().create_task(
+            self.manifest.run_endpoint(
+                endpoint, payload,
+                cast=True,
+                trace=trace
+            )
+        )
+        res = {
+            'trace_id': trace.key
+        }
+        return web.json_response(res)
+
+    async def get_traces(self, request):
+        res = {
+            'traces': list(self.manifest.traces.keys())
+        }
+        return web.json_response(res)
+
+    async def get_trace(self, request):
+        trace_id = request.match_info['trace']
+        trace = self.manifest.get_trace(trace_id)
+        if trace is None:
+            raise aiohttp.web.HTTPNotFound()
+        else:
+            res = {
+                'trace_id': trace_id
+            }
+            return web.json_response(res)
+
+
+    def add_routes(self):
+        self.app.add_routes([
+            web.get('/ws/', self.websocket_handler),
+            web.get('/endpoint/', self.get_endpoints),
+            web.get('/endpoint/{endpoint}', self.get_endpoint),
+            web.post('/endpoint/{endpoint}/run', self.run_endpoint),
+            web.get('/trace/', self.get_traces),
+            web.get('/trace/{trace}', self.get_trace),
+        ])
+
+    async def run(self):
+        runner = aiohttp.web.AppRunner(self.app)
+        await runner.setup()
+        site = web.TCPSite(runner, 'localhost', 8086)
+        await site.start()
+        await self.manifest.daemon_task(trace=True)
+        while True:
+            await asyncio.sleep(1)
+
