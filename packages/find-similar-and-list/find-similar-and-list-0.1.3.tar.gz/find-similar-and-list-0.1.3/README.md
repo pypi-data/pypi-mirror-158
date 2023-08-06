797e3acce636ff1a3173c168a666eb5075e93f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:59 +0000

    Add package find-similar-and-list/0.1.3/find-similar-and-list-0.1.3.tar.gz

---
+++ packages/find-similar-and-list/find-similar-and-list-0.1.3.tar.gz/find-similar-and-list-0.1.3/README.md
@@ -0,0 +1,17 @@
+# find-similar-and-list
+## Installation
+```
+pip install find-similar-and-list
+```
+
+## How to use
+```
+usage: find-similar-and-list [-h] [--files FILES [FILES ...]]
+                             [--ignore-file IGNORE_FILE]
+
+options:
+  -h, --help            show this help message and exit
+  --files FILES [FILES ...]
+                        Files to filter
+  --ignore-file IGNORE_FILE
+```
---
+++ packages/find-similar-and-list/find-similar-and-list-0.1.3.tar.gz/find-similar-and-list-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['find_similar_and_list']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['find-similar-and-list = find_similar_and_list.cli:main']}
+
+setup_kwargs = {
+    'name': 'find-similar-and-list',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '# find-similar-and-list\n## Installation\n```\npip install find-similar-and-list\n```\n\n## How to use\n```\nusage: find-similar-and-list [-h] [--files FILES [FILES ...]]\n                             [--ignore-file IGNORE_FILE]\n\noptions:\n  -h, --help            show this help message and exit\n  --files FILES [FILES ...]\n                        Files to filter\n  --ignore-file IGNORE_FILE\n```\n',
+    'author': '0djentd',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/0djentd/find-similar-and-list',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
