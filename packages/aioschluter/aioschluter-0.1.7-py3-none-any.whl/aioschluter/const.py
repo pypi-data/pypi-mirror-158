5db56693cc5e48800e0286dd84e7762228b731d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package aioschluter/0.1.7/aioschluter-0.1.7-py3-none-any.whl

---
+++ packages/aioschluter/aioschluter-0.1.7-py3-none-any.whl/aioschluter/const.py
@@ -0,0 +1,12 @@
+""" constants for aioschluter """
+
+API_BASE_URL = "https://ditra-heat-e-wifi.schluter.com"
+API_AUTH_URL = API_BASE_URL + "/api/authenticate/user"
+API_GET_THERMOSTATS_URL = API_BASE_URL + "/api/thermostats"
+API_SET_TEMPERATURE_URL = API_BASE_URL + "/api/thermostat"
+API_APPLICATION_ID = 7
+HTTP_UNAUTHORIZED: int = 401
+HTTP_OK: int = 200
+HVAC_MODE_SCHEDULE = 1
+HVAC_MODE_MANUAL = 2
+HVAC_MODE_AWAY = 3
