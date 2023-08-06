26ab2f1ae77d0da8553b33807f5d59e43c55b1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package great-ai/0.0.12/great-ai-0.0.12.tar.gz

---
+++ packages/great-ai/great-ai-0.0.12.tar.gz/great-ai-0.0.12/README.md
@@ -0,0 +1,54 @@
+# GreatAI
+
+**work in progress, do not use!**
+
+[![Test](https://github.com/schmelczer/great-ai/actions/workflows/test.yml/badge.svg)](https://github.com/schmelczer/great-ai/actions/workflows/check.yml)
+[![Quality Gate Status](https://sonar.scoutinscience.com/api/project_badges/measure?project=great-ai&metric=alert_status)](https://sonar.schmelczer.com/dashboard?id=great-ai)
+[![Publish on PyPI](https://github.com/schmelczer/great-ai/actions/workflows/publish.yaml/badge.svg)](https://github.com/schmelczer/great-ai/actions/workflows/publish.yaml)
+[![Publish on DockerHub](https://github.com/schmelczer/great-ai/actions/workflows/docker.yaml/badge.svg)](https://github.com/schmelczer/great-ai/actions/workflows/docker.yaml)
+[![Downloads](https://pepy.tech/badge/great-ai/month)](https://pepy.tech/project/great-ai)
+
+
+## Find `great-ai` on [DockerHub](https://hub.docker.com/repository/docker/schmelczera/great-ai)
+
+```sh
+docker run -p6060:6060 schmelczera/great-ai
+```
+
+Find the dashboard at [http://localhost:6060](http://localhost:6060/dashboard/).
+
+
+## Find `great-ai` on [PyPI](https://pypi.org/project/great-ai/)
+
+```sh
+pip install great-ai
+```
+
+```python
+from great_ai import GreatAI
+
+@GreatAI.create
+def hello_world(name: str) -> str:
+    return f"Hello {name}!"
+```
+> Create a new file called `main.py`
+
+Deploy by executing `python3 -m great-ai main.py`
+> Or: `python3 -m great-ai main.py`
+
+Find the dashboard at [http://localhost:6060](http://localhost:6060/dashboard/).
+
+### Contribute
+
+
+#### Install
+
+```sh
+pip install 'great-ai[dev]'
+```
+
+#### Documentation
+
+```sh
+mkdocs serve
+```
---
+++ packages/great-ai/great-ai-0.0.12.tar.gz/great-ai-0.0.12/great_ai/parameters/parameter.py
@@ -0,0 +1,51 @@
+from functools import wraps
+from typing import Any, Callable, Dict, TypeVar, cast
+
+from typeguard import check_type
+
+from ..exceptions import ArgumentValidationError
+from ..helper import get_arguments, get_function_metadata_store
+from ..helper.assert_function_is_not_finalised import assert_function_is_not_finalised
+from ..tracing.tracing_context import TracingContext
+
+F = TypeVar("F", bound=Callable)
+
+
+def parameter(
+    parameter_name: str,
+    *,
+    validator: Callable[[Any], bool] = lambda _: True,
+    disable_logging: bool = False,
+) -> Callable[[F], F]:
+    def decorator(func: F) -> F:
+        get_function_metadata_store(func).input_parameter_names.append(parameter_name)
+        assert_function_is_not_finalised(func)
+
+        actual_name = f"arg:{parameter_name}"
+
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Dict[str, Any]) -> Any:
+            arguments = get_arguments(func, args, kwargs)
+            argument = arguments.get(parameter_name)
+
+            expected_type = func.__annotations__.get(parameter_name)
+
+            if expected_type is not None:
+                check_type(parameter_name, argument, expected_type)
+
+            if not validator(argument):
+                raise ArgumentValidationError(
+                    f"Argument {parameter_name} in {func.__name__} did not pass validation"
+                )
+
+            context = TracingContext.get_current_tracing_context()
+            if context and not disable_logging:
+                context.log_value(name=f"{actual_name}:value", value=argument)
+                if isinstance(argument, str):
+                    context.log_value(name=f"{actual_name}:length", value=len(argument))
+
+            return func(*args, **kwargs)
+
+        return cast(F, wrapper)
+
+    return decorator
