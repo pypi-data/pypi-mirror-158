2b8b6997e66197d7a6cfe7ebf11096df20a22b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:32 +0000

    Add package easyplayer/0.2.2/easyplayer-0.2.2.tar.gz

---
+++ packages/easyplayer/easyplayer-0.2.2.tar.gz/easyplayer-0.2.2/test/test.py
@@ -0,0 +1,7 @@
+import easyplayer as ep
+from easyplayer.core import *
+from easyplayer.utils import *
+from easyplayer.core.widgets import *
+from easyplayer.utils.senior import *
+from easyplayer.utils.wintools import *
+from easyplayer.utils.cs import *
