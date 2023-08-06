aa4f7b8c4954b44e9ed6e8afe6f00c9129985973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:47 +0000

    Add package typed-environment-configuration/0.1.5/typed_environment_configuration-0.1.5-py2.py3-none-any.whl

---
+++ packages/typed-environment-configuration/typed_environment_configuration-0.1.5-py2.py3-none-any.whl/typed_environment_configuration/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Typed Environment Configuration."""
+
+__author__ = """Eugene Dementyev"""
+__email__ = "eugene@springload.co.nz"
+__version__ = '0.1.5'
+
+from .typed_environment_configuration import *
