823c82ccdf25db005c750b63108e2e8021ba46c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:16 +0000

    Add package python-ramda/0.6.0/python_ramda-0.6.0-py3-none-any.whl

---
+++ packages/python-ramda/python_ramda-0.6.0-py3-none-any.whl/ramda/private/_stepCat.py
@@ -0,0 +1,40 @@
+from ..objOf import objOf
+from ._identity import _identity
+from ._isArrayLike import _isArrayLike
+from ._isTransformer import _isTransformer
+
+
+def _array_step(xs, x):
+  return xs + [x]
+
+
+_stepCatArray = {
+    '@@transducer/init': list,
+    '@@transducer/step': _array_step,
+    '@@transducer/result': _identity
+}
+_stepCatString = {
+    '@@transducer/init': str,
+    '@@transducer/step': lambda a, b: str(a) + str(b),
+    '@@transducer/result': _identity
+}
+
+_stepCatDict = {
+    '@@transducer/init': dict,
+    '@@transducer/step': lambda result, input: {**result, **(objOf(input[0], input[1]) if _isArrayLike(input) else input)},
+    '@@transducer/result': _identity
+}
+
+# TODO: add _stepCatObject
+
+
+def _stepCat(obj):
+  if _isTransformer(obj):
+    return obj
+  if _isArrayLike(obj):
+    return _stepCatArray
+  if isinstance(obj, str):
+    return _stepCatString
+  if isinstance(obj, dict):
+    return _stepCatDict
+  raise Exception(f'Cannot create transformer for {obj}')
