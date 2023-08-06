e204aa94a8eaaa9c483ede000f5f7815f18c3d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:50 +0000

    Add package pyremoteplay/0.7.2/pyremoteplay-0.7.2-py3-none-any.whl

---
+++ packages/pyremoteplay/pyremoteplay-0.7.2-py3-none-any.whl/pyremoteplay/gui/__main__.py
@@ -0,0 +1,26 @@
+"""GUI Main Methods for pyremoteplay."""
+import sys
+import logging
+
+from PySide6 import QtCore, QtWidgets
+
+from .main_window import MainWindow
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    """Run GUI."""
+    QtWidgets.QApplication.setHighDpiScaleFactorRoundingPolicy(
+        QtCore.Qt.HighDpiScaleFactorRoundingPolicy.Floor
+    )
+    app = QtWidgets.QApplication([])
+    app.setApplicationName("PyRemotePlay")
+    widget = MainWindow()
+    widget.resize(800, 600)
+    widget.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
