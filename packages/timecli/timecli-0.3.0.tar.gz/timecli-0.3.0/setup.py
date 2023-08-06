8bf0b3066e2cff517f4495a14aa58386989b579a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:37 +0000

    Add package timecli/0.3.0/timecli-0.3.0.tar.gz

---
+++ packages/timecli/timecli-0.3.0.tar.gz/timecli-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['timecli', 'timecli.commands', 'timecli.core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['toml>=0.10.2,<0.11.0', 'typer[all]>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['timecli = timecli.main:app']}
+
+setup_kwargs = {
+    'name': 'timecli',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': '# Time\n\n## Description\nThe simple time cli.\n',
+    'author': 'gelleson',
+    'author_email': 'go.gelleson@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
