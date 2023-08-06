579df585c4b926bc4a46756024175474c190b007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:56 +0000

    Add package AioCrypto/1.0.2/AioCrypto-1.0.2.tar.gz

---
+++ packages/AioCrypto/AioCrypto-1.0.2.tar.gz/AioCrypto-1.0.2/aiocrypto/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2022 AioCrypto
+
+__author__ = "ErVinTract"
+__description__ = "Async Crypto API for humans"
+
+__github__ = "https://github.com/ErVinTract/AioCrypto"
+
+__release__ = "1.0.2"
+__version__ = "1.0.2b"
+
+
+from .Errors import *
+from .types import *
+from .app import *
