6c61789b2d78f1709e7de102a123fe0b1d97f59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:07 +0000

    Add package tablite/2022.7.dev5/tablite-2022.7.dev5.tar.gz

---
+++ packages/tablite/tablite-2022.7.dev5.tar.gz/tablite-2022.7.dev5/setup.py
@@ -0,0 +1,65 @@
+"""
+tablite
+"""
+from setuptools import setup
+from pathlib import Path
+
+from tablite import __version__
+
+
+folder = Path(__file__).parent
+for file in (folder / 'dist').iterdir():
+    if __version__ in file.name:
+        raise ValueError(f"{__version__} already in dist")
+
+file = "README.md"
+readme = folder / file
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+keywords = list({
+    'tablite', 'table', 'tables', 'csv', 'txt', 'excel', 'xlsx', 'ods', 'zip', 'log',
+    'any', 'all', 'filter', 'column', 'columns', 'rows', 'from', 'json', 'to',
+    'inner join', 'outer join', 'left join', 'groupby', 'pivot', 'pivot table',
+    'sort', 'is sorted', 'show', 'use disk', 'out-of-memory', 'list on disk',
+    'stored list', 'min', 'max', 'sum', 'first', 'last', 'count', 'unique',
+    'average', 'standard deviation', 'median', 'mode', 'in-memory', 'index',
+    'indexing', 'product'
+})
+
+keywords.sort(key=lambda x: x.lower())
+
+with open('requirements.txt', 'r') as fi:
+    requirements = [v.rstrip('\n') for v in fi.readlines()]
+
+setup(
+    name="tablite",
+    version=__version__,  
+    url="https://github.com/root-11/tablite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="dr.bjorn.madsen@gmail.com",
+    description="A library for cleaning tabular data.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=keywords,
+    packages=["tablite"],
+    include_package_data=True,
+    data_files=[(".", ["LICENSE", "README.md", "requirements.txt"])],
+    platforms="any",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
+
