612e1fe47e69c52b25e046699867b6c7ac6de8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:39 +0000

    Add package watchmen-rest/16.1.10/watchmen-rest-16.1.10.tar.gz

---
+++ packages/watchmen-rest/watchmen-rest-16.1.10.tar.gz/watchmen-rest-16.1.10/src/watchmen_rest/util/raise_http_exception.py
@@ -0,0 +1,40 @@
+from logging import getLogger
+from typing import Any, Optional
+
+from fastapi import HTTPException
+from starlette import status
+
+from watchmen_utilities import is_not_blank
+
+logger = getLogger(__name__)
+
+
+def raise_400(detail: Any) -> None:
+	raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+
+def raise_401(detail: Any, headers: Optional[dict] = None) -> None:
+	if headers is not None:
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail, headers=headers)
+	else:
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+
+
+def raise_403() -> None:
+	raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized visit.")
+
+
+def raise_404(detail: Any = 'Data not found.') -> None:
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+def raise_500(e: Optional[Exception] = None, detail: Optional[str] = 'Unpredicted exception occurred.') -> None:
+	if e is not None:
+		logger.error(e, exc_info=True, stack_info=True)
+	if is_not_blank(detail):
+		logger.error(detail)
+
+	raise HTTPException(
+		status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+		detail=detail
+	)
