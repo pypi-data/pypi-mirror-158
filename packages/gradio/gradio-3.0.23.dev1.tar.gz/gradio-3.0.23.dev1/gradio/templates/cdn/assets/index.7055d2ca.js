f3b2160d6fc96dfc13f5de6f46052586bf05e782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:20 +0000

    Add package gradio/3.0.23.dev1/gradio-3.0.23.dev1.tar.gz

---
+++ packages/gradio/gradio-3.0.23.dev1.tar.gz/gradio-3.0.23.dev1/gradio/templates/cdn/assets/index.7055d2ca.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.50dc598e.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.0.23.dev1.tar.gz/gradio-3.0.23.dev1/gradio/templates/frontend/assets/index.ef9c203a.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.47d775d3.js';
+
+const modes = ["static"];
+
+export { modes };
