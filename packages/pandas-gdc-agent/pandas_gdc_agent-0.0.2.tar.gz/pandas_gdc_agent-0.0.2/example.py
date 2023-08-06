e1368328194ebacf2be7993f4784bb8799fad778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:19 +0000

    Add package pandas-gdc-agent/0.0.2/pandas_gdc_agent-0.0.2.tar.gz

---
+++ packages/pandas-gdc-agent/pandas_gdc_agent-0.0.2.tar.gz/pandas_gdc_agent-0.0.2/example.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from src.pandas_gdc_agent import GDCAgent
+
+# setting up dataframes
+df1 = pd.read_csv('chinook/Artist.csv')
+df1.set_index("Id", inplace=True)
+df2 = pd.read_csv("chinook/Album.csv")
+df2.set_index("Id", inplace=True)
+
+# setting up GDC Agent
+agent = GDCAgent([("Artist",df1),("Album", df2)])
+
+# running the GDC Agent
+agent.run_agent()
