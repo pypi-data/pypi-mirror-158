78b480acabcaf93f1933b1355d0aa1d65282da6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:17 +0000

    Add package snips-skill/0.1.8/snips-skill-0.1.8.tar.gz

---
+++ packages/snips-skill/snips-skill-0.1.8.tar.gz/snips-skill-0.1.8/snips_skill/logging.py
@@ -0,0 +1,39 @@
+from colors import cyan, green, magenta, red, yellow
+import logging
+
+
+__all__ = ('LoggingMixin', )
+
+
+class LoggingMixin:
+    'Logging for Snips events'
+    
+    INDENT = 10
+
+    def colored_log(self, level, format, *args, color=None):
+        if color and self.tty_log: args = map(color, args)
+        self.log.log(level, format, *args)
+    
+
+    def tabular_log(self, level, key, value, color=None, width=INDENT):
+        label = '%-*s' % (width, key)
+        self.colored_log(level, '%s %s', label, str(value), color=color)
+    
+    
+    def log_intent(self, payload, level=logging.DEBUG):
+        'Log an intent message'
+        self.tabular_log(level, 'intent', '%s, confidence: %.1f' % (
+            red(payload.intent.intent_name, style='bold'),
+            payload.intent.confidence_score), color=green)
+        for k in ('site_id', 'input'):
+            self.tabular_log(level, k, getattr(payload, k), color=cyan)
+        for name, slot in payload.slots.items():
+            self.tabular_log(level, name, slot.value, color=magenta)
+        if payload.custom_data:
+            self.tabular_log(level, 'data', payload.custom_data, color=yellow)
+            
+    
+    def log_response(self, response, level=logging.DEBUG):
+        'Log an action response'
+        if response: self.tabular_log(level, 'answer',
+            red(response, style='bold'), color=green)
