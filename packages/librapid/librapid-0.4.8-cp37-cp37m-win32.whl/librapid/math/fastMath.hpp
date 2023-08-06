bf892a613f73626dbe1ba5158434ec7aab144f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:57 +0000

    Add package librapid/0.4.8/librapid-0.4.8-cp37-cp37m-win32.whl

---
+++ packages/librapid/librapid-0.4.8-cp37-cp37m-win32.whl/librapid/math/fastMath.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../internal/config.hpp"
+
+namespace librapid {
+	float sqrtApprox(float);
+	float invSqrtApprox(float);
+} // namespace librapid
+
+/*
+ * Sources:
+ *
+ * sqrtApprox,
+ * invSqrtApprox => https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+ *
+ */
