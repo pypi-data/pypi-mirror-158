b662b448dfa81b66b8cad2e44ed98bc06dc98582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:29 +0000

    Add package Atom-avatar/0.0.3/Atom_avatar-0.0.3.tar.gz

---
+++ packages/Atom-avatar/Atom_avatar-0.0.3.tar.gz/Atom_avatar-0.0.3/setup.py
@@ -0,0 +1,11 @@
+import glob
+import setuptools
+
+setuptools.setup(
+    name="Atom_avatar",
+    version="0.0.3",
+    author="Quadri Tosin Micheal",
+    description="python avater maker with avatermaker.com svg",
+    packages=["avatar"],
+include_package_data=True
+)
