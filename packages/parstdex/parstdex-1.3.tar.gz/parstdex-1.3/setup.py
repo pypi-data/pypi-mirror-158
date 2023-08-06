fa3725bbb0366a1cab81e4da13ce33fac2fab097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:51 +0000

    Add package parstdex/1.3/parstdex-1.3.tar.gz

---
+++ packages/parstdex/parstdex-1.3.tar.gz/parstdex-1.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", encoding='utf-8-sig') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='parstdex',
+    version="1.3",
+    description="Persian time and date marker extractor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kargaranamir/parstdex',
+    packages=setuptools.find_packages(),
+    package_data={
+        'parstdex.utils': ['pattern/*.txt',
+                           'annotation/ax/*.txt',
+                           'annotation/adv/*.txt',
+                           'annotation/date/*.txt',
+                           'annotation/time/*.txt',
+                           'special_words/*.txt'],
+        'tests': ['data.json']
+    },
+    include_package_data=True,
+    classifiers=[
+        'Topic :: Text Processing',
+        'Natural Language :: Persian',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=required,
+    author='Amir Hossein Kargaran, Sajad Mirzababaei',
+    author_email='kargaranamir@gmail.com, ss.mirzababaei@gmail.com'
+)
