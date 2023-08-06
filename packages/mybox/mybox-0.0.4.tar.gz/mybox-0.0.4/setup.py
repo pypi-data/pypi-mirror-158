ae2b0a820addcf0d37e76f49149fda4603ebdb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:56 +0000

    Add package mybox/0.0.4/mybox-0.0.4.tar.gz

---
+++ packages/mybox/mybox-0.0.4.tar.gz/mybox-0.0.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mybox', 'mybox.package', 'mybox.state']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'requests>=2.28.1,<3.0.0', 'tqdm>=4.64.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['mybox = mybox.main:main']}
+
+setup_kwargs = {
+    'name': 'mybox',
+    'version': '0.0.4',
+    'description': 'Manage the configuration and tools on your workstation without bothering the OS too much',
+    'long_description': "# Mybox\n\n🖥️ This is a box. 📦 And it is mine. 🐱\n\nThere are many 🍱 nice things in there. I wouldn't want 🧰 to be without them.\n\nEven if I move 🏠 or work 🏢 I want to be comfortable.\n\n---\n\nManage the configuration and tools on your workstation without bothering the OS\ntoo much (maybe your favorite one isn't supported by `$WORK` or you have\ndifferent ones for different roles).\n\n## Status\n\nIn development, for the current horrible state of affairs see\n[install](https://github.com/koterpillar/desktop/blob/main/install).\n",
+    'author': 'Alexey Kotlyarov',
+    'author_email': 'a@koterpillar.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/koterpillar/mybox',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
