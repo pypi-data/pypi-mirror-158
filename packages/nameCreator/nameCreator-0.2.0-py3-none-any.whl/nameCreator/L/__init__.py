5b8a8c6068159c79e856b88478924a97be088d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package nameCreator/0.2.0/nameCreator-0.2.0-py3-none-any.whl

---
+++ packages/nameCreator/nameCreator-0.2.0-py3-none-any.whl/nameCreator/L/__init__.py
@@ -0,0 +1,7 @@
+"""
+Generate a last name
+
+"""
+
+__version__ = "0.2.0"
+__author__ = 'Justin Huddleston'
---
+++ packages/nameCreator/nameCreator-0.2.0-py3-none-any.whl/nameCreator/M/__init__.py
@@ -0,0 +1,7 @@
+"""
+Generate a male first name
+
+"""
+
+__version__ = "0.2.0"
+__author__ = 'Justin Huddleston'
