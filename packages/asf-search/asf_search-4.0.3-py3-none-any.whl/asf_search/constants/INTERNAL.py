225b0cec0d5861f37d0ad38a6021a8890fec270d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:21 +0000

    Add package asf-search/4.0.3/asf_search-4.0.3-py3-none-any.whl

---
+++ packages/asf-search/asf_search-4.0.3-py3-none-any.whl/asf_search/constants/INTERNAL.py
@@ -0,0 +1,15 @@
+SEARCH_API_HOST = 'api.daac.asf.alaska.edu'
+ASF_AUTH_HOST = 'auth.asf.alaska.edu'
+
+HEALTH_PATH = '/health'
+SEARCH_PATH = '/services/search/param'
+BASELINE_PATH = '/services/search/baseline'
+MISSION_PATH = '/services/utils/mission_list'
+DATE_PATH = '/services/utils/date'
+
+CMR_HOST = 'cmr.earthdata.nasa.gov'
+CMR_COLLECTIONS = '/search/collections.umm_json_v1_4'
+EDL_HOST = 'urs.earthdata.nasa.gov'
+EDL_CLIENT_ID = 'BO_n7nTIlMljdvU6kRRB3g'
+
+DEFAULT_PROVIDER = 'ASF'
