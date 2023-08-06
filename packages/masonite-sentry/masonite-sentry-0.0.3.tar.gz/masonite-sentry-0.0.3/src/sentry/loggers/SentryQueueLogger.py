586193aca676abbc68152b4fcf38b913aebf48d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:07 +0000

    Add package masonite-sentry/0.0.3/masonite-sentry-0.0.3.tar.gz

---
+++ packages/masonite-sentry/masonite-sentry-0.0.3.tar.gz/masonite-sentry-0.0.3/src/sentry/loggers/SentryQueueLogger.py
@@ -0,0 +1,23 @@
+import logging
+from sentry_sdk import add_breadcrumb
+
+
+class SentryQueryLogger(logging.Handler):
+    def __init__(self, level=logging.DEBUG):
+        super().__init__(level)
+
+    def handle(self, log):
+
+        add_breadcrumb(
+            category="query",
+            message=log.msg,
+            level="info",
+            data={
+                "time": f"{log.query_time}ms",
+                "query_time": log.query_time,
+                "query": log.query,
+                "bindings": log.bindings,
+                "level": log.levelname,
+                "name": log.name,
+            },
+        )
