c292b9b64a0b8f2e5d7a54b2cfd9859503de1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:33 +0000

    Add package logger-util/0.3.0/logger_util-0.3.0.tar.gz

---
+++ packages/logger-util/logger_util-0.3.0.tar.gz/logger_util-0.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='logger_util',
+      version='0.3.0',
+      description='Dead simple Python logger. Redirects stdout to a file while maintaining console printing.',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8'
+      ],
+      keywords='logger_util logging python wsarce',
+      url='https://github.com/wsarce/logger-util',
+      author='Walker Arce (wsarce)',
+      author_email='wsarcera@gmail.com',
+      license='MIT',
+      packages=['logger_util'],
+      install_requires=[
+          'wmi'
+      ],
+      include_package_data=True,
+      zip_safe=False
+      )
