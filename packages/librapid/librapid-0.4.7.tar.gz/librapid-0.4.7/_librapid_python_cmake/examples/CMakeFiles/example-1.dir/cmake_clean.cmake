af6a019793aa2ed407dce164ae4d0f54cf10155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:57 +0000

    Add package librapid/0.4.7/librapid-0.4.7.tar.gz

---
+++ packages/librapid/librapid-0.4.7.tar.gz/librapid-0.4.7/_librapid_python_cmake/examples/CMakeFiles/example-1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example-1.dir/example-1.cpp.o"
+  "CMakeFiles/example-1.dir/example-1.cpp.o.d"
+  "example-1"
+  "example-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
