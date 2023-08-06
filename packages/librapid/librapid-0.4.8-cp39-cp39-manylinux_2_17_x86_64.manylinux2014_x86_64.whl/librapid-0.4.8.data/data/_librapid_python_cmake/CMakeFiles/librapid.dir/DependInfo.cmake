295e2fca7d87ff602b40e17434e1e918006744f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:09 +0000

    Add package librapid/0.4.8/librapid-0.4.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.8-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.8.data/data/_librapid_python_cmake/CMakeFiles/librapid.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/src/librapid/src/cudaCodeLoader.cpp" "CMakeFiles/librapid.dir/src/librapid/src/cudaCodeLoader.cpp.o" "gcc" "CMakeFiles/librapid.dir/src/librapid/src/cudaCodeLoader.cpp.o.d"
+  "/project/src/librapid/src/dotCBlas.cpp" "CMakeFiles/librapid.dir/src/librapid/src/dotCBlas.cpp.o" "gcc" "CMakeFiles/librapid.dir/src/librapid/src/dotCBlas.cpp.o.d"
+  "/project/src/librapid/src/fastMath.cpp" "CMakeFiles/librapid.dir/src/librapid/src/fastMath.cpp.o" "gcc" "CMakeFiles/librapid.dir/src/librapid/src/fastMath.cpp.o.d"
+  "/project/src/librapid/src/kernelHelper.cpp" "CMakeFiles/librapid.dir/src/librapid/src/kernelHelper.cpp.o" "gcc" "CMakeFiles/librapid.dir/src/librapid/src/kernelHelper.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/project/_librapid_python_cmake/src/librapid/vendor/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/scnlib/CMakeFiles/scn.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/CMakeFiles/Vc.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/mpir/CMakeFiles/mpir.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
