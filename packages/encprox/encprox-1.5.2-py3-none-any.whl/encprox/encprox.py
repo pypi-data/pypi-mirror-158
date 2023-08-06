63054b29933b3ee089b57ecca4dbda9c1f98fb48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:37 +0000

    Add package encprox/1.5.2/encprox-1.5.2-py3-none-any.whl

---
+++ packages/encprox/encprox-1.5.2-py3-none-any.whl/encprox/encprox.py
@@ -0,0 +1,20 @@
+from itertools import cycle
+import os
+
+class prox:
+    def proxy_format(self, file):
+        if os.path.exists(file) == False:
+            return 'Provide a valid file.'
+
+        with open(file, 'r') as temp_file:
+            try:
+                proxy = [line.rstrip('\n') for line in temp_file]
+            except:
+                return 'The provided file is empty.'
+
+            proxy_pool = cycle(proxy)
+            proxy = next(proxy_pool)
+            if len(proxy.split(':')) == 4:
+                splitted = proxy.split(':') 
+                return f'http://{splitted[2]}:{splitted[3]}@{splitted[0]}:{splitted[1]}'
+            return f'http://{proxy}'
