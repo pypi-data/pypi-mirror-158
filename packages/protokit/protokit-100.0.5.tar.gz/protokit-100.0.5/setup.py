51d60d2ef768462cc2ec135652b687bc39d957c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:50 +0000

    Add package protokit/100.0.5/protokit-100.0.5.tar.gz

---
+++ packages/protokit/protokit-100.0.5.tar.gz/protokit-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="protokit",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="protokit of protokit of protokit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
