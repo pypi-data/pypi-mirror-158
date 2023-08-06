f5c931d367a53acfe50c53ed560a38179531fe82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:31 +0000

    Add package hammerspace-api/0.0/hammerspace-api-0.0.tar.gz

---
+++ packages/hammerspace-api/hammerspace-api-0.0.tar.gz/hammerspace-api-0.0/setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+args = ' '.join(sys.argv).strip()
+if not any(args.endswith(suffix) for suffix in ['setup.py check -r -s', 'setup.py sdist']):
+    raise ImportError('Parked for upcoming work on the Pocket/Hammerspace project. If you believe that it has been parked in error, please contact the package owner.')
+
+setup(
+    author='Fakas',
+    author_email='matthew@caza.ly',
+    classifiers=['Development Status :: 7 - Inactive'],
+    description='Parked for upcoming work on the Pocket/Hammerspace project. If you believe that it has been parked in error, please contact the package owner.',
+    long_description='Parked for upcoming work on the Pocket/Hammerspace project. If you believe that it has been parked in error, please contact the package owner.',
+    name='hammerspace-api',
+    url='https://github.com/MCazaly',
+    version='0.0'
+)
