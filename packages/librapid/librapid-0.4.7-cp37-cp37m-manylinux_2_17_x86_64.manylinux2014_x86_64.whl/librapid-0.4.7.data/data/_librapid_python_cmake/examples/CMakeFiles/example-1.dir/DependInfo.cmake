c1dd311c0223154217eb11fbbcf73e9ba2e5a7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:54 +0000

    Add package librapid/0.4.7/librapid-0.4.7-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/librapid/librapid-0.4.7-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/librapid-0.4.7.data/data/_librapid_python_cmake/examples/CMakeFiles/example-1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/examples/example-1.cpp" "examples/CMakeFiles/example-1.dir/example-1.cpp.o" "gcc" "examples/CMakeFiles/example-1.dir/example-1.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/project/_librapid_python_cmake/CMakeFiles/librapid.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/scnlib/CMakeFiles/scn.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/Vc/CMakeFiles/Vc.dir/DependInfo.cmake"
+  "/project/_librapid_python_cmake/src/librapid/vendor/mpir/CMakeFiles/mpir.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
