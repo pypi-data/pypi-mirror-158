4306bc1cb49c83196a926495c0ba9c0ca0200844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:51:04 +0000

    Add package stixdcpy/2.0/stixdcpy-2.0.tar.gz

---
+++ packages/stixdcpy/stixdcpy-2.0.tar.gz/stixdcpy-2.0/stixdcpy/io.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import joblib
+import numpy as np
+import pandas as pd
+
+from stixdcpy.logger import logger
+'''
+    Methods in this module allow to save/load  objects to/from  npy  files
+
+    Author: Hualin Xiao (hualin.xiao@fhnw.ch)
+    Date: Sep. 1, 2021
+
+'''
+ALLOWED_EXTENSIONS = ['.pickle', '.npy', '.h5', '.fits']
+
+
+class IO(object):
+    """
+        Base object
+    """
+    def __init__(self):
+        pass
+
+    def dump(self, data, filename):
+        """Dump object to joblib file
+        Args
+            data: object
+                data object
+            filename: str
+                filename
+
+        Returns:
+            filename: str
+                filename
+        """
+        joblib.dump(data, filename)
+        return filename
+        #logger.error("this feature has not been implemented!")
+        # pass
+
+    def load(self, filename):
+        """load object from file
+
+        Args:
+            filename:  str
+
+        Returns:
+            data: object
+
+
+        """
+        return joblib.load(filename)
+        #logger.error("this feature has not been implemented!")
+        # pass
+
+    def to_pandas(self):
+        if self.data:
+            return pd.DataFrame(self.data)
+        return None
