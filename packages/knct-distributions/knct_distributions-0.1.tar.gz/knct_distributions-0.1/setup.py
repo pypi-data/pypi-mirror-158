eff06457e3de899ce43f0d96ba630ef4c1d83b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:00 +0000

    Add package knct-distributions/0.1/knct_distributions-0.1.tar.gz

---
+++ packages/knct-distributions/knct_distributions-0.1.tar.gz/knct_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='knct_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Saurabh Sharma',
+      author_email='srbh453@gmail.com',
+      zip_safe=False)
