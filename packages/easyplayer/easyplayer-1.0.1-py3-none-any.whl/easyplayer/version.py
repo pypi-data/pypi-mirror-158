711a6f22c2c546a5fc7e13cf7fb461e3dfe73229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:36 +0000

    Add package easyplayer/1.0.1/easyplayer-1.0.1-py3-none-any.whl

---
+++ packages/easyplayer/easyplayer-1.0.1-py3-none-any.whl/easyplayer/version.py
@@ -0,0 +1,33 @@
+"""
+Easy Player's version manager.
+You can use it for compatibility checks.
+
+Such as:
+
+>>> import easyplayer as ep
+>>> if ep.version.major != 0:
+>>>     raise SystemExit('Version wrong.')
+"""
+
+from typing import Tuple
+
+__all__ = ['version']
+
+
+class _Version(tuple):
+    def __init__(self, *nums: Tuple[int]):
+        """
+        Easy Player version class.
+        
+        :param nums: Major, minor and micro.
+        """
+        self.major, self.minor, self.micro = nums
+    
+    def __new__(cls, *args):
+        return tuple.__new__(cls, args)
+    
+    def __str__(self):
+        return '{}.{}.{}'.format(*self)
+    
+    
+version = _Version(1, 0, 1)
