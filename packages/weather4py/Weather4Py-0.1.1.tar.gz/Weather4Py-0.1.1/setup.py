1e2e9554e2d7cc2d188760bc5116fdab92238fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:54 +0000

    Add package weather4py/0.1.1/Weather4Py-0.1.1.tar.gz

---
+++ packages/weather4py/Weather4Py-0.1.1.tar.gz/Weather4Py-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['weather4py']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'weather4py',
+    'version': '0.1.1',
+    'description': 'a library for your weathery needs.',
+    'long_description': None,
+    'author': 'MintTeaNeko',
+    'author_email': 'neonwinternight1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
