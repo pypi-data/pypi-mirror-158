19aa39968e29f071e92f17ba1b542150cc43047b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:17 +0000

    Add package jhcrawler/0.0.5/jhcrawler-0.0.5.tar.gz

---
+++ packages/jhcrawler/jhcrawler-0.0.5.tar.gz/jhcrawler-0.0.5/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="jhcrawler",
+    version="0.0.5",
+    description="*&*(&^*&*",
+    author='sjh',
+    author_email='1607837367@qq.com',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    url="https://github.com/apammaaaa/jhcrawler"
+)
+    
