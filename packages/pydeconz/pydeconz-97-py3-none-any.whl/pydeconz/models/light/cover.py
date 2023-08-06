1e97f4cef738406249ba1d01376246750bc8e3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:40 +0000

    Add package pydeconz/97/pydeconz-97-py3-none-any.whl

---
+++ packages/pydeconz/pydeconz-97-py3-none-any.whl/pydeconz/models/light/cover.py
@@ -0,0 +1,120 @@
+"""Python library to connect deCONZ and Home Assistant to work together."""
+
+from __future__ import annotations
+
+import enum
+from typing import Any, TypedDict
+
+from . import LightBase
+
+
+class TypedCoverState(TypedDict):
+    """Cover state type definition."""
+
+    bri: int
+    lift: int
+    open: bool
+    sat: int
+    tilt: int
+
+
+class TypedCover(TypedDict):
+    """Cover type definition."""
+
+    state: TypedCoverState
+
+
+class CoverAction(enum.Enum):
+    """Possible cover actions."""
+
+    CLOSE = enum.auto()
+    OPEN = enum.auto()
+    STOP = enum.auto()
+
+
+class Cover(LightBase):
+    """Cover and Damper class.
+
+    Position 0 means open and 100 means closed.
+    """
+
+    raw: TypedCover
+
+    @property
+    def is_open(self) -> bool:
+        """Is cover open."""
+        if "open" not in self.raw["state"]:  # Legacy support
+            return self.state is False
+        return self.raw["state"]["open"]
+
+    @property
+    def lift(self) -> int:
+        """Amount of closed position.
+
+        0 is fully open.
+        100 is fully closed.
+        """
+        if "lift" not in self.raw["state"]:  # Legacy support
+            return int(self.raw["state"]["bri"] / 2.54)
+        return self.raw["state"]["lift"]
+
+    @property
+    def tilt(self) -> int | None:
+        """Amount of tilt.
+
+        0 is fully open.
+        100 is fully closed.
+        """
+        if "tilt" in self.raw["state"]:
+            return self.raw["state"]["tilt"]
+        elif "sat" in self.raw["state"]:  # Legacy support
+            return int(self.raw["state"]["sat"] / 2.54)
+        return None
+
+    async def set_position(
+        self, *, lift: int | None = None, tilt: int | None = None
+    ) -> dict[str, Any]:
+        """Set amount of closed position and/or tilt of cover.
+
+        Lift [int] between 0-100.
+        Scale to brightness 0-254.
+        Tilt [int] between 0-100.
+        Scale to saturation 0-254.
+        """
+        data = {}
+
+        if lift is not None:
+            if "lift" in self.raw["state"]:
+                data["lift"] = lift
+            elif "bri" in self.raw["state"]:  # Legacy support
+                data["bri"] = int(lift * 2.54)
+
+        if tilt is not None:
+            if "tilt" in self.raw["state"]:
+                data["tilt"] = tilt
+            elif "sat" in self.raw["state"]:  # Legacy support
+                data["sat"] = int(tilt * 2.54)
+
+        return await self.request(field=f"{self.deconz_id}/state", data=data)
+
+    async def open(self) -> dict[str, Any]:
+        """Fully open cover."""
+        data = {"open": True}
+        if "open" not in self.raw["state"]:  # Legacy support
+            data = {"on": False}
+        return await self.request(field=f"{self.deconz_id}/state", data=data)
+
+    async def close(self) -> dict[str, Any]:
+        """Fully close cover."""
+        data = {"open": False}
+        if "open" not in self.raw["state"]:  # Legacy support
+            data = {"on": True}
+        return await self.request(field=f"{self.deconz_id}/state", data=data)
+
+    async def stop(self) -> dict[str, Any]:
+        """Stop cover motion."""
+        data: dict[str, bool | int]
+        data = {"stop": True}
+        if "lift" not in self.raw["state"]:  # Legacy support
+            data = {"bri_inc": 0}
+        return await self.request(field=f"{self.deconz_id}/state", data=data)
---
+++ packages/pydeconz/pydeconz-97-py3-none-any.whl/pydeconz/models/scene.py
@@ -0,0 +1,79 @@
+"""Python library to connect deCONZ and Home Assistant to work together."""
+
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable
+from typing import Any, TypedDict
+
+from . import ResourceGroup
+from .api import APIItem
+
+
+class TypedScene(TypedDict):
+    """Scene type definition."""
+
+    id: str
+    lightcount: int
+    transitiontime: int
+    name: str
+
+
+class Scene(APIItem):
+    """deCONZ scene representation.
+
+    Dresden Elektroniks documentation of scenes in deCONZ
+    http://dresden-elektronik.github.io/deconz-rest-doc/scenes/
+    """
+
+    raw: TypedScene
+    resource_group = ResourceGroup.SCENE
+
+    def __init__(
+        self,
+        resource_id: str,
+        raw: Any,
+        request: Callable[..., Awaitable[dict[str, Any]]],
+    ) -> None:
+        """Set initial information about scene."""
+        super().__init__(resource_id, raw, request)
+
+        self.group_id: str = raw["group_id"]
+        self.group_deconz_id: str = f"/groups/{self.group_id}"
+        self.group_name: str = raw["group_name"]
+
+    async def store(self) -> dict[str, Any]:
+        """Store current group state in scene.
+
+        The actual state of each light in the group will become the lights scene state.
+        """
+        return await self.request(field=f"{self.deconz_id}/store", data={})
+
+    @property
+    def deconz_id(self) -> str:
+        """Id to call scene over API e.g. /groups/1/scenes/1."""
+        return f"{self.group_deconz_id}/{self.resource_group.value}/{self.id}"
+
+    @property
+    def id(self) -> str:
+        """Scene ID."""
+        return self.raw["id"]
+
+    @property
+    def light_count(self) -> int:
+        """Lights in group."""
+        return self.raw["lightcount"]
+
+    @property
+    def transition_time(self) -> int:
+        """Transition time for scene."""
+        return self.raw["transitiontime"]
+
+    @property
+    def name(self) -> str:
+        """Scene name."""
+        return self.raw["name"]
+
+    @property
+    def full_name(self) -> str:
+        """Full name."""
+        return f"{self.group_name} {self.name}"
