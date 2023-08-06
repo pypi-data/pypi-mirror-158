f343324add10fcb9fbd28e96482daaaa70d274b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:21 +0000

    Add package pysaver/0.0.4/pysaver-0.0.4.tar.gz

---
+++ packages/pysaver/pysaver-0.0.4.tar.gz/pysaver-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name = 'pysaver',
+    version = '0.0.4',
+    py_modules = ['pysaver'],
+    author = 'M J Lally',
+    author_email = 'm.j.lally@outlook.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/mjlally/pysaver',
+    license = 'MIT',
+    keywords = 'pysaver',
+)
