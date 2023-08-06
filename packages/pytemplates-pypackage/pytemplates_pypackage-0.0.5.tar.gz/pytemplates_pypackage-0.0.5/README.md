2f64c747a457e9782db58eb8d18389cd5a738a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:24 +0000

    Add package pytemplates-pypackage/0.0.5/pytemplates_pypackage-0.0.5.tar.gz

---
+++ packages/pytemplates-pypackage/pytemplates_pypackage-0.0.5.tar.gz/pytemplates_pypackage-0.0.5/README.md
@@ -0,0 +1,74 @@
+```bash
+
+   ___        _____                         _         _
+  / _ \ _   _/__   \ ___  _ __ ___   _ __  | |  __ _ | |_  ___  ___
+ / /_)/| | | | / /\// _ \| '_ ` _ \ | '_ \ | | / _` || __|/ _ \/ __|
+/ ___/ | |_| |/ /  |  __/| | | | | || |_) || || (_| || |_|  __/\__ \
+\/      \__, |\/    \___||_| |_| |_|| .__/ |_| \__,_| \__|\___||___/
+        |___/                       |_|
+
+```
+<!-- source - https://patorjk.com/software/taag/#p=display&h=1&f=Ogre&t=PyTemplates -->
+
+[![test workflow](https://github.com/PyTemplate/python_package/actions/workflows/test-and-lint.yaml/badge.svg)](https://github.com/PyTemplate/python_package/actions?query=workflow%3A%22Test+and+Lint%22++)
+[![documentation workflow](https://github.com/PyTemplate/python_package/actions/workflows/pages/pages-build-deployment/badge.svg)](https://pytemplate.github.io/python_package/)
+[![release workflow](https://github.com/PyTemplate/python_package/actions/workflows/release.yaml/badge.svg)](https://github.com/PyTemplate/python_package/releases/latest)
+[![codecov](https://codecov.io/gh/PyTemplate/python_package/branch/main/graph/badge.svg?token=HG1NQ8HRA4)](https://codecov.io/gh/PyTemplate/python_package)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/PyTemplate/python_package/main.svg)](https://results.pre-commit.ci/latest/github/PyTemplate/python_package/main)
+[![PyPI version](https://badge.fury.io/py/pytemplates_pypackage.svg)](https://pypi.org/project/pytemplates-pypackage/)
+
+## Description
+
+A basic python package template which includes proper package structure with a functioning package installation. The package is built using poetry; metadata and dependency information is stored in the pyproject.toml. This template includes flake8, pylint, isort, and pytest settings with configurations compatible with the black autoformatter. Pylint settings are based on the Google style standards for python and adapted for black compatibility.  Testing is automated using github workflows, codecov.io, and pre-commit.ci. Application deployment is managed using multi-staged docker builds for fast develop/deploy cycles.
+
+## Setup
+
+Using `poetry`:
+
+```bash
+git clone https://github.com/crabtr26/pytemplates.git
+cd pytemplates
+poetry install --no-dev
+```
+
+## Usage
+
+From a `.py` file:
+
+```python
+import pytemplates
+pytemplates.__version__
+pytemplates.greet(user="Jacob")
+
+from pytemplates import wish_farewell
+wish_farewell(user="Jacob")
+```
+
+## Development Setup
+
+Using `poetry`:
+
+```bash
+git clone https://github.com/crabtr26/pytemplates.git
+cd pytemplates
+poetry install
+```
+
+## Testing
+
+To run the tests locally using the development environment:
+
+```bash
+cd pytemplates
+poetry run pytest
+```
+
+## Documentation
+
+To build and view the documentation locally using the development environment:
+
+```bash
+cd pytemplates/docs
+make html
+google-chrome build/html/index.html
+```
---
+++ packages/pytemplates-pypackage/pytemplates_pypackage-0.0.5.tar.gz/pytemplates_pypackage-0.0.5/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pytemplates_pypackage', 'pytemplates_pypackage.core']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'dev': ['pytest>=7.1.2,<8.0.0',
+         'pytest-cov>=3.0.0,<4.0.0',
+         'black>=22.3.0,<23.0.0',
+         'isort>=5.10.1,<6.0.0',
+         'flake8>=4.0.1,<5.0.0',
+         'pylint>=2.13.8,<3.0.0',
+         'mypy>=0.961,<0.962',
+         'pre-commit>=2.19.0,<3.0.0',
+         'Sphinx>=4.5.0,<5.0.0',
+         'sphinx-rtd-theme>=1.0.0,<2.0.0',
+         'bump2version>=1.0.1,<2.0.0'],
+ 'docs': ['Sphinx>=4.5.0,<5.0.0', 'sphinx-rtd-theme>=1.0.0,<2.0.0'],
+ 'lint': ['black>=22.3.0,<23.0.0',
+          'isort>=5.10.1,<6.0.0',
+          'flake8>=4.0.1,<5.0.0',
+          'pylint>=2.13.8,<3.0.0',
+          'mypy>=0.961,<0.962',
+          'pre-commit>=2.19.0,<3.0.0'],
+ 'test': ['pytest>=7.1.2,<8.0.0', 'pytest-cov>=3.0.0,<4.0.0']}
+
+setup_kwargs = {
+    'name': 'pytemplates-pypackage',
+    'version': '0.0.5',
+    'description': 'A template for a python package.',
+    'long_description': '```bash\n\n   ___        _____                         _         _\n  / _ \\ _   _/__   \\ ___  _ __ ___   _ __  | |  __ _ | |_  ___  ___\n / /_)/| | | | / /\\// _ \\| \'_ ` _ \\ | \'_ \\ | | / _` || __|/ _ \\/ __|\n/ ___/ | |_| |/ /  |  __/| | | | | || |_) || || (_| || |_|  __/\\__ \\\n\\/      \\__, |\\/    \\___||_| |_| |_|| .__/ |_| \\__,_| \\__|\\___||___/\n        |___/                       |_|\n\n```\n<!-- source - https://patorjk.com/software/taag/#p=display&h=1&f=Ogre&t=PyTemplates -->\n\n[![test workflow](https://github.com/PyTemplate/python_package/actions/workflows/test-and-lint.yaml/badge.svg)](https://github.com/PyTemplate/python_package/actions?query=workflow%3A%22Test+and+Lint%22++)\n[![documentation workflow](https://github.com/PyTemplate/python_package/actions/workflows/pages/pages-build-deployment/badge.svg)](https://pytemplate.github.io/python_package/)\n[![release workflow](https://github.com/PyTemplate/python_package/actions/workflows/release.yaml/badge.svg)](https://github.com/PyTemplate/python_package/releases/latest)\n[![codecov](https://codecov.io/gh/PyTemplate/python_package/branch/main/graph/badge.svg?token=HG1NQ8HRA4)](https://codecov.io/gh/PyTemplate/python_package)\n[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/PyTemplate/python_package/main.svg)](https://results.pre-commit.ci/latest/github/PyTemplate/python_package/main)\n[![PyPI version](https://badge.fury.io/py/pytemplates_pypackage.svg)](https://pypi.org/project/pytemplates-pypackage/)\n\n## Description\n\nA basic python package template which includes proper package structure with a functioning package installation. The package is built using poetry; metadata and dependency information is stored in the pyproject.toml. This template includes flake8, pylint, isort, and pytest settings with configurations compatible with the black autoformatter. Pylint settings are based on the Google style standards for python and adapted for black compatibility.  Testing is automated using github workflows, codecov.io, and pre-commit.ci. Application deployment is managed using multi-staged docker builds for fast develop/deploy cycles.\n\n## Setup\n\nUsing `poetry`:\n\n```bash\ngit clone https://github.com/crabtr26/pytemplates.git\ncd pytemplates\npoetry install --no-dev\n```\n\n## Usage\n\nFrom a `.py` file:\n\n```python\nimport pytemplates\npytemplates.__version__\npytemplates.greet(user="Jacob")\n\nfrom pytemplates import wish_farewell\nwish_farewell(user="Jacob")\n```\n\n## Development Setup\n\nUsing `poetry`:\n\n```bash\ngit clone https://github.com/crabtr26/pytemplates.git\ncd pytemplates\npoetry install\n```\n\n## Testing\n\nTo run the tests locally using the development environment:\n\n```bash\ncd pytemplates\npoetry run pytest\n```\n\n## Documentation\n\nTo build and view the documentation locally using the development environment:\n\n```bash\ncd pytemplates/docs\nmake html\ngoogle-chrome build/html/index.html\n```\n',
+    'author': 'crabtr26',
+    'author_email': 'crabtr26@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/PyTemplate/python_package',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
