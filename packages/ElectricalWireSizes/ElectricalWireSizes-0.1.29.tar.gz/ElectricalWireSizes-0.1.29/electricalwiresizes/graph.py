0ba2a8d102a095a6ad569505b4e69a735f60d995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:00 +0000

    Add package ElectricalWireSizes/0.1.29/ElectricalWireSizes-0.1.29.tar.gz

---
+++ packages/ElectricalWireSizes/ElectricalWireSizes-0.1.29.tar.gz/ElectricalWireSizes-0.1.29/electricalwiresizes/graph.py
@@ -0,0 +1,130 @@
+'''
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+| PYEWS, ElectricalWireSizes, 07/07/2022                                 |
+| Version : 0.1.29                                                       |
+| Autor : Marco Polo Jacome Toss                                         |
+| License: GNU Affero General Public License v3 (GPL-3.0)                |
+| Requires: Python >=3.5                                                 |
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Changelog:
+
+0.1.29     Versión estable, en esta nueva actualización se agrega al módulo
+           graph una línea indicadora de pérdida de tensión.
+
+0.1.29rc1: Se modifican los módulos mbtcu, mbtal, mbtcustd, dbcircuit, dbcircuitcd
+           adicionando un nuevo argumento Fcond y condiciones para el cumplimento
+           del 125% de ampacidad en alimentadores y circuitos derivados sin considerar
+           cualquier factor de ajuste, todas las versiones anteriores no cuentan con
+           esta condición y esto puede causar error cuando se tienen las condiciones
+           ideales en un conductor, sin agrupar y a temperatura ambiente de 30°C.
+
+0.1.28   : Versión estable.
+
+0.1.28rc2: Separación de operaciones, conductor y protección.
+
+0.1.28rc1: En esta versión se actualiza las protecciones y se actualiza
+           la fórmula de corriente incluyendo el factor de sobrecorriente,
+           en la versión 0.1.27 no se logra ver la actualización de la
+           corriente nominal.
+
+0.1.27rc3: En esta versión los módulos se han clasificado e independizado
+           en distintos archivos además se mejora la salida de datos
+           del módulo dbcircuit para funciones futuras.
+
+0.1.27:    Versione estable.
+
+'''
+
+import math, time
+import numpy as np
+import matplotlib.pyplot as plt
+
+def autolabel(rects):
+    """Funcion para agregar una etiqueta con el valor en cada barra"""
+    for rect in rects:
+        height = rect.get_height()
+        plt.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 1),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+def graph(mydata=None,condA=None,condB=None,w=None,h=None,material=None,color=None,sistema=None, vd=None):
+
+
+    if((mydata==None or not mydata) or condA==None or condB==None or w==None or h==None or material==None or color==None or sistema==None or vd==None):
+        t = time.localtime()
+        print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+        print("                    ElectricalWireSizes                      ")
+        print("                 ",time.asctime(t))
+        print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+        print("                                                             ")
+        print("                         ─▄▀─▄▀")
+        print("                         ──▀──▀")
+        print("                         █▀▀▀▀▀█▄")
+        print("                         █░░░░░█─█")
+        print("                         ▀▄▄▄▄▄▀▀")
+        print("                                                             ")
+        print("-------------------------------------------------------------")
+        print("| Los parámetros no son correctos                           |")
+        print("| para el módulo                                            |")
+        print("| graph(mydata,Cal,Cal,Ancho,Alto,Material,Color,Sistema,Vd)|") 
+        print("----------------------------------------------- -------------")
+        return         
+
+
+    xdata=[]
+    ydata=[]
+    y1data=[]
+    y2data=[]
+
+    for i in range(len(mydata)):
+        xdata.append(mydata[i][0])
+            
+        if(sistema==1):
+            slabel="%Vd 1F/2H"
+            ydata.append(mydata[i][8])
+        elif(sistema==2):
+            slabel="%Vd 2F/3H"
+            ydata.append(mydata[i][9])
+        elif(sistema==3):
+            slabel="%Vd 3F/3H"
+            ydata.append(mydata[i][10])
+        elif(sistema==4):
+            slabel="%Vd 3F/4H"
+            ydata.append(mydata[i][10])
+  
+    if material==1:
+        xConductor =["14 AWG", "12 AWG", "10 AWG", "8 AWG", "6 AWG", "4 AWG", "2 AWG", "1/0 AWG", "2/0 AWG", "3/0 AWG", "4/0 AWG", "250 KCM", "300 KCM", "350 KCM", "400 KCM", "500 KCM", "600 KCM", "750 KCM", "1000 KCM"]
+    elif material==2:
+        xConductor =["6 AWG", "4 AWG", "2 AWG", "1/0 AWG", "2/0 AWG", "3/0 AWG", "4/0 AWG", "250 KCM", "300 KCM", "350 KCM", "400 KCM", "500 KCM", "600 KCM", "750 KCM", "1000 KCM"]
+
+
+    for i in range(len(xConductor)):
+        if condA==xConductor[i]:
+            a=i
+            break
+
+    for i in range(len(xConductor)):
+        if condB==xConductor[i]:
+            b=i+1
+            break
+
+
+    x=xdata
+    #print(x)
+    numero_de_grupos = len(ydata[a:b])
+    indice_barras = np.arange(numero_de_grupos)
+    ancho_barras =0.5
+    plt.figure(figsize=(w,h))
+    rects1 = plt.bar(indice_barras + ancho_barras/10,ydata[a:b], width=ancho_barras,label=slabel, color=color)
+    plt.legend(loc='best')
+    autolabel(rects1)
+    plt.xticks(indice_barras, (x[a:b]))
+    plt.axhline(y=vd, xmin=0, xmax=1.0, color='black')
+    plt.ylabel('Caída de tensión porcentual [%Vd]')
+    plt.xlabel('Calibre de conductores elécticos')
+    plt.title('Caída de tensión en conductores eléctricos')
+    plt.grid()
+    plt.show()
---
+++ packages/ElectricalWireSizes/ElectricalWireSizes-0.1.29.tar.gz/ElectricalWireSizes-0.1.29/electricalwiresizes/shortcircuit.py
@@ -0,0 +1,204 @@
+'''
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+| PYEWS, ElectricalWireSizes, 07/07/2022                                 |
+| Version : 0.1.29                                                       |
+| Autor : Marco Polo Jacome Toss                                         |
+| License: GNU Affero General Public License v3 (GPL-3.0)                |
+| Requires: Python >=3.5                                                 |
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Changelog:
+
+0.1.29     Versión estable, en esta nueva actualización se agrega al módulo
+           graph una línea indicadora de pérdida de tensión.
+
+0.1.29rc1: Se modifican los módulos mbtcu, mbtal, mbtcustd, dbcircuit, dbcircuitcd
+           adicionando un nuevo argumento Fcond y condiciones para el cumplimento
+           del 125% de ampacidad en alimentadores y circuitos derivados sin considerar
+           cualquier factor de ajuste, todas las versiones anteriores no cuentan con
+           esta condición y esto puede causar error cuando se tienen las condiciones
+           ideales en un conductor, sin agrupar y a temperatura ambiente de 30°C.
+
+0.1.28   : Versión estable.
+
+0.1.28rc2: Separación de operaciones, conductor y protección.
+
+0.1.28rc1: En esta versión se actualiza las protecciones y se actualiza
+           la fórmula de corriente incluyendo el factor de sobrecorriente,
+           en la versión 0.1.27 no se logra ver la actualización de la
+           corriente nominal.
+
+0.1.27rc3: En esta versión los módulos se han clasificado e independizado
+           en distintos archivos además se mejora la salida de datos
+           del módulo dbcircuit para funciones futuras.
+
+0.1.27:    Versione estable.
+
+'''
+
+from tabulate import tabulate
+import math, time
+from .bd import dbConductorCu, dbConductorAl
+
+def icc(conductor=None,T1=None,T2=None,fhz=None,view=None):
+
+    if((conductor==None or T1==None or T2==None or fhz==None or view==None)):
+        t = time.localtime()
+        print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+        print("                    ElectricalWireSizes                      ")
+        print("                 ",time.asctime(t))
+        print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+        print("                                                             ")
+        print("                         ─▄▀─▄▀")
+        print("                         ──▀──▀")
+        print("                         █▀▀▀▀▀█▄")
+        print("                         █░░░░░█─█")
+        print("                         ▀▄▄▄▄▄▀▀")
+        print("                                                             ")
+        print("-------------------------------------------------------------")
+        print("| Los parámetros no son correctos                           |")
+        print("| para el módulo                                            |")
+        print("| icc(conductor,T1,T2,fhz,view)                             |") 
+        print("----------------------------------------------- -------------")
+        return  
+    
+    if conductor==1:
+        datos=[["14 AWG"],
+            ["12 AWG"],
+            ["10 AWG"],
+            ["8 AWG"],
+            ["6 AWG"],
+            ["4 AWG"],
+            ["2 AWG"],
+            ["1/0 AWG"],
+            ["2/0 AWG"],
+            ["3/0 AWG"],
+            ["4/0 AWG"],
+            ["250 KCM"],
+            ["300 KCM"],
+            ["350 KCM"],
+            ["400 KCM"],
+            ["500 KCM"],
+            ["600 KCM"],
+            ["750 KCM"],
+            ["1000 KCM"]]
+
+        k=0.0297
+
+        for i in range(len(dbConductorCu)):
+            Seccion=dbConductorCu[i][10]
+            datos[i].append(Seccion)
+
+        t=1/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=2/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=4/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+        t=8/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=16/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=30/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=60/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=100/fhz
+        for i in range(len(dbConductorCu)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        if view==1:
+            headers = ["Calibre","S[mm²]","1C[kA]","2C[kA]","4C[kA]", "8C[kA]", "16C[kA]", "30C[kA]", "60C[kA]", "100C[kA]"]
+            print(tabulate(datos, headers, tablefmt="pretty"))
+        elif view==2:
+            return datos
+    
+    elif conductor==2:
+        
+        datos=[["6 AWG"],
+            ["4 AWG"],
+            ["2 AWG"],
+            ["1/0 AWG"],
+            ["2/0 AWG"],
+            ["3/0 AWG"],
+            ["4/0 AWG"],
+            ["250 KCM"],
+            ["300 KCM"],
+            ["350 KCM"],
+            ["400 KCM"],
+            ["500 KCM"],
+            ["600 KCM"],
+            ["750 KCM"],
+            ["1000 KCM"]]
+
+        k=0.0125
+
+        for i in range(len(dbConductorAl)):
+            Seccion=dbConductorAl[i][10]
+            datos[i].append(Seccion)
+
+        t=1/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=2/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=4/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+        t=8/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=16/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=30/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorCu[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=60/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        t=100/fhz
+        for i in range(len(dbConductorAl)):
+            CM=round(dbConductorAl[i][10]*1973.525241,2)
+            datos[i].append(round(math.sqrt(((pow(CM,2)*(k*math.log10((T2+234)/(T1+234)))))/t)/1000,2))
+
+        if view==1:
+            headers = ["Calibre","S[mm²]","1C[kA]","2C[kA]","4C[kA]", "8C[kA]", "16C[kA]", "30C[kA]", "60C[kA]", "100C[kA]"]
+            print(tabulate(datos, headers, tablefmt="pretty"))
+        elif view==2:
+            return datos
