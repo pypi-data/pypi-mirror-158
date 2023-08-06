0e1c2318650c648694f6e6b77647d9fb2e0f6c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:29 +0000

    Add package gr-freddy/3.0.25.dev0/gr-freddy-3.0.25.dev0.tar.gz

---
+++ packages/gr-freddy/gr-freddy-3.0.25.dev0.tar.gz/gr-freddy-3.0.25.dev0/gradio/templates/cdn/assets/index.b5562f14.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.d88a3c69.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gr-freddy/gr-freddy-3.0.25.dev0.tar.gz/gr-freddy-3.0.25.dev0/gradio/templates/frontend/assets/index.978f1935.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.90adc4f4.js';
+
+const modes = ["static"];
+
+export { modes };
