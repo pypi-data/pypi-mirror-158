6c6e201ac4f859677fed8c6bdea77dfd8703416b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:51 +0000

    Add package ibrahimasy-tut/0.0.1/ibrahimasy_tut-0.0.1-py3-none-any.whl

---
+++ packages/ibrahimasy-tut/ibrahimasy_tut-0.0.1-py3-none-any.whl/ficelle/main.py
@@ -0,0 +1,19 @@
+
+
+def add_num(num1: int, num2: int): 
+
+    return num1 + num2 
+
+
+def mult_num(num1, num2):
+    return num1 * num2 
+
+
+def div_num(num1, num2):
+    return num1 / num2 
+
+
+def sub_num(num1, num2):
+    return num1 - num2 
+
+
