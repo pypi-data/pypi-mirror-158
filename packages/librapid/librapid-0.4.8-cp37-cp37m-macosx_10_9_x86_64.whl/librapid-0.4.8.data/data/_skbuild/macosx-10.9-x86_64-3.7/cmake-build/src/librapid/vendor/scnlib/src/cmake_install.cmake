8d4ecac82a4079c64d642f4ae26ea43fc81af909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:54 +0000

    Add package librapid/0.4.8/librapid-0.4.8-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/librapid/librapid-0.4.8-cp37-cp37m-macosx_10_9_x86_64.whl/librapid-0.4.8.data/data/_skbuild/macosx-10.9-x86_64-3.7/cmake-build/src/librapid/vendor/scnlib/src/cmake_install.cmake
@@ -0,0 +1,45 @@
+# Install script for directory: /Users/runner/work/librapid/librapid/src/librapid/vendor/scnlib/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/Users/runner/work/librapid/librapid/_skbuild/macosx-10.9-x86_64-3.7/cmake-install/src/librapid")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/runner/work/librapid/librapid/_skbuild/macosx-10.9-x86_64-3.7/cmake-build/src/librapid/vendor/scnlib/src/deps/cmake_install.cmake")
+
+endif()
+
