8acf3d8923f6a925f8e09b82b4cdaf0688953b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:19 +0000

    Add package pylejandria/0.1.5/pylejandria-0.1.5.tar.gz

---
+++ packages/pylejandria/pylejandria-0.1.5.tar.gz/pylejandria-0.1.5/src/pylejandria/__init__.py
@@ -0,0 +1,16 @@
+#################################
+#   MADE BY: ARMANDO CHAPARRO   #
+#################################
+
+from pylejandria import gui
+from pylejandria import math
+from pylejandria import module
+from pylejandria import tools
+from pylejandria import upload
+
+MAFER = """
+It´s hard to forget
+someone who gave you
+so much to remember.
+I´ll always love you, Mafer.
+"""
