1f2f32b4aed3f8c01e21813e1a5770a4f84a805b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:50 +0000

    Add package sortingview/0.9.2/sortingview-0.9.2.tar.gz

---
+++ packages/sortingview/sortingview-0.9.2.tar.gz/sortingview-0.9.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    scripts=[
+        'bin/sortingview-start-backend'
+    ],
+    include_package_data = True,
+    install_requires=[
+        'spikeinterface>=0.94.0',
+        'spikeextractors>=0.9.9',
+        'kachery-cloud>=0.1.24',
+        'figurl>=0.2.4',
+        'click',
+        'pynwb',
+        'pyyaml',
+        'h5py',
+        'google-auth',
+        'cachecontrol'
+    ]
+)
