c2cfb504614d90ae5f5cad16a674eb00d6fb76dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:13 +0000

    Add package s4sdk/0.1.1a2/s4sdk-0.1.1a2.tar.gz

---
+++ packages/s4sdk/s4sdk-0.1.1a2.tar.gz/s4sdk-0.1.1a2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['s4sdk', 's4sdk.package', 's4sdk.resource']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'argparse>=1.4.0,<2.0.0',
+ 'click>=8.1.3,<9.0.0',
+ 'pandas>=1.4.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 's4sdk',
+    'version': '0.1.1a2',
+    'description': '',
+    'long_description': None,
+    'author': 'Roy Chotechuang',
+    'author_email': 'htraexd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
