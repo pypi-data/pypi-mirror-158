48975c44a0846aabab3c79c00dd12c69a8f176bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:31 +0000

    Add package lawnmowerlatte/1.0.6/lawnmowerlatte-1.0.6-py3-none-any.whl

---
+++ packages/lawnmowerlatte/lawnmowerlatte-1.0.6-py3-none-any.whl/lawnmowerlatte/util.py
@@ -0,0 +1,21 @@
+import os
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+
+def tomlrc(*path, filename="~/.tomlrc"):
+    filename = os.path.expanduser(filename)
+
+    with open(filename, "rb") as f:
+        toml = tomllib.load(f)
+
+    if path is None:
+        return toml
+    else:
+        for key in path:
+            toml = toml[key]
+
+        return toml
