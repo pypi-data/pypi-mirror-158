5e07fd7417885c2cdda7c3e61a3a9673a4c65115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:35 +0000

    Add package encomp/0.5.0/encomp-0.5.0-py3-none-any.whl

---
+++ packages/encomp/encomp-0.5.0-py3-none-any.whl/encomp/utypes.py
@@ -0,0 +1,455 @@
+"""
+Contains type definitions for :py:class:`encomp.units.Quantity` objects.
+If ``encomp.settings.SETTINGS.type_checking`` is ``True``,
+these types will be enforced everywhere.
+
+The dimensionalities defined in this module can be combined with ``*`` and ``/``.
+Some commonly used derived dimensionalities (like density) are defined for convenience.
+"""
+
+from __future__ import annotations
+
+from typing import TypeVar, Union, Optional, Literal, _LiteralGenericAlias
+from typing import Union
+from abc import ABC
+
+
+import numpy as np
+import pandas as pd
+from pint.unit import UnitsContainer
+
+
+# type alias for the magnitude input to Quantity
+MagnitudeValue = Union[float, int]
+
+Magnitude = Union[MagnitudeValue,
+                  list[MagnitudeValue],
+                  tuple[MagnitudeValue, ...],
+                  np.ndarray,
+                  pd.Series]
+
+
+_BASE_SI_UNITS: tuple[str, ...] = ('m', 'kg', 's', 'K', 'mol', 'A', 'cd')
+
+# these string literals are used to infer the dimensionality of commonly created quantities
+DimensionlessUnits = Literal['', '%']
+LengthUnits = Literal['m', 'meter', 'km', 'cm', 'mm', 'um']
+MassUnits = Literal['kg', 'g', 'ton', 'tonne', 't', 'mg', 'ug']
+TimeUnits = Literal['s', 'second', 'min', 'minute', 'h', 'hr',
+                    'hour', 'd', 'day', 'w', 'week', 'y', 'yr',
+                    'a', 'year', 'ms']
+
+TemperatureUnits = Literal['C', 'degC', '°C', 'K', 'F', 'degF', '°F',
+                           'delta_C', 'delta_degC', 'Δ°C',
+                           'delta_F', 'delta_degF', 'Δ°F']
+
+SubstanceUnits = Literal['mol', 'kmol']
+CurrentUnits = Literal['A', 'mA']
+LuminosityUnits = Literal['lm']
+
+AreaUnits = Literal['m2', 'm^2', 'm**2', 'm²', 'cm2', 'cm^2', 'cm**2', 'cm²']
+VolumeUnits = Literal['L', 'l', 'liter', 'm3', 'm^3', 'm³', 'm**3',
+                      'cm3', 'cm^3', 'cm³', 'cm**3']
+NormalVolumeUnits = Literal['normal liter', 'Nm3',
+                            'nm3', 'Nm^3', 'nm^3', 'Nm³',
+                            'nm³', 'Nm**3', 'nm**3']
+
+PressureUnits = Literal['bar', 'kPa', 'Pa', 'MPa', 'mbar', 'mmHg']
+
+MassFlowUnits = Literal['kg/s', 'kg/h', 'kg/hr', 'g/s', 'g/h', 'g/hr',
+                        'ton/h', 't/h', 'ton/hr',
+                        't/hr', 't/d', 'ton/day', 't/w', 'ton/week', 't/y',
+                        't/a', 't/year', 'ton/y', 'ton/a', 'ton/year']
+
+VolumeFlowUnits = Literal['m3/s', 'm3/h', 'm3/hr',
+                          'm**3/s', 'm**3/h', 'm**3/hr',
+                          'm^3/s', 'm^3/h', 'm^3/hr',
+                          'm³/s', 'm³/h', 'm³/hr',
+                          'liter/second', 'l/s', 'L/s',
+                          'liter/hour', 'l/h', 'L/h', 'L/hr', 'l/hr']
+
+NormalVolumeFlowUnits = Literal['Nm3/s', 'Nm3/h', 'Nm3/hr',
+                                'nm3/s', 'nm3/h', 'nm3/hr',
+                                'Nm^3/s', 'Nm^3/h', 'Nm^3/hr',
+                                'nm^3/s', 'nm^3/h', 'nm^3/hr',
+                                'Nm³/s', 'Nm³/h', 'Nm³/hr',
+                                'nm³/s', 'nm³/h', 'nm³/hr',
+                                'Nm**3/s', 'Nm**3/h', 'Nm**3/hr'
+                                'nm**3/s', 'nm**3/h', 'nm**3/hr']
+
+DensityUnits = Literal['kg/m3', 'kg/m**3',
+                       'kg/m^3', 'kg/m³', 'g/l', 'g/L', 'gram/liter']
+SpecificVolumeUnits = Literal['m3/kg', 'm^3/kg', 'm³/kg', 'l/g', 'L/g']
+
+EnergyUnits = Literal['J', 'kJ', 'MJ', 'GJ', 'PJ', 'kWh', 'MWh', 'Wh', 'GWh']
+PowerUnits = Literal['W', 'kW', 'MW', 'GW', 'mW']
+
+VelocityUnits = Literal['m/s', 'km/s', 'm/min',
+                        'cm/s', 'cm/min', 'km/h', 'kmh', 'kph']
+DynamicViscosityUnits = Literal['Pa*s', 'Pa s', 'cP']
+
+KinematicViscosityUnits = Literal['m2/s', 'm**2/s', 'm^2/s',
+                                  'm²/s', 'cSt', 'cm2/s',
+                                  'cm**2/s', 'cm^2/s', 'cm²/s']
+
+
+def get_registered_units() -> dict[str, tuple[str, ...]]:
+
+    ret = {}
+
+    for k, v in globals().items():
+        if isinstance(v, _LiteralGenericAlias):
+            if k.endswith('Units'):
+                ret[k.removesuffix('Units')] = v.__args__
+
+    return ret
+
+class Dimensionality(ABC):
+
+    dimensions: Optional[UnitsContainer]
+
+    # keeps track of all the dimensionalities that have been
+    # used in the current process
+    # use the class definition as key, since multiple dimensionalities
+    # might have the same UnitsContainer
+    _registry: dict[type[Dimensionality], UnitsContainer] = {}
+
+    # also store a reversed map, this might not contain all items in _registry
+    _registry_reversed: dict[UnitsContainer, type[Dimensionality]] = {}
+
+    def __init_subclass__(cls) -> None:
+
+        if not hasattr(cls, 'dimensions'):
+            raise AttributeError(
+                'Subtypes of Dimensionality must define the '
+                'attribute "dimensions" (an instance of pint.unit.UnitsContainer)'
+            )
+
+        # the Unknown and Impossible dimensionalities subclass has dimensions=None
+        # it will never be used at runtime, only during type checking
+        if cls.dimensions is None:
+            return
+
+        if not isinstance(cls.dimensions, UnitsContainer):
+            raise TypeError(
+                'The "dimensions" attribute of the Dimensionality type '
+                'must be an instance of pint.unit.UnitsContainer, '
+                f'passed {cls} with dimensions: {cls.dimensions} ({type(cls.dimensions)})'
+            )
+
+        # NOTE: dict keys are class definitions, not class names
+        # for example, re-running a notebook cell will
+        # create a new class each time, which will have its own entry in
+        # the dimensionality registry
+        if cls in cls._registry:
+            return
+
+        cls._registry[cls] = cls.dimensions
+
+        # in case there are multiple class definitions with the same UnitRegistry,
+        # do not overwrite previously defined ones
+        if cls.dimensions not in cls._registry_reversed:
+            cls._registry_reversed[cls.dimensions] = cls
+
+    @classmethod
+    def get_dimensionality(cls, dimensions: UnitsContainer) -> type[Dimensionality]:
+
+        if dimensions in cls._registry_reversed:
+            return cls._registry_reversed[dimensions]
+
+        # create a new, custom Dimensionality
+        # not possible to generate a proper name for this,
+        # so it will just contain the literal dimensions
+        # this will call __init_subclass__ to register the type
+        _Dimensionality = type(
+            f'Dimensionality[{dimensions}]',
+            (Dimensionality,),
+            {
+                'dimensions': dimensions
+            }
+        )
+
+        return _Dimensionality
+
+
+# type variables that represent a certain dimensionality
+# the DT_ type is used to signify a different dimensionality than DT
+DT = TypeVar('DT', bound=Dimensionality)
+DT_ = TypeVar('DT_', bound=Dimensionality)
+
+_DimensionlessUC = UnitsContainer({})
+_NormalUC = UnitsContainer({'[normal]': 1})
+_LengthUC = UnitsContainer({'[length]': 1})
+_MassUC = UnitsContainer({'[mass]': 1})
+_TimeUC = UnitsContainer({'[time]': 1})
+_TemperatureUC = UnitsContainer({'[temperature]': 1})
+_SubstanceUC = UnitsContainer({'[substance]': 1})
+_CurrentUC = UnitsContainer({'[current]': 1})
+_LuminosityUC = UnitsContainer({'[luminosity]': 1})
+
+
+# NOTE: each subclass definition will create an entry in Dimensionality._registry
+# reloading (re-importing) this module will clear and reset the registry
+
+class Unknown(Dimensionality):
+    dimensions = None
+
+
+class Impossible(Dimensionality):
+    dimensions = None
+
+
+class Dimensionless(Dimensionality):
+    dimensions = _DimensionlessUC
+
+
+class Normal(Dimensionality):
+    dimensions = _NormalUC
+
+
+class Length(Dimensionality):
+    dimensions = _LengthUC
+
+
+class Mass(Dimensionality):
+    dimensions = _MassUC
+
+
+class Time(Dimensionality):
+    dimensions = _TimeUC
+
+
+class Temperature(Dimensionality):
+    dimensions = _TemperatureUC
+
+
+class Substance(Dimensionality):
+    dimensions = _SubstanceUC
+
+
+class Current(Dimensionality):
+    dimensions = _CurrentUC
+
+
+class Luminosity(Dimensionality):
+    dimensions = _LuminosityUC
+
+
+# derived dimensionalities
+_AreaUC = _LengthUC**2
+_VolumeUC = _LengthUC**3
+_NormalVolumeUC = _VolumeUC * _NormalUC
+_PressureUC = _MassUC / _LengthUC / _TimeUC**2
+_MassFlowUC = _MassUC / _TimeUC
+_VolumeFlowUC = _VolumeUC / _TimeUC
+_NormalVolumeFlowUC = _NormalVolumeUC / _TimeUC
+_DensityUC = _MassUC / _VolumeUC
+_SpecificVolumeUC = 1 / _DensityUC
+_EnergyUC = _MassUC * _LengthUC**2 / _TimeUC**2
+_PowerUC = _EnergyUC / _TimeUC
+_VelocityUC = _LengthUC / _TimeUC
+_DynamicViscosityUC = _MassUC / _LengthUC / _TimeUC
+_KinematicViscosityUC = _LengthUC**2 / _TimeUC
+_FrequencyUC = 1 / _TimeUC
+_MolarMassUC = _MassUC / _SubstanceUC
+_MolarDensityUC = _SubstanceUC / _VolumeUC
+
+
+class Area(Dimensionality):
+    dimensions = _AreaUC
+
+
+class Volume(Dimensionality):
+    dimensions = _VolumeUC
+
+
+class NormalVolume(Dimensionality):
+    dimensions = _NormalVolumeUC
+
+
+class Pressure(Dimensionality):
+    dimensions = _PressureUC
+
+
+class MassFlow(Dimensionality):
+    dimensions = _MassFlowUC
+
+
+class VolumeFlow(Dimensionality):
+    dimensions = _VolumeFlowUC
+
+
+class NormalVolumeFlow(Dimensionality):
+    dimensions = _NormalVolumeFlowUC
+
+
+class Density(Dimensionality):
+    dimensions = _DensityUC
+
+
+class SpecificVolume(Dimensionality):
+    dimensions = _SpecificVolumeUC
+
+
+class Energy(Dimensionality):
+    dimensions = _EnergyUC
+
+
+class Power(Dimensionality):
+    dimensions = _PowerUC
+
+
+class Velocity(Dimensionality):
+    dimensions = _VelocityUC
+
+
+class DynamicViscosity(Dimensionality):
+    dimensions = _DynamicViscosityUC
+
+
+class KinematicViscosity(Dimensionality):
+    dimensions = _KinematicViscosityUC
+
+
+class Frequency(Dimensionality):
+    dimensions = _FrequencyUC
+
+
+class MolarMass(Dimensionality):
+    dimensions = _MolarMassUC
+
+
+class MolarDensity(Dimensionality):
+    dimensions = _MolarDensityUC
+
+
+# # these dimensionalities might have different names depending on the context
+_HeatingValueUC = _EnergyUC / _MassUC
+_LowerHeatingValueUC = _EnergyUC / _MassUC
+_HigherHeatingValueUC = _EnergyUC / _MassUC
+_SpecificHeatCapacityUC = _EnergyUC / _MassUC / _TemperatureUC
+
+_SpecificEnthalpyUC = _EnergyUC / _MassUC
+_MolarSpecificEnthalpyUC = _EnergyUC / _SubstanceUC
+_SpecificEntropyUC = _SpecificEnthalpyUC / _TemperatureUC
+_MolarSpecificEntropyUC = _SpecificEnthalpyUC / _SubstanceUC
+_SpecificInternalEnergyUC = _EnergyUC / _MassUC
+_MolarSpecificInternalEnergyUC = _EnergyUC / _SubstanceUC
+
+_HeatCapacityUC = _EnergyUC / _MassUC / _TemperatureUC
+_ThermalConductivityUC = _PowerUC / _LengthUC / _TemperatureUC
+_HeatTransferCoefficientUC = _PowerUC / _AreaUC / _TemperatureUC
+_MassPerNormalVolumeUC = _MassUC / _NormalVolumeUC
+_MassPerEnergyUC = _MassUC / _EnergyUC
+_CurrencyUC = _DimensionlessUC
+_CurrencyPerEnergyUC = _CurrencyUC / _EnergyUC
+
+# related to CoolProp humid air
+_SpecificHeatPerDryAirUC = _EnergyUC / _MassUC / _TemperatureUC
+_SpecificHeatPerHumidAirUC = _EnergyUC / _MassUC / _TemperatureUC
+_MixtureEnthalpyPerDryAirUC = _EnergyUC / _MassUC
+_MixtureEnthalpyPerHumidAirUC = _EnergyUC / _MassUC
+_MixtureEntropyPerDryAirUC = _EnergyUC / _MassUC / _TemperatureUC
+_MixtureEntropyPerHumidAirUC = _EnergyUC / _MassUC / _TemperatureUC
+_MixtureVolumePerDryAirUC = _VolumeUC / _MassUC
+_MixtureVolumePerHumidAirUC = _VolumeUC / _MassUC
+
+
+class HeatingValue(Dimensionality):
+    dimensions = _HeatingValueUC
+
+
+class LowerHeatingValue(Dimensionality):
+    dimensions = _LowerHeatingValueUC
+
+
+class HigherHeatingValue(Dimensionality):
+    dimensions = _HigherHeatingValueUC
+
+
+class SpecificHeatCapacity(Dimensionality):
+    dimensions = _SpecificHeatCapacityUC
+
+
+class SpecificEnthalpy(Dimensionality):
+    dimensions = _SpecificEnthalpyUC
+
+
+class MolarSpecificEnthalpy(Dimensionality):
+    dimensions = _MolarSpecificEnthalpyUC
+
+
+class SpecificEntropy(Dimensionality):
+    dimensions = _SpecificEntropyUC
+
+
+class MolarSpecificEntropy(Dimensionality):
+    dimensions = _MolarSpecificEntropyUC
+
+
+class SpecificInternalEnergy(Dimensionality):
+    dimensions = _SpecificInternalEnergyUC
+
+
+class MolarSpecificInternalEnergy(Dimensionality):
+    dimensions = _MolarSpecificInternalEnergyUC
+
+
+class HeatCapacity(Dimensionality):
+    dimensions = _HeatCapacityUC
+
+
+class ThermalConductivity(Dimensionality):
+    dimensions = _ThermalConductivityUC
+
+
+class HeatTransferCoefficient(Dimensionality):
+    dimensions = _HeatTransferCoefficientUC
+
+
+class MassPerNormalVolume(Dimensionality):
+    dimensions = _MassPerNormalVolumeUC
+
+
+class MassPerEnergy(Dimensionality):
+    dimensions = _MassPerEnergyUC
+
+
+class Currency(Dimensionality):
+    dimensions = _CurrencyUC
+
+
+class CurrencyPerEnergy(Dimensionality):
+    dimensions = _CurrencyPerEnergyUC
+
+
+class SpecificHeatPerDryAir(Dimensionality):
+    dimensions = _SpecificHeatPerDryAirUC
+
+
+class SpecificHeatPerHumidAir(Dimensionality):
+    dimensions = _SpecificHeatPerHumidAirUC
+
+
+class MixtureEnthalpyPerDryAir(Dimensionality):
+    dimensions = _MixtureEnthalpyPerDryAirUC
+
+
+class MixtureEnthalpyPerHumidAir(Dimensionality):
+    dimensions = _MixtureEnthalpyPerHumidAirUC
+
+
+class MixtureEntropyPerDryAir(Dimensionality):
+    dimensions = _MixtureEntropyPerDryAirUC
+
+
+class MixtureEntropyPerHumidAir(Dimensionality):
+    dimensions = _MixtureEntropyPerHumidAirUC
+
+
+class MixtureVolumePerDryAir(Dimensionality):
+    dimensions = _MixtureVolumePerDryAirUC
+
+
+class MixtureVolumePerHumidAir(Dimensionality):
+    dimensions = _MixtureVolumePerHumidAirUC
