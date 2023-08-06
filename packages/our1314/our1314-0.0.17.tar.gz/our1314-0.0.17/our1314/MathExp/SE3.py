b010953eae1e60b9403c6d21b0ca8417b1e76f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:28 +0000

    Add package our1314/0.0.17/our1314-0.0.17.tar.gz

---
+++ packages/our1314/our1314-0.0.17.tar.gz/our1314-0.0.17/our1314/MathExp/SE3.py
@@ -0,0 +1,6 @@
+from our1314.MathExp import MathExp
+
+
+class SE3:
+    def __init__(self, vec6d):
+        return MathExp.SE3(vec6d)
---
+++ packages/our1314/our1314-0.0.17.tar.gz/our1314-0.0.17/our1314/test1.py
@@ -0,0 +1,19 @@
+import our1314.MathExp
+from MathExp import *
+import numpy as np
+
+# H = SE3([10, 20, 15, 10, 20, 30])
+#
+# print(f'H\r\n{H}')
+# print(f'H\r\n{np.linalg.inv(H)}')
+# print(f'H\r\n{H.T}')
+# print(f'H.T\r\n{np.linalg.inv(H) - H.T}')
+#
+# H1 = SE3([1, 0, 0, 0, 0, 0])
+# H2 = H1.dot(H)
+#
+# print(f'H2\r\n{H2}')
+# pass
+# our1314.MathExp.SE3()
+#
+# our1314.
