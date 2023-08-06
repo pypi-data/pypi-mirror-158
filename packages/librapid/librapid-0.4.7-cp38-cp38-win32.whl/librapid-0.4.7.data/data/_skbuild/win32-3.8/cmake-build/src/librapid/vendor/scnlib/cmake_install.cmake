d16853bba21413f1fa7d716b9933bc69b82bebe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:02 +0000

    Add package librapid/0.4.7/librapid-0.4.7-cp38-cp38-win32.whl

---
+++ packages/librapid/librapid-0.4.7-cp38-cp38-win32.whl/librapid-0.4.7.data/data/_skbuild/win32-3.8/cmake-build/src/librapid/vendor/scnlib/cmake_install.cmake
@@ -0,0 +1,41 @@
+# Install script for directory: D:/a/librapid/librapid/src/librapid/vendor/scnlib
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "D:/a/librapid/librapid/_skbuild/win32-3.8/cmake-install/src/librapid")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/a/librapid/librapid/_skbuild/win32-3.8/cmake-build/src/librapid/vendor/scnlib/src/cmake_install.cmake")
+  include("D:/a/librapid/librapid/_skbuild/win32-3.8/cmake-build/src/librapid/vendor/scnlib/test/cmake_install.cmake")
+
+endif()
+
