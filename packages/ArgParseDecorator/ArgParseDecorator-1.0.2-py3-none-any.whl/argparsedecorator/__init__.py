37560081c764eee2920c1145cac82a2de32373ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:25 +0000

    Add package ArgParseDecorator/1.0.2/ArgParseDecorator-1.0.2-py3-none-any.whl

---
+++ packages/ArgParseDecorator/ArgParseDecorator-1.0.2-py3-none-any.whl/argparsedecorator/__init__.py
@@ -0,0 +1,14 @@
+# This file is part of the ArgParseDecorator library.
+#
+# Copyright (c) 2022 Thomas Holland
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see the accompaning LICENSE.txt.txt file or go to <https://opensource.org/licenses/MIT>.
+
+# import Literal right away as it is used by the Choices annotation.
+from typing import Literal
+
+from .annotations import *
+from .argparse_decorator import ArgParseDecorator
+from .nonexiting_argumentparser import NonExitingArgumentParser
+
