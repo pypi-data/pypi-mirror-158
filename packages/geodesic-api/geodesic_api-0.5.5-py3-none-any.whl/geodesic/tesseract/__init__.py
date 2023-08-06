7bc78c3081dcde004cec7bca0f053a5b7eeb4bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:42 +0000

    Add package geodesic-api/0.5.5/geodesic_api-0.5.5-py3-none-any.whl

---
+++ packages/geodesic-api/geodesic_api-0.5.5-py3-none-any.whl/geodesic/tesseract/__init__.py
@@ -0,0 +1,13 @@
+from geodesic.tesseract.job import Job
+from geodesic.tesseract.components import GlobalProperties, AssetSpec, FeatureAggregation, \
+                                          User, Equal, TemporalBinning
+
+__all__ = [
+    'Job',
+    'AssetSpec',
+    'GlobalProperties',
+    'User',
+    'Equal',
+    'TemporalBinning',
+    'FeatureAggregation'
+]
