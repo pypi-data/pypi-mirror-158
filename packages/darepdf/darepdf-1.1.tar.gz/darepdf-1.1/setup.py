ad99e0a284902ba683c9fd5eeb026fdae3404ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:08 +0000

    Add package darepdf/1.1/darepdf-1.1.tar.gz

---
+++ packages/darepdf/darepdf-1.1.tar.gz/darepdf-1.1/setup.py
@@ -0,0 +1,4 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(name='darepdf', version=1.1, long_description=Path('README.md').read_text(),
+                 packages=setuptools.find_packages(exclude=['tests', 'data']), url='https://pypi.org')
