996f6c6838296b0d87ac36b15a9b29f8d6b3e353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:53 +0000

    Add package dichecker/1.0.2/dichecker-1.0.2-py3-none-any.whl

---
+++ packages/dichecker/dichecker-1.0.2-py3-none-any.whl/dichecker/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: MIT
+"""A runtime checker for modern TypedDicts"""
+
+__version__ = "1.0.2"
+__author__ = "ooliver1"
+__license__ = "MIT"
+__title__ = "dichecker"
+
+from .checker import *
+from .errors import *
+from .handlers import *
