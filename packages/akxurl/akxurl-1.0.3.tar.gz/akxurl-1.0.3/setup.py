1a12d0b4b237a90080bf0707e7a111ae0d183771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:34 +0000

    Add package akxurl/1.0.3/akxurl-1.0.3.tar.gz

---
+++ packages/akxurl/akxurl-1.0.3.tar.gz/akxurl-1.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+#MAO2116
+setup(
+    name='akxurl',
+    packages=find_packages(),
+    include_package_data=True,
+    version="1.0.3",
+    description='URL SHORTENER WITH PYTHON - 9 SERVER',
+    author='AKXVAU',
+    author_email='dev.akxvau@gmail.com',
+    long_description=(open("README.md","r")).read(),
+    long_description_content_type="text/markdown",
+   install_requires=['lolcat','requests'],
+ 
+    keywords=['hacker', 'spam', 'tool', 'sms', 'bomber', 'call', 'prank', 'termux', 'hack','sms bomber','sms bomber', 'AKXVAU', 'URL', 'URL SHORTNER', 'AKXURL'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    
+    license='MIT',
+    entry_points={
+            'console_scripts': [
+                'akxurl = akxurl.akxurl',
+                
+            ],
+    },
+    python_requires='>=3.9'
+)
