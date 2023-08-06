f8abafc260ef1dfa83a9ddae05f34e530e8af4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:28 +0000

    Add package cpkg/0.3.19/cpkg-0.3.19.tar.gz

---
+++ packages/cpkg/cpkg-0.3.19.tar.gz/cpkg-0.3.19/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="cpkg",
+    version="0.3.19",
+    author="xiongtianshuo",
+    author_email="Mr_Xiongts@163.com",
+    description="A package that creates a package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seoul2k/cPkg",
+    project_urls={
+        "Bug Tracker": "https://github.com/seoul2k/cPkg/issues",
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+    ],
+    packages=['cpkg/'],
+    python_requires=">=3",
+    install_requires=[''],
+    platforms='any',
+    options={
+        'entry_points': {
+            'console_scripts': [
+                'createpkg = cpkg.cpkg:main'
+            ]
+        }
+    },
+    entry_points={
+        'console_scripts': [
+            'createpkg = cpkg.cpkg:main'
+        ]
+    }
+)
