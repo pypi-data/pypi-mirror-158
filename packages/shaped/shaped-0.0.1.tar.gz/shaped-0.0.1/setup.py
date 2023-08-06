7b8a0772dc3df96509b6aed04bd1c1ba492720b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:26 +0000

    Add package shaped/0.0.1/shaped-0.0.1.tar.gz

---
+++ packages/shaped/shaped-0.0.1.tar.gz/shaped-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='shaped',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://shaped.ai',
+    author='tullie',
+    author_email='tullie@shaped.ai',
+    packages=[],
+    classifiers=['Development Status :: 3 - Alpha'],
+)
