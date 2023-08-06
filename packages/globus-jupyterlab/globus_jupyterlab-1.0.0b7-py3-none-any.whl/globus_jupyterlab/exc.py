4599a2f71a60d24cea1f1bbd49ba94ba4a6a866e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:59 +0000

    Add package globus-jupyterlab/1.0.0b7/globus_jupyterlab-1.0.0b7-py3-none-any.whl

---
+++ packages/globus-jupyterlab/globus_jupyterlab-1.0.0b7-py3-none-any.whl/globus_jupyterlab/exc.py
@@ -0,0 +1,26 @@
+class GlobusJupyterlabException(Exception):
+    """A generic exception that happened within the Globus JupyterLab extension"""
+
+    pass
+
+
+class TransferSubmission(GlobusJupyterlabException):
+    """Something happened when attempting to submit a transfer"""
+
+    pass
+
+
+class LoginException(GlobusJupyterlabException):
+    """There was a problem with the login process"""
+
+    pass
+
+
+class DataAccessScopesRequired(LoginException):
+    pass
+
+
+class TokenStorageError(GlobusJupyterlabException):
+    """A problem loading user Globus tokens"""
+
+    pass
