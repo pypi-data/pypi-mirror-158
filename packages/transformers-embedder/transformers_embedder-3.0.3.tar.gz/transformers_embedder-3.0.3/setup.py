ec06ccb7ce54688b07b8c387adfa168adbd9a6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:49:36 +0000

    Add package transformers-embedder/3.0.3/transformers_embedder-3.0.3.tar.gz

---
+++ packages/transformers-embedder/transformers_embedder-3.0.3.tar.gz/transformers_embedder-3.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extras = {}
+extras["torch"] = ["torch>=1.5,<1.13"]
+extras["all"] = extras["torch"]
+
+install_requires = ["transformers>=4.14,<4.21"]
+
+setuptools.setup(
+    name="transformers_embedder",
+    version="3.0.3",
+    author="Riccardo Orlando",
+    author_email="orlandoricc@gmail.com",
+    description="Word level transformer based embeddings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Riccorl/transformers-embedder",
+    keywords="NLP deep learning transformer pytorch BERT google subtoken wordpieces embeddings",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license="Apache",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require=extras,
+    install_requires=install_requires,
+    python_requires=">=3.6",
+)
