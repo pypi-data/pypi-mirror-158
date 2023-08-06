8d44c1b14f8e67f4b6714ad4e5398f1c90e9c312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:01 +0000

    Add package configs-generator/0.0.7/configs_generator-0.0.7-py3-none-any.whl

---
+++ packages/configs-generator/configs_generator-0.0.7-py3-none-any.whl/configs_generator/__init__.py
@@ -0,0 +1,50 @@
+__doc__ = '''Configuration Generation Tool'''
+
+"""
+Mandatory Required modules
+==========================
+pandas, nettoolkit
+
+
+usage example:
+==============
+
+import configs_generator as cg
+
+# -------- define inputs ----------
+db = "data.xlsx"
+template = "template.txt"
+output = "output.txt"
+
+# -------- ConfGen ----------
+cfg = cg.ConfGen(
+	# ~~~~~~~~~~ Mandatory Arguments ~~~~~~~~~~
+	template_file=template,					# template
+	db=db,									# database
+
+	# ~~~~~~~~~~ Optional Arguments ~~~~~~~~~~
+	output_file=output,						# output filename ( default: output.txt)
+	confGen_minimal=True,					# execution of var sheet replacement only.( default: False)
+
+	find_column_name="FIND_KEYS",			## FIND/REPLACE column headers from 'var' tab
+	replace_column_name="REPLACE_VALUES",
+
+	condition_starter="IF",					## conditional block identifiers
+	condition_stopper="ENDIF",
+	repeat_starter   ="FOR EACH",			## repeat block identifiers
+	repeat_stopper   ="NEXT FOR",
+
+	nested_section_var_identifier= "FROM_PARENT"	# nested section variable identifier string
+	)
+
+cfg.generate()
+"""
+
+__all__ = [
+	"ConfGen", "Read", "Section", "Replicate"
+	]
+
+__version__ = "0.0.7"
+
+from .confGen import ConfGen, Read, Section, Replicate
+
