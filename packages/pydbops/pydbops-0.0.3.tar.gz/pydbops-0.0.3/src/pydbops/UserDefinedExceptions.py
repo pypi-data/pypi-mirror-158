0f532255ff7c44796a933c98f43718c06e2b62f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:47 +0000

    Add package pydbops/0.0.3/pydbops-0.0.3.tar.gz

---
+++ packages/pydbops/pydbops-0.0.3.tar.gz/pydbops-0.0.3/src/pydbops/UserDefinedExceptions.py
@@ -0,0 +1,16 @@
+class InvalidReturnTypeError(Exception):
+    def __init__(self, ReturnType, function):
+        self.__returnType = ReturnType
+        self.__function = function
+
+    def __str__(self) -> str:
+        if self.__function == "getFieldNames":
+            self.__returnTypeList = ["list", "int"]
+            return (f"\treturnType cannot be '{self.__returnType}'\nDid you mean {self.__returnTypeList}?")
+        elif self.__function == "searchEntry":
+            self.__returnTypeList = ["values", "ids"]
+            return (f"\treturnType cannot be '{self.__returnType}'\nDid you mean any of {self.__returnTypeList} ?")
+        elif self.__function == "tableNames":
+            self.__returnTypeList == ["int", "list", "dict"]
+            return (f"\trReturn type cannot be two or more.\nDid you mean any one of {self.__returnTypeList} ?")
+    
