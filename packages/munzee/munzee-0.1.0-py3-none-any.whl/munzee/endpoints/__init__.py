039164b55bf212570a8e72d2af562733fbb4488c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:53 +0000

    Add package munzee/0.1.0/munzee-0.1.0-py3-none-any.whl

---
+++ packages/munzee/munzee-0.1.0-py3-none-any.whl/munzee/endpoints/__init__.py
@@ -0,0 +1,6 @@
+from munzee.endpoints.clan import Clans
+from munzee.endpoints.map import Map
+from munzee.endpoints.munzee import Munzees
+from munzee.endpoints.player import Players
+
+__all__ = ["Players", "Munzees", "Map", "Clans"]
