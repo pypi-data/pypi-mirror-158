61d155864112529d36d4a76f246adbed7897648b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:20 +0000

    Add package musicvideos/1.1.0/musicvideos-1.1.0.tar.gz

---
+++ packages/musicvideos/musicvideos-1.1.0.tar.gz/musicvideos-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.0'
+DESCRIPTION = 'Old slowedvideos now musicvideos, Various scripts from justcow.'
+
+# Setting up
+setup(
+    name="musicvideos",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['moviepy', 'pedalboard', 'pillow', 'soundfile'],
+    keywords=['python', 'video', 'audio', 'justcow'],
+    include_package_data=True ,
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
