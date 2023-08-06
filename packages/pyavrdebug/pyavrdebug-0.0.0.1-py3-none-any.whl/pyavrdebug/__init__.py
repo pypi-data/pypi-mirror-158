1d6102b3b5913b8598e0214d716585896fb46ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:08 +0000

    Add package pyavrdebug/0.0.0.1/pyavrdebug-0.0.0.1-py3-none-any.whl

---
+++ packages/pyavrdebug/pyavrdebug-0.0.0.1-py3-none-any.whl/pyavrdebug/__init__.py
@@ -0,0 +1,6 @@
+"""
+Python AVR debugger
+~~~~~~~~~~~~~~~~~~~
+
+pyavrdebug is a GDB RSP server for connecting GDB debug clients to AVR target devices through pymcuprog
+"""
---
+++ packages/pyavrdebug/pyavrdebug-0.0.0.1-py3-none-any.whl/pyavrdebug/version.py
@@ -0,0 +1,4 @@
+""" This file was generated when pyavrdebug was built """
+VERSION = '0.0.0.1'
+COMMIT_ID = 'dde5ae105ce6b74b0d2c6ece81c24bb8adec3716'
+BUILD_DATE = '2022-07-12 04:01:00 +0000'
