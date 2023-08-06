92e5f1e126fe89cb6f1e138296eeb5c14527d103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:37 +0000

    Add package sinai/0.0.6/sinai-0.0.6.tar.gz

---
+++ packages/sinai/sinai-0.0.6.tar.gz/sinai-0.0.6/sinai/__init__.py
@@ -0,0 +1,11 @@
+"""Sinai is a library to help you monitor everything."""
+from abc import ABC
+
+from sinai.types import MonitorInstance
+
+VERSION = "0.0.6"
+
+
+class BaseView(ABC):
+    def __init__(self, monitor: MonitorInstance):
+        self.monitor = monitor
