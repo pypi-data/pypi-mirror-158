5c38fa7c9d08b46c9aa06bcc5b6711520c209697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:15 +0000

    Add package django-digid-eherkenning/0.4.1/django_digid_eherkenning-0.4.1-py3-none-any.whl

---
+++ packages/django-digid-eherkenning/django_digid_eherkenning-0.4.1-py3-none-any.whl/digid_eherkenning/views/base.py
@@ -0,0 +1,24 @@
+# Django 1.11 compatibility
+try:
+    from django.utils.http import url_has_allowed_host_and_scheme
+except ImportError:
+    from django.utils.http import is_safe_url as url_has_allowed_host_and_scheme
+
+
+def get_redirect_url(request, redirect_to, require_https=True):
+    """
+    Make sure the URL we redirect to is safe. HTTPs
+    is always required.
+    """
+
+    url_is_safe = url_has_allowed_host_and_scheme(
+        url=redirect_to,
+        allowed_hosts=[
+            request.get_host(),
+        ],
+        require_https=require_https,
+    )
+    if url_is_safe:
+        return redirect_to
+
+    return ""
