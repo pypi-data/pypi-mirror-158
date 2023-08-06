ddb8fac3d45882c4aeb1363cc1ac2f09d4fdd0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:53 +0000

    Add package volume-segmantics/0.2.0/volume-segmantics-0.2.0.tar.gz

---
+++ packages/volume-segmantics/volume-segmantics-0.2.0.tar.gz/volume-segmantics-0.2.0/volume_segmantics/data/__init__.py
@@ -0,0 +1,4 @@
+from volume_segmantics.data.settings_data import get_settings_data
+from volume_segmantics.data.slicers import TrainingDataSlicer
+
+__all__ = [get_settings_data, TrainingDataSlicer]
