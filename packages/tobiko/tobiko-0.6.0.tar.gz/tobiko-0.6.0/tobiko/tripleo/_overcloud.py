5436925aef6e98f1c0e5b4cdc909c507f1b0ccc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:59 +0000

    Add package tobiko/0.6.0/tobiko-0.6.0.tar.gz

---
+++ packages/tobiko/tobiko-0.6.0.tar.gz/tobiko-0.6.0/tobiko/tripleo/_overcloud.py
@@ -0,0 +1,256 @@
+# Copyright 2019 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import absolute_import
+
+import io
+import os
+import typing
+
+from oslo_log import log
+
+import tobiko
+from tobiko import config
+from tobiko.openstack import keystone
+from tobiko.openstack import ironic
+from tobiko.openstack import metalsmith
+from tobiko.openstack import topology
+from tobiko.shell import sh
+from tobiko.shell import ssh
+from tobiko.tripleo import _undercloud
+
+
+CONF = config.CONF
+LOG = log.getLogger(__name__)
+
+
+def has_overcloud():
+    # rewrite this function
+    return _undercloud.has_undercloud()
+
+
+def load_overcloud_rcfile() -> typing.Dict[str, str]:
+    return _undercloud.fetch_os_env(*CONF.tobiko.tripleo.overcloud_rcfile)
+
+
+skip_if_missing_overcloud = tobiko.skip_unless(
+    'TripleO overcloud not configured', has_overcloud)
+
+
+class OvercloudKeystoneCredentialsFixture(
+        keystone.EnvironKeystoneCredentialsFixture):
+
+    def get_environ(self) -> typing.Dict[str, str]:
+        LOG.debug('Looking for credentials from TripleO undercloud host...')
+        if _undercloud.has_undercloud():
+            return load_overcloud_rcfile()
+        else:
+            LOG.debug("TripleO undercloud host not available for fetching "
+                      'credentials files.')
+            return {}
+
+
+def list_overcloud_nodes(**params):
+    session = _undercloud.undercloud_keystone_session()
+    client = metalsmith.get_metalsmith_client(session=session)
+    return metalsmith.list_instances(client=client, **params)
+
+
+def find_overcloud_node(**params):
+    session = _undercloud.undercloud_keystone_session()
+    client = metalsmith.get_metalsmith_client(session=session)
+    return metalsmith.find_instance(client=client, **params)
+
+
+def power_on_overcloud_node(instance: metalsmith.MetalsmithInstance,
+                            timeout: tobiko.Seconds = 120.,
+                            sleep_time: tobiko.Seconds = 5.):
+    session = _undercloud.undercloud_keystone_session()
+    client = ironic.get_ironic_client(session=session)
+    ironic.power_on_node(client=client,
+                         node=instance.uuid,
+                         timeout=timeout,
+                         sleep_time=sleep_time)
+
+
+def power_off_overcloud_node(instance: metalsmith.MetalsmithInstance,
+                             timeout: tobiko.Seconds = None,
+                             sleep_time: tobiko.Seconds = None):
+    session = _undercloud.undercloud_keystone_session()
+    client = ironic.get_ironic_client(session=session)
+    ironic.power_off_node(client=client,
+                          node=instance.uuid,
+                          timeout=timeout,
+                          sleep_time=sleep_time)
+
+
+def overcloud_ssh_client(ip_version: int = None,
+                         network_name: str = None,
+                         instance: metalsmith.MetalsmithInstance = None,
+                         host_config=None):
+    if host_config is None:
+        host_config = overcloud_host_config(ip_version=ip_version,
+                                            network_name=network_name,
+                                            instance=instance)
+    tobiko.check_valid_type(host_config.host, str)
+    return ssh.ssh_client(host=host_config.host,
+                          **host_config.connect_parameters)
+
+
+def overcloud_host_config(ip_version: int = None,
+                          network_name: str = None,
+                          instance: metalsmith.MetalsmithInstance = None):
+    host_config = OvercloudHostConfig(ip_version=ip_version,
+                                      network_name=network_name,
+                                      instance=instance)
+    return tobiko.setup_fixture(host_config)
+
+
+def overcloud_node_ip_address(ip_version: int = None,
+                              network_name: str = None,
+                              instance: metalsmith.MetalsmithInstance = None,
+                              **params):
+    if instance is None:
+        instance = find_overcloud_node(**params)
+    ip_version = ip_version or CONF.tobiko.tripleo.overcloud_ip_version
+    network_name = network_name or CONF.tobiko.tripleo.overcloud_network_name
+    address = metalsmith.find_instance_ip_address(instance=instance,
+                                                  ip_version=ip_version,
+                                                  network_name=network_name)
+    LOG.debug(f"Got Overcloud node address '{address}' from Undercloud "
+              f"(ip_version={ip_version}, network_name={network_name}, "
+              f"instance={instance})")
+    return address
+
+
+class OvercloudSshKeyFileFixture(tobiko.SharedFixture):
+
+    @property
+    def key_filename(self):
+        return tobiko.tobiko_config_path(
+            CONF.tobiko.tripleo.overcloud_ssh_key_filename)
+
+    def setup_fixture(self):
+        self.setup_key_file()
+
+    def setup_key_file(self):
+        key_filename = self.key_filename
+        key_dirname = os.path.dirname(key_filename)
+        tobiko.makedirs(key_dirname, mode=0o700)
+
+        ssh_client = _undercloud.undercloud_ssh_client()
+        _get_undercloud_file(ssh_client=ssh_client,
+                             source='~/.ssh/id_rsa',
+                             destination=key_filename,
+                             mode=0o600)
+        _get_undercloud_file(ssh_client=ssh_client,
+                             source='~/.ssh/id_rsa.pub',
+                             destination=key_filename + '.pub',
+                             mode=0o600)
+
+
+def _get_undercloud_file(ssh_client, source, destination, mode):
+    content = sh.execute(['cat', source],
+                         ssh_client=ssh_client).stdout
+    with io.open(destination, 'wb') as fd:
+        fd.write(content.encode())
+    os.chmod(destination, mode)
+
+
+class OvercloudHostConfig(tobiko.SharedFixture):
+
+    key_file = tobiko.required_fixture(OvercloudSshKeyFileFixture)
+
+    def __init__(self,
+                 host: str = None,
+                 hostname: str = None,
+                 ip_version: int = None,
+                 instance: metalsmith.MetalsmithInstance = None,
+                 key_filename: str = None,
+                 network_name: str = None,
+                 port: int = None,
+                 username: str = None,
+                 **kwargs):
+        super(OvercloudHostConfig, self).__init__()
+        self.host = host
+        self.instance = instance
+        self.ip_version = ip_version
+        self.key_filename = key_filename
+        self.host = host
+        self.hostname = hostname
+        self.network_name = network_name
+        self.port = port
+        self.username = username
+        self._connect_parameters = ssh.gather_ssh_connect_parameters(**kwargs)
+
+    def setup_fixture(self):
+        if self.hostname is None:
+            self.hostname = str(overcloud_node_ip_address(
+                hostname=self.host,
+                ip_version=self.ip_version,
+                network_name=self.network_name,
+                instance=self.instance))
+        if self.host is None:
+            self.host = self.hostname
+        if self.port is None:
+            self.port = CONF.tobiko.tripleo.overcloud_ssh_port
+        if self.username is None:
+            self.username = CONF.tobiko.tripleo.overcloud_ssh_username
+        if self.key_filename is None:
+            self.key_filename = self.key_file.key_filename
+
+    @property
+    def connect_parameters(self):
+        parameters = ssh.ssh_host_config(
+            host=str(self.hostname)).connect_parameters
+        parameters.update(ssh.gather_ssh_connect_parameters(self))
+        parameters.update(self._connect_parameters)
+        return parameters
+
+
+def setup_overcloud_keystone_crederntials():
+    keystone.DEFAULT_KEYSTONE_CREDENTIALS_FIXTURES.append(
+        OvercloudKeystoneCredentialsFixture)
+
+
+def get_overcloud_nodes_dataframe(
+        oc_node_df_function: typing.Callable[[ssh.SSHClientType],
+                                             typing.Any]):
+    """
+     :param oc_node_df_function : a function that queries a oc node
+     using a cli command and returns a datraframe with an added
+     hostname field.
+
+     This function concats oc nodes dataframes into a unified overcloud
+     dataframe, seperated by hostname field
+
+    :return: dataframe of all overcloud nodes processes
+    """
+    import pandas
+    oc_nodes_dfs = list()
+    for instance in list_overcloud_nodes():
+        ssh_client = overcloud_ssh_client(instance=instance)
+        oc_nodes_dfs.append(oc_node_df_function(ssh_client))
+    oc_procs_df = pandas.concat(oc_nodes_dfs, ignore_index=True)
+    return oc_procs_df
+
+
+def is_redis_expected():
+    if topology.verify_osp_version('17.0', lower=True):
+        return True
+    services_requiring_redis = (
+        'designate', 'octavia', 'ceilometer', 'gnocchi', 'panko')
+    for service in services_requiring_redis:
+        if keystone.has_service(name=service):
+            return True
+    return False
---
+++ packages/tobiko/tobiko-0.6.0.tar.gz/tobiko-0.6.0/tobiko/tripleo/_undercloud.py
@@ -0,0 +1,199 @@
+# Copyright 2019 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import absolute_import
+
+import json
+import os
+import typing
+
+from oslo_log import log
+
+import tobiko
+from tobiko import config
+from tobiko.openstack import keystone
+from tobiko.shell import ssh
+from tobiko.shell import sh
+
+CONF = config.CONF
+
+LOG = log.getLogger(__name__)
+
+
+def undercloud_ssh_client() -> ssh.SSHClientFixture:
+    host_config = undercloud_host_config()
+    if not host_config.hostname:
+        raise NoSuchUndercloudHostname('No such undercloud hostname')
+    return ssh.ssh_client(host=host_config.hostname,
+                          **host_config.connect_parameters)
+
+
+class NoSuchUndercloudHostname(tobiko.TobikoException):
+    message = "Undercloud hostname not specified"
+
+
+class InvalidRCFile(tobiko.TobikoException):
+    message = "Invalid RC file: {rcfile}"
+
+
+def fetch_os_env(rcfile, *rcfiles) -> typing.Dict[str, str]:
+    rcfiles = (rcfile,) + rcfiles
+    LOG.debug('Fetching OS environment variables from TripleO undercloud '
+              f'host files: {",".join(rcfiles)}')
+    errors = []
+    for rcfile in rcfiles:
+        LOG.debug(f'Reading rcfile: {rcfile}...')
+        try:
+            result = sh.execute(f". {rcfile}; env | grep '^OS_'",
+                                ssh_client=undercloud_ssh_client())
+        except sh.ShellCommandFailed as ex:
+            LOG.debug(f"Unable to get overcloud RC file '{rcfile}' content "
+                      f"({ex})")
+            errors.append(tobiko.exc_info)
+        else:
+            LOG.debug(f'Parsing environment variables from: {rcfile}...')
+            env = {}
+            for line in result.stdout.splitlines():
+                name, value = line.split('=')
+                env[name] = value
+            if env:
+                env_dump = json.dumps(env, sort_keys=True, indent=4)
+                LOG.debug(f'Environment variables read from: {rcfile}:\n'
+                          f'{env_dump}')
+                return env
+    for error in errors:
+        LOG.exception(f"Unable to get overcloud RC file '{rcfile}' "
+                      "content", exc_info=error)
+    raise InvalidRCFile(rcfile=", ".join(rcfiles))
+
+
+def load_undercloud_rcfile() -> typing.Dict[str, str]:
+    return fetch_os_env(*CONF.tobiko.tripleo.undercloud_rcfile)
+
+
+class EnvironUndercloudKeystoneCredentialsFixture(
+        keystone.EnvironKeystoneCredentialsFixture):
+    def get_environ(self) -> typing.Dict[str, str]:
+        return load_undercloud_rcfile()
+
+
+class CloudsFileUndercloudKeystoneCredentialsFixture(
+        keystone.CloudsFileKeystoneCredentialsFixture):
+
+    def __init__(self, credentials=None, cloud_name=None,
+                 clouds_content=None, clouds_file=None, clouds_files=None):
+        cloud_name = cloud_name or load_undercloud_rcfile()['OS_CLOUD']
+
+        super(CloudsFileUndercloudKeystoneCredentialsFixture, self).__init__(
+            credentials=credentials, cloud_name=cloud_name,
+            clouds_content=clouds_content, clouds_file=clouds_file,
+            clouds_files=clouds_files)
+
+
+class HasUndercloudFixture(tobiko.SharedFixture):
+
+    has_undercloud: typing.Optional[bool] = None
+
+    def setup_fixture(self):
+        self.has_undercloud = check_undercloud()
+
+
+def check_undercloud() -> bool:
+    try:
+        ssh_client = undercloud_ssh_client()
+    except NoSuchUndercloudHostname:
+        LOG.debug('TripleO undercloud hostname not found')
+        return False
+    try:
+        ssh_client.connect(retry_count=1,
+                           connection_attempts=1,
+                           timeout=15.)
+    except Exception as ex:
+        LOG.debug(f'Unable to connect to TripleO undercloud host: {ex}',
+                  exc_info=1)
+        return False
+
+    LOG.debug('TripleO undercloud host found')
+    return True
+
+
+def has_undercloud() -> bool:
+    return tobiko.setup_fixture(HasUndercloudFixture).has_undercloud
+
+
+skip_if_missing_undercloud = tobiko.skip_unless(
+    'TripleO undercloud hostname not configured', has_undercloud)
+
+
+class UndecloudHostConfig(tobiko.SharedFixture):
+
+    hostname: typing.Optional[str] = None
+    port: typing.Optional[int] = None
+    username: typing.Optional[str] = None
+
+    def __init__(self, **kwargs):
+        super(UndecloudHostConfig, self).__init__()
+        self._connect_parameters = ssh.gather_ssh_connect_parameters(**kwargs)
+        self.key_filename: typing.List[str] = []
+
+    def setup_fixture(self):
+        self.hostname = CONF.tobiko.tripleo.undercloud_ssh_hostname.strip()
+        self.port = CONF.tobiko.tripleo.undercloud_ssh_port
+        self.username = CONF.tobiko.tripleo.undercloud_ssh_username
+        self.key_filename = self.get_key_filenames()
+
+    @staticmethod
+    def get_key_filenames() -> typing.List[str]:
+        key_filenames: typing.List[str] = []
+        conf = tobiko.tobiko_config()
+        key_filename = conf.tripleo.undercloud_ssh_key_filename
+        if key_filename:
+            key_filename = tobiko.tobiko_config_path(key_filename)
+            if os.path.isfile(key_filename):
+                key_filenames.append(key_filename)
+        key_filenames.extend(ssh.list_proxy_jump_key_filenames())
+        key_filenames.extend(ssh.list_key_filenames())
+        return tobiko.select_uniques(key_filenames)
+
+    @property
+    def connect_parameters(self):
+        parameters = ssh.gather_ssh_connect_parameters(self)
+        parameters.update(self._connect_parameters)
+        return parameters
+
+
+def undercloud_host_config() -> UndecloudHostConfig:
+    return tobiko.setup_fixture(UndecloudHostConfig)
+
+
+def undercloud_keystone_client():
+    session = undercloud_keystone_session()
+    return keystone.get_keystone_client(session=session)
+
+
+def _get_keystone_credentials():
+    environ = load_undercloud_rcfile()
+    if 'OS_CLOUD' in environ:
+        credentials = CloudsFileUndercloudKeystoneCredentialsFixture
+    else:
+        credentials = EnvironUndercloudKeystoneCredentialsFixture
+    return credentials
+
+
+def undercloud_keystone_session():
+    return keystone.get_keystone_session(
+        credentials=_get_keystone_credentials())
+
+
+def undercloud_keystone_credentials():
+    return tobiko.setup_fixture(_get_keystone_credentials()).credentials
