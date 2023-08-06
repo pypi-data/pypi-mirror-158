9b806782b8dd5e4a82a8285bd0815475ff005239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:40 +0000

    Add package mokkari/2.3.0/mokkari-2.3.0.tar.gz

---
+++ packages/mokkari/mokkari-2.3.0.tar.gz/mokkari-2.3.0/mokkari/arc.py
@@ -0,0 +1,88 @@
+"""
+Arc module.
+
+This module provides the following classes:
+
+- Arc
+- ArcSchema
+- ArcsList
+"""
+from marshmallow import EXCLUDE, Schema, ValidationError, fields, post_load
+
+from mokkari import exceptions
+
+
+class Arc:
+    """
+    The Arc object contains information for story arcs.
+
+    :param `**kwargs`: The keyword arguments is used for setting arc data from Metron.
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize a new Arc."""
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class ArcSchema(Schema):
+    """
+    Schema for the Arc API.
+
+    .. versionchanged:: 1.0.0
+
+        - Added ``modified`` field
+    """
+
+    id = fields.Int()
+    name = fields.Str()
+    desc = fields.Str()
+    image = fields.Url()
+    modified = fields.DateTime()
+
+    class Meta:
+        """Any unknown fields will be excluded."""
+
+        unknown = EXCLUDE
+        datetime = "%Y-%m-%dT%H:%M:%S%z"
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        """
+        Make the arc object.
+
+        :param data: Data from Metron response.
+
+        :returns: :class:`Arc` object
+        :rtype: Arc
+        """
+        return Arc(**data)
+
+
+class ArcsList:
+    """The ArcsList object contains a list of `Arc` objects."""
+
+    def __init__(self, response):
+        """Initialize a new ArcsList."""
+        self.arcs = []
+
+        schema = ArcSchema()
+        for arc_dict in response["results"]:
+            try:
+                result = schema.load(arc_dict)
+            except ValidationError as error:
+                raise exceptions.ApiError(error) from error
+
+            self.arcs.append(result)
+
+    def __iter__(self):
+        """Return an iterator object."""
+        return iter(self.arcs)
+
+    def __len__(self):
+        """Return the length of the object."""
+        return len(self.arcs)
+
+    def __getitem__(self, index: int):
+        """Return the object of a at index."""
+        return self.arcs[index]
---
+++ packages/mokkari/mokkari-2.3.0.tar.gz/mokkari-2.3.0/mokkari/variant.py
@@ -0,0 +1,47 @@
+"""
+Variant module.
+
+This module provides the following classes:
+
+- Variant
+- VariantSchema
+"""
+from marshmallow import EXCLUDE, Schema, fields, post_load
+
+
+class Variant:
+    """
+    The Variant object contains information for variant issues.
+
+    :param `**kwargs`: The keyword arguments is used for setting variant issue data.
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize a new Variant."""
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class VariantSchema(Schema):
+    """Schema for the Variant API."""
+
+    name = fields.Str()
+    sku = fields.Str()
+    image = fields.Url()
+
+    class Meta:
+        """Any unknown fields will be excluded."""
+
+        unknown = EXCLUDE
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        """
+        Make the Variant object.
+
+        :param data: Data from Metron response.
+
+        :returns: :class:`Variant` object
+        :rtype: Variant
+        """
+        return Variant(**data)
