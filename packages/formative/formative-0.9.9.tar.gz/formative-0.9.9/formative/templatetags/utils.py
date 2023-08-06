014f1f40ce1ad2e8d00e24fc2276c3205a4ad85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:43 +0000

    Add package formative/0.9.9/formative-0.9.9.tar.gz

---
+++ packages/formative/formative-0.9.9.tar.gz/formative-0.9.9/formative/templatetags/utils.py
@@ -0,0 +1,34 @@
+from django import template
+from django.conf import settings
+import os
+
+from .. import __version__
+from ..utils import thumbnail_path, human_readable_filesize
+
+register = template.Library()
+
+@register.simple_tag
+def get_formative_version():
+    return __version__
+
+@register.simple_tag
+def file_thumbnail(file):
+    path = thumbnail_path(file.path, ext='jpg')
+    if os.path.isfile(path): return thumbnail_path(file.url, ext='jpg')
+    path = thumbnail_path(file.path)
+    if os.path.isfile(path): return thumbnail_path(file.url)
+    
+    return None
+
+@register.simple_tag
+def env_bundle():
+    if settings.ENV != 'prod': return 'bundles/' + settings.ENV
+    return settings.ENV
+
+register.filter('human_readable', human_readable_filesize)
+
+@register.filter
+def underscore(obj, name):
+    attr = getattr(obj, '_' + name)
+    if callable(attr): return attr()
+    return attr
