f5d70a0a15d44ee339ff114abc264251d6e31c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:51 +0000

    Add package project-config/0.5.0/project-config-0.5.0.tar.gz

---
+++ packages/project-config/project-config-0.5.0.tar.gz/project-config-0.5.0/src/project_config/__init__.py
@@ -0,0 +1,17 @@
+"""Reproducible configuration across projects."""
+
+from __future__ import annotations
+
+from project_config.constants import Error, InterruptingError, ResultValue
+from project_config.tree import Tree
+from project_config.types import Results, Rule
+
+
+__all__ = (
+    "Tree",
+    "Rule",
+    "Results",
+    "Error",
+    "InterruptingError",
+    "ResultValue",
+)
