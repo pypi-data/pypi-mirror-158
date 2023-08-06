6f6bc7ce62d69efe0bbd5ffd2b76a367c6be2b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:53 +0000

    Add package xmip/0.6.1rc0/xmip-0.6.1rc0-py3-none-any.whl

---
+++ packages/xmip/xmip-0.6.1rc0-py3-none-any.whl/xmip/__init__.py
@@ -0,0 +1,17 @@
+try:
+    from importlib.metadata import (
+        version,
+        PackageNotFoundError,
+    )  # only works for python 3.8 and upwards
+except:
+    from importlib_metadata import (
+        version,
+        PackageNotFoundError,
+    )  # works for python <3.8
+
+try:
+    __version__ = version("xmip")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+    pass
