847b9cc1b52ff8265580948c6d15137f83cfdc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package samtariFirstPackage/0.0.2/samtariFirstPackage-0.0.2.tar.gz

---
+++ packages/samtariFirstPackage/samtariFirstPackage-0.0.2.tar.gz/samtariFirstPackage-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+  
+
+setuptools.setup(
+    name="samtariFirstPackage",
+    version="0.0.2",
+    author="Sam Yang",
+    author_email="samtari@163.com",
+    description="samtari's first package",
+    long_description="This is samtari's first package",
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+    install_requires=[]
+)
