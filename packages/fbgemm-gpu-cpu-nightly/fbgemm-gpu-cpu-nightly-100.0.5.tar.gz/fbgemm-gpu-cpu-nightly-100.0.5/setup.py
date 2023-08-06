d7119f136a8d8ff2dd8ac0497f8bf1287aa9cb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:52 +0000

    Add package fbgemm-gpu-cpu-nightly/100.0.5/fbgemm-gpu-cpu-nightly-100.0.5.tar.gz

---
+++ packages/fbgemm-gpu-cpu-nightly/fbgemm-gpu-cpu-nightly-100.0.5.tar.gz/fbgemm-gpu-cpu-nightly-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="fbgemm-gpu-cpu-nightly",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="fbgemm-gpu-cpu-nightly of fbgemm-gpu-cpu-nightly of fbgemm-gpu-cpu-nightly",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
