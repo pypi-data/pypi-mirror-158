1faa3a11800d400cc9a767072f33cb73b601640a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:08 +0000

    Add package Harvard-Lib/1.0.0/Harvard%20Lib-1.0.0.tar.gz

---
+++ packages/Harvard-Lib/Harvard%20Lib-1.0.0.tar.gz/Harvard Lib-1.0.0/README.md
@@ -0,0 +1,10 @@
+# Harvard by Ulif
+Harvard is a python package that contains all of Ulif Systems python APIS and applications.
+
+# Libraries
+* TwoFALib - `import twofalib`
+* UlifUtils - Hosts config file for ulif apis   
+* DiscordRPC = `import discordrpc`
+
+# Applications
+* UManage - Project Manager and Live code collab
---
+++ packages/Harvard-Lib/Harvard%20Lib-1.0.0.tar.gz/Harvard Lib-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Harvard Lib",
+    version="1.0.0",
+    author="Ulif Systems",
+    author_email="support@ulifsystems.tech",
+    description="A Python library that contains multiple other libraries created by Ulif Systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://54.234.189.76",
+    project_urls={
+        "DiscordRPC Website": "http://54.234.189.76/rpc",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
