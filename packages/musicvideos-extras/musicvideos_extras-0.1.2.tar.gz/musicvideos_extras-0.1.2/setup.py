21b6b31a49ce792307544e19f0d2b7855b34726b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:57:38 +0000

    Add package musicvideos-extras/0.1.2/musicvideos_extras-0.1.2.tar.gz

---
+++ packages/musicvideos-extras/musicvideos_extras-0.1.2.tar.gz/musicvideos_extras-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Extras for video creation things.'
+
+# Setting up
+setup(
+    name="musicvideos_extras",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['musicvideos', 'moviepy', 'yt_dlp', 'requests', 'oauth2client', 'google-api-python-client', 'google-auth-oauthlib', 'google-auth-httplib2'],
+    keywords=['python', 'musicvideos', 'extras', 'youtube'],
+    include_package_data=True ,
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
