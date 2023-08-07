ee7eade37eab944d1e888cb5cc09fd83e9979833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:57:01 +0000

    Add package MeUtils/2022.7.13.13.54.58/MeUtils-2022.7.13.13.54.58-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.7.13.13.54.58-py3-none-any.whl/meutils/clis/monitor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : monitor
+# @Time         : 2022/7/13 下午1:28
+# @Author       : yuanjie
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+from meutils.tools import service_monitor as _service_monitor
+
+
+@cli.command()
+def service_monitor(filename):
+    """传入配置文件"""
+    return _service_monitor.main(filename)
+
+
+
+@cli.command()
+def xx(filename):
+    """传入配置文件"""
+    return _service_monitor.main(filename)
+
+
+if __name__ == '__main__':
+    cli()
